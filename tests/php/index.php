f66695f0d4f30190d02b8a19256568e8d4c728b7
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-08-20 08:53:59 -0700

    #4: Minor language, and stylistic changes

--- tests/php/index.php
+++ tests/php/index.php
@@ -5,13 +5,13 @@
  *  index.php: defines 'training' or 'analysis' session, prompts users to provide an upload file
  *             or URL to an XML (dataset), then defines which attributes within the dataset
  *             previously defined, to use for the SVM analysis.  After the configurations and 
- *             attributes have been defined, it is sent to 'logic_loader.py' for futher analysis.
+ *             attributes have been defined, it is sent to 'logic_loader.py' for further analysis.
  *
  *  @form:     sends form-data to 'logic_loader.py' using the 'post' method.
  *
  *  @datalist: an HTML5 tag that specifies a list of pre-defined options for an input element.
- *             However, is not supported in IE9-, or in Safari.  So, we add the 'select' element
- *             inside the 'datalist' element to provide a graceful degradation.
+ *             Since this is not supported in IE9-, or Safari, the 'select' element is nested
+ *             inside the 'datalist'.  This allows for graceful degradation.
  *
  *  @input:    uses the 'list' attribute to refer to the datalist element which contains
  *             pre-defined options for the input element.
@@ -22,7 +22,7 @@
   </head>
   <body>
 
-    <form action="../../python/logic_loader.py" method="post">
+    <form action='../../python/logic_loader.py' method='post'>
 
       <input list='session_type' name='svm_session'>
       <datalist id='session_type'>
