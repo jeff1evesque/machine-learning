2abafa8021cf9361b5235853d03651dc11ea82f5
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-08-22 15:10:30 -0700

    #20: define 'flask_form.py'

--- tests/python/flask_form.py
+++ tests/python/flask_form.py
@@ -1,2 +1,12 @@
 ## flask_form.py
 #  This file simply echos the post values from 'form.html'
+
+from flask import Flask
+
+app = Flask(__name__)
+# handles 'get', and 'post'
+@app.route('/machine-learning/tests/python/flask_form.py', methods=['GET', 'POST'])
+def parse_request():
+  temp = request.form['svm_session']
+  print temp
+  return temp
