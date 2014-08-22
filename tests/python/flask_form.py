61c7d45729958a9a8e8e1d38f73ac70a773378af
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-08-22 15:13:25 -0700

    #20: flask_form.py, define python 'she-bang'

--- tests/python/flask_form.py
+++ tests/python/flask_form.py
@@ -1,9 +1,11 @@
 ## flask_form.py
 #  This file simply echos the post values from 'form.html'
 
-from flask import Flask
+#!/usr/bin/python
 
+from flask import Flask
 app = Flask(__name__)
+
 # handles 'get', and 'post'
 @app.route('/machine-learning/tests/python/flask_form.py', methods=['GET', 'POST'])
 def parse_request():
