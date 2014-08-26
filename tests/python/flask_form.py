9a9c21252eb7a1b59349c5efeb5a59af5c9e25eb
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-08-26 17:19:52 -0400

    #20: flask_form.py, add conditional POST / GET logic

--- tests/python/flask_form.py
+++ tests/python/flask_form.py
@@ -3,7 +3,7 @@
 
 #!/usr/bin/python
 
-from flask import Flask, request
+from flask import Flask, request, redirect, url_for
 app = Flask(__name__)
 
 # debug options
@@ -13,9 +13,11 @@ app.config['TRAP_HTTP_EXCEPTIONS'] = True
 # handles 'get', and 'post'
 @app.route('/machine-learning/tests/python/flask_form.py', methods=['GET', 'POST'])
 def parse_request():
-  temp = request.form['svm_session']
-  print temp
-  return temp
+  if request.method == 'POST':
+    temp = request.form['svm_session']
+    return temp
+  else:
+    return redirect(url_for('/machine-learning/tests/python/form.html'));
 
 if __name__ == '__main__':
     app.run()
