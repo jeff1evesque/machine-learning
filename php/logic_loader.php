cf0da173d93ce2d5a79eff19308c534523710210
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-08-28 19:04:23 -0400

    #6: logic_loader.php, reorganize code, add conditional statements

--- php/logic_loader.php
+++ php/logic_loader.php
@@ -5,7 +5,13 @@
  */
 
 /**
- * creates an object where properties are POST data
+ * instantiate 'form_data' class
+ */
+ $obj = new form_data($_POST);
+ logic_loader($obj);
+
+/**
+ * form_data: creates an object where properties are POST data
  *
  * @post: the post array
  */
@@ -18,8 +24,19 @@
  }
 
 /**
- * instantiate 'form_data' class
+ * logic_loader(): receives the form object and determines where to allocate
+ *                 parameters to respective python scripts.
  */
- $obj = new form_data($_POST);
+ function logic_loader($form) {
+   if ($form == 'training') {
+     print 'Welcome to training';
+   }
+   elseif ($form == 'analysis') {
+     print 'Welcome to analysis';
+   }
+   else {
+     print 'Error: ' . basename(__FILE__) . ', logic_loader()';
+   }
+ }
 
 ?>
