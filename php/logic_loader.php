0c20d56dfa227a25828ced61d19e8e5b63ad7dc6
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-08-28 19:20:29 -0400

    #6: logic_loader.php, minor language changes

--- php/logic_loader.php
+++ php/logic_loader.php
@@ -1,20 +1,22 @@
 <?php
 
-/**
- * logic_loader.php
- */
+ /**
+  * logic_loader.php
+  */
 
-/**
- * instantiate 'form_data' class
- */
+ /**
+  * instantiate 'form_data' class
+  */
+
  $obj = new form_data($_POST);
  logic_loader($obj);
 
-/**
- * form_data: creates an object where properties are POST data
- *
- * @post: the post array
- */
+ /**
+  * form_data: 'form_data' object with properties being POST data
+  *
+  * @post: the post array
+  */
+
  class form_data {
    public function __construct($post) {
      foreach($post as $key => $value) {
@@ -23,10 +25,11 @@
    }
  }
 
-/**
- * logic_loader(): receives the form object and determines where to allocate
- *                 parameters to respective python scripts.
- */
+ /**
+  * logic_loader(): receive the 'form_data' object and determine the allocation 
+  *                 of its properties as parameters to respective python scripts.
+  */
+
  function logic_loader($form) {
    if ($form->session_type == 'training') {
      print 'Welcome to training';
