ac74d15592ca18be322f4fdfbe994ab5c98aabbf
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-08-29 16:05:01 -0400

    #47: html_form.js, change 'delegator_form()' -> 'delegator_callback()'

--- src/js/html_form.js
+++ src/js/html_form.js
@@ -1,23 +1,23 @@
 /**
- *  html_form.js: adds additional form elements to the DOM, when the user
- *                clicks 'Add more'.
+ *  html_form.js: adds additional form elements to the DOM, when the user clicks
+ *                'Add more'.
  *
- *                This script implements a 'delegation listener' which attaches
- *                a single event listener to a parent element, and fires for all
+ *                This script implements a 'delegation listener' which attaches a
+ *                single event listener to a parent element, and fires for all
  *                descendants matching a selector.
  */
 
 $(document).ready(function() {
 
 // delegation listener
-  $(body).on('click', '.add_element', delegator_form(event));
+  $(body).on('click', '.add_element', delegator_callback(event));
 
 /**
- * delegator_form: callback used within 'delegator_form' listener.  It creates
- *                 additional form elements when the event listener is fired.
+ * delegator_callback: callback used within 'delegator_form' listener.  It creates
+ *                     additional form elements when the event listener is fired.
  */
 
-  delegator_form(e) {
+  delegator_callback(e) {
 
   }
 
