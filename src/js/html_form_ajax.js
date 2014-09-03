5e03a977a6b8fed53eae1169c410cf030c8eef37
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-09-03 19:02:55 -0400

    #71: html_form_ajax.js, add initial file

---
+++ src/js/html_form_ajax.js
@@ -0,0 +1,5 @@
+/**
+ * html_form_ajax.js: this script prevents the form to redirect upon submission to
+ *                    the 'action' page.  Instead, ajax is used to relay the forms
+ *                    POST data to the 'action' script.
+ */
