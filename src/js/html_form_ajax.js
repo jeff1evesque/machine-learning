884080480e97a597b583bd91d833c4f9eaf8c616
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-09-03 19:26:01 -0400

    #71: html_form_ajax.js, initial definition

--- src/js/html_form_ajax.js
+++ src/js/html_form_ajax.js
@@ -2,4 +2,31 @@
  * html_form_ajax.js: this script prevents the form to redirect upon submission to
  *                    the 'action' page.  Instead, ajax is used to relay the forms
  *                    POST data to the 'action' script.
+ *
+ * @event.preventDefault, when this method is called, the default action of the
+ *                    element will not be fired.
  */
+
+$(document).ready(function() {
+  $('.svm_form_submit').on('submit', function(event) {
+    event.preventDefault();
+    var svm_feedback;
+
+    $.ajax({
+      url: $(this).attr('action'),
+      type: 'POST',
+      data: $(this).serialize(),
+      beforeSend: function() {
+        if ( $('.svm_analysis_results).text().length > 0 ) {
+          var svm_feedback = $('.svm_analysis_results).text();
+          $('.svm_analysis_results').html('sending...');
+        }
+      },
+      success: function(data) {
+        if ( $('.svm_analysis_results).text().length > 0 ) {
+          $('.svm_analysis_results').html(svm_feedback);
+        }
+      }
+    });
+  });
+});
