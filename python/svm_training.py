1eaadc87daca8ebf63087980e1590e144b9f418e
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-09-08 00:30:41 -0400

    #9: Add 'python/svm_training.py'

---
+++ python/svm_training.py
@@ -0,0 +1,5 @@
+## @svm_training.py
+#  This file properly escapes POST data received from 'php/logic_loader.php',
+#      parses the data, and sends respective portions of the POST data to
+#      'python/data_creator.py'. data_creator.py is responsible for saving
+#      the SVM dataset into the mySQL database.
