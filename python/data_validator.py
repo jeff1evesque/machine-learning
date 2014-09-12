e7091f74064f1419141dc12e8f6391cbd242df88
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-09-12 16:46:40 -0400

    #159: data_validator.py, empty skeleton file

---
+++ python/data_validator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+## @data_validator.py
+
+## Class: Validator
+class Validator:
+
+  ## constructor
+  def __init__(self):
+
+  ## form_validation()
+  def form_validation(self):
