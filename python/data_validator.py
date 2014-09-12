a0bcd09982028c7475e39f112648a757edf06ddc
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-09-12 16:53:25 -0400

    #159, data_validator.py, add internal docblock comment

--- python/data_validator.py
+++ python/data_validator.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 
 ## @data_validator.py
+#  This script performs various data sanitation on the form data, and 
+#  validates the same data to ensure that the SVM algorithm will work
+#  on the given dataset.
 
 ## Class: Validator
 class Validator:
