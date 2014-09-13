c24645f8fd1154f82277b71ef5c6fe29683a5c7c
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-09-13 14:11:10 -0400

    #172: svm_training.py, minor comment change

--- python/svm_training.py
+++ python/svm_training.py
@@ -33,7 +33,7 @@ import sys
 from data_creator import Training
 from data_validator import Validator
 
-# validate form data
+# validate input data
 validator = Validator( sys.argv[1], 'training' )
 validator.data_validation()
 
