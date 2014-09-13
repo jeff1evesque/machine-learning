31a1ccbe4dc1fab5cefa28a5b12b5bb66dbf332a
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-09-13 13:53:14 -0400

    #172: svm_training.py, add 'session_type' argument 'training'

--- python/svm_training.py
+++ python/svm_training.py
@@ -34,7 +34,7 @@ from data_creator import Training
 from data_validator import Validator
 
 # validate form data
-validator = Validator( sys.argv[1] )
+validator = Validator( sys.argv[1], 'training' )
 validator.data_validation()
 
 # instantiate 'Training' object
