c74e43d942e99a3721c8e2865984a6f6a0908e80
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-09-13 10:08:21 -0400

    #159: data_validator.py, add initial 'form_sanitation()' method

--- python/data_validator.py
+++ python/data_validator.py
@@ -12,5 +12,8 @@ class Validator:
   def __init__(self, form_data):
     self.form_data = form_data
 
-  ## form_validation(): 
+  ## form_sanitation(): sanitizes a subset of the passed-in form data
+  def form_sanitation(self):
+
+  ## form_validation():
   def form_validation(self):
