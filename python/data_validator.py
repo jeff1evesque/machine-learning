5f058fa4aea93dcea6f7037d3366aeeac3f1ab13
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-09-13 13:50:54 -0400

    #172: data_validator.py, add 'session_type' argument to constructor

--- python/data_validator.py
+++ python/data_validator.py
@@ -10,8 +10,9 @@ import json
 class Validator:
 
   ## constructor: saves a subset of the passed-in form data
-  def __init__(self, form_data):
+  def __init__(self, form_data, session_type):
     self.form_data = form_data
+    self.form_data = session_type
 
   ## data_validation(): ensure passed in dataset is json formatted
   def data_validation(self):
