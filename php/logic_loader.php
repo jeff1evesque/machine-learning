05d320a3f58872b8f11c092c3871b7912816f3fd
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-09-14 23:51:57 -0400

    #193: logic_loader.php, move 'remote_quote()' function

--- php/logic_loader.php
+++ php/logic_loader.php
@@ -101,23 +101,23 @@
      }
    }
 
- }
+   /**
+    * remove_quote(): takes an array, and removes the outer quotes the from
+    *                 each array element.  This function handles both single,
+    *                 and double quotes.  However, the use of both (i.e. "var')
+    *                 simulataneously, is incorrect syntax, and not recognized
+    *                 within this function.
+    *
+    * @arr: passed-in array reference
+    */
 
- /**
-  * remove_quote(): takes an array, and removes the outer quotes the from
-  *                 each array element.  This function handles both single,
-  *                 and double quotes.  However, the use of both (i.e. "var')
-  *                 simulataneously, is incorrect syntax, and not recognized
-  *                 within this function.
-  *
-  * @arr: passed-in array reference
-  */
-
- function remove_quote(&$arr) {
-   foreach ($arr as $key => $value) {
-     $new_value = preg_replace('/^(\'(.*)\'|"(.*)")$/', '$2$3', $arr[$key]);
-     $arr[$key] = $new_value;
+   function remove_quote(&$arr) {
+     foreach ($arr as $key => $value) {
+       $new_value = preg_replace('/^(\'(.*)\'|"(.*)")$/', '$2$3', $arr[$key]);
+       $arr[$key] = $new_value;
+     }
    }
+
  }
 
  /**
