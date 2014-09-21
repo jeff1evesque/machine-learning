0dcee45566767a865f6a10ee5f3e1107394e63bf
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-09-21 15:26:09 -0400

    #223: data_creator.py, initial 'Analysis' class definition

--- python/data_creator.py
+++ python/data_creator.py
@@ -9,3 +9,10 @@ class Training:
   ## constructor:
   def __init__(self, svm_data):
     self.svm_data = svm_data
+
+## Class: Analysis
+class Analysis:
+
+  ## constructor:
+  def __init__(self, svm_data):
+    self.svm_data = svm_data
