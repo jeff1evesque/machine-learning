8dba9df7e3bb026f88c94d4c1b1bd2122a72f5b7
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-09-29 19:08:51 -0400

    #251: ajax_data.js, store 'file uploads' in array

--- src/js/ajax_data.js
+++ src/js/ajax_data.js
@@ -9,5 +9,14 @@ $(document).ready(function() {
   // local variables
     $arr_data = new Array();
 
+  // store 'file uploads' in array
+    var dataset = $('input[name="svm_dataset[]"]');
+    if ( dataset.length > 0 && dataset.attr('type') == 'file' ) {
+      $( dataset ).each(function( index ) {
+        arr_data.push( dataset.eq(index).prop('files'));
+      });
+      console.log(arr_data);
+    }
+
   });
 });
