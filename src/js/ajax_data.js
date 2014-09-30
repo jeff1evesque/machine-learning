6f6789513b4eec4ef482f6a1014bced14f212c22
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-09-29 22:52:09 -0400

    #244: ajax_data.js, move 'local variable'

--- src/js/ajax_data.js
+++ src/js/ajax_data.js
@@ -13,9 +13,9 @@ $(document).ready(function() {
   // local variables
     var arr_data = new Array();
     var form_data = new FormData();
+    var dataset = $('input[name="svm_dataset[]"]');
 
   // store 'file uploads' in array
-    var dataset = $('input[name="svm_dataset[]"]');
     if ( dataset.length > 0 && dataset.attr('type') == 'file' ) {
       $( dataset ).each(function( index ) {
         var file_data = dataset.eq(index).prop('files')[0];
