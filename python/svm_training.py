de8fcf6ce87bb1cc4966167bb0eb7c95e4a75ba5
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-10-05 23:36:40 -0400

    #272: svm_training.py, temporarily remove all code except print('yex, sir')

--- python/svm_training.py
+++ python/svm_training.py
@@ -29,18 +29,5 @@
 #
 #  @import sys, provides various functions, and variables that can be used to
 #      manipulate different parts of the Python runtime environment (i.e. argv).
-import sys, json
-from data_creator import Training
-from data_validator import Validator
 
-if len(sys.argv) > 1:
-  # validate input data is json format
-  validator = Validator( sys.argv[1], 'training' )
-  validator.data_validation()
-
-  # instantiate 'Training' object
-  data = Training( sys.argv[1] )
-else:
-  msg = 'Please provide a training dataset in json format'
-  print json.dumps({'error':msg}, separators=(',', ': '))
-  sys.exit()
+print('yes, sir')
