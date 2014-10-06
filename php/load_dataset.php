77d1d2becfbf015b2173db27ee2e163aa7295286
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-10-05 20:28:40 -0400

    #272: load_dataset.php, components of the '_FILES' array

--- php/load_dataset.php
+++ php/load_dataset.php
@@ -9,9 +9,13 @@
   include(dirname(__FILE__) . '/helper.php');
 
 // debug: return 'file upload(s)' to AJAX
-  //var_dump($_FILES);
+  var_dump($_FILES);
 
 // create JSON encoded 'file upload'
+  $file_name    = $_FILES['file_upload_0']['name'];
+  $file_type    = $_FILES['file_upload_0']['type'];
+  $file_content = $_FILES['file_upload_0']['tmp_name'];
+  $file_size    = $_FILES['file_upload_0']['size'];
 
 // send 'file upload' to python
   $result = shell_command('python ../python/svm_training.py', $_FILES);
