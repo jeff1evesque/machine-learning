1f6ba6f6e79de47e663552d134a68612605a77e8
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-10-08 20:28:55 -0400

    #287: load_dataset.php, change 'file_pointer' -> 'file_temp'

--- php/load_dataset.php
+++ php/load_dataset.php
@@ -14,14 +14,14 @@
 // local variables
   $file_name    = $_FILES['file_upload_0']['name'];
   $file_type    = $_FILES['file_upload_0']['type'];
-  $file_pointer = $_FILES['file_upload_0']['tmp_name'];
+  $file_temp    = $_FILES['file_upload_0']['tmp_name'];
   $file_size    = $_FILES['file_upload_0']['size'];
 
 // JSON encoded 'file upload'
   $json = json_encode(array(
     'file_name'    => $file_name,
     'file_type'    => $file_type,
-    'file_pointer' => $file_pointer,
+    'file_temp'    => $file_temp,
     'file_size'    => $file_size,
     'json_creator' => basename(__FILE__),
   ));
