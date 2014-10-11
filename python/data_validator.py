a1da9efc44fad34ee1c3e064cd1ea37a264d3415
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-10-11 10:58:05 -0400

    #267: data_validator.py, define error message

--- python/data_validator.py
+++ python/data_validator.py
@@ -87,3 +87,5 @@ class Validator:
   def file_upload_validation(self, json_file_obj):
     acceptable_type = ['application/docx', 'application/doc', 'application/txt']
     if ( magic.from_file( json.loads(json_file_obj)['file_temp'], mime=True ) not in acceptable_type ):
+      msg =  '''Error: Uploaded file, \'''' + json.loads(json_file_obj)['file_name'] + '''\', must be one of the formats:'''
+      msg += '\n       ' + ', '.join(acceptable_type)
