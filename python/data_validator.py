f9f2f99b80b6cc19bc764f0fc8f7574254daf1c0
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-10-17 19:36:27 -0400

    #300: data_validator.py, properly add file reference(s) to list

--- python/data_validator.py
+++ python/data_validator.py
@@ -97,7 +97,7 @@ class Validator:
           print msg
           sys.exit()
         # add file reference to 'list'
-        list_file_uploads[index] = hashlib.md5(json.loads(json_file_obj)['file_upload'][index]['file_temp']).hexdigest()
+        list_file_uploads.insert( index, hashlib.md5(json.loads(json_file_obj)['file_upload'][index]['file_temp']).hexdigest() )
       except:
         msg = 'Error: problem with file upload #' + str(index) + '. Please re-upload the file.'
         print msg
