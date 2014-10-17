4be378aa35d470126e8e67ba0566607ad552b475
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-10-16 20:53:26 -0400

    #300: data_validator.py, 'import itertools'

--- python/data_validator.py
+++ python/data_validator.py
@@ -5,7 +5,7 @@
 #  validates the same data to ensure that the SVM algorithm will work
 #  on the given dataset.  This adds an extra layer of security,
 #  especially if the script later is used without a web interface.
-import json, sys, magic, filecmp
+import json, sys, magic, filecmp, itertools
 
 ## Class: Validator
 class Validator:
@@ -103,4 +103,4 @@ class Validator:
         print msg
         sys.exit()
 
-      # ensure unique file uploads
+    # ensure unique file uploads
