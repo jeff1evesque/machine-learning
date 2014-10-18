742534c0fad43974040fcea7bb186b0675249bfe
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-10-18 14:03:11 -0400

    #318: helper.py, add minor comment

--- python/helper.py
+++ python/helper.py
@@ -26,6 +26,7 @@ def md5_for_file(path, block_size=256*128, hr=False):
 #    {'A': [0, 3, 5, 7], 'R': [2, 9], 'B': [1, 8], 'C': [4], 'D': [6]}
 def duplicate_list_index(list_to_check):
   duplicates = collections.defaultdict(list_to_check)
+  # store each element instance into dictionary
   for index, value in enumerate(list_to_check):
     duplicates[value].append(index)
   return duplicates
