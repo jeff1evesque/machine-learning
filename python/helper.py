25fce15e82285deea5972a7f02c6f8b7217b5e84
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-10-18 04:51:27 -0400

    #316: helper.py, minor comment change

--- python/helper.py
+++ python/helper.py
@@ -7,9 +7,6 @@ import hashlib
 ## md5_for_file: Convert a file to a hash value, equivalent. Note,
 #                block size directly depends on the block size of
 #                the filesystem.
-#
-#  Note: this method requires `import hashlib` from the file using
-#        this method.
 def md5_for_file(path, block_size=256*128, hr=False):
   md5 = hashlib.md5()
   with open(path,'rb') as f: 
