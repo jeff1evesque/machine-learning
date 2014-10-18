8f4fddf46155dcda84d29543b8ce4277741d24bf
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-10-18 04:32:14 -0400

    #316: helper.py, add 'md5_for_file' method

--- python/helper.py
+++ python/helper.py
@@ -2,3 +2,15 @@
 
 ## @helper.py
 #  This file contains helper functions.
+
+## md5_for_file: Convert a file to a hash value, equivalent. Note,
+#                block size directly depends on the block size of
+#                the filesystem.
+def md5_for_file(path, block_size=256*128, hr=False):
+  md5 = hashlib.md5()
+  with open(path,'rb') as f: 
+    for chunk in iter(lambda: f.read(block_size), b''): 
+      md5.update(chunk)
+  if hr:
+    return md5.hexdigest()
+  return md5.digest()
