daa3168516cae61bac40d0e1700e08082e3e75ca
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-10-19 14:48:56 -0400

    #300: data_validator.py, 'import duplicate_list_index'

--- python/data_validator.py
+++ python/data_validator.py
@@ -7,6 +7,7 @@
 #  especially if the script later is used without a web interface.
 import json, sys, magic
 from helper import md5_for_file
+from helper import duplicate_list_index
 
 ## Class: Validator
 class Validator:
