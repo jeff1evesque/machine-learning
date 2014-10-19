2703497acf2ae14d9d0c4032fce9319cc3447d52
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-10-19 17:34:42 -0400

    #300: data_validator.py, temporarily 'print value'

--- python/data_validator.py
+++ python/data_validator.py
@@ -111,4 +111,5 @@ class Validator:
     duplicate_indexes = duplicate_list_index( list_file_upload )
     for key, index_remove in duplicate_indexes.iteritems():
       for key, value in enumerate(index_remove):
+        print value
         del json_data['file_upload'][value]
