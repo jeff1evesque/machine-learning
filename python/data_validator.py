b33a9269a172c409ffc84c12a806a390f1590d49
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-10-19 17:22:49 -0400

    #300: data_validator.py, properly nest 'for' loops

--- python/data_validator.py
+++ python/data_validator.py
@@ -110,4 +110,5 @@ class Validator:
     # remove duplicate file upload(s)
     duplicate_indexes = duplicate_list_index( list_file_upload )
     for key, index_remove in duplicate_indexes.iteritems():
-      del json_data['file_upload'][index_remove]
+      for key, value in enumerate(index_remove):
+        del json_data['file_upload'][index_remove]
