24b5e86753b756266efbdec397d6bad01bc8ddff
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-10-20 22:33:11 -0400

    #327: svm_training.py, 'print' minor language change

--- python/svm_training.py
+++ python/svm_training.py
@@ -43,7 +43,7 @@ if len(sys.argv) > 1:
     try:
       Training(json_file_upload)
     except:
-      print '''Error: \'file upload(s)\' not correctly formatted, or does not exist''' 
+      print '''Error: \'file upload(s)\' not correctly formatted, or not provided''' 
       sys.exit()
   # validate, and send 'training' properties to 'data_creator.py'
   else:
