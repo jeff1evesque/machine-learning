1a988092cdbe22c90f6ccc64f5ba3e35b7627b17
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-10-26 19:03:33 -0400

    #356: svm_training.py, temporary file change

--- python/svm_training.py
+++ python/svm_training.py
@@ -37,15 +37,7 @@ if len(sys.argv) > 1:
   # validate input data is json format
   validator = Validator( sys.argv[1], 'training' )
 
-  # validate, and send 'file upload(s)' to 'data_creator.py'
-  if ( json.loads(sys.argv[1])['data']['json_creator'] == 'load_dataset.php' ):
-    json_file_upload = validator.file_upload_validation( sys.argv[1] )
-    if (json_file_upload is False): sys.exit()
-    else: Training(json_file_upload)
-  # validate, and send 'training' properties to 'data_creator.py'
-  elif ( json.loads(sys.argv[1])['data']['json_creator'] == 'load_logic.php' ):
-    validator.data_validation()
-    Training( sys.argv[1] )
+  print json.dumps({'error':sys.argv[1]}, separators=(',', ': '))
 else:
   msg = 'Please provide a training dataset in json format'
   print json.dumps({'error':msg}, separators=(',', ': '))
