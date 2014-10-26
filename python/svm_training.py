bf20eae1ee406fc89c778a0130cdcf20652a7310
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-10-26 18:45:42 -0400

    #362: svm_training.py, repair 'if-elif' statement

--- python/svm_training.py
+++ python/svm_training.py
@@ -38,12 +38,12 @@ if len(sys.argv) > 1:
   validator = Validator( sys.argv[1], 'training' )
 
   # validate, and send 'file upload(s)' to 'data_creator.py'
-  if ( json.loads(sys.argv[1])['data']['json_creator'] == 'load_dataset.php' ):
+  if ( json.loads(sys.argv[1])['json_creator'] == 'load_dataset.php' ):
     json_file_upload = validator.file_upload_validation( sys.argv[1] )
     if (json_file_upload is False): sys.exit()
     else: Training(json_file_upload)
   # validate, and send 'training' properties to 'data_creator.py'
-  elif ( json.loads(sys.argv[1])['data']['json_creator'] == 'load_logic.php' ):
+  elif ( json.loads(sys.argv[1])['json_creator'] == 'load_logic.php' ):
     validator.data_validation()
     Training( sys.argv[1] )
 else:
