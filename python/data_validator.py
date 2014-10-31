6cc438c3931ffc987a7636280d1e7ae36e519601
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-10-30 22:46:47 -0400

    #375: data_validator.py, create 'elif' statement

--- python/data_validator.py
+++ python/data_validator.py
@@ -83,6 +83,8 @@ class Validator:
         print json.dumps({'error':msg}, separators=(',', ': '))
         return False
 
+    elif self.svm_session == 'analysis':
+
   ## file_upload_validation(): validate 'file upload' MIME type, and return JSON object
   #                            with duplicate 'file upload' references removed.
   def file_upload_validation(self, json_file_obj):
