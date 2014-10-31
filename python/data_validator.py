f092ab6b9dbdaa0bb515fbb48ebf3002e95a9b1b
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-10-30 22:54:50 -0400

    #375: data_validator.py, add minor comment

--- python/data_validator.py
+++ python/data_validator.py
@@ -84,6 +84,7 @@ class Validator:
         print json.dumps({'error':msg}, separators=(',', ': '))
         return False
 
+    # validation on 'analysis' session
     elif self.svm_session == 'analysis':
 
   ## file_upload_validation(): validate 'file upload' MIME type, and return JSON object
