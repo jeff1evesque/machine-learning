356007017b60189592bc6e2c60b2120651f8c499
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-11-02 20:29:31 -0500

    #422: config.py, define 'jsonschema_training' method

--- python/config.py
+++ python/config.py
@@ -7,7 +7,42 @@
 #                       This validation schema is used in data_validation.py.
 def jsonschema_training():
   schema = {
-    
+    'type': 'object',
+    'properties': {
+      'data': {
+        'type': 'object',
+        'properties': {
+          'result': {
+            'type': 'object',
+            'properties': {
+              'svm_session': { 
+                'type': 'string',
+                'enum': ['training', 'analysis']
+              },
+              'svm_dataset_type': { 
+                'type': 'string',
+                'enum': ['upload file', 'xml file']
+              },
+              'svm_model_type': {
+                'type': 'string',
+                'enum': ['analysis', 'training']
+              },
+              'svm_dep_variable': {
+                'type': 'array',
+                'items': { 'type': 'string' },
+                'minItems': 1
+              },
+              'svm_indep_variable': {
+                'type': 'array',
+                'items': { 'type': 'string' },
+                'minItems': 1
+              },
+            }
+          },
+          'json_creator': { 'type': 'string' }
+        }
+      }
+    }
   }
   return schema
 
