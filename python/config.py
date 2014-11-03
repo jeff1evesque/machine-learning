c7d713edac8a75f82a3bb38f914c16727b0b297b
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-11-02 19:27:52 -0500

    #422: config.py, outline 'jsonschema_training' method

--- python/config.py
+++ python/config.py
@@ -5,6 +5,11 @@
 
 ## jsonschema_training: contains the jsonschema for the 'training' session.
 #                       This validation schema is used in data_validation.py.
+def jsonschema_training():
+  schema = {
+    
+  }
+  return schema
 
 ## jsonschema_analysis: contains the jsonschema for the 'analysis' session.
 #                       This validation schema is used in data_validation.py.
