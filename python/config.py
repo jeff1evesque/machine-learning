d08bb58fb5063a7c5adc69aad1120a5680308ed6
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-11-02 19:22:55 -0500

    #422: config.py, docblock for 'jsonschema_training' method

--- python/config.py
+++ python/config.py
@@ -2,3 +2,6 @@
 
 ## @config.py
 #  This file contains python configuration settings.
+
+## jsonschema_training: contains the jsonschema for the 'training' session.
+#                       This validation schema is used in data_validation.py.
