de153359f1bcd14a0654a8e650ca02f3f2f4a74f
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-11-04 21:15:26 -0500

    #455: data_validator.py, properly define 'json_data'

--- python/data_validator.py
+++ python/data_validator.py
@@ -30,7 +30,7 @@ class Validator:
   def data_validation(self):
     # local variables
     flag_json = False
-    json_data = json.loads(json_file_obj)['data']['result']
+    json_data = json.loads(self.svm_data)['data']['result']
 
     # determine if input data is a JSON object
     try:
