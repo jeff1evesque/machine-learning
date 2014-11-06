5cfc50c17767a6a154918dfecfcf8a298560eaf1
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-11-05 21:44:28 -0500

    #243: form_validator.js, add docblock

---
+++ src/js/form_validator.js
@@ -0,0 +1,6 @@
+/**
+ * @form_validator.js: this script calls the 'validate()' method as suggested on
+ *                     the github documentation:
+ *
+ *     https://github.com/jzaefferer/jquery-validation#including-it-on-your-page
+ */
