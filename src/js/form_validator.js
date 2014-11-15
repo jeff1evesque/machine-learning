e6bd48bf5ea97c776fd30544004a16afcbf9aad1
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-11-15 16:13:08 -0500

    #475: form_validator.js, add source to overall docblock

--- src/js/form_validator.js
+++ src/js/form_validator.js
@@ -4,6 +4,7 @@
  *     http://jqueryvalidation.org/documentation/
  *     http://jqueryvalidation.org/category/validator/
  *     http://jqueryvalidation.org/jQuery.validator.addMethod/
+ *     http://stackoverflow.com/questions/10843399#answer-10843593
  */
 
 /**
