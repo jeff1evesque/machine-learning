c01c29f7c137952487dd58dc44a8c9db0bf80a90
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-11-25 17:49:49 -0500

    #461: svm_json.py, define initial file

---
+++ python/svm_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+## @svm_json.py
+#  This file contains methods to convert svm data to a JSON structure.
+
+## Class: JSON
+class JSON:
+
+  ## constructor
+  def __init__(self, svm_data):
+  self.svm_data = svm_data
