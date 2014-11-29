87a1ad7325c45292c943549b218456d358f6e513
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-11-29 17:19:09 -0500

    #461: svm_json.py, open temporary 'file upload(s)'

--- python/svm_json.py
+++ python/svm_json.py
@@ -13,8 +13,7 @@ class JSON:
 
   ## csv_to_json: convert csv file to JSON object
   def csv_to_json(self):
-    print self.svm_file
-    #file_csv  = open( self.svm_file, 'r' )
+    file_csv  = open( self.svm_file, 'r' )
 
   ## xml_to_json: convert xml to JSON object
   def xml_to_json(self):
