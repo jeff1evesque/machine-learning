eb2486198e062e3d938f8c783e198ee6cce7357c
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-11-29 17:44:39 -0500

    #461: svm_json.py, create json object

--- python/svm_json.py
+++ python/svm_json.py
@@ -16,6 +16,8 @@ class JSON:
     file_csv  = open( self.svm_file, 'r' )
     reader = csv.DictReader( file_csv )
 
+    for row in reader:
+      json_object = json.dump( row )
   ## xml_to_json: convert xml to JSON object
   def xml_to_json(self):
     print 'dummy code'
