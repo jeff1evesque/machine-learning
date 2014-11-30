d1bdeebc585223039cffe382c14c71beea0c9f3a
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-11-30 17:09:38 -0500

    #675: svm_training.py, implement 'try-except' for json 'merge()' method

--- python/svm_training.py
+++ python/svm_training.py
@@ -56,11 +56,19 @@ if len(sys.argv) > 1:
       else:
         for val in json_file_upload['file_upload']:
           if val['type'] in ('text/plain', 'text/csv'):
-            json_dataset = merge( json_dataset, JSON( val['filedata']['file_temp']).csv_to_json() )
+            try:
+              json_dataset = merge( json_dataset, JSON( val['filedata']['file_temp']).csv_to_json() )
+            except:
+              json_dataset = JSON( val['filedata']['file_temp']).csv_to_json()
+
             validator_json = Validator( json_dataset )
             Training( json_dataset )
           elif val['type'] in ('application/xml', 'text/xml' ):
-            json_dataset = merge( JSON( val['filedata']['file_temp']).xml_to_json() )
+            try:
+              json_dataset = merge( json_dataset, JSON( val['filedata']['file_temp']).xml_to_json() )
+            except:
+              json_dataset = JSON( val['filedata']['file_temp']).xml_to_json()
+
             validator_json = Validator( json_dataset )
             Training( json_dataset )
 
