b3af218ac0df63dee8a2909da77fa02facb62291
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-11-29 19:26:26 -0500

    #654: data_validator.py, correct method names

--- python/data_validator.py
+++ python/data_validator.py
@@ -57,7 +57,7 @@ class Validator:
     # validation on 'training' session
     if self.svm_session == 'training' and flag_json:
       try:
-        validate(json.loads(self.svm_data), jsonschema_training())
+        validate(json.loads(self.svm_data), jsonschema_training)
       except Exception, e:
         print str(e)
         return False
@@ -70,7 +70,7 @@ class Validator:
     # validation on 'analysis' session
     if self.svm_session == 'analysis' and flag_json:
       try:
-        validate(json.loads(self.svm_data), jsonschema_analysis())
+        validate(json.loads(self.svm_data), jsonschema_analysis)
       except Exception, e:
         print str(e)
         return False
