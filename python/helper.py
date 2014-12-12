3fbee0609ee680653ed7126311517110a56bd075
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-12-11 19:26:12 -0500

    #694: helper.py, define 'jsonmerge' method

--- python/helper.py
+++ python/helper.py
@@ -3,15 +3,18 @@
 ## @helper.py
 #  This file contains helper functions.
 import hashlib, collections
+from collections import defaultdict
 
 ## jsonmerge: this method takes two json objects, and merges them into one.
 def jsonmerge( json1, json2 ):
-  if bool( json1 ) == True and bool( json2 ) == True:
-    return json1, json2
-  elif bool( json1 ) == True and bool( json2 ) == False:
-    return json1
-  elif bool( json1 ) == False and bool( json2 ) == True:
-    return json2
+  dd = defaultdict(list)
+
+  for d in ( json1, json2 ):
+    # concatenate list items
+    for key, value in d.iteritems():
+      dd[key] += value
+
+  return dd
 
 ## md5_for_file: Convert the contents of a given file to a hash value,
 #                equivalent. Note, block size directly depends on the
