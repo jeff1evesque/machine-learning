d51b80b4b9ac1cdba6ed5ceaa8e3c789bfc2c1b2
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-12-14 18:27:27 -0500

    #7: data_creator.py, create 'db_machine_learning' database if not exist

--- python/data_creator.py
+++ python/data_creator.py
@@ -15,6 +15,20 @@ class Training:
   def db_save_dataset(self):
     print self.svm_data
 
+    # create database if doesn't exist
+    try:
+      con    = DB.connect( host='localhost', 'username', passwd='**' )
+      cursor = con.cursor()
+      sql    = 'CREATE DATABASE IF NOT EXISTS db_machine_learning'
+      cursor.execute( sql )
+      con.close()
+    except DB.Error, e:
+      print "Error %d: %s" % (e.args[0], e.args[1])
+      return False
+    finally:
+      if con:
+        con.close()
+
 ## Class: Analysis
 class Analysis:
 
