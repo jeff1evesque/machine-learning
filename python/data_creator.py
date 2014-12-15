b69170e776b0a3f405949161d80b6fdb0b14cfe5
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-12-14 22:35:52 -0500

    #7: data_creator.py, fix minor typo

--- python/data_creator.py
+++ python/data_creator.py
@@ -57,7 +57,7 @@ class Training:
 
       for dep_variable, indep_variables in self.svm_data.iteritems():
         sql  = '''
-               INSERT INTO tbl_dataset ( dep_variable, indep_variable ) VALUES ( %s, %s );
+               INSERT INTO tbl_dataset ( dep_variable, indep_variables ) VALUES ( %s, %s );
                '''
         cursor.execute( sql % ( dep_variable, indep_variables ) )
     except DB.Error, e:
