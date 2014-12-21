bae99fa12251e83599b5be9c6f930baa388ed844
Jeff Levesque <jeff1evesque@yahoo.com>
2014-12-21 13:48:45 -0500

    #752: data_validator.py, add comment

--- python/data_validator.py
+++ python/data_validator.py
@@ -82,6 +82,8 @@ class Validator:
           print json.dumps({'error':msg}, separators=(',', ': '))
           return False
 
+        # validate remaining records of first column
+
       # iterate dependent, and list of independent variables (except first column)
       for dependent, indep_list in self.svm_data.items()[:-1]:
         # validate SVM dependent variables
