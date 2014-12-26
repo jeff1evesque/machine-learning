ae3841e75e0d2a6f282da6148593736e502d9676
Jeff Levesque <jeff1evesque@yahoo.com>
2014-12-26 13:56:25 -0500

    #785: svm_json.py, add 'list_dataset' list variable

--- python/svm_json.py
+++ python/svm_json.py
@@ -20,7 +20,7 @@ class JSON:
   #        understood regardless, if the newline character was created in
   #        osx, windows, or linux.
   def csv_to_json(self):
-    columns = defaultdict( list )
+    list_dataset = []
     dep_variable = {}
 
     # open temporary 'csvfile'
