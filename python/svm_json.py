560a6bcbeba1061340e00f61b5c6ce108ca69716
Jeff Levesque <jeff1evesque@yahoo.com>
2014-12-26 17:58:05 -0500

    #785: svm_json.py, return 'list_dataset'

--- python/svm_json.py
+++ python/svm_json.py
@@ -30,7 +30,7 @@ class JSON:
     dep_variable_label   = []
     indep_variable_label = []
 
-    # open temporary 'csvfile'
+    # open temporary 'csvfile' reader object
     with open( self.svm_file, 'rU' ) as csvfile:
       dataset_reader = csv.reader( csvfile, delimiter=' ', quotechar='|' )
 
@@ -45,7 +45,7 @@ class JSON:
       # iterate all rows of csvfile (except first)
       for dep_index, row in enumerate( islice( dataset_reader, 1, None ) ):
 
-        # iterate first element of each row (except first)
+        # iterate first column of each row (except first)
         row_dep_label = row[0].split(',')
         for value in row_dep_label[:1]:
           dep_variable_label.append( value )
@@ -55,8 +55,8 @@ class JSON:
         for indep_index, value in enumerate( islice( row_indep_variable, 1, None) ):
           list_dataset.append( { dep_variable_label[dep_index]: {indep_variable_label[indep_index]: value} } )
 
-    #return json.dumps(columns)
-    print list_dataset
+    return json.dumps( list_dataset )
+
   ## xml_to_json: convert xml to JSON object
   def xml_to_json(self):
     print 'dummy code'
