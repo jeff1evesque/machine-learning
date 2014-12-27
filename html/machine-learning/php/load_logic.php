7fd75fe88312d7576edeaea7f8b385cd9019b479
Jeff Levesque <jeff1evesque@yahoo.com>
2014-12-27 15:25:51 -0500

    #791: load_logic.php, account for no 'datalist_support'

--- html/machine-learning/php/load_logic.php
+++ html/machine-learning/php/load_logic.php
@@ -81,7 +81,7 @@
         }
       }
     // HTML5 datalist not supported: use counterpart, remove 'session_type'
-      elseif (mb_check_encoding($this->form->datalist_support, 'UTF-8')) {
+      elseif (!this->form->datalist_support && mb_check_encoding($this->form->datalist_support, 'UTF-8')) {
         if (in_array(strtolower($this->form->svm_session), $arr_session_type)) {
           $session_type            = $this->form->session_type;
           $this->form->svm_session = $session_type;
