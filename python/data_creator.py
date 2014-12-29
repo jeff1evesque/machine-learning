932385888be9e80b089cc198bf6ac184ac206b4c
Jeff Levesque <jeff1evesque@yahoo.com>
2014-12-29 08:51:19 -0500

    #787: data_creator.py, implement 'LAST_INSERT_ID' as mysql code, not python

--- python/data_creator.py
+++ python/data_creator.py
@@ -112,8 +112,8 @@ class Training:
         sql  = 'INSERT INTO tbl_dataset_entity (uid, entity, datetime_saved) VALUES( %s, %s, UTC_TIMESTAMP() )'
         cursor.execute( sql, (self.uid, data_instance['dep_variable_label']) )
 
-        sql = 'INSERT INTO tbl_dataset_attribute (id, attribute) VALUES( %s, %s )'
-        cursor.execute( sql, (LAST_INSERT_ID(), data_instance['indep_variable_label']) )
+        sql = 'INSERT INTO tbl_dataset_attribute (id, attribute) VALUES( LAST_INSERT_ID(), %s )'
+        cursor.execute( sql, (data_instance['indep_variable_label']) )
 
         sql = 'INSERT INTO tbl_dataset_value (attribute, value) VALUES( %s, %s )'
         cursor.execute( sql, (data_instance['indep_variable_label'], data_instance['indep_variable_value']) )
