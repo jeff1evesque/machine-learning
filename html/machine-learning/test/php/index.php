ee5f6ae77214a14b0418931ffbe5669ebce98c00
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-02 13:38:09 -0500

    #858: index.php, remove unnecessary subset of docblock

--- html/machine-learning/test/php/index.php
+++ html/machine-learning/test/php/index.php
@@ -5,21 +5,6 @@
  *             or URL to an XML (dataset), then defines which attributes within the dataset
  *             previously defined, to use for the SVM analysis.  After the configurations and 
  *             attributes have been defined, it is sent to 'logic_loader.py' for further analysis.
- *
- *  @form:     sends form-data to 'logic_loader.py' using the 'post' method.
- *
- *  @input:    uses the 'list' attribute to refer to the datalist element which contains
- *             pre-defined options for this input element.
- *
- *  @datalist: an HTML5 tag that specifies a list of pre-defined options for an input element.
- *             Since this is not supported in IE9-, or Safari, the 'select' element is nested
- *             inside the 'datalist'.  This allows for graceful degradation.  The input element
- *             uses a 'list' attribute to reference the 'id' attribute of the datalist.
- *
- *             In the case where datalist is not supported in the browser, the corresponding
- *             input element becomes orphanted.  This means, any ajax scripts (or form 'action'
- *             script) need to reference the nested 'select' element, instead of the orphanted
- *             'input' element.
  */
 ?>
 <html>
