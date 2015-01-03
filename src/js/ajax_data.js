054d2128feb83d9b8f70e8d3b71654208374b97e
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-03 16:25:25 -0500

    #871: ajax_data.js, remove redundant docblock content

--- src/js/ajax_data.js
+++ src/js/ajax_data.js
@@ -1,21 +1,6 @@
 /**
  * ajax_data.js: this script utilizes ajax to relay the form POST data, to a defined
  *               'action' script.
- *
- * @contentType, the content type, some refer to as the 'MIME', is used when sending
- *    data to the server. The Default is 'application/x-www-form-urlencoded'. Setting
- *    this attribute to 'false', forces jQuery not to add a 'Content-Type' header. This
- *    property tells the browser, or server, how to open the specified file. 
- *
- * @processData, boolean value specifying whether or not data sent to server should be
- *   transformed into a query string.  The default is 'true'.  Setting this value to
- *   'false' tells jQuery not to convert the 'data' object into a serialized parameter
- *   string, which would be done before sending the 'data' to the server.
- *
- * Note: 'xml_upload(s)' is sent to python scripts via 'ajax_json.js'.
- *
- * Note: the implemented AJAX methods, and properties, are discussed more fully within
- *       'ajax_json.js'
  */
 
 $(document).ready(function() {
