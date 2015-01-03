995f8dfe674352f9dc57b17d04b653b3008c6afe
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-03 16:22:15 -0500

    #871: ajax_data.js, add entire 'ajax_json' logic

--- src/js/ajax_data.js
+++ src/js/ajax_data.js
@@ -64,3 +64,80 @@ $(document).ready(function() {
 
   });
 });
+
+$(document).ready(function() {
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+
+  // serialize data into array
+    var data_formatted = $('form').serializeArray();
+
+  // ajax request: form fields (except 'svm_dataset[]')
+    $.ajax({
+      url: $(this).attr('action'),
+      type: 'POST',
+      data : data_formatted,
+      dataType : 'json',
+      beforeSend: function() {
+
+    // ajax overlay
+        ajaxLoader( $(event.currentTarget) );
+
+    // form validation
+        $("form").validate({
+          submitHandler: function(form) {
+            $(form).ajaxSubmit();
+          }
+        });
+
+      }
+    }).done(function(data) {
+  // JSON object from Server
+      json_server = ( !$.isEmptyObject( data ) ) ? JSON.stringify(data, undefined, 2) : 'none';
+      console.log( 'JSON object from Server: ' + json_server );
+
+  // server side error
+      $('form .fieldset_error').remove();
+      if (data.hasOwnProperty('error')) {
+        var msg_error = '\
+            <fieldset class="fieldset_error">\
+              <legend>Submission Error</legend>\
+              <p>Error: '+data.result.error+'</p>\
+            </fieldset>\
+          ';
+        $('form').prepend(msg_error);
+      }
+
+  // remove ajax overlay
+      $('form .ajax_overlay').fadeOut(200, function(){ $(this).remove() });
+    }).fail(function(jqXHR, textStatus, errorThrown) {
+      $('form .fieldset_error').remove();
+
+      var msg_error = '\
+          <fieldset class="fieldset_error">\
+              <legend>Submission Error</legend>\
+              <p>Error: '+data.result.error+'</p>\
+            </fieldset>\
+          ';
+        $('form').prepend(msg_error);
+      }
+
+  // remove ajax overlay
+      $('form .ajax_overlay').fadeOut(200, function(){ $(this).remove() });
+    }).fail(function(jqXHR, textStatus, errorThrown) {
+      $('form .fieldset_error').remove();
+
+      var msg_error = '\
+          <fieldset class="fieldset_error">\
+            <legend>Submission Error</legend>\
+            <p>Error: '+errorThrown+'</p>\
+          </fieldset>\
+        ';
+     $('form').prepend(msg_error);
+
+      console.log('Error Thrown: '+errorThrown);
+      console.log('Error Status: '+textStatus);
+    });
+
+  });
+});
