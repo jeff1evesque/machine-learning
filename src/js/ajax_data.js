413e30189147405d6d261c0f008356fa0bfb25ee
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-03 17:08:53 -0500

    #871: ajax_data.js, minor comment changes

--- src/js/ajax_data.js
+++ src/js/ajax_data.js
@@ -7,15 +7,15 @@ $(document).ready(function() {
   $('form').on('submit', function(event) {
     event.preventDefault();
 
-  // local variables
+  // Local Variables
     var form_data = new FormData();
     var dataset   = $('input[name="svm_dataset[]"]');
     var flag_ajax = true;
 
-  // serialize data into array (not file-uploads)
+  // Serialize Data into Array (not file-uploads)
     var data_formatted = $('form').serializeArray();
 
-  // store 'file upload(s)' into array
+  // Store 'file upload(s)' into Array
     if ( dataset.length > 0 && dataset.attr('type') == 'file' ) {
       $( dataset ).each(function( index ) {
         var file_data = dataset[index].files[0];
@@ -23,7 +23,7 @@ $(document).ready(function() {
       });
     }
 
-  // undefined 'file upload(s)' sets 'flag_ajax = false'
+  // Undefined 'file upload(s)' sets 'flag_ajax = false'
     dataset.each(function() {
       if ( typeof $(this).val() === 'undefined' ) {
         flag_ajax = false;
@@ -31,7 +31,7 @@ $(document).ready(function() {
       }
     });
 
-  // ajax request: 'svm_dataset[]' file upload(s)
+  // AJAX Process
     if ( flag_ajax ) {
       $.ajax({
         url: '../../php/load_dataset.php',
@@ -42,10 +42,10 @@ $(document).ready(function() {
         processData: false,
         beforeSend: function() {
 
-        // ajax overlay
+        // AJAX Overlay
           ajaxLoader( $(event.currentTarget) );
 
-        // form validation
+        // Form Validation
           $("form").validate({
             submitHandler: function(form) {
               $(form).ajaxSubmit();
@@ -55,11 +55,11 @@ $(document).ready(function() {
         }
       }).done(function(data) {
 
-      // JSON object from Server
+      // JSON Object from Server
         json_server = ( !$.isEmptyObject( data ) ) ? JSON.stringify(data, undefined, 2) : 'none';
         console.log( 'JSON object from Server: ' + json_server );
 
-      // remove ajax overlay
+      // Remove AJAX Overlay
         $('form .ajax_overlay').fadeOut(200, function(){ $(this).remove() });
 
       }).fail(function(jqXHR, textStatus, errorThrown) {
