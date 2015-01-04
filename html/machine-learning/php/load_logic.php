1a92605fa1528155e65143ce359605864c45d0d9
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-04 11:08:53 -0500

    #872: load_logic.php, change 'this->form' -> 'this->settings'

--- html/machine-learning/php/load_logic.php
+++ html/machine-learning/php/load_logic.php
@@ -117,7 +117,7 @@
         $arr_result = array('data' => $arr_result);
         $arr_result = array_merge($arr_result, array('json_creator' => basename(__FILE__)), $arr_result);
 
-        if ($this->form->svm_session == 'training') {
+        if ($this->settings->svm_session == 'training') {
           $result = shell_command('python ../../../python/svm_training.py', json_encode($arr_result));
         }
         else {
