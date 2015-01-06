9a110ad837dc08447822e577982f669890be4d95
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-06 08:07:46 -0500

    #899: data_validator.py, change 'response' -> 'responses'

--- python/data_validator.py
+++ python/data_validator.py
@@ -32,7 +32,7 @@ class Validator:
   def data_validation(self):
     # local variables
     flag_json = False
-    responses = {}
+    response  = {}
 
     # determine if input data is a JSON object
     try:
