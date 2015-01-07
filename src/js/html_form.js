0014e6cf7a1d02b7e6c815fd5b85de8de079b95a
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-07 10:18:34 -0500

    #156: html_form.js, boolean for value, not substring

--- src/js/html_form.js
+++ src/js/html_form.js
@@ -25,7 +25,7 @@ $(document).ready(function() {
           </fieldset><br>\
         ';
     }
-    else if ( $(this).val().toLowerCase().substring(0, 4) == 'data' ) {
+    else if ( $(this).val().toLowerCase() == 'data_new' ) {
       obj_form.session = '\
           <fieldset class="fieldset_session_training">\
             <legend>Training Session</legend>\
