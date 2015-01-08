d1f83bdaf334bcd540c58b82636136b032444b4c
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-07 22:31:39 -0500

    #971: ajax_session.js, add skeleton file

---
+++ src/js/ajax_session.js
@@ -0,0 +1,9 @@
+/**
+ * ajax_session.js: this script utilizes ajax to retrieve data from
+ *                  'retriever_session.php'. Specifically, the 'svm_title', and
+ *                  'id_entity' values are taken from EAV data model, database
+ *                  tables, and inserted into the DOM element 'svm_session_id'.
+ */
+
+$(document).ready(function() {
+});
