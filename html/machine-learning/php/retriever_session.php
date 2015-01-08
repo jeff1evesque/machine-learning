dc8757421c0d39c7d5335749db045d83eaf40cac
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-08 17:34:04 -0500

    #972: retriever_session.php, initial skeleton file

---
+++ html/machine-learning/php/retriever_session.php
@@ -0,0 +1,9 @@
+<?php
+
+ /**
+  * retriever_session.php: query 'svm_title', and 'id_entity' value from database.
+  *                        Then, return the two corresponding values via AJAX to
+  *                        'ajax_session.js'
+  */
+
+?>
