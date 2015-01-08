6bb066168a007cea8142cf1e5d07a917b1e8f35c
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-07 21:57:54 -0500

    #976: html_form.js, add 'svm_dataset_type' for 'Append Data' session

--- src/js/html_form.js
+++ src/js/html_form.js
@@ -57,6 +57,11 @@ $(document).ready(function() {
               <select name="svm_session_id">\
                 <option value="none" selected="selected">--Select--</option>\
               </select>\
+              <select name="svm_dataset_type">\
+                <option value="none" selected="selected">--Select--</option>\
+                <option value="file_upload">Upload file</option>\
+                <option value="xml_url">XML URL</option>\
+              </select>\
             </fieldset>\
           </fieldset>\
         ';
