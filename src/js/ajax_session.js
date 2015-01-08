d459a0392590736494cec863185404a0ce0c5e02
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-08 16:43:29 -0500

    #971: ajax_session.js, remove mutation observer

--- src/js/ajax_session.js
+++ src/js/ajax_session.js
@@ -5,85 +5,33 @@
  *                  tables, and inserted into the DOM element 'svm_session_id'.
  */
 
-$(document).ready(function() {
-
-// local variables
-  var flag_ajax        = false;
-  var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
-  var target           = document.querySelector('form');
-
-// Mutation Observation: create observer instance
-  var observer = new MutationObserver(function(mutations) {
-    mutations.forEach(function(mutation) {
-
-      if ( mutation.type == 'childList' && typeof mutation.addedNodes == 'object' && mutation.addedNodes.length > 0 ) {
-        for (var i=0; i < mutation.addedNodes.length; ++i) {
-          var data_upload_container = mutation.addedNodes[i];
-
-          if ( data_upload_container.nodeName !== '#text' ) {
-            for (var j=0; j < data_upload_container.childNodes.length; ++j) {
-              var data_upload_elements = data_upload_container.childNodes[j];
-
-              if ( data_upload_elements.nodeName !== '#text' ) {
-                for (var k=0; k < data_upload_elements.childNodes.length; ++k) {
-                  var configuration_elements = data_upload_elements.childNodes[k];
-
-                  if ( configuration_elements.nodeName.toLowerCase() === 'select' ) {
-                    var attr_name = configuration_elements.name;
-                    if ( attr_name === 'svm_session_id' ) {
-                      flag_ajax = true;
-                    }
-                  }
-
-                }
-              }
-
-            }
-          }
-
-        }
-      }
-
-    });
-
-  // AJAX Process
-    if ( $('.fieldset_session_data_upload').length > 0 && flag_ajax ) {
-      $.ajax({
-        type: 'POST',
-        url: '../../php/retriever_sesion.php',
-        dataType: 'json',
-        beforeSend: function() {
-          ajaxLoader( $(target) );
-        }
-      }).done(function(data) {
-
-      // Append to DOM
-        $.each( data['return'], function( index, value ) {
-          var value_id    = value['value_id'];
-          var value_title = value['value_title'];
-          var element     = '<option ' + 'value="' + value_id + '">' + value_title + '</option>';
-
-          $('select[name="svm_session_id"]').append( element );
-
-        // Remove AJAX Overlay
-          $('form .ajax_overlay').fadeOut(200, function(){ $(this).remove() });
-        })
-
-      }).fail(function(jqXHR, textStatus, errorThrown) {
-        console.log('Error Thrown: '+errorThrown);
-        console.log('Error Status: '+textStatus);
-
-      // Remove AJAX Overlay
-        $('form .ajax_overlay').fadeOut(200, function(){ $(this).remove() });
-      });
+// AJAX Process
+  $.ajax({
+    type: 'POST',
+    url: '../../php/retriever_sesion.php',
+    dataType: 'json',
+    beforeSend: function() {
+      ajaxLoader( $(event.currentTarget) );
     }
+  }).done(function(data) {
 
+  // Append to DOM
+    $.each( data['return'], function( index, value ) {
+      var value_id    = value['value_id'];
+      var value_title = value['value_title'];
+      var element     = '<option ' + 'value="' + value_id + '">' + value_title + '</option>';
+
+      $('select[name="svm_session_id"]').append( element );
+
+    // Remove AJAX Overlay
+      $('form .ajax_overlay').fadeOut(200, function(){ $(this).remove() });
+    })
+
+  }).fail(function(jqXHR, textStatus, errorThrown) {
+    console.log('Error Thrown: '+errorThrown);
+    console.log('Error Status: '+textStatus);
+
+  // Remove AJAX Overlay
+    $('form .ajax_overlay').fadeOut(200, function(){ $(this).remove() });
   });
 
-// Mutation Observation: configuration for observer
-  var config = { attributes: true, childList: true, subtree: true };
-
-// Mutation Observation: pass target, and configuration to observer
-  observer.observe(target, config);
-
-});
