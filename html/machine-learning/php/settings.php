d844656c2249108ab2375ac25cada1fde20bc13f
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-08 20:51:19 -0500

    #972: settings.php, add 'get' method for 'Dataabase' class

--- html/machine-learning/php/settings.php
+++ html/machine-learning/php/settings.php
@@ -17,4 +17,26 @@
       $this->db_username = 'authenticated';
       $this->db_password = 'password';
     }
+
+  /**
+   * get_db_host
+   */
+    public function get_db_host() {
+      return $this->db_host;
+    }
+
+  /**
+   * get_db_username
+   */
+    public function get_db_username() {
+      return $this->db_username;
+    }
+
+  /**
+   * get_db_password
+   */
+    public function get_db_password() {
+      return $this->db_password;
+    }
+
  }
