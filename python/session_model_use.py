63a7bd5c785bd9f9e4aadacdd73abd3ce68c8cc2
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-12 12:26:56 -0500

    #1048: session_model_use.py, adjust docblock

--- python/session_model_use.py
+++ python/session_model_use.py
@@ -7,28 +7,33 @@
 #      during this session.
 #
 #  Note: This script is executed from 'logic_loader.php' using the 'exec( ... )'
-#        method. To return data to php, use a simple 'print' statement. For
-#        example the following will return the arguments passed to this script:
+#        equivalent method when implemented via the web-interface. Since the
+#        web-interface is an AJAX process, the shelled into python script requires
+#        print statements, when data is needed to be returned to the client-end.
+#
+#        The following will return the arguments passed into this AJAX shelled
+#        into python script:
 #
 #            print sys.argv[1]
 #
-#        Then, in php, we can capture this data by:
+#        Then, in php, we can capture the returned data:
 #
 #            $output = exec("$command $parameters");
 #            $arr_result = array('result' => $output);
 #            $json = array_merge($json, $arr_result);
 #
-#        Which will allow us to return it via ajax, as follows:
+#        Which will allow us to return it to the client-end:
 #
 #            print json_encode($json);
 #
 #        Note: we can only call `print json_encode($json)` once. The receiving
-#              receiving javascript can interpret the data as follows:
+#              javascript will interpret the data as follows:
 #
 #            console.log( data.result );
 #
-#  @import sys, provides various functions, and variables that can be used to
-#      manipulate different parts of the Python runtime environment (i.e. argv).
+#  Note: the term 'dataset' used throughout various comments in this file,
+#        synonymously implies the user supplied 'file upload(s)', and XML url
+#        references.
 import sys, json
 from data_creator import Analysis
 from data_validator import Validator
