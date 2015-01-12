5f0e65cafa1d4eb04a9db1e6d9b6b0321e3fb408
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-12 12:52:31 -0500

    #152: session_model_generate.py, minor language change

--- python/session_model_generate.py
+++ python/session_model_generate.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 
 ## @session_model_generate.py
-#  This file receives data (i.e. settings) required to query from the database a
-#      previously stored session, involving one or more stored dataset uploads,
-#      and generates a corresponding SVM model. The new SVM model, is stored into
-#      a database table, which later can be retrieved via 'session_model_use.py'.
+#  This file receives data (i.e. settings) required to query from the database,
+#      a previously stored session, involving one or more stored dataset uploads,
+#      and generates an SVM model, respectively. The new SVM model, is stored
+#      into respective database table(s), which later can be retrieved within
+#      'session_model_use.py'.
 
