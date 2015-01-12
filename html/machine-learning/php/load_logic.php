e7879d1452cf8ba03582d836bbe78fef391ec5e9
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-12 14:32:48 -0500

    mend
    #1066: connector_python.php, remove closing bracket

--- html/machine-learning/php/load_logic.php
+++ html/machine-learning/php/load_logic.php
@@ -14,9 +14,9 @@
 //  print json_encode($_FILES);
 
 // instantiate data / loader
-  $obj_data   = new Obj_Data($_POST);
-  $obj_loader = new Obj_Loader($obj_data, $_FILES);
-  $messages   = $obj_loader->logic_loader($json);
+//  $obj_data   = new Obj_Data($_POST);
+//  $obj_loader = new Obj_Loader($obj_data, $_FILES);
+//  $messages   = $obj_loader->logic_loader($json);
 
 // Return feedback to AJAX
   if ( sizeof($messages['error']) > 0 ) print json_encode( $messages['error'] );
