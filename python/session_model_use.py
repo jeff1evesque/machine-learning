5dca4575d0a1e91c0c3f1806b67a735af3a955a4
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-12 12:22:33 -0500

    #1048: session_model_use.py, adjust docblock

--- python/session_model_use.py
+++ python/session_model_use.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python
 
-## @data_analyzer.py
-#  This file properly escapes POST data received from 'php/logic_loader.php',
-#      parses the data, and sends respective portions of the POST data to
-#      'python/svm_model.py'. svm_model.py is responsible for loading the
-#      correct SVM model from the mySQL database, which will be used to make
-#      necessary calculations / estimations.
+## @session_model_use.py
+#  This file receives data (i.e. settings) required to query from the database,
+#      a previously stored SVM model, generated from 'session_model_generate.py'.
+#      The SVM model is then used for analysis based on the input data provided
+#      during this session.
 #
 #  Note: This script is executed from 'logic_loader.php' using the 'exec( ... )'
 #        method. To return data to php, use a simple 'print' statement. For
