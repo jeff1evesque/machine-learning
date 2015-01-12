686cbe8147a2bcd1a13a5a9ab55557990413db2f
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-12 12:40:53 -0500

    #1049: session_data_append.py, minor language change

--- python/session_data_append.py
+++ python/session_data_append.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
 ## @session_data_append.py
-#  This file receives data (i.e. settings) required to query from the database a
-#      previously stored session, involving one or more stored dataset uploads,
+#  This file receives data (i.e. settings) required to query from the database,
+#      a previously stored session, involving one or more stored dataset uploads,
 #      and appends additional dataset(s) provided during the current session. The
-#      new stored dataset can later be retrieved in 'session_model_generate.py'.
-
+#      superset of appended dataset(s), can later be retrieved in another instance
+#      of this script, or within 'session_model_generate.py'.
