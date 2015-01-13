2c3ac57169ea18a5653bacad1356a132e2e908a7
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-13 13:24:24 -0500

    #1051: load_logic.py, add 'import' for each 'session_xxx_xxx.py' scripts

--- python/load_logic.py
+++ python/load_logic.py
@@ -29,6 +29,10 @@
 #
 #      console.log( data.result );
 import sys, json
+from session_data_new import Data_New
+from session_data_append import Data_Append
+from session_model_generate import Model_Generate
+from session_model_use import Model_Use
 
 # Local Variables
 list_error = []
