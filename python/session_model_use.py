98c1315800cf1e20038d89e542a0bf277beed2d4
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-18 13:37:34 -0500

    #1159: session_model_use.py, instantiate correct class

--- python/session_model_use.py
+++ python/session_model_use.py
@@ -50,7 +50,7 @@ class Model_Use:
   def CHANGE_METHOD(self):
     if len(sys.argv) > 1:
       # validate input data is json format
-      validator = Validator( sys.argv[1], self.svm_session )
+      validator = Validate_Settings( sys.argv[1], self.svm_session )
 
       # validate, and set SVM properties to 'data_creator.py'
       if ( json.loads(sys.argv[1])['json_creator'] == 'load_logic.php' ):
