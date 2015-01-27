27327299f627db233e5de0a64368a751f44e285d
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-26 21:48:47 -0500

    #1174: load_logic.py, implement class methods for 'data_append'

--- python/load_logic.py
+++ python/load_logic.py
@@ -74,6 +74,22 @@ if len(sys.argv) > 1:
     session = Data_Add( sys.argv[1] )
 
     # implement class methods
+    if not session.validate_arg_none():
+      session.validate_svm_settings()
+      session.validate_mime_type()
+      session.save_svm_entity()
+      if len(session.return_error()) > 0:
+        for val in session.return_error():
+          print val
+        sys.exit()
+
+      session.dataset_to_json()
+      session.validate_dataset_json()
+      session.save_svm_dataset()
+      if len(session.return_error()) > 0:
+        for val in session.return_error():
+          print val
+        sys.exit()
 
   elif session_type == 'model_generate':
 
