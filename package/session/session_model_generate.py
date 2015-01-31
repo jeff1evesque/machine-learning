40a6a285fc19c8c4432df0f04a42cedb8292cd97
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-31 12:20:39 -0500

    #1072: session_model_generate.py, define 'validate_arg_none'

--- package/session/session_model_generate.py
+++ package/session/session_model_generate.py
@@ -14,3 +14,9 @@ class Model_Generate:
   def __init__(self, svm_data):
     self.svm_data       = svm_data
     self.response_error = []
+
+  ## validate_arg_none: check if class variable 'svm_data' is defined.
+  def validate_arg_none(self):
+    if self.svm_data == None: return True
+    else: return False
+
