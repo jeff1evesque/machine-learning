7cc4da788528cf69675497f514c9387a75cd8d12
Jeff Levesque <jeff1evesque@yahoo.com>
2015-02-02 12:49:18 -0500

    #1258: data_saver.py, create database tables using 'import db_query'

--- package/database/data_saver.py
+++ package/database/data_saver.py
@@ -46,41 +46,35 @@ class Training:
     sql.sql_command( sql_statement, 'create')
     sql.sql_disconnect()
 
-    # create 'type' table if doesn't exist
-    try:
-      conn   = DB.connect( host=self.db_settings.get_db_host(), user=self.db_settings.get_db_username(), passwd=self.db_settings.get_db_password(), db='db_machine_learning' )
-      cursor = conn.cursor()
+    # create 'tbl_dataset_entity', 'tbl_dataset_value' if doesn't exist
+    sql.sql_connect()
 
-      if self.svm_cmd == 'save_entity':
-        sql    = '''\
-                 CREATE TABLE IF NOT EXISTS tbl_dataset_entity (
-                   id_entity INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                   title VARCHAR (50) NOT NULL,
-                   uid_created INT NOT NULL,
-                   datetime_created DATETIME NOT NULL,
-                   uid_modified INT NULL,
-                   datetime_modified DATETIME NULL
-                 );
-                 '''
+    if self.svm_cmd == 'save_entity':
+      sql_statement = '''\
+                        CREATE TABLE IF NOT EXISTS tbl_dataset_entity (
+                          id_entity INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                          title VARCHAR (50) NOT NULL,
+                          uid_created INT NOT NULL,
+                          datetime_created DATETIME NOT NULL,
+                          uid_modified INT NULL,
+                          datetime_modified DATETIME NULL
+                         );
+                      '''
 
-      elif self.svm_cmd == 'save_value':
-        sql    = '''\
-                 CREATE TABLE IF NOT EXISTS tbl_dataset_value (
-                   id_attribute INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                   id_entity INT NOT NULL,
-                   dep_variable_label VARCHAR (50) NOT NULL,
-                   indep_variable_label VARCHAR (50) NOT NULL,
-                   indep_variable_value FLOAT NOT NULL,
-                   CONSTRAINT FK_dataset_entity FOREIGN KEY (id_entity) REFERENCES tbl_dataset_entity (id_entity)
-                 );
-                 '''
-      cursor.execute( sql )
+    elif self.svm_cmd == 'save_value':
+      sql_statement = '''\
+                        CREATE TABLE IF NOT EXISTS tbl_dataset_value (
+                          id_attribute INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                          id_entity INT NOT NULL,
+                          dep_variable_label VARCHAR (50) NOT NULL,
+                          indep_variable_label VARCHAR (50) NOT NULL,
+                          indep_variable_value FLOAT NOT NULL,
+                          CONSTRAINT FK_dataset_entity FOREIGN KEY (id_entity) REFERENCES tbl_dataset_entity (id_entity)
+                        );
+                      '''
 
-    except DB.Error, error:
-      list_error.append(error)
-    finally:
-      if conn:
-        conn.close()
+    sql.sql_command( sql_statement, 'create')
+    sql.sql_disconnect()
 
     # insert dataset values
     try:
