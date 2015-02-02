3ef5e52ff4bc24a612151f429b530bfc1014004c
Jeff Levesque <jeff1evesque@yahoo.com>
2015-02-02 10:53:32 -0500

    #1250: db_query.py, define 'SQL' class, and corresponding constructor

--- package/database/db_query.py
+++ package/database/db_query.py
@@ -4,3 +4,10 @@
 #  This file contains various generic SQL methods.
 import MySQLdb as DB
 from database.db_settings import Database
+
+## Class: SQL
+class SQL:
+
+  ## constructor:
+  def __init__(self):
+    self.db_setting = Database()
