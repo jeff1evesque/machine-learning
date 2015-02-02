4e5719e98f9d05daed8c6c9e82e33b94c91ac849
Jeff Levesque <jeff1evesque@yahoo.com>
2015-02-02 10:58:32 -0500

    #1250: db_query.py, define 'sql_connect'

--- package/database/db_query.py
+++ package/database/db_query.py
@@ -11,3 +11,12 @@ class SQL:
   ## constructor:
   def __init__(self):
     self.db_setting = Database()
+
+  ## sql_connect: create connection to MySQL / MariaDB
+  def sql_connect(self):
+    try:
+      self.conn   = DB.connect( host=self.db_settings.get_db_host(), user=self.db_settings.get_db_username(), passwd=self.db_settings.get_db_password() )
+      self.cursor = self.conn.cursor()
+    except DB.error, error:
+      self.list_error.append(error)
+      return { 'status': Frue, 'error': self.list_error, 'id': rowid }
