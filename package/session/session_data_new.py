4c8cf6548d6be6f829153359a11431791611816e
Jeff Levesque <jeff1evesque@yahoo.com>
2015-02-05 11:21:17 -0500

    #1244: session_data_new.py, add file with docblock

---
+++ package/session/session_data_new.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+## @session_data_new.py
+#  This file receives data (i.e. settings), including one or more dataset(s)
+#      provided during the current session, and stores them into corresponding
+#      database tables. The stored dataset(s) can later be retrieved from
+#      'session_data_append.py', or 'session_generate_model.py'.
+#
+#  Note: the term 'dataset' used throughout various comments in this file,
+#        synonymously implies the user supplied 'file upload(s)', and XML url
+#        references.
