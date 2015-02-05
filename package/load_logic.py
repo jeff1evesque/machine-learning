b674b8cac7dd3fbe1979bcb42b900c4b2f560f2b
Jeff Levesque <jeff1evesque@yahoo.com>
2015-02-05 15:10:58 -0500

    #1174: load_logic.py, remove duplicate 'session_entity' invocation

--- package/load_logic.py
+++ package/load_logic.py
@@ -83,10 +83,9 @@ if len(sys.argv) > 1:
       session.validate_mime_type()
       session.check()
 
-      session_entity = session.save_svm_entity(session_type)
+      session_entity = session.save_svm_entity(session_type, session_id)
       if session_entity['status']:
         session.set_entity_id(session_id)
-        session.save_svm_entity(session_type, session_id)
         session.check()
 
         session.dataset_to_json(session_id)
