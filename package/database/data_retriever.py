e3eb91e856a8040225198c9a71204b30e3ea6cb7
Jeff Levesque <jeff1evesque@yahoo.com>
2015-02-06 14:33:56 -0500

    #1249: data_retriever.py, add 'Retriever' class, and constructor

--- package/database/data_retriever.py
+++ package/database/data_retriever.py
@@ -2,3 +2,13 @@
 
 ## @data_retriever.py
 from database.db_query import SQL
+
+## Class: Retriever, explicitly inherit 'new-style' class
+class Retriever(object):
+
+  ## constructor:
+  def __init__(self, svm_data, cmd, session_type):
+    # class variables
+    self.svm_data     = svm_data
+    self.svm_cmd      = cmd
+    self.session_type = session_type
