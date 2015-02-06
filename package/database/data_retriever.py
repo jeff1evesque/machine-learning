9e6ad9b0f5643e758e645664c2077adbd7acc226
Jeff Levesque <jeff1evesque@yahoo.com>
2015-02-06 18:32:33 -0500

    #1249: data_retriever.py, add skeleton 'db_data_retriever'

--- package/database/data_retriever.py
+++ package/database/data_retriever.py
@@ -12,3 +12,7 @@ class Data_Retrieve(object):
     self.svm_data     = svm_data
     self.svm_cmd      = cmd
     self.session_type = session_type
+
+  ## db_data_retrieve: retrieve an SVM dataset from corresponding 'EAV data
+  #                    model' database table(s).
+  def db_data_retrieve(self):
