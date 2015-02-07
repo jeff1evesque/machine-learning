2ddcf0fe7cfa300633183ffb358eae8848aa3926
Jeff Levesque <jeff1evesque@yahoo.com>
2015-02-07 07:23:37 -0500

    #1334: converter_md5.py, update docblock

--- package/converter/converter_md5.py
+++ package/converter/converter_md5.py
@@ -8,10 +8,25 @@ from collections import defaultdict
 ## md5_for_file: Convert the contents of a given file, from a supplied path,
 #                to a hash value equivalent.
 #
+#  @md5.update, generate an md5 checksum fingerprint, of the given file. Calling
+#      this method repeatedly, is equivalent to a single call with the concatenation
+#      of all arguments:
+#
+#          md5.update(a), md5.update(b)
+#
+#      is equivalent to the following:
+#
+#          md5.update(a+b)
+#
+#      In some cases, the entire file is too large as a single argument to the
+#      checksum update operation. Therefore, it is best practice to break the
+#      operation into smaller chunks.
+#
 #  Note: block size directly depends on the block size of the filesystem.
 def md5_for_file(path, block_size=256*128, hr=False):
   md5 = hashlib.md5()
   with open(path,'rb') as f: 
+    # generate md5 checksum fingerprint
     for chunk in iter(lambda: f.read(block_size), b''): 
       md5.update(chunk)
   if hr:
