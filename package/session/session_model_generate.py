dab2d6bd930bae889f6189269c2b12cb8db1f398
Jeff Levesque <jeff1evesque@yahoo.com>
2015-02-10 14:00:11 -0500

    #1072: session_model_generate.py, dockblock for 'format_dataset'

--- package/session/session_model_generate.py
+++ package/session/session_model_generate.py
@@ -36,6 +36,20 @@ class Model_Generate(Session_Base):
     self.dataset = db_return['result']
 
   ## format_dataset:
+  #
+  #  @self.dataset, will have the following structure:
+  #
+  #      (('dep-variable-1', 'indep-variable-1', 23.45), ('dep-variable-1', 'indep-variable-2', 98.01),
+  #       ('dep-variable-1', 'indep-variable-3', 0.432), ('dep-variable-1', 'indep-variable-4', 325.0),
+  #       ('dep-variable-1', 'indep-variable-5', 54.64), ('dep-variable-1', 'indep-variable-6', 0.002),
+  #       ('dep-variable-1', 'indep-variable-7', 25.0), ('dep-variable-2', 'indep-variable-1', 24.32),
+  #       ('dep-variable-2', 'indep-variable-2', 92.22), ('dep-variable-2', 'indep-variable-3', 0.356),
+  #       ('dep-variable-2', 'indep-variable-4', 235.0), ('dep-variable-2', 'indep-variable-5', 64.45),
+  #       ('dep-variable-2', 'indep-variable-6', 0.001), ('dep-variable-2', 'indep-variable-7', 31.0),
+  #       ('dep-variable-3', 'indep-variable-1', 22.67), ('dep-variable-3', 'indep-variable-2', 101.21),
+  #       ('dep-variable-3', 'indep-variable-3', 0.832), ('dep-variable-3', 'indep-variable-4', 427.0),
+  #       ('dep-variable-3', 'indep-variable-5', 75.45), ('dep-variable-3', 'indep-variable-6', 0.002),
+  #       ('dep-variable-3', 'indep-variable-7', 24.0))
   def format_dataset(self):
     dataset_formatted = {}
 
