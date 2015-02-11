8ac7c91afd0beda60e37410a1d98b20a46d5f6d7
Jeff Levesque <jeff1evesque@yahoo.com>
2015-02-10 21:11:21 -0500

    #1357: session_data_new.py, change 'sort' -> 'sorted'

--- package/session/session_data_new.py
+++ package/session/session_data_new.py
@@ -85,8 +85,8 @@ class Data_New(Session_Base):
             dataset_converted = dataset_converter.csv_to_json()
 
             # check label consistency, append label(s) to 'feature_labels'
-            if not sort(dataset_converter.get_feature_labels()) == feature_labels): self.response_error.append('The supplied features (independent variables) are inconsistent'))
-            feature_labels.append( sort(dataset_converter.get_feature_labels()) )
+            if not sorted(dataset_converter.get_feature_labels()) == feature_labels): self.response_error.append('The supplied features (independent variables) are inconsistent'))
+            feature_labels = sorted(dataset_converter.get_feature_labels())
 
              # build new (relevant) dataset
             self.json_dataset.append({'id_entity': id_entity, 'svm_dataset': dataset_converted})
@@ -102,8 +102,8 @@ class Data_New(Session_Base):
             dataset_converted = dataset_converter.xml_to_json()
 
             # check label consistency, append label(s) to 'feature_labels'
-            if not sort(dataset_converter.get_feature_labels()) == feature_labels): self.response_error.append('The supplied features (independent variables) are inconsistent'))
-            feature_labels.append( sort(dataset_converter.get_feature_labels()) )
+            if not sorted(dataset_converter.get_feature_labels()) == feature_labels): self.response_error.append('The supplied features (independent variables) are inconsistent'))
+            feature_labels.append( sorted(dataset_converter.get_feature_labels()) )
 
              # build new (relevant) dataset
             self.json_dataset.append({'id_entity': id_entity, 'svm_dataset': dataset_converted})
