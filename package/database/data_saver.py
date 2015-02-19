63830890907fc7988c122ebc39f6db993a2f2767
Jeff Levesque <jeff1evesque@yahoo.com>
2015-02-19 15:53:57 -0500

    #1375: data_saver.py, adjust 'select' observation check statement

--- package/database/data_saver.py
+++ package/database/data_saver.py
@@ -138,8 +138,8 @@ class Data_Save(object):
       if self.session_type in ['data_append', 'data_add']:
 
         # check if observation label exists in database
-        sql_statement = 'SELECT dep_variable_label WHERE id_entity=%s'
-        args          = (self.svm_data['id_entity'])
+        sql_statement = 'SELECT %s WHERE id_entity=%s'
+        args          = (self.svm_data['label'], self.svm_data['id_entity'])
         response      = sql.sql_command( sql_statement, 'select', args )
 
         # add labels (new, and append case)
