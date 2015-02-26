72c22a8ee1d5938c1b73b95dae14e0c29255cc57
Jeff Levesque <jeff1evesque@yahoo.com>
2015-02-26 12:18:18 -0500

    #1399: app.py, add 'import flask'

--- html/machine-learning/app.py
+++ html/machine-learning/app.py
@@ -1,3 +1,4 @@
 ## @app.py
 #   This file loads corresponding logic, and html template file(s), which
 #       allows the presentation of (asynchronous) content.
+from flask import Flask, render template
