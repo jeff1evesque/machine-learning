734eaa87f3fc94a7426dfb913d764cff20282fa3
Jeff Levesque <jeff1evesque@yahoo.com>
2015-02-27 12:57:56 -0500

    #1435: app.py, remove corresponding code now in 'views.py'

--- app.py
+++ app.py
@@ -6,17 +6,6 @@ from flask import Flask, render_template, request
 # Initialize: create flask instance
 app = Flask(__name__)
 
-# Define Route: assign corresponding template, or logic to given path
-@app.route('/')
-def index():
-  return render_template('index.html')
-
-@app.route('/load-data/', methods=['POST', 'GET'])
-def load_data():
-  if request.method == 'POST':
-    if request.files: files = request.files
-    settings = request.form
-
 # Execute: run application directly, instead of import
 if __name__ == '__main__':
   app.run(
