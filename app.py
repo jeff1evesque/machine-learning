7af8532d94dc94dd4309a7bed46f3878a9cfa9e4
Jeff Levesque <jeff1evesque@yahoo.com>
2015-02-27 16:59:35 -0500

    #1435: app.py, remove logic that have been delegated to flask-views model

--- app.py
+++ app.py
@@ -1,14 +1,4 @@
 ## @app.py
-#   This file loads corresponding logic, and html template file(s), which
-#       allows the presentation of (asynchronous) content.
-from flask import Flask, render_template, request
-import web_interface.views
-
-# Initialize: create flask instance
-app = Flask(__name__)
-
-# Execute: run application directly, instead of import
-if __name__ == '__main__':
-  app.run(
-  debug=True
-)
+#  This file is the acting web server.
+from web_interface import app
+app.run(debug=True)
