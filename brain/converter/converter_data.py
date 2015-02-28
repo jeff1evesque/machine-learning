70447157a35e5eed447a0db9c0736fde54f25a90
Jeff Levesque <jeff1evesque@yahoo.com>
2015-02-28 16:33:34 -0500

    #1406: converter_data.py, build 'self.list_error' using 'except' statement

--- brain/converter/converter_data.py
+++ brain/converter/converter_data.py
@@ -29,15 +29,23 @@ class Convert_Data(object):
     formatted_files    = {}
 
     # restructure settings
-    for key, value in self.settings.items():
-      formatted_settings[key.lower()] = value.lower()
+    try:
+      for key, value in self.settings.items():
+        formatted_settings[key.lower()] = value.lower()
+    except Exception as error:
+      self.list_error.append( error )
+      return {'data': None, 'error': self.list_error}
 
     # restructure files: not all sessions involve files
     if self.files:
-      for file in self.files.getlist('svm_dataset[]'):
-        formatted_files[file.filename] = file
+      try:
+        for file in self.files.getlist('svm_dataset[]'):
+          formatted_files[file.filename] = file
 
-      dataset = {'upload_quantity': len(self.files.getlist('svm_dataset[]')), 'file_upload': formatted_files}
+        dataset = {'upload_quantity': len(self.files.getlist('svm_dataset[]')), 'file_upload': formatted_files}
+      except Exception as error:
+      self.list_error.append( error )
+      return {'data': None, 'error': self.list_error}
     else: dataset = None
 
     # build JSON structure
