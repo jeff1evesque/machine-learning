3552977087bbd207d083b65e4e6b7aa9e0d26024
Jeff Levesque <jeff1evesque@yahoo.com>
2015-02-27 23:08:55 -0500

    #1404: session_retriever.py, add file with docblock

---
+++ brain/database/session_retriever.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+## @session_retriever.py
+#  This file queries, and returns 'svm_title', and 'id_entity from the
+#      'tbl_dataset_entity' database table of 'db_machine_learning'.
