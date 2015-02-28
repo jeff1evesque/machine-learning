3f7fd049ad71d78a5d4a1c95f8ecbfccb9f9246f
Jeff Levesque <jeff1evesque@yahoo.com>
2015-02-27 22:11:42 -0500

    #1407: ajax_session.js, change filename in docblock

--- src/js/ajax_session.js
+++ src/js/ajax_session.js
@@ -1,6 +1,6 @@
 /**
  * ajax_session.js: this script utilizes ajax to retrieve data from
- *                  'retriever_session.php'. Specifically, the 'svm_title', and
+ *                  'retriever_session.py'. Specifically, the 'svm_title', and
  *                  'id_entity' values are taken from EAV data model, database
  *                  tables, and inserted into the DOM element 'svm_session_id'.
  */
