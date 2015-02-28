8c88520e8279ba4b2aa78765bf35c9cf69d42f9c
Jeff Levesque <jeff1evesque@yahoo.com>
2015-02-27 23:29:48 -0500

    #1404: session_retriever.py, define class with constructor

--- brain/database/session_retriever.py
+++ brain/database/session_retriever.py
@@ -4,3 +4,10 @@
 #  This file queries, and returns 'svm_title', and 'id_entity from the
 #      'tbl_dataset_entity' database table of 'db_machine_learning'.
 from brain.database.db_query import SQL
+
+## Class: Retrieve_Session, explicitly inherit 'new-style' class
+class Retrieve_Session(object):
+
+  ## constructor:
+  def __init__(self):
+    self.list_error = []
