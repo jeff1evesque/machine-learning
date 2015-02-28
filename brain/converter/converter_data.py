304fc86f0c6c3f97c48ff115426d1cd76f72da09
Jeff Levesque <jeff1evesque@yahoo.com>
2015-02-28 12:36:53 -0500

    #1449: converter_data.py, create class with constructor

--- brain/converter/converter_data.py
+++ brain/converter/converter_data.py
@@ -2,3 +2,10 @@
 
 ## @converter_json.py
 #  This file contains methods needed to correctly format input data.
+
+## Class: Convert_Data, explicitly inherit 'new-style class
+class Convert_Data(object):
+
+  ## constructor
+  def __init__(self):
+    self.list_error = []
