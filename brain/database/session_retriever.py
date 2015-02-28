4c7da7fd8f842df7776e45bc8e3a414bf3687ee8
Jeff Levesque <jeff1evesque@yahoo.com>
2015-02-27 23:23:14 -0500

    #1404: session_retriever.py, 'import SQL'

--- brain/database/session_retriever.py
+++ brain/database/session_retriever.py
@@ -3,3 +3,4 @@
 ## @session_retriever.py
 #  This file queries, and returns 'svm_title', and 'id_entity from the
 #      'tbl_dataset_entity' database table of 'db_machine_learning'.
+from brain.database.db_query import SQL
