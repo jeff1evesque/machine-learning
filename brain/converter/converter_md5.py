c491820c90e142df2ab23393982a75c8f774467f
Jeff Levesque <jeff1evesque@yahoo.com>
2015-03-02 08:50:51 -0500

    #1515: converter_md5.py, adjust docblock, and comments

--- brain/converter/converter_md5.py
+++ brain/converter/converter_md5.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 
 ## @converter_md5.py
-#  This file converts a given file to a hash value equivalent.
+#  This file converts a given object to a hash value equivalent.
 import hashlib
 from collections import defaultdict
 
-## md5_for_object: Convert the contents of a given file, from a supplied path,
-#                  to a hash value equivalent.
+## md5_for_object: Convert the contents of a given object, to a hash value
+#                  equivalent.
 #
 #  @md5.update, generate an md5 checksum fingerprint, of the given file. Calling
 #      this method repeatedly, is equivalent to a single call with the concatenation
@@ -22,7 +22,7 @@ from collections import defaultdict
 #      checksum update operation. Therefore, it is best practice to break the
 #      operation into smaller chunks.
 #
-#  @block_size, the block size to break the supplied file (from given path).
+#  @block_size, the block size to break the supplied object (item).
 #
 #  @hr, determines whether to use the default 'digest' method, or to use the
 #      'hexdigest' algorithm.
@@ -30,7 +30,7 @@ from collections import defaultdict
 #  Note: block size directly depends on the block size of the filesystem.
 def md5_for_object(item, block_size=256*128, hr=False):
   md5 = hashlib.md5()
-  # use lambda anonymous function to iterate given file
+  # use lambda anonymous function to iterate given object
   for chunk in iter(lambda: item.read(block_size), b''):
     md5.update(chunk)
   # return the digest of strings passed into 'update'
