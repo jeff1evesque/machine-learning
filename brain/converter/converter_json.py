3c65c7b86ae4a4722c2cfcec4c5a7577b2ebad54
Jeff Levesque <jeff1evesque@yahoo.com>
2015-03-02 17:12:55 -0500

    #1543: converter_json.py, correctly adjust 'csv_to_json'

--- brain/converter/converter_json.py
+++ brain/converter/converter_json.py
@@ -33,35 +33,34 @@ class JSON(object):
     indep_variable_label = []
 
     # open temporary 'csvfile' reader object
-    with open( self.svm_file, 'rU' ) as csvfile:
-      dataset_reader = csv.reader( csvfile, delimiter=' ', quotechar='|' )
+    dataset_reader = csv.reader( self.svm_file, delimiter=' ', quotechar='|' )
 
-      # iterate first row of csvfile
-      for row in islice( dataset_reader, 0, 1 ):
+    # iterate first row of csvfile
+    for row in islice( dataset_reader, 0, 1 ):
 
-        # iterate each column in a given row
-        row_indep_label = row[0].split(',')
-        for value in islice( row_indep_label, 1, None ):
-          indep_variable_label.append( value )
+      # iterate each column in a given row
+      row_indep_label = row[0].split(',')
+      for value in islice( row_indep_label, 1, None ):
+        indep_variable_label.append( value )
 
-      # iterate all rows of csvfile
-      for dep_index, row in enumerate( islice( dataset_reader, 0, None ) ):
+    # iterate all rows of csvfile
+    for dep_index, row in enumerate( islice( dataset_reader, 0, None ) ):
 
-        # iterate first column of each row (except first)
-        row_dep_label = row[0].split(',')
-        for value in row_dep_label[:1]:
-          observation_label.append( value )
+      # iterate first column of each row (except first)
+      row_dep_label = row[0].split(',')
+      for value in row_dep_label[:1]:
+        observation_label.append( value )
 
-        # iterate each column in a given row
-        row_indep_variable = row[0].split(',')
-        for indep_index, value in enumerate( islice( row_indep_variable, 1, None) ):
-          try:
-            value = float( value )
-          except Exception as e:
-            print e
-            return False
+      # iterate each column in a given row
+      row_indep_variable = row[0].split(',')
+      for indep_index, value in enumerate( islice( row_indep_variable, 1, None) ):
+        try:
+          value = float( value )
+        except Exception as e:
+          print e
+          return False
 
-          list_dataset.append( { 'dep_variable_label': observation_label[dep_index], 'indep_variable_label': indep_variable_label[indep_index], 'indep_variable_value': value} )
+        list_dataset.append( { 'dep_variable_label': observation_label[dep_index], 'indep_variable_label': indep_variable_label[indep_index], 'indep_variable_value': value} )
 
     self.observation_labels = observation_label
     return list_dataset
