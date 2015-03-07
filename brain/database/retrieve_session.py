26fc926e0d3da9d2dd9ff67341b731dc268f1d92
Jeff Levesque <jeff1evesque@yahoo.com>
2015-03-07 08:01:45 -0500

    #1554: retrieve_session.py, adjust overall docblock

--- brain/database/retrieve_session.py
+++ brain/database/retrieve_session.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-## @session_retriever.py
+## @retrieve_session.py
 #  This file queries, and returns 'svm_title', and 'id_entity from the
 #      'tbl_dataset_entity' database table of 'db_machine_learning'.
 from brain.database.db_query import SQL
