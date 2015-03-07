cceab8d1c1d0319d22adfed0b3acc5fc3898f40f
Jeff Levesque <jeff1evesque@yahoo.com>
2015-03-07 14:14:11 -0500

    #1560: retrieve_dataset.py, remove whitespace

--- brain/database/retrieve_dataset.py
+++ brain/database/retrieve_dataset.py
@@ -29,13 +29,13 @@ class Retrieve_Dataset(object):
         # select dataset
         self.sql.sql_connect('db_machine_learning')
         sql_statement = 'SELECT dep_variable_label, indep_variable_label, indep_variable_value FROM tbl_dataset_value where id_entity=%s'
-        args          = ( id_entity )
-        response      = self.sql.sql_command( sql_statement, 'select', args )
+        args          = (id_entity)
+        response      = self.sql.sql_command(sql_statement, 'select', args)
 
         # retrieve any error(s), disconnect from database
         response_error = sql.return_error()
         self.sql.sql_disconnect()
 
         # return result
-        if response_error: return { 'status': False, 'error': response_error, 'result': None }
-        else: return { 'status': True, 'error': None, 'result': response['result'] }
+        if response_error: return {'status': False, 'error': response_error, 'result': None}
+        else: return {'status': True, 'error': None, 'result': response['result']}
