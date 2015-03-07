fc03a5d727ecce438b8aa19dc556ef74110f06ca
Jeff Levesque <jeff1evesque@yahoo.com>
2015-03-07 15:11:58 -0500

    #1560: views.py, implement 4 space indentation

--- web_interface/views.py
+++ web_interface/views.py
@@ -12,46 +12,46 @@ from brain.database.retrieve_session import Retrieve_Session
 # Define Route: assign corresponding template, or logic to given path
 @app.route('/')
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
 
 @app.route('/load-data/', methods=['POST', 'GET'])
 def load_data():
-  if request.method == 'POST':
-    # local variables
-    files = None
+    if request.method == 'POST':
+        # local variables
+        files = None
 
-    # get post data
-    if request.files:
-      files = request.files
-    settings = request.form
+        # get post data
+        if request.files:
+            files = request.files
+            settings = request.form
 
-    # format post data
-    sender         = Restructure_Data(settings, files)
-    data_formatted = sender.restructure()
+        # format post data
+        sender         = Restructure_Data(settings, files)
+        data_formatted = sender.restructure()
 
-    # send data to brain
-    loader = Load_Data(data_formatted)
-    if loader.get_session_type()['session_type']:
-      session_type = loader.get_session_type()['session_type']
+        # send data to brain
+        loader = Load_Data(data_formatted)
+        if loader.get_session_type()['session_type']:
+            session_type = loader.get_session_type()['session_type']
 
-      if session_type == 'data_new': response = loader.load_data_new()
-      elif session_type == 'data_append': response = loader.load_data_append()
-      elif session_type == 'model_generate': response = loader.load_model_generate()
-      elif session_type == 'model_use': response = loader.load_model_use()
-      else: response = loader.get_errors()
+            if session_type == 'data_new': response = loader.load_data_new()
+            elif session_type == 'data_append': response = loader.load_data_append()
+            elif session_type == 'model_generate': response = loader.load_model_generate()
+            elif session_type == 'model_use': response = loader.load_model_use()
+            else: response = loader.get_errors()
 
-    else: response = loader.get_errors()
+        else: response = loader.get_errors()
 
-    # return response
-    return json.dumps(response)
+        # return response
+        return json.dumps(response)
 
 @app.route('/retrieve-session/', methods=['POST', 'GET'])
 def retrieve_session():
-  if request.method == 'POST':
-    # get all sessions
-    session      = Retrieve_Session()
-    session_list = session.get_all_sessions()
+    if request.method == 'POST':
+        # get all sessions
+        session      = Retrieve_Session()
+        session_list = session.get_all_sessions()
 
-    # return all sessions
-    if session_list['result']: return json.dumps(session_list['result'])
-    else: return json.dumps(session_list['error'])
+        # return all sessions
+        if session_list['result']: return json.dumps(session_list['result'])
+        else: return json.dumps(session_list['error'])
