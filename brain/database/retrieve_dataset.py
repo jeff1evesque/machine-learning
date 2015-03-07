6754388ae0b2247ede29ad43c78a3fe29276bd22
Jeff Levesque <jeff1evesque@yahoo.com>
2015-03-07 07:52:57 -0500

    #1554: retrieve_dataset.py, adjust class docblock

--- brain/database/retrieve_dataset.py
+++ brain/database/retrieve_dataset.py
@@ -6,6 +6,8 @@
 from brain.database.db_query import SQL
 
 ## Class: Retrieve_Dataset, explicitly inherit 'new-style' class
+#
+#  Note: this class is invoked within 'views.py'
 class Retrieve_Dataset(object):
 
   ## constructor:
