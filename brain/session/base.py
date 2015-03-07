efbb95435ec2392f5d3f76b3211a129128cab131
Jeff Levesque <jeff1evesque@yahoo.com>
2015-03-07 14:38:39 -0500

    #1560: base.py, implement 4 space indentation

--- brain/session/base.py
+++ brain/session/base.py
@@ -14,34 +14,34 @@ from brain.validator.validate_settings import Validate_Settings
 #  Note: this class is invoked within 'data_new.py', 'model_xx.py'
 class Base(object):
 
-  ## constructor:
-  def __init__(self, svm_data):
-    self.svm_data       = svm_data
-    self.svm_session    = self.svm_data['data']['settings']['svm_session']
-    self.response_error = []
+    ## constructor:
+    def __init__(self, svm_data):
+        self.svm_data       = svm_data
+        self.svm_session    = self.svm_data['data']['settings']['svm_session']
+        self.response_error = []
 
-  ## validate_arg_none: check if class variable 'svm_data' is defined.
-  def validate_arg_none(self):
-    if self.svm_data == None: return True
-    else: return False
+    ## validate_arg_none: check if class variable 'svm_data' is defined.
+    def validate_arg_none(self):
+        if self.svm_data == None: return True
+        else: return False
 
-  ## validate_svm_settings: validate svm session settings (not dataset).
-  def validate_svm_settings(self):
-    validator = Validate_Settings( self.svm_data, self.svm_session )
-    validator.validate()
+    ## validate_svm_settings: validate svm session settings (not dataset).
+    def validate_svm_settings(self):
+        validator = Validate_Settings( self.svm_data, self.svm_session )
+        validator.validate()
 
-    if validator.validate()['error'] != None:
-      self.response_error.append( validator.validate()['error'] )
+        if validator.validate()['error'] != None:
+            self.response_error.append( validator.validate()['error'] )
 
-  ## return_error: return appended error messages.
-  def return_error(self):
-    return self.response_error
+    ## return_error: return appended error messages.
+    def return_error(self):
+        return self.response_error
 
-  ## check: check if the class instance contains any errors appended to the list
-  #         'self.response_error'. If any error(s) exists, it is printed, and the
-  #         program exits.
-  def check(self):
-    if len(self.response_error) > 0:
-      for error in self.response_error:
-        print error
-      sys.exit()
+    ## check: check if the class instance contains any errors appended to the list
+    #         'self.response_error'. If any error(s) exists, it is printed, and the
+    #         program exits.
+    def check(self):
+        if len(self.response_error) > 0:
+            for error in self.response_error:
+                print error
+            sys.exit()
