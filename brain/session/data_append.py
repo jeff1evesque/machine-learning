ad674f66335bc512a6243619085a362dc3745c81
Jeff Levesque <jeff1evesque@yahoo.com>
2015-03-07 08:36:04 -0500

    #1554: data_append.py, adjust overall docblock

--- brain/session/data_append.py
+++ brain/session/data_append.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 
-## @session_data_append.py
+## @data_append.py
 #  This file receives data (i.e. settings), including one or more dataset(s)
 #      provided during the current session, and stores them into corresponding
 #      database tables. The stored dataset(s) can later be retrieved from
-#      'session_data_append.py', or 'session_generate_model.py'.
+#      'data_append.py', or 'generate_model.py'.
 #
 #  Note: the term 'dataset' used throughout various comments in this file,
 #        synonymously implies the user supplied 'file upload(s)', and XML url
