0058c68e71fbc703275322c10b42a44be3e3c723
Jeff Levesque <jeff1evesque@yahoo.com>
2015-03-19 16:39:04 -0400

    #1559: save_dataset.py, adjust filename in overall docblock

--- brain/database/save_dataset.py
+++ brain/database/save_dataset.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-## @data_saver.py
+## @save_dataset.py
 #  This file saves SVM related data into corresponding 'EAV data model' database
 #      table(s), from the 'db_machine_learning' database.
 from brain.database.db_query import SQL
