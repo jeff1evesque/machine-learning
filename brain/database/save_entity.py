200d6c5381cc35263984105840eea4d4bebd784f
Jeff Levesque <jeff1evesque@yahoo.com>
2015-03-20 12:58:53 -0400

    #1575: save_entity.py, add file with docblock

---
+++ brain/database/save_entity.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+## @save_entity.py
+#  This file saves SVM related entity into corresponding 'EAV data model' database
+#      table(s), from the 'db_machine_learning' database.
