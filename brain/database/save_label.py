b9400c39d65f8b26f43426fdeab39e03b71242c3
Jeff Levesque <jeff1evesque@yahoo.com>
2015-03-20 13:32:50 -0400

    #1575: save_label.py, 'import SQL'

--- brain/database/save_label.py
+++ brain/database/save_label.py
@@ -3,3 +3,4 @@
 ## @save_label.py
 #  This file saves SVM related data into corresponding 'EAV data model' database
 #      table(s), from the 'db_machine_learning' database.
+from brain.database.db_query import SQL
