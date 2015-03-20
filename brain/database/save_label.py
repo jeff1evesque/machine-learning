4b97eaf8a0cee45fa750336acb9ff1c46c3eb454
Jeff Levesque <jeff1evesque@yahoo.com>
2015-03-20 13:34:44 -0400

    #1575: save_label.py, define constructor

--- brain/database/save_label.py
+++ brain/database/save_label.py
@@ -4,3 +4,17 @@
 #  This file saves SVM related data into corresponding 'EAV data model' database
 #      table(s), from the 'db_machine_learning' database.
 from brain.database.db_query import SQL
+
+## Class: Save_Label, explicitly inherit 'new-style' class
+#
+#  Note: this class is invoked within 'data_new.py'
+class Save_Label(object):
+
+    ## constructor: stores an SVM label (python dict), database configurations
+    #               into their own corresponding class variable.
+    def __init__(self, svm_data, session_type):
+        # class variables
+        self.svm_data     = svm_data
+        self.session_type = session_type
+        self.list_error   = []
+        self.sql          = SQL()
