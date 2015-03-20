e7d109f3f9ee6996e5773ffd284817eccdaa8694
Jeff Levesque <jeff1evesque@yahoo.com>
2015-03-20 12:59:27 -0400

    #1575: save_entity.py, 'import SQL'

--- brain/database/save_entity.py
+++ brain/database/save_entity.py
@@ -3,3 +3,4 @@
 ## @save_entity.py
 #  This file saves SVM related entity into corresponding 'EAV data model' database
 #      table(s), from the 'db_machine_learning' database.
+from brain.database.db_query import SQL
