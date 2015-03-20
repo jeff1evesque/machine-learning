f3126585204f6d8e7cca4d96fd11c0bbdb792043
Jeff Levesque <jeff1evesque@yahoo.com>
2015-03-20 13:02:08 -0400

    #1575: save_entity.py, define constructor, and 'save' method

--- brain/database/save_entity.py
+++ brain/database/save_entity.py
@@ -4,3 +4,47 @@
 #  This file saves SVM related entity into corresponding 'EAV data model' database
 #      table(s), from the 'db_machine_learning' database.
 from brain.database.db_query import SQL
+
+## Class: Save_Entity, explicitly inherit 'new-style' class
+#
+#  Note: this class is invoked within 'data_new.py'
+class Save_Dataset(object):
+
+    ## constructor:
+    def __init__(self, svm_data, cmd, session_type):
+        # class variables
+        self.svm_data     = svm_data
+        self.svm_cmd      = cmd
+        self.session_type = session_type
+        self.list_error   = []
+        self.sql          = SQL()
+
+    ## save: store, or update SVM dataset(s) into corresponding 'EAV data model'
+    #        database table(s).
+    #
+    #  @sql_statement, is a sql format string, and not a python string. Therefore, '%s'
+    #      is used for argument substitution.
+    #
+    #  Note: 'UTC_TIMESTAMP' returns the universal UTC datetime
+    def save(self):
+        # insert / update dataset entity value
+        if self.svm_cmd == 'save_entity':
+            self.sql.sql_connect('db_machine_learning')
+
+            if self.session_type == 'data_append':
+                sql_statement = 'UPDATE tbl_dataset_entity SET uid_modified=%s, datetime_modified=UTC_TIMESTAMP() WHERE id_entity=%s'
+                args          = (self.svm_data['uid'], self.svm_data['id_entity'])
+                response      = self.sql.sql_command(sql_statement, 'update', args)
+
+            elif self.session_type == 'data_new':
+                sql_statement = 'INSERT INTO tbl_dataset_entity (title, uid_created, datetime_created) VALUES(%s, %s, UTC_TIMESTAMP())'
+                args          = (self.svm_data['title'], self.svm_data['uid'])
+                response      = self.sql.sql_command(sql_statement, 'insert', args)
+
+            # retrieve any error(s), disconnect from database
+            response_error = self.sql.return_error()
+            self.sql.sql_disconnect()
+
+            # return result
+            if response_error: return {'status': False, 'error': response_error, 'id': response['id']}
+            else: return {'status': True, 'error': None, 'id': response['id']}
