7bdca0b3aaf2c788519438a4c2b4f68a19a64fbe
Jeff Levesque <jeff1evesque@yahoo.com>
2015-03-20 13:37:16 -0400

    #1575: save_dataset.py, remove 'feature_label' logic

--- brain/database/save_dataset.py
+++ brain/database/save_dataset.py
@@ -19,10 +19,9 @@ class Save_Dataset(object):
     #            {'svm_dataset': {'dep_variable_label': 'yyy',
     #                               'indep_variable_label': 'yyy',
     #                               'indep_variable_value': zz.zz}},
-    def __init__(self, svm_data, cmd, session_type):
+    def __init__(self, svm_data, session_type):
         # class variables
         self.svm_data     = svm_data
-        self.svm_cmd      = cmd
         self.session_type = session_type
         self.list_error   = []
         self.sql          = SQL()
@@ -35,44 +34,17 @@ class Save_Dataset(object):
     #
     #  Note: 'UTC_TIMESTAMP' returns the universal UTC datetime
     def save(self):
-        # insert / update feature label(s)
-        if self.svm_cmd == 'save_label':
-            self.sql.sql_connect('db_machine_learning')
-
-            # add labels (append case)
-            if self.session_type in ['data_append', 'data_new']:
-
-                # check if observation label exists in database
-                sql_statement = 'SELECT * FROM tbl_observation_label WHERE dep_variable_label=%s AND id_entity=%s'
-                args          = (self.svm_data['label'], self.svm_data['id_entity'])
-                response      = self.sql.sql_command(sql_statement, 'select', args)
-
-                # add labels if not exist
-                if not response['result']:
-                    sql_statement  = 'INSERT INTO tbl_observation_label (id_entity, dep_variable_label) VALUES(%s, %s)'
-                    args           = (self.svm_data['id_entity'], self.svm_data['label'])
-                    response_added = self.sql.sql_command(sql_statement, 'insert', args)
-
-            # retrieve any error(s), disconnect from database
-            response_error = self.sql.return_error()
-            self.sql.sql_disconnect()
-
-            # return result
-            if response_error: return {'status': False, 'error': response_error}
-            else: return {'status': True, 'error': None}
-
         # insert / update dataset value(s)
-        elif self.svm_cmd == 'save_value':
-            self.sql.sql_connect('db_machine_learning')
-            sql_statement = 'INSERT INTO tbl_dataset_value (id_entity, dep_variable_label, indep_variable_label, indep_variable_value) VALUES(%s, %s, %s, %s)'
-            dataset       = self.svm_data['svm_dataset']
-            args          = (self.svm_data['id_entity'], dataset['dep_variable_label'], dataset['indep_variable_label'], dataset['indep_variable_value'])
-            response      = self.sql.sql_command(sql_statement, 'insert', args)
+        self.sql.sql_connect('db_machine_learning')
+        sql_statement = 'INSERT INTO tbl_dataset_value (id_entity, dep_variable_label, indep_variable_label, indep_variable_value) VALUES(%s, %s, %s, %s)'
+        dataset       = self.svm_data['svm_dataset']
+        args          = (self.svm_data['id_entity'], dataset['dep_variable_label'], dataset['indep_variable_label'], dataset['indep_variable_value'])
+        response      = self.sql.sql_command(sql_statement, 'insert', args)
 
-            # retrieve any error(s), disconnect from database
-            response_error = self.sql.return_error()
-            self.sql.sql_disconnect()
+        # retrieve any error(s), disconnect from database
+        response_error = self.sql.return_error()
+        self.sql.sql_disconnect()
 
-            # return result
-            if response: return {'status': False, 'error': response_error, 'id': response['id']}
-            else: return {'status': True, 'error': None, 'id': response['id']}
+        # return result
+        if response: return {'status': False, 'error': response_error, 'id': response['id']}
+        else: return {'status': True, 'error': None, 'id': response['id']}
