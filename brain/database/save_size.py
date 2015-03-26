866192913f9ced9ffca20b19aa797201147ea72e
Jeff Levesque <jeff1evesque@yahoo.com>
2015-03-26 07:07:20 -0400

    #1621: save_size.py, define constructor

--- brain/database/save_size.py
+++ brain/database/save_size.py
@@ -4,3 +4,15 @@
 #  This file saves the number of features that can be expected in a given
 #      observation with respect to 'id_entity'.
 from brain.database.db_query import SQL
+
+## Class: Save_Size, explicitly inherit 'new-style' class
+#
+#  Note: this class is invoked within 'base_data.py'
+class Save_Size(object):
+
+    ## constructor:
+    def __init__(self, svm_data):
+        # class variables
+        self.svm_data     = svm_data
+        self.list_error   = []
+        self.sql          = SQL()
