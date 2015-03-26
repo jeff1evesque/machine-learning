16a281dd9dd41e7b73d7bcd713dfba1df5844ff1
Jeff Levesque <jeff1evesque@yahoo.com>
2015-03-26 07:05:39 -0400

    #1621: save_size.py, 'import SQL'

--- brain/database/save_size.py
+++ brain/database/save_size.py
@@ -3,4 +3,4 @@
 ## @save_size.py
 #  This file saves the number of features that can be expected in a given
 #      observation with respect to 'id_entity'.
-
+from brain.database.db_query import SQL
