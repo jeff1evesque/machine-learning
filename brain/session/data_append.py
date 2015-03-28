60636752657b145a22fcbf36391abf8ca20902de
Jeff Levesque <jeff1evesque@yahoo.com>
2015-03-28 08:23:56 -0400

    #1669: data_append.py, implement 'self.uid'

--- brain/session/data_append.py
+++ brain/session/data_append.py
@@ -38,7 +38,7 @@ class Data_Append(Base, Base_Data):
     #  @session_id, is synonymous to 'entity_id', and provides context to update
     #      'modified_xx' columns within the 'tbl_dataset_entity' database table.
     def save_svm_entity(self, session_type, session_id):
-        svm_entity = {'title': self.svm_data['data']['settings'].get('svm_title', None), 'uid': 1, 'id_entity': session_id}
+        svm_entity = {'title': self.svm_data['data']['settings'].get('svm_title', None), 'uid': self.uid, 'id_entity': session_id}
         db_save    = Save_Entity(svm_entity, session_type)
 
         # save dataset element
