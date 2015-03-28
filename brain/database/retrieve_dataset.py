df66a178862fb19d2354c3f5f95a1767d8b06e70
Jeff Levesque <jeff1evesque@yahoo.com>
2015-03-28 09:01:02 -0400

    #1667: retrieve_dataset.py, select from 'tbl_feature_value'

--- brain/database/retrieve_dataset.py
+++ brain/database/retrieve_dataset.py
@@ -27,7 +27,7 @@ class Retrieve_Dataset(object):
     def get_dataset(self, id_entity):
         # select dataset
         self.sql.sql_connect('db_machine_learning')
-        sql_statement = 'SELECT dep_variable_label, indep_variable_label, indep_variable_value FROM tbl_dataset_value where id_entity=%s'
+        sql_statement = 'SELECT dep_variable_label, indep_variable_label, indep_variable_value FROM tbl_feature_value where id_entity=%s'
         args          = (id_entity)
         response      = self.sql.sql_command(sql_statement, 'select', args)
 
