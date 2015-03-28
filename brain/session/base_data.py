177b3c6b4d9d116ad490eadbf3d2b85a8cd6c15b
Jeff Levesque <jeff1evesque@yahoo.com>
2015-03-28 08:49:59 -0400

    #1666: base_data.py, change 'Save_Dataset' to 'Save_Feature' implementation

--- brain/session/base_data.py
+++ brain/session/base_data.py
@@ -84,10 +84,10 @@ class Base_Data(object):
     def save_svm_dataset(self, session_type):
         for data in self.dataset:
             for dataset in data['svm_dataset']:
-                db_save = Save_Dataset({'svm_dataset': dataset, 'id_entity': data['id_entity']}, session_type)
+                db_save = Save_Feature({'svm_dataset': dataset, 'id_entity': data['id_entity']}, session_type)
 
                 # save dataset element, append error(s)
-                db_return = db_save.save()
+                db_return = db_save.save_feature()
                 if db_return['error']: self.list_error.append(db_return['error'])
 
     ## save_observation_label: save the list of unique independent variable labels
