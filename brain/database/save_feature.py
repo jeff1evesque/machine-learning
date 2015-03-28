d3d4fd35ccfc74b9428856a0dbb41e00891e955f
Jeff Levesque <jeff1evesque@yahoo.com>
2015-03-28 08:44:28 -0400

    #1666: save_feature.py, move 'save' as 'save_feature'

--- brain/database/save_feature.py
+++ brain/database/save_feature.py
@@ -36,3 +36,26 @@ class Save_Feature(object):
         # return result
         if response_error: return {'status': False, 'error': response_error}
         else: return {'status': True, 'error': None, 'id': response['id']}
+
+    ## save_feature: store, or update SVM dataset(s) into corresponding 'EAV data model'
+    #                database table(s).
+    #
+    #  @sql_statement, is a sql format string, and not a python string. Therefore, '%s'
+    #      is used for argument substitution.
+    #
+    #  Note: 'UTC_TIMESTAMP' returns the universal UTC datetime
+    def save_feature(self):
+        # insert / update dataset value(s)
+        self.sql.sql_connect('db_machine_learning')
+        sql_statement = 'INSERT INTO tbl_dataset_value (id_entity, dep_variable_label, indep_variable_label, indep_variable_value) VALUES(%s, %s, %s, %s)'
+        dataset       = self.svm_data['svm_dataset']
+        args          = (self.svm_data['id_entity'], dataset['dep_variable_label'], dataset['indep_variable_label'], dataset['indep_variable_value'])
+        response      = self.sql.sql_command(sql_statement, 'insert', args)
+
+        # retrieve any error(s), disconnect from database
+        response_error = self.sql.get_errors()
+        self.sql.sql_disconnect()
+
+        # return result
+        if response_error: return {'status': False, 'error': response_error}
+        else: return {'status': True, 'error': None, 'id': response['id']}
