5edd8b3b8e46d502694aedf83efa39e3c8dbfe03
Jeff Levesque <jeff1evesque@yahoo.com>
2015-04-04 11:44:50 -0400

    #1700: retrieve_feature.py, add file with docblock

---
+++ brain/database/retrieve_feature.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+## @retrieve_feature.py
+#  This file retrieves SVM related data from corresponding 'EAV data model' database
+#      table(s), from the 'db_machine_learning' database.
+
