7e6e803d3acf43345bfb8bb14a26a57a7e0ee0af
Jeff Levesque <jeff1evesque@yahoo.com>
2015-04-04 11:47:28 -0400

    #1700: retrieve_feature.py, do not create new file

--- brain/database/retrieve_feature.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-
-## @retrieve_feature.py
-#  This file retrieves SVM related data from corresponding 'EAV data model' database
-#      table(s), from the 'db_machine_learning' database.
-
