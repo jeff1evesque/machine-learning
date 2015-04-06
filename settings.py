6c64cbdbfa4495ac3561e3433233c3c7dcb324f1
Jeff Levesque <jeff1evesque@yahoo.com>
2015-04-05 21:46:49 -0400

    #1712: settings.py, define 'HOST', 'PORT'

--- settings.py
+++ settings.py
@@ -3,3 +3,6 @@
 ## @settings.py
 #  This file contains sensitive configuration settings used to implement
 #      memcached, and to establish required database connection(s).
+
+HOST = 'localhost'
+PORT = 5000
