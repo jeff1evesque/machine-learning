e84cf7711edeaf80762408bab01379b989ffb655
Jeff Levesque <jeff1evesque@yahoo.com>
2015-04-06 12:25:09 -0400

    #1736: redis_query.py, adjust class methods (and docblocks)

--- brain/cache/redis_query.py
+++ brain/cache/redis_query.py
@@ -17,14 +17,18 @@ class Memcached(object):
         self.port   = Redis_Settings().get_port()
         self.server = redis.StrictRedis(host=self.host, port=self.port, db=0)
  
-    ## set: set value in memcached server.
-    def set(self, key, value, expiry=900):
-        self.server.set(key, value, expiry)
+    ## set: set value into redis server.
+    #
+    #  Note: by default, redis keys are created without an associated time
+    #        to live.  Therefore, the key will live until it is removed
+    #        explicitly.
+    def set(self, key, value):
+        self.server.set(key, value)
  
-    ## get: retrieve value from memcached server.
+    ## get: retrieve value from redis server.
     def get(self, key):
         return self.server.get(key)
  
-    ## delete: delete value from memcached server.
+    ## delete: delete value from redis server.
     def delete(self, key):
         self.server.delete(key)
