12a6c2fdde0051a8a25ea7a543b0f90d408b930f
Jeff Levesque <jeff1evesque@yahoo.com>
2015-04-06 19:46:19 -0400

    #1736: redis_query.py, add list methods

--- brain/cache/redis_query.py
+++ brain/cache/redis_query.py
@@ -41,3 +41,37 @@ class Memcached(object):
     ## delete: delete value from redis server.
     def delete(self, key):
         self.server.delete(key)
+
+    ## lset: set value to the index of the redis list.
+    def lset(self, name, index, value):
+        self.server.lset(name, index, value)
+
+    ## lrem: remove the first count occurences of elements equal to value
+    #        within the redist list.
+    def lrem(self, name, count, value)
+        self.lrem(name, count, value)
+
+    ## lpush: push values onto the beginning of a redis list.
+    def lpush(self, name, *values):
+        self.server.lpush(name, *values)
+
+    ## rpush: push values onto the ending of a redis list.
+    def rpush(self, name, *values):
+        self.server.rpush(name, *values)
+
+    ## lpop: remove, and return the first item of the redis list.
+    def lpop(self, name):
+        self.server.lpop(name)
+
+    ## rpop: remove, and return the last item of the redis list.
+    def rpop(self, name):
+        self.server.rpop(name)
+
+    ## ltrim: trim the redis list, removing all elements not within the
+    #         slice bounds.
+    def ltrim(self, name, start, end):
+        self.server.ltrim(name, start, end)
+
+    ## lrange: return a slice of the redis list between the slice bounds.
+    def lrange(self, name, start, end):
+        self.server.lrange(name, start, end)
