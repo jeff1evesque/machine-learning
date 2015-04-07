78dbfe2c6ff21af7e3786748e2d52b837766d230
Jeff Levesque <jeff1evesque@yahoo.com>
2015-04-06 20:51:26 -0400

    #1736: redis_settings.py, adjust constructor, and method docblocks

--- brain/cache/redis_settings.py
+++ brain/cache/redis_settings.py
@@ -8,23 +8,23 @@ from settings import HOST, PORT
 ## Class: Redis_Settings, explicitly inherit 'new-style' class.
 class Redis_Settings(object):
 
-    ## constructor: define default memcached host, and port.
+    ## constructor: define default redis host, and port.
     def __init__(self):
         self.host = HOST
         self.port = PORT
 
-    ## get_host: return the memcached host.
+    ## get_host: return the redis host.
     def get_host(self):
         return self.host
 
-    ## get_port: return the memcached port.
+    ## get_port: return the redis port.
     def get_port(self):
         return self.port
 
-    ## set_host: set the memcached host.
+    ## set_host: set the redis host.
     def set_host(self, host):
         self.host = host
 
-    ## set_port: set the memcached port.
+    ## set_port: set the redis port.
     def set_port(self, port):
         self.port = port
