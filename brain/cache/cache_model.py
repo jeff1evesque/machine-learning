0438782140826ac518909fc9bf2a7ab47b34a155
Jeff Levesque <jeff1evesque@yahoo.com>
2015-04-08 15:55:32 -0400

    #1759: cache_model.py, 'import Redis_Query', and 'import Serialize_Model'

--- brain/cache/cache_model.py
+++ brain/cache/cache_model.py
@@ -2,3 +2,5 @@
 
 ## @cache_model.py
 #  This file caches the supplied SVM model into the redis cache.
+from brain.cache.redis_query import Redis_Query
+from brain.converter.serialize_model import Serialize_Model
