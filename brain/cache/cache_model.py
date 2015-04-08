cbb950bab124eb23c1e28f426a4ad0a135050a99
Jeff Levesque <jeff1evesque@yahoo.com>
2015-04-08 16:11:17 -0400

    #1759: cache_model.py, define class, constructor, and 'cache' method

--- brain/cache/cache_model.py
+++ brain/cache/cache_model.py
@@ -4,3 +4,21 @@
 #  This file caches the supplied SVM model into the redis cache.
 from brain.cache.redis_query import Redis_Query
 from brain.converter.serialize_model import Serialize_Model
+
+    ## Class: Cache_Model, explicitly inherit 'new-style class.
+    #
+    #  Note: this class is invoked within 'model_generate.py', and
+    class Cache_Model(object):
+
+        ## constructor
+        def __init__(self, model):
+            self.model = model
+
+        ## cache: serialize the provided svm model, then store into the
+        #         redis cache.
+        def cache(self, key):
+            serialized = Serialize_Model().serialize(self.model)
+            myRedis    = Redis_Query()
+
+            myRedis.start_redis()
+            myRedis.hset('svm_models', key, serialized)
