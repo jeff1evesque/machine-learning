49ea7ed2538d8a65aace154d2576e7b098e0ff47
Jeff Levesque <jeff1evesque@yahoo.com>
2015-04-08 12:58:02 -0400

    #1752: serialize_model.py, add 'type' test in 'serialize' method

--- brain/converter/serialize_model.py
+++ brain/converter/serialize_model.py
@@ -7,10 +7,12 @@ from six.moves import cPickle as pickle
     ## constructor
     def __init__(self, obj):
         self.obj = obj
+        self.acceptable = [svm.classes.SVC]
 
     ## serialize: serializes the provided object.
     def serialize(self, obj):
-        return pickle.dumps(obj)
+        if type(obj) in self.acceptable:
+            return pickle.dumps(obj)
 
     ## deserialize: deserializes the provided object.
     def deserialize(self, obj):
