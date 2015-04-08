bcfc74dd165f936aa392b061905e73214aa72c8f
Jeff Levesque <jeff1evesque@yahoo.com>
2015-04-08 12:31:49 -0400

    #1752: serialize_model.py, define file

--- brain/converter/serialize_model.py
+++ brain/converter/serialize_model.py
@@ -3,3 +3,15 @@
 ## @serialize_model.py
 #  This file serializes a python object in memory.
 from six.moves import cPickle as pickle
+
+    ## constructor
+    def __init__(self, obj):
+        self.obj = obj
+
+    ## serialize: serializes the provided object.
+    def serialize(self, obj):
+        return pickle.dumps(obj)
+
+    ## deserialize: deserializes the provided object.
+    def deserialize(self, obj):
+        return pickle.loads(obj)
