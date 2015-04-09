d194e28fccd0b8cd790901bc534c42e00fe315da
Jeff Levesque <jeff1evesque@yahoo.com>
2015-04-09 18:46:04 -0400

    #1752: serialize_model.py, adjust indentation

--- brain/converter/serialize_model.py
+++ brain/converter/serialize_model.py
@@ -4,21 +4,21 @@
 #  This file serializes, and deserializes an SVM object in memory.
 from six.moves import cPickle as pickle
 
-    ## Class: Serialize_Model, explicitly inherit 'new-style class.
-    #
-    #  Note: this class is invoked within 'cache_model.py'.
-    class Serialize_Model(object):
+## Class: Serialize_Model, explicitly inherit 'new-style class.
+#
+#  Note: this class is invoked within 'cache_model.py'.
+class Serialize_Model(object):
 
-        ## constructor
-        def __init__(self, obj):
-            self.obj = obj
-            self.acceptable = [svm.classes.SVC]
+    ## constructor
+    def __init__(self, obj):
+        self.obj = obj
+        self.acceptable = [svm.classes.SVC]
 
-        ## serialize: serializes the provided object.
-        def serialize(self, obj):
-            if type(obj) in self.acceptable:
-                return pickle.dumps(obj)
+    ## serialize: serializes the provided object.
+    def serialize(self, obj):
+        if type(obj) in self.acceptable:
+            return pickle.dumps(obj)
 
-        ## deserialize: deserializes the provided object.
-        def deserialize(self, obj):
-            return pickle.loads(obj)
+    ## deserialize: deserializes the provided object.
+    def deserialize(self, obj):
+        return pickle.loads(obj)
