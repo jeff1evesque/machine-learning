a75f20286472974955759c1a6e083de34a71dd6f
Jeff Levesque <jeff1evesque@yahoo.com>
2015-04-10 22:04:48 -0400

    #991: uncache_model.py, 'Cache_Model' skeleton

--- brain/cache/uncache_model.py
+++ brain/cache/uncache_model.py
@@ -3,3 +3,9 @@
 ## @uncache_model.py
 #  This file uncaches SVM models from the redis cache.
 from brain.cache.redis_query import Redis_Query
+
+## Class: Uncache_Model, explicitly inherit 'new-style class.
+#
+#  Note: this class is invoked within 'views.py'.
+class Uncache_Model(object):
+
