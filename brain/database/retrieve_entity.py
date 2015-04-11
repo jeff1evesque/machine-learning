73729ba63038c06f62d40cfe29f7c222a25ab407
Jeff Levesque <jeff1evesque@yahoo.com>
2015-04-11 19:29:12 -0400

    #1813: retrieve_entity.py, add file with docblock

---
+++ brain/database/retrieve_entity.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+## @retrieve_entity.py
+#  This file retrieves SVM related enity from corresponding 'EAV data model' database
+#      table(s), from the 'db_machine_learning' database.
