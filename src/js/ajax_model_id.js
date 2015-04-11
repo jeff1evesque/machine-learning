26350b62762532d4c59ef962975a99592d7b06bd
Jeff Levesque <jeff1evesque@yahoo.com>
2015-04-10 20:09:29 -0400

    #990: ajax_model_id.js, add file with docblock

---
+++ src/js/ajax_model_id.js
@@ -0,0 +1,5 @@
+/**
+ * ajax_model_id.js: this script utilizes ajax to retrieve data from
+ *                  'retriever_model.py'. Specifically, every 'svm_model' is
+ *                  returned.
+ */
