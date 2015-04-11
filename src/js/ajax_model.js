3baae1c0826e0e43c0528649c432e5d0e109600e
Jeff Levesque <jeff1evesque@yahoo.com>
2015-04-10 20:12:28 -0400

    #990: ajax_model.js, adjust docblock

--- src/js/ajax_model.js
+++ src/js/ajax_model.js
@@ -1,5 +1,5 @@
 /**
- * ajax_model_id.js: this script utilizes ajax to retrieve data from
- *                  'retriever_model.py'. Specifically, every 'svm_model' is
- *                  returned.
+ * ajax_model.js: this script utilizes ajax to retrieve data from
+ *                'retriever_model.py'. Specifically, every 'svm_model' is
+ *                returned.
  */
