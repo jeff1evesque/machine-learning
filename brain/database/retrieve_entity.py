a7c7e09271e22623bba5f88b9a930ea0bc38631f
Jeff Levesque <jeff1evesque@yahoo.com>
2015-04-11 19:32:13 -0400

    #1813: retrieve_entity.py, define class with constructor

--- brain/database/retrieve_entity.py
+++ brain/database/retrieve_entity.py
@@ -3,3 +3,15 @@
 ## @retrieve_entity.py
 #  This file retrieves SVM related enity from corresponding 'EAV data model' database
 #      table(s), from the 'db_machine_learning' database.
+from brain.database.db_query import SQL
+
+## Class: Retrieve_Entity, explicitly inherit 'new-style' class
+#
+#  Note: this class is invoked within 'model_generate.py'
+class Retrieve_Entity(object):
+
+    ## constructor:
+    def __init__(self):
+        # class variables
+        self.list_error = []
+        self.sql        = SQL()
