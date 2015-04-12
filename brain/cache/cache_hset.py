6ed6edb3f9234c8cf1314793f2de33938e699374
Jeff Levesque <jeff1evesque@yahoo.com>
2015-04-11 21:28:33 -0400

    #1815: cache_hset.py, add class with constructor

--- brain/cache/cache_hset.py
+++ brain/cache/cache_hset.py
@@ -4,3 +4,20 @@
 #  This file caches, and uncaches supplied data using the redis hashed
 #      cache.
 from brain.cache.redis_query import Redis_Query
+
+## Class: Cache_Hset, explicitly inherit 'new-style class.
+#
+#  Note: this class is invoked within 'cache_model.py'.
+class Cache_Model(object):
+
+    ## constructor
+    def __init__(self):
+        # class variables
+        self.list_error = []
+        self.myRedis    = Redis_Query()
+
+        # start redis client
+        try:
+            self.myRedis.start_redis()
+        except Exception, error:
+            self.list_error.append(str(error))
