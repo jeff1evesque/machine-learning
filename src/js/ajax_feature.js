7bad7c4e858ade5e9664e7a31dfc3b1eff546542
Jeff Levesque <jeff1evesque@yahoo.com>
2015-04-12 22:46:23 -0400

    #1801: ajax_feature.js, add file with docblock

---
+++ src/js/ajax_feature.js
@@ -0,0 +1,7 @@
+/**
+ * ajax_feature.js: this script utilizes ajax to retrieve data from 'views.py'.
+ *                  Specifically, an array of feature (independent variables)
+ *                  names, and the generalized count of features that can be
+ *                  expected within an observation, is inserted to respective
+ *                  DOM elements.
+ */
