cca8c78972f99b43eadfaecfccff5fc5b8992f7a
Jeff Levesque <jeff1evesque@yahoo.com>
2015-04-17 23:05:00 -0400

    #1911: form_validator.js, add invalid condition '-0', and '-0.0'

--- src/js/form_validator.js
+++ src/js/form_validator.js
@@ -36,8 +36,12 @@
   jQuery.validator.addMethod(
     'numericOnly',
     function(value, element, parameter) {
+    // invalid condition: -0, and -0.0
+      if (value.match(/^-?(0)?.?(0)/)) {
+        return false;
+      }
     // validate integers: cannot start with 0 (except trivial 0)
-      if (value.match(/^-?(0|[1-9][0-9]*)$/)) {
+      else if (value.match(/^-?(0|[1-9][0-9]*)$/)) {
         return true;
     // validate floats: cannot start with 0 (except trivial 0.x)
       } else if (value.match(/^-?(0|[1-9][0-9]*).\d+$/)) {
