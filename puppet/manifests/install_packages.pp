4fec100be1332598b5740a891926822f6545116e
Jeffrey Levesque <jeff1evesque@yahoo.com>
2015-06-23 17:35:59 -0400

    #1959: install_packages.pp, fix result of previous merge-conflice

--- puppet/manifests/install_packages.pp
+++ puppet/manifests/install_packages.pp
@@ -3,14 +3,22 @@ include apt
 include nodejs
 
 ## variables
-$packages_general_apt = ['inotify-tools', 'python-pip']
-$packages_general_pip = ['redis', 'jsonschema', 'xmltodict', 'six', 'matplotlib']
-$packages_flask_pip   = ['flask', 'requests']
-$packages_mariadb_apt = ['mariadb-server', 'mariadb-client', 'python-mysqldb']
-$packages_build_dep   = ['matplotlib', 'scikit-learn']
-$packages_build_size  = size($packages_build_dep) - 1
+case $::osfamily {
+    'redhat': {
+    }
+    'debian': {
+    }
+    default: {
+        $packages_general_apt = ['inotify-tools', 'python-pip']
+        $packages_general_pip = ['redis', 'jsonschema', 'xmltodict', 'six', 'matplotlib']
+        $packages_flask_pip   = ['flask', 'requests']
+        $packages_mariadb_apt = ['mariadb-server', 'mariadb-client', 'python-mysqldb']
+        $packages_build_dep   = ['matplotlib', 'scikit-learn']
+        $packages_build_size  = size($packages_build_dep) - 1
+    }
+}
 
-## define $PATH for all execs
+## define $PATH for all execs, paths
 Exec {path => ['/usr/bin/', '/bin/', '/usr/local', '/usr/sbin/', '/sbin/']}
 
 ## enable 'multiverse' repository (part 1, replace line)
