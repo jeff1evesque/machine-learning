22ab53717e4b30680195cb294556abd97cb9160b
Jeffrey Levesque <jeff1evesque@yahoo.com>
2015-06-25 19:33:28 -0400

    #1965: install_sklearn.pp, install 'scikit-learn' from github

--- puppet/manifests/install_sklearn.pp
+++ puppet/manifests/install_sklearn.pp
@@ -1,4 +1,12 @@
-# create a directory      
+## create '/vagrant/build/' directory
 file {'/vagrant/build/':
     ensure => 'directory',
+}
+
+## install scikit-learn
+vcsrepo {'/vagrant/build/':
+    ensure => present,
+    provider => git,
+    source => 'https://github.com/scikit-learn/scikit-learn',
+    revision => '0.16.1',
 }
