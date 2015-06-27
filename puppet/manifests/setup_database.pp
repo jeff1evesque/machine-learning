9ceac745fdc4e10bbd4571b0fe3921a0f10a31d2
Jeffrey Levesque <jeff1evesque@yahoo.com>
2015-06-27 16:34:02 -0400

    #1962: setup_database.pp, create database

--- puppet/manifests/setup_database.pp
+++ puppet/manifests/setup_database.pp
@@ -5,3 +5,10 @@ class {'::mysql::server':
     package_name = 'mariadb-server',
     root_password => 'password',
 }
+
+mysql::db {'db_machine_learning':
+    user     => 'authenticated',
+    password => 'password',
+    host     => 'localhost',
+    grant    => ['CREATE', 'INSERT', 'DELETE', 'UPDATE', 'DROP', 'EXECUTE', 'SELECT', 'SHOW DATABASES'],
+}
