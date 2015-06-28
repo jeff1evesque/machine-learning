a8830805ee886657ac26ed258e07dd002dbaa91a
Jeffrey Levesque <jeff1evesque@yahoo.com>
2015-06-28 00:16:25 -0400

    1962: setup_database.pp, create database user

--- puppet/manifests/setup_database.pp
+++ puppet/manifests/setup_database.pp
@@ -16,6 +16,17 @@ mysql::db {'db_machine_learning':
     host     => 'localhost',
 }
 
+## create database user
+#
+#  Note: '0' specifices no limit.
+mysql_user {'authenticated@localhost':
+    ensure => 'present',
+    max_connections_per_hour => '0',
+    max_queries_per_hour => '0',
+    max_updates_per_hour => '0',
+    max_user_connections => '0',
+}
+
 ## grant database permission(s)
 mysql_grant {'authenticated@localhost/*.*',
     ensure => 'present',
