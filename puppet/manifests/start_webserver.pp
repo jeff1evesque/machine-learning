079f24fb0a3fec1949dd6fb8124ca25bcce96cb6
Jeffrey Levesque <jeff1evesque@yahoo.com>
2015-07-02 18:48:20 -0400

    #1997: start_webserver.pp, add skeleton for puppet 'case'

--- puppet/manifests/start_webserver.pp
+++ puppet/manifests/start_webserver.pp
@@ -1,3 +1,13 @@
 include python
 include python::flask
 include python::requests
+
+## startup script for flask
+case $::osfamily {
+    'redhat': {
+    }
+    'debian': {
+    }
+    default: {
+    }
+}
