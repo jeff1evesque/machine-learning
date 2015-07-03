4f4f56478908372768f1284a5ff1221073dd6725
Jeffrey Levesque <jeff1evesque@yahoo.com>
2015-07-03 18:04:06 -0400

    #1997: start_webserver.pp, move initialization logic

--- puppet/manifests/start_webserver.pp
+++ puppet/manifests/start_webserver.pp
@@ -2,16 +2,15 @@ include python
 include python::flask
 include python::requests
 
-## initialize empty startup script
-file {'/etc/init/start_flask.conf':
-    ensure => present,
-}
-
 ## startup script for flask
 case $::osfamily {
     'redhat': {
     }
     'debian': {
+        ## initialize empty startup script
+        file {'/etc/init/start_flask.conf':
+            ensure => present,
+        }
     }
     default: {
     }
