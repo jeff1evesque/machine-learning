f183ffd73292d79d981e506e786b7b8cb27669be
Jeffrey Levesque <jeff1evesque@yahoo.com>
2015-07-05 15:16:44 -0400

    #2005: puppet_updater.sh, remove file

--- puppet/scripts/puppet_updater.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ ! -e /tmp/puppet-updated ]; then
-  wget -O /tmp/puppetlabs-release-trusty64.deb http://apt.puppetlabs.com/puppetlabs-release-trusty.deb
-  dpkg -i /tmp/puppetlabs-release-trusty64.deb
-  apt-get update
-  apt-get --assume-yes install puppet
-  touch /tmp/puppet-updated
-fi
