3e34c8d62064f5b5ccb18904f95ecd851f1a1ce9
Jeffrey Levesque <jeff1evesque@yahoo.com>
2015-07-18 10:01:31 -0400

    #1963: compile_asset.pp, define file

---
+++ puppet/manifests/compile_asset.pp
@@ -0,0 +1,81 @@
+## define $PATH for all execs, and packages
+Exec {path => ['/usr/bin/']}
+
+## variables
+$compilers = ['uglifyjs', 'sass', 'imagemin']
+
+## dynamically create compilers
+$compilers.each |String $compiler| {
+    ## create log directory
+    file {'/vagrant/log/':
+        ensure => 'directory',
+        before => File['${compiler}-startup-script'],
+    }
+
+    ## create startup script (heredoc syntax)
+    #
+    #  @("EOT"), the use double quotes on the end tag, allows variable interpolation within the puppet heredoc.
+    file {'${compiler}-startup-script':
+        path    => "/etc/init/${compiler}",
+        ensure  => 'present',
+        content => @("EOT"),
+                   #!upstart
+                   description 'start ${compiler}'
+
+                   ## start job defined in this file after system services, and processes have already loaded
+                   #       (to prevent conflict).
+                   #
+                   #  @vagrant-mounted, an event that executes after the shared folder is mounted
+                   #  @[2345], represents all configuration states with general linux, and networking access
+                   start on (vagrant-mounted and runlevel [2345])
+
+                   ## stop upstart job
+                   stop on runlevel [!2345]
+
+                   ## restart upstart job continuously
+                   respawn
+
+                   # required for permission to write to '/vagrant/' files (pre-stop stanza)
+                   setuid vagrant
+                   setgid vagrant
+
+                   ## run upstart job as a background process
+                   expect fork
+
+                   ## start upstart job: defined within 'package.json'
+                   exec npm run ${compiler}
+
+                   ## log start-up date
+                   #
+                   #  @[`date`], current date script executed
+                   pre-start script
+                       echo "[`date`] flask server starting" >> /vagrant/log/${compiler}.log 
+                   end script
+
+                   ## log shut-down date, remove process id from log before '/vagrant' is unmounted
+                   #
+                   #  @[`date`], current date script executed
+                   pre-stop script
+                       echo "[`date`] flask server stopping" >> /vagrant/log/${compiler}.log
+                   end script
+                   | EOT
+               notify  => Exec['dos2unix-line-endings'],
+        }
+
+    ## convert clrf (windows to linux) in case host machine is windows.
+    #
+    #  @notify, ensure the webserver service is started. This is similar to an exec statement, where the
+    #      'refreshonly => true' would be implemented on the corresponding listening end point. But, the
+    #      'service' end point does not require the 'refreshonly' attribute.
+    exec {'dos2unix-line-endings':
+        command => 'dos2unix /etc/init/${compiler}.conf',
+        refreshonly => true,
+        notify => Service['${compiler}'],
+    }
+
+    ## start ${compiler} service
+    service {'${compiler}':
+        ensure => 'running',
+        enable => 'true',
+    }
+}
