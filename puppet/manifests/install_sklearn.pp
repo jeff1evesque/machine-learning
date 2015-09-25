09e9c3e0e97d8e0dc049c3ffa8a8fe122b537991
Jeffrey Levesque <jeff1evesque@yahoo.com>
2015-09-25 00:07:49 -0400

    #2116: install_sklearn.pp, ensure same indentation within blocks

--- puppet/manifests/install_sklearn.pp
+++ puppet/manifests/install_sklearn.pp
@@ -12,25 +12,25 @@ file {'/vagrant/build/':
 
 ## download scikit-learn
 vcsrepo {'/vagrant/build/scikit-learn':
-    ensure => present,
+    ensure   => present,
     provider => git,
-    source => 'https://github.com/scikit-learn/scikit-learn',
+    source   => 'https://github.com/scikit-learn/scikit-learn',
     revision => '0.16.1',
-    before => Exec['build-sklearn'],
-    notify => Exec['build-sklearn'],
+    before   => Exec['build-sklearn'],
+    notify   => Exec['build-sklearn'],
 }
 
 ## build scikit-learn
 exec {'build-sklearn':
-    command => 'python setup.py build',
-    cwd => '/vagrant/build/scikit-learn/',
-    notify => Exec['install-sklearn'],
+    command     => 'python setup.py build',
+    cwd         => '/vagrant/build/scikit-learn/',
+    notify      => Exec['install-sklearn'],
     refreshonly => true,
 }
 
 ## install scikit-learn
 exec {'install-sklearn':
-    command => 'python setup.py install',
-    cwd => '/vagrant/build/scikit-learn/',
+    command     => 'python setup.py install',
+    cwd         => '/vagrant/build/scikit-learn/',
     refreshonly => true,
 }
