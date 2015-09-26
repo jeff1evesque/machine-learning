094abf002b24d9fc4c0120f3e5e938b5893c1fc1
Jeffrey Levesque <jeff1evesque@yahoo.com>
2015-09-26 18:43:39 -0400

    #2116: __init__.py, implement correct docstring docblock

--- interface/__init__.py
+++ interface/__init__.py
@@ -1,13 +1,4 @@
-## @__init__.py
-#  This file allows the containing directory to be considered a python
-#    package, consisting of python module(s). Also, this file initializes
-#    Flask object, which allows each module to import it.
-#
-#  Note: the last 'views' import is required to be after initializing flask.
-from flask import Flask
-
-# initialize flask instance
-app = Flask(__name__)
-
-# required circular import
-import interface.views
+"""@__init__
+This file allows the containing directory to be considered a python package,
+consisting of python module(s).
+"""
