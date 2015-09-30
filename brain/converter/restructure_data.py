db58ac926d482ae17a1d69f8cf76225f38c6857b
Jeffrey Levesque <jeff1evesque@yahoo.com>
2015-09-29 22:29:04 -0400

    #2070: restructure_data.py, restructure 'dataset'

--- brain/converter/restructure_data.py
+++ brain/converter/restructure_data.py
@@ -14,6 +14,9 @@ class Restructure_Data(object):
         self.files      = files
         self.list_error = []
 
+        self.type_web   = "<class 'werkzeug.datastructures.ImmutableMultiDict'>"
+        self.type_programmatic = "<type 'dict'>"
+
     ## restructure: restructure input data
     def restructure(self):
 
@@ -25,7 +28,7 @@ class Restructure_Data(object):
         try:
             for key, value in self.settings.items():
                 # web-interface case: 'isinstance' implementation did not work
-                if str(type(self.settings)) == "<class 'werkzeug.datastructures.ImmutableMultiDict'>":
+                if str(type(self.settings)) == self.type_web:
                     for lvalue in self.settings.getlist(key):
                         # base case
                         if key.lower() not in formatted_settings:
@@ -40,7 +43,7 @@ class Restructure_Data(object):
                                 formatted_settings[key.lower()].append(lvalue)
 
                 # programmatic-interface case:
-                elif str(type(self.settings)) == "<type 'dict'>":
+                elif str(type(self.settings)) == self.type_programmatic:
                     formatted_settings = self.settings
 
         except Exception as error:
@@ -50,10 +53,17 @@ class Restructure_Data(object):
         # restructure files: not all sessions involve files
         if self.files:
             try:
-                for file in self.files.getlist('svm_dataset[]'):
-                    formatted_files.append({'filename': file.filename, 'file': file})
+                # web-interface case: 'isinstance' implementation did not work
+                if str(type(self.settings)) == self.type_web:
+                    for file in self.files.getlist('svm_dataset[]'):
+                        formatted_files.append({'filename': file.filename, 'file': file})
 
-                dataset = {'upload_quantity': len(self.files.getlist('svm_dataset[]')), 'file_upload': formatted_files}
+                    dataset = {'upload_quantity': len(self.files.getlist('svm_dataset[]')), 'file_upload': formatted_files, 'json_string': None}
+
+                # programmatic-interface case:
+                elif str(type(self.settings)) == self.type_programmatic:
+                    dataset = {'upload_quantity': 1, 'file_upload': None, 'json_string': self.files}
+
             except Exception as error:
                 self.list_error.append(error)
                 return {'data': None, 'error': self.list_error}
