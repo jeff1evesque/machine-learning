f08e687557a5eba0ab77a91c5802fbe3430c2ac5
Jeffrey Levesque <jeff1evesque@yahoo.com>
2015-10-05 20:34:31 -0400

    #2116: retrieve_feature.py, remove extra spacing before operator

--- brain/database/retrieve_feature.py
+++ brain/database/retrieve_feature.py
@@ -49,8 +49,8 @@ class Retrieve_Feature(object):
         # select dataset
         self.sql.sql_connect('db_machine_learning')
         sql_statement = 'SELECT dep_variable_label, indep_variable_label, indep_variable_value FROM tbl_feature_value where id_entity=%s'
-        args          = (id_entity)
-        response      = self.sql.sql_command(sql_statement, 'select', args)
+        args = (id_entity)
+        response = self.sql.sql_command(sql_statement, 'select', args)
 
         # retrieve any error(s), disconnect from database
         response_error = self.sql.get_errors()
