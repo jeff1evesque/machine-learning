b184a09a17b52f87f72c974228a45e51c0696f2e
Jeffrey Levesque <jeff1evesque@yahoo.com>
2015-10-12 19:32:43 -0400

    #2116: calculate_md5.py, implement pep8 dockblock

--- brain/converter/calculate_md5.py
+++ brain/converter/calculate_md5.py
@@ -1,33 +1,42 @@
 #!/usr/bin/python
 
-## @calculate_md5.py
-#  This file converts a given object to a hash value equivalent.
+"""@calculate_md5
+
+This file converts an object, to hash value equivalent.
+
+"""
+
 import hashlib
 
-## calculate_md5: Convert the contents of a given object, to a hash value
-#                 equivalent.
-#
-#  @md5.update, generate an md5 checksum fingerprint, of the given file. Calling
-#      this method repeatedly, is equivalent to a single call with the concatenation
-#      of all arguments:
-#
-#          md5.update(a), md5.update(b)
-#
-#      is equivalent to the following:
-#
-#          md5.update(a+b)
-#
-#      In some cases, the entire file is too large as a single argument to the
-#      checksum update operation. Therefore, it is best practice to break the
-#      operation into smaller chunks.
-#
-#  @block_size, the block size to break the supplied object (item).
-#
-#  @hr, determines whether to use the default 'digest' method, or to use the
-#      'hexdigest' algorithm.
-#
-#  Note: block size directly depends on the block size of the filesystem.
 def calculate_md5(item, block_size=256*128, hr=False):
+    """calculate_md5
+
+    This method converts the contents of a given object, to a hash value
+    equivalent.
+
+    @md5.update, generate an md5 checksum fingerprint, of the given file.
+        Calling this method repeatedly, is equivalent to a single call with
+        the concatenation of all arguments:
+
+            md5.update(a), md5.update(b)
+
+        is equivalent to the following:
+
+            md5.update(a+b)
+
+        In some cases, the entire file is too large as a single argument to
+        the checksum update operation. Therefore, it is best practice to break
+        the operation into smaller chunks.
+
+    @block_size, the block size to break the supplied object (item).
+
+    @hr, determines whether to use the default 'digest' method, or to use the
+        'hexdigest' algorithm.
+
+    Note: block size directly depends on the block size of the filesystem.
+
+    """
+
     md5 = hashlib.md5()
 
     # use lambda anonymous function to iterate given object
