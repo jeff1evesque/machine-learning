c25c61cba9475092742019e4b8868d103787a205
Jeffrey Levesque <jeff1evesque@yahoo.com>
2015-10-14 23:48:29 -0400

    #2116: restructure_data.py, ensure line 80 characters or less

--- brain/converter/restructure_data.py
+++ brain/converter/restructure_data.py
@@ -48,7 +48,7 @@ class Restructure_Data(object):
         # restructure settings
         try:
             for key, value in self.settings.items():
-                # web-interface case: 'isinstance' implementation did not work
+                # web-interface: 'isinstance' did not work
                 if str(type(self.settings)) == self.type_web:
                     for lvalue in self.settings.getlist(key):
                         # base case
@@ -57,13 +57,15 @@ class Restructure_Data(object):
                         else:
                             # step case 1
                             if type(formatted_settings[key.lower()]) == unicode:
-                                formatted_settings[key.lower()] = [formatted_settings[key.lower()]]
+                                formatted_settings[key.lower()] = [
+                                    formatted_settings[key.lower()]
+                                ]
                                 formatted_settings[key.lower()].append(lvalue)
                             # step case n
                             elif type(formatted_settings[key.lower()]) == list:
                                 formatted_settings[key.lower()].append(lvalue)
 
-                # programmatic-interface case: 'isinstance' implementation did not work
+                # programmatic-interface: 'isinstance' did not work
                 elif str(type(self.settings)) == self.type_programmatic:
                     formatted_settings = self.settings
 
@@ -74,16 +76,26 @@ class Restructure_Data(object):
         # restructure dataset: not all sessions involve files
         if self.dataset:
             try:
-                # web-interface case: 'isinstance' implementation did not work
+                # web-interface: 'isinstance' did not work
                 if str(type(self.settings)) == self.type_web:
                     for file in self.dataset.getlist('svm_dataset[]'):
-                        formatted_files.append({'filename': file.filename, 'file': file})
+                        formatted_files.append({
+                            'filename': file.filename,
+                            'file': file
+                        })
 
-                    dataset = {'upload_quantity': len(self.dataset.getlist('svm_dataset[]')), 'file_upload': formatted_files, 'json_string': None}
+                    dataset = {
+                        'upload_quantity': len(self.dataset.getlist('svm_dataset[]')),
+                        'file_upload': formatted_files, 'json_string': None
+                    }
 
-                # programmatic-interface case: 'isinstance' implementation did not work
+                # programmatic-interface: 'isinstance' did not work
                 elif str(type(self.settings)) == self.type_programmatic:
-                    dataset = {'upload_quantity': 1, 'file_upload': None, 'json_string': self.dataset}
+                    dataset = {
+                        'upload_quantity': 1,
+                        'file_upload': None,
+                        'json_string': self.dataset
+                    }
 
             except Exception as error:
                 self.list_error.append(error)
