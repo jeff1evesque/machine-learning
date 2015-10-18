194c7d89e2fac1125c5da09f719f794ee97a4635
Jeffrey Levesque <jeff1evesque@yahoo.com>
2015-10-18 09:05:35 -0400

    #2116: model_predict.py, implement pep8 docblock

--- brain/session/model_predict.py
+++ brain/session/model_predict.py
@@ -1,43 +1,66 @@
 #!/usr/bin/python
 
-## @model_predict.py
-#  This file receives data (i.e. settings) required to query from the database,
-#      a previously stored SVM model, generated from 'model_generate.py'. The
-#      determined SVM Model is then used for analysis based on the input data
-#      provided during the current session.
-#
-#  Note: the term 'dataset' used throughout various comments in this file,
-#        synonymously implies the user supplied 'file upload(s)', and XML url
-#        references.
+"""@model_predict
+
+This file receives data (i.e. settings) required to query from the database,
+a previously stored SVM model, generated from 'model_generate.py'. The
+determined SVM Model is then used for analysis based on the input data
+provided during the current session.
+
+Note: the term 'dataset' used throughout various comments in this file,
+      synonymously implies the user supplied 'file upload(s)', and XML url
+      references.
+
+"""
+
 from brain.session.base import Base
 from brain.validator.validate_settings import Validate_Settings
 from brain.cache.cache_hset import Cache_Hset
 from brain.cache.cache_model import Cache_Model
 
-## Class: Model_Predict, inherit base methods from superclass 'Base'
-#
-#  Note: this class is invoked within 'load_data.py'
-class Model_Predict(Base):
 
-    ## constructor: define class properties using the superclass 'Base'
-    #               constructor, along with the constructor in this subclass.
-    #
-    #  @super(), implement 'Base' superclass constructor within this child class
-    #      constructor.
-    #
-    #  Note: the superclass constructor expects the same 'svm_data' argument.
+class Model_Predict(Base):
+    """@Model_Predict
+
+    This class provides an interface to generate an svm prediction, using the
+    provided prediction input(s), and the stored corresponding model, within
+    the NoSQL datastore.
+
+    Note: inherit base methods from superclass 'Base'
+
+    """
+
     def __init__(self, svm_data):
+        """@__init__
+
+        This constructor is responsible for defining class variables, using the
+        superclass 'Base' constructor, along with the
+        constructor in this subclass.
+
+        @super(), implement 'Base', and 'Base_Data' superclass constructor
+            within this child class constructor.
+
+        Note: the superclass constructor expects the same 'svm_data' argument.
+
+        """
+
         super(Model_Predict, self).__init__(svm_data)
         self.svm_data   = svm_data
         self.model_id   = self.svm_data['data']['settings']['svm_model_id']
         self.list_error = []
 
-    ## svm_prediction: using supplied arguments, return an svm prediction from a
-    #                  determined model.
-    #
-    #  @prediction_input, a list of arguments (floats) required to make an SVM
-    #      prediction, against the respective svm model.
     def svm_prediction(self):
+        """@svm_prediction
+
+        This method generates an svm prediction using the provided prediction
+        input(s), and the stored corresponding model, within the NoSQL
+        datastore.
+    
+        @prediction_input, a list of arguments (floats) required to make an SVM
+            prediction, against the respective svm model.
+
+        """
+
         # local variables
         prediction_input = self.svm_data['data']['settings']['prediction_input[]']
 
