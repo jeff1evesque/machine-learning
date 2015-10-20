97bdc14e3e1fbe185997bf3b495f81304926f5d7
Jeffrey Levesque <jeff1evesque@yahoo.com>
2015-10-19 21:46:36 -0400

    #2116: validate_dataset.py, implement pep8 docblock

--- brain/validator/validate_dataset.py
+++ brain/validator/validate_dataset.py
@@ -1,40 +1,71 @@
 #!/usr/bin/python
 
-## @validate_dataset.py
-#  This script performs validation on the svm data.
+"""@validate_dataset
+
+This script performs validation on the svm dataset(s).
+
+"""
+
 import json
 import sys
 from jsonschema.validators import Draft4Validator
 from brain.schema.jsonschema_definition import jsonschema_string
 
-## Class: Validate_Dataset, explicitly inherit 'new-style' class
-#
-#  Note: this class is invoked within 'base_data.py'
-class Validate_Dataset(object):
 
-    ## constructor: saves a subset of the passed-in form data
+class Validate_Dataset(object):
+    """Validate_Dataset
+
+    This class provies an interface to validate provided dataset(s) during
+    'data_new', and 'data_append' sessions.
+
+    Note: this class explicitly inherits the 'new-style' class.
+
+    """
+
     def __init__(self, data, svm_session=None):
+        """@__init__
+
+        This constructor saves a subset of the passed-in form data.
+
+        """
+
         self.data        = data
         self.svm_session = svm_session
         self.list_error  = []
 
-    ## validate_label: validate either the dependent variable (observation) label,
-    #                  or the independent variable (feature) label.
     def validate_label(self):
+        """@validate_label
+
+        This method validates either the dependent variable (observation)
+        label(s), or the independent variable (feature) label(s).
+
+        """
+
         try:
             Draft4Validator(jsonschema_string()).validate({'value': self.data})
         except Exception, error:
             self.list_error.append(str(error))
 
-    ## validate_value: validate the independent variable (feature) value.
     def validate_value(self):
+        """@validate_value
+
+        This method validates the independent variable (feature) value(s).
+
+        """
+
         try:
             float(self.data)
         except Exception, error:
             self.list_error.append(str(error))
 
-    # get_errors: get all current errors.
     def get_errors(self):
+        """get_errors
+
+        This method gets all current errors. associated with this class
+        instance.
+
+        """
+
         if len(self.list_error) > 0:
             return self.list_error
         else:
