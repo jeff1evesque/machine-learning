2eb36b547844155428cf2afef16ec8a6f77de3e6
Jeffrey Levesque <jeff1evesque@yahoo.com>
2015-11-03 07:20:29 -0500

    #2116: ajax_data.js, implement 'jscs --fix'

--- src/js/ajax_data.js
+++ src/js/ajax_data.js
@@ -7,42 +7,42 @@ $(document).ready(function() {
   $('form').on('submit', function(event) {
     event.preventDefault();
 
-  // Local Variables
+    // Local Variables
     var dataset      = $('input[name="svm_dataset[]"]');
     var pInput       = $('input[name="prediction_input[]"]');
     var flag_dataset = true;
     var flag_pInput  = true;
 
-  // Check if data supplied
+    // Check if data supplied
     dataset.each(function() {
-      if ( typeof $(this).val() === 'undefined' ) {
+      if (typeof $(this).val() === 'undefined') {
         flag_dataset = false;
         return false;
       }
     });
     pInput.each(function() {
-      if ( typeof $(this).val() === 'undefined' ) {
+      if (typeof $(this).val() === 'undefined') {
         flag_pInput = false;
         return false;
       }
     });
 
-  // AJAX Process
-    if ( flag_dataset || flag_pInput ) {
+    // AJAX Process
+    if (flag_dataset || flag_pInput) {
       $.ajax({
         url: $(this).attr('action'),
         type: 'POST',
-        data: new FormData( this ),
+        data: new FormData(this),
         dataType: 'json',
         contentType: false,
         processData: false,
         beforeSend: function() {
 
-        // AJAX Overlay
-          ajaxLoader( $(event.currentTarget) );
+          // AJAX Overlay
+          ajaxLoader($(event.currentTarget));
 
-        // Form Validation
-          $("form").validate({
+          // Form Validation
+          $('form').validate({
             submitHandler: function(form) {
               $(form).ajaxSubmit();
             }
@@ -50,7 +50,7 @@ $(document).ready(function() {
         }
       }).done(function(data) {
 
-      // JSON Object from Server
+        // JSON Object from Server
         if (data.result) {
           var obj_result = '\
                 <fieldset class="fieldset-prediction-result">\
@@ -63,28 +63,25 @@ $(document).ready(function() {
             $('.fieldset-prediction-result').remove();
             $('.fieldset-session-predict').append(obj_result);
             $('.result').append(data.result.error);
-          }
-
-          else if (data.result.result) {
+          } else if (data.result.result) {
             $('.fieldset-prediction-result').remove();
             $('.fieldset-session-predict').append(obj_result);
             $('.result').append(data.result.result);
           }
-        }
-        else {
-          json_server = ( !$.isEmptyObject( data ) ) ? JSON.stringify(data, undefined, 2) : 'none';
-          console.log( 'JSON object from Server: ' + json_server );
+        } else {
+          json_server = (!$.isEmptyObject(data)) ? JSON.stringify(data, undefined, 2) : 'none';
+          console.log('JSON object from Server: ' + json_server);
         }
 
-      // Remove AJAX Overlay
-        $('form .ajax_overlay').fadeOut(200, function(){ $(this).remove(); });
+        // Remove AJAX Overlay
+        $('form .ajax_overlay').fadeOut(200, function() { $(this).remove(); });
 
       }).fail(function(jqXHR, textStatus, errorThrown) {
-        console.log('Error Thrown: '+errorThrown);
-        console.log('Error Status: '+textStatus);
+        console.log('Error Thrown: ' + errorThrown);
+        console.log('Error Status: ' + textStatus);
 
-      // Remove AJAX Overlay
-        $('form .ajax_overlay').fadeOut(200, function(){ $(this).remove(); });
+        // Remove AJAX Overlay
+        $('form .ajax_overlay').fadeOut(200, function() { $(this).remove(); });
       });
     }
 
