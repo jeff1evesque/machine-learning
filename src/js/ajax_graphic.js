bef52ef3b4eb53775c002e0697540fef417ffbb6
Jeffrey Levesque <jeff1evesque@yahoo.com>
2015-11-03 07:20:57 -0500

    #2116: ajax_graphic.js, implement 'jscs --fix'

--- src/js/ajax_graphic.js
+++ src/js/ajax_graphic.js
@@ -7,36 +7,36 @@
  * ajaxLoader: primary function for this file
  */
 
-function ajaxLoader (el) {
+function ajaxLoader(el) {
   var defaults = {
-    bgColor      : '#fff',
-    duration     : 800,
-    opacity      : 0.45,
-    classOveride : false
+    bgColor: '#fff',
+    duration: 800,
+    opacity: 0.45,
+    classOveride: false
   };
 
-    this.options    = defaults;
-    this.container  = el;
+  this.options    = defaults;
+  this.container  = el;
 
-    this.init = function() {
-      var container = this.container;
+  this.init = function() {
+    var container = this.container;
 
-  // Delete any other loaders
-      this.remove();
+    // Delete any other loaders
+    this.remove();
 
-  // Create the overlay
-      var overlay = $('<div></div>').css({
+    // Create the overlay
+    var overlay = $('<div></div>').css({
         'background-color': this.options.bgColor,
-        'opacity':this.options.opacity,
-        'width':container.width(),
-        'height':container.height()+45, // 45, overlay form submit button
-        'position':'absolute',
-        'top':'0px',
-        'left':'0px',
-        'z-index':99999
+        'opacity': this.options.opacity,
+        'width': container.width(),
+        'height': container.height() + 45, // 45, overlay form submit button
+        'position': 'absolute',
+        'top': '0px',
+        'left': '0px',
+        'z-index': 99999
       }).addClass('ajax_overlay');
 
-  // insert overlay and loader into DOM
+    // insert overlay and loader into DOM
     container.append(
       overlay.append(
         $('<div></div>').addClass('ajax_loader')
