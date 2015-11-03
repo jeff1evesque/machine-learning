4b11ef94684d739b76623b3a3c068c0026ebb3c9
Jeffrey Levesque <jeff1evesque@yahoo.com>
2015-11-03 07:21:30 -0500

    #2116: ajax_session.js, implement 'jscs --fix'

--- src/js/ajax_session.js
+++ src/js/ajax_session.js
@@ -12,33 +12,32 @@
       url: '/retrieve-session/',
       dataType: 'json',
       beforeSend: function() {
-        ajaxLoader( $('form') );
+        ajaxLoader($('form'));
       }
     }).done(function(data) {
 
-    // Remove AJAX Overlay
-      $('form .ajax-overlay').fadeOut(200, function(){ $(this).remove(); });
+      // Remove AJAX Overlay
+      $('form .ajax-overlay').fadeOut(200, function() { $(this).remove(); });
 
-    // Append to DOM
+      // Append to DOM
       if (data.error) {
         $('.fieldset-dataset-type').append('<div class="error">' + data.error + '</div>');
         $('.fieldset-select-model').append('<div class="error">' + data.error + '</div>');
-      }
-      else {
-        $.each( data, function( index, value ) {
+      } else {
+        $.each(data, function(index, value) {
           var value_id    = value.id;
           var value_title = value.title;
           var element     = '<option ' + 'value="' + value_id + '">' + value_id + ': ' + value_title + '</option>';
 
-          $('select[name="svm_session_id"]').append( element );
+          $('select[name="svm_session_id"]').append(element);
         });
       }
 
     }).fail(function(jqXHR, textStatus, errorThrown) {
-      console.log('Error Thrown: '+errorThrown);
-      console.log('Error Status: '+textStatus);
+      console.log('Error Thrown: ' + errorThrown);
+      console.log('Error Status: ' + textStatus);
 
-    // Remove AJAX Overlay
-      $('form .ajax-overlay').fadeOut(200, function(){ $(this).remove(); });
+      // Remove AJAX Overlay
+      $('form .ajax-overlay').fadeOut(200, function() { $(this).remove(); });
     });
   }
