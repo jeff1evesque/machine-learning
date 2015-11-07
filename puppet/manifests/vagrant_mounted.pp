f7c0d6ac3be80f8b2b7bc32918004e8b336ae9bd
Jeffrey Levesque <jeff1evesque@yahoo.com>
2015-11-07 14:19:58 -0500

    #2230: vagrant_mounted.pp, remove heredoc string, use template

--- puppet/manifests/vagrant_mounted.pp
+++ puppet/manifests/vagrant_mounted.pp
@@ -8,34 +8,8 @@ Exec {path => ['/usr/bin/']}
 file {"vagrant-startup-script":
     path    => "/etc/init/workaround-vagrant-bug-6074.conf",
     ensure  => 'present',
-    content => @("EOT"),
-               #!upstart
-               description 'workaround for https://github.com/mitchellh/vagrant/issues/6074'
-
-               ## start job defined in this file after system services, and processes have already loaded
-               #      (to prevent conflict).
-               #
-               #  @filesystem, an event that fires after all filesystems have mounted
-               start on filesystem
-
-               ## block all jobs until the 'post-stop' event from this corresponding job has completed
-               #     (short-lived). When the 'task' directive is absent, then all other jobs are blocked
-               #     until the 'starting' event has completed (longer-lived).
-               task
-
-               ## until successful mount, sleep with 1s delay, then emit 'vagrant-mounted' event
-               #
-               #  @-q, run 'mountpoint' silently
-               #  @--no-wait, do not wait for the emit command to finish
-               #  @MOUNTPOINT, specifies the environment variable to be included with the 'emit' event, where
-               #      [key=value] being [MOUNTPOINT=${mountpoint}]. This allows the receiving process(es) to use
-               #      the corresponding environment variable.
-               script
-                   until mountpoint -q ${mountpoint}; do sleep 1; done
-                   /sbin/initctl emit --no-wait vagrant-mounted MOUNTPOINT=${mountpoint}
-               end script
-               | EOT
-               notify  => Exec["dos2unix-upstart-vagrant"],
+    content => template('/vagrant/puppet/template/vagrant_mounted.erb'),
+    notify  => Exec["dos2unix-upstart-vagrant"],
 }
 
 ## dos2unix upstart: convert clrf (windows to linux) in case host machine is windows.
