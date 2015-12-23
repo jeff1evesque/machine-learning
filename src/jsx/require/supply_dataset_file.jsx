bc36f4267db46eaa2d5f0101cbcae73a83cfcaba
jeff levesque <jeff1evesque@yahoo.com>
2015-12-23 13:35:23 -0500

    #2332: supply_dataset_file.jsx, implement 'export default ...'

--- src/jsx/require/supply_dataset_file.jsx
+++ src/jsx/require/supply_dataset_file.jsx
@@ -32,6 +32,9 @@ var Upload_File = React.createClass({
      }
 });
 
+// indicate which class can be exported, and instantiated via 'require'
+export default Upload_File
+
 // render a ReactElement into the DOM, in the supplied container
 $(document).ready(function() {
     ReactDOM.render(<Upload_File />, document.querySelector('.fieldset-dataset-type'));
