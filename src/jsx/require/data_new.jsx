e4c278687d78383bb43c3a9cd767340394aba03f
jeff levesque <jeff1evesque@yahoo.com>
2016-01-07 14:43:54 -0500

    #2332: data_new.jsx, self close 'input' element

--- src/jsx/require/data_new.jsx
+++ src/jsx/require/data_new.jsx
@@ -30,7 +30,7 @@ var Data_New = React.createClass({
                 <fieldset class='fieldset-dataset-type'>
                     <legend>Configurations</legend>
                     <p>Please save the <i>Session Name</i>, then provide dataset type</p>
-                    <input type='text' name='svm_title' placeholder='Session Name' onChange={this.change} value={this.state.value}>
+                    <input type='text' name='svm_title' placeholder='Session Name' onChange={this.change} value={this.state.value} />
                     <select name='svm_dataset_type' autoComplete='off' onChange={this.change} value={this.state.value_dataset_type}>
                         <option value='' defaultValue>--Select--</option>
                         <option value='file_upload'>Upload file</option>
