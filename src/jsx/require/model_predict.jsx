976262b3cca3f925514832ac0a342e112060cfd5
jeff levesque <jeff1evesque@yahoo.com>
2016-01-10 08:39:52 -0500

    #2332: model_predict.jsx, implement CamelCase variables

--- src/jsx/require/model_predict.jsx
+++ src/jsx/require/model_predict.jsx
@@ -1,13 +1,13 @@
 /**
  * model_predict.jsx: append 'model_predict' fieldset.
  *
- * @Model_Predict, must be capitalized in order for reactjs to render it as a
+ * @ModelPredict, must be capitalized in order for reactjs to render it as a
  *     component. Otherwise, the variable is rendered as a dom node.
  *
  * Note: this script implements jsx (reactjs) syntax.
  */
 
-var Model_Predict = React.createClass({
+var ModelPredict = React.createClass({
   // initial 'state properties'
     getInitialState: function() {
         return {
@@ -36,4 +36,4 @@ var Model_Predict = React.createClass({
 });
 
 // indicate which class can be exported, and instantiated via 'require'
-export default Model_Predict
+export default ModelPredict
