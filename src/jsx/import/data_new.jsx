01b2b48c57c61690610d25893e48a44daf4b2de3
jeff levesque <jeff1evesque@yahoo.com>
2016-01-19 18:19:20 -0500

    #2332: data_new.jsx, implement 'if-else', fix 'title'

--- src/jsx/import/data_new.jsx
+++ src/jsx/import/data_new.jsx
@@ -34,7 +34,7 @@ var DataNew = React.createClass({
                 <fieldset className='fieldset-dataset-type'>
                     <legend>Configurations</legend>
                     <p>Please save the <i>Session Name</i>, then provide dataset type</p>
-                    <input type='text' name='svm_title' placeholder='Session Name' onChange={this.changeTitle} value={this.state.value} />
+                    <input type='text' name='svm_title' placeholder='Session Name' onInput={this.changeTitle} value={this.state.value_title} />
                     <select name='svm_dataset_type' autoComplete='off' onChange={this.changeDatasetType} value={this.state.value_dataset_type}>
                         <option value='' defaultValue>--Select--</option>
                         <option value='file_upload'>Upload file</option>
@@ -48,11 +48,14 @@ var DataNew = React.createClass({
     },
   // call back: used for the above 'render' (return 'span' if undefined)
     getSupplyDataset: function() {
-        if (this.state.value_title !== null) {
+        if (typeof this.state.value_title !== 'undefined' || this.state.value_title.length > 0) {
             return {
                 file_upload: SupplyDatasetFile,
                 dataset_url: SupplyDatasetUrl
             }[this.state.value_dataset_type] || 'span';
+        }
+        else {
+            return null;
         }
     }
 });
