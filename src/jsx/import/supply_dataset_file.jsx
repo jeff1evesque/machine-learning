6ae18a2e05557947a6ef3c7b6571970806343ea7
jeff levesque <jeff1evesque@yahoo.com>
2016-01-19 21:39:50 -0500

    #2332: supply_dataset_file.jsx, implement 'validStringEntered'

--- src/jsx/import/supply_dataset_file.jsx
+++ src/jsx/import/supply_dataset_file.jsx
@@ -7,8 +7,6 @@
  * Note: this script implements jsx (reactjs) syntax.
  */
 
-import Submit from './submit.jsx';
-
 var SupplyDatasetFile = React.createClass({
   // initial 'state properties'
      getInitialState: function() {
@@ -16,34 +14,31 @@ var SupplyDatasetFile = React.createClass({
              value: null
          };
      },
-  // update 'state properties'
-     change: function(event){
-         this.setState({value: event.target.value});
+  // update 'state properties': allow parent component(s) to access properties
+     validStringEntered: function(event){
+        if (typeof this.state.value === 'string' && String(this.state.value).length > 0) {
+            this.props.onChange({display_submit: true});
+        }
+        else {
+            this.props.onChange({display_submit: false});
+        }
      },
   // triggered when 'state properties' change
      render: function(){
-        var SubmitButton = this.getSubmitButton();
         return(
-            <fieldset className='fieldset-supply-dataset'>
-                <legend>Supply Dataset</legend>
-                <input type='file' name='svm_dataset[]' className='svm-dataset-file' onChange={this.change} value={this.state.value} />
-                <input type='button' value='Add more' className='add-element svm-dataset-file-add' />
-                <input type='button' value='Remove' className='remove-element svm-dataset-file-remove' />
-                <p className='form-note'>*<span className='bold'>Note:</span> Uploaded file(s) must be formatted as <span className='italic'>csv</span>, <span className='italic'>json</span>, or <span className='italic'>xml</span> format.</p>
+            <div>
+                <fieldset className='fieldset-supply-dataset'>
+                    <legend>Supply Dataset</legend>
+                    <input type='file' name='svm_dataset[]' className='svm-dataset-file' onChange={this.change} value={this.state.value} />
+                    <input type='button' value='Add more' className='add-element svm-dataset-file-add' />
+                    <input type='button' value='Remove' className='remove-element svm-dataset-file-remove' />
+                    <p className='form-note'>*<span className='bold'>Note:</span> Uploaded file(s) must be formatted as <span className='italic'>csv</span>, <span className='italic'>json</span>, or <span className='italic'>xml</span> format.</p>
+                </fieldset>
 
                 <SubmitButton/>
-            </fieldset>
+            </div>
         );
-     },
-  // call back: used for the above 'render' (return 'span' if undefined)
-    getSubmitButton: function() {
-        if (typeof this.state.value === 'string' && String(this.state.value).length > 0) {
-            return Submit;
-        }
-        else {
-            return 'span';
-        }
-    }
+     }
 });
 
 // indicate which class can be exported, and instantiated via 'require'
