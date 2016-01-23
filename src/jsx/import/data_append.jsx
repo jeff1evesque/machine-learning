f3bc4a3663feb088b1ca6cae9a9d5c580d74af9c
jeff levesque <jeff1evesque@yahoo.com>
2016-01-22 20:41:31 -0500

    #2332: data_append.jsx, revamp logic similar to 'data_new.jsx'

--- src/jsx/import/data_append.jsx
+++ src/jsx/import/data_append.jsx
@@ -7,6 +7,9 @@
  * Note: this script implements jsx (reactjs) syntax.
  */
 
+import SupplyDatasetFile from './supply_dataset_file.jsx';
+import SupplyDatasetUrl from './supply_dataset_url.jsx';
+
 var DataAppend = React.createClass({
   // initial 'state properties'
     getInitialState: function() {
@@ -16,31 +19,65 @@ var DataAppend = React.createClass({
         };
     },
   // update 'state properties'
-    change: function(event){
-        this.setState({
-            value_session_id: event.target.value_session_id,
-            value_dataset_type: event.target.value_dataset_type
-        });
+    changeSessionId: function(event){
+        if (event.target.value) {
+            this.setState({value_session_id: event.target.value});
+        }
+        else {
+            this.props.onChange({render_submit: false});
+        }
+    },
+    changeDatasetType: function(event){
+        if (event.target.value) {
+            this.setState({value_dataset_type: event.target.value});
+        }
+        else {
+            this.props.onChange({render_submit: false});
+        }
+    },
+  // update 'state properties' from children component (i.e. 'validStringEntered')
+    displaySubmit: function(event) {
+        if (event.display_submit) {
+            this.props.onChange({render_submit: event.display_submit});
+        }
+        else {
+            this.props.onChange({render_submit: false});
+        }
     },
   // triggered when 'state properties' change
     render: function(){
+        var SupplyDataset = this.getSupplyDataset();
         return(
             <fieldset className='fieldset-session-data-upload'>
                 <legend>Data Upload</legend>
                 <fieldset className='fieldset-dataset-type'>
                     <legend>Configurations</legend>
                     <p>Select past session, and upload type</p>
-                    <select name='svm_session_id' autoComplete='off' onChange={this.change} value={this.state.value_session_id}>
+                    <select name='svm_session_id' autoComplete='off' onChange={this.changeSessionId} value={this.state.value_session_id}>
                         <option value='' defaultValue>--Select--</option>
                     </select>
-                    <select name='svm_dataset_type' autoComplete='off' onChange={this.change} value={this.state.value_dataset_type}>
+                    <select name='svm_dataset_type' autoComplete='off' onChange={this.changeDatasetType} value={this.state.value_dataset_type}>
                         <option value='' defaultValue>--Select--</option>
                         <option value='file_upload'>Upload file</option>
                         <option value='dataset_url'>Dataset URL</option>
                     </select>
                 </fieldset>
+
+                <SupplyDataset onChange={this.displaySubmit}/>
             </fieldset>
         );
+    },
+  // call back: used for the above 'render' (return 'span' if undefined)
+    getSupplyDataset: function() {
+        if (typeof this.state.value_title === 'string' && String(this.state.value_title).length > 0) {
+            return {
+                file_upload: SupplyDatasetFile,
+                dataset_url: SupplyDatasetUrl
+            }[this.state.value_dataset_type] || 'span';
+        }
+        else {
+            return 'span';
+        }
     }
 });
 
