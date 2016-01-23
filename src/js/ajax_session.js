faddb9cda04ba817fb6792b13ffe4fee2fb2f476
jeff levesque <jeff1evesque@yahoo.com>
2016-01-23 01:10:02 -0500

    #2332: ajax_session.js, remove append dom logic

--- src/js/ajax_session.js
+++ src/js/ajax_session.js
@@ -18,19 +18,8 @@
       // Remove AJAX Overlay
       $('form .ajax-overlay').fadeOut(200, function() { $(this).remove(); });
 
-      // Append to DOM
-      if (data.error) {
-        $('.fieldset-dataset-type').append('<div class="error">' + data.error + '</div>');
-        $('.fieldset-select-model').append('<div class="error">' + data.error + '</div>');
-      } else {
-        $.each(data, function(index, value) {
-          var valueId    = value.id;
-          var valueTitle = value.title;
-          var element     = '<option ' + 'value="' + valueId + '">' + valueId + ': ' + valueTitle + '</option>';
-
-          $('select[name="svm_session_id"]').append(element);
-        });
-      }
+      // Return server side data
+      return data;
 
     }).fail(function(jqXHR, textStatus, errorThrown) {
       console.log('Error Thrown: ' + errorThrown);
