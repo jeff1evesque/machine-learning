f8db3864f7cda134db784401178882967894b1c3
jeff levesque <jeff1evesque@yahoo.com>
2016-01-29 14:11:33 -0500

    #2332: supply_predictors.jsx, correctly define 'value' for 'input'

--- src/jsx/import/supply_predictors.jsx
+++ src/jsx/import/supply_predictors.jsx
@@ -32,7 +32,7 @@ var SupplyPredictors = React.createClass({
 
                 {/* array components require unique 'key' value */}
                 {options && options.map(function(value, index){ 
-                    return <input type='text' name='prediction_input[]' placeholder={value} key={index} onChange={this.validIntegerEntered} value={this.state.value_predictor+index} />;
+                    return <input type='text' name='prediction_input[]' placeholder={value} key={index} onChange={this.validIntegerEntered} value={this.state['value_predictor' + index.toString()]} />;
                 })}
 
             </fieldset>
