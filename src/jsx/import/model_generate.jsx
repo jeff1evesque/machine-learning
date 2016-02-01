72811b7c16a64e6d5014fbf77099884a7ff11b5c
jeff levesque <jeff1evesque@yahoo.com>
2016-01-31 23:04:31 -0500

    #2359: model_generate.jsx, implement ajax arguments

--- src/jsx/import/model_generate.jsx
+++ src/jsx/import/model_generate.jsx
@@ -88,8 +88,15 @@ var ModelGenerate = React.createClass({
     },
   // call back: get session id(s) from server side, and append to form
     componentDidMount: function () {
+      // ajax arguments
+        var ajaxEndpoint = '/retrieve-session/';
+        var ajaxArguments = {
+            'endpoint': ajaxEndpoint,
+            'data': null
+        };
+
       // asynchronous callback: ajax 'done' promise
-        sessionId(function (asynchObject) {
+        ajaxCaller(function (asynchObject) {
         // Append to DOM
             if (asynchObject && asynchObject.error) {
                 this.setState({ajax_done_error: asynchObject.error});
@@ -107,7 +114,9 @@ var ModelGenerate = React.createClass({
                 this.setState({ajax_fail_error: asynchError});
                 console.log('Error Thrown: ' + asynchError);
             }
-        }.bind(this));
+        }.bind(this),
+      // pass ajax arguments
+        ajaxArguments);
     }
 });
 
