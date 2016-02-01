161905c9cc3209e0e924eb632d0abd04ca8f2d6c
jeff levesque <jeff1evesque@yahoo.com>
2016-02-01 10:54:19 -0500

    #2362: submit.jsx, define 'componentDidMount', skeleton 'formSubmit'

--- src/jsx/import/submit.jsx
+++ src/jsx/import/submit.jsx
@@ -8,9 +8,47 @@
  */
 
 var Submit = React.createClass({
+  // submit form to server side, get response (if exists)
+     formSubmit: function(event){
+
+     },
   // triggered when 'state properties' change
     render: function(){
-        return(<input type='submit' className='svm-form-submit' />);
+        return(<input type='submit' className='svm-form-submit' onChange={this.formSubmit} />);
+    },
+  // call back: get session id(s) from server side, and append to form
+    componentDidMount: function () {
+      // ajax arguments
+        var ajaxEndpoint = $(this).attr('action');
+        var ajaxArguments = {
+            'endpoint': ajaxEndpoint,
+            'data': new FormData(this),
+            'contentType': false,
+            'processData': false,
+        };
+
+      // asynchronous callback: ajax 'done' promise
+        ajaxCaller(function (asynchObject) {
+        // Append to DOM
+            if (asynchObject && asynchObject.error) {
+                this.setState({ajax_done_error: asynchObject.error});
+            } else if (asynchObject) {
+                this.setState({ajax_done_options: asynchObject});
+            }
+        }.bind(this),
+      // asynchronous callback: ajax 'fail' promise
+        function (asynchStatus, asynchError) {
+            if (asynchStatus) {
+                this.setState({ajax_fail_status: asynchStatus});
+                console.log('Error Status: ' + asynchStatus);
+            }
+            if (asynchError) {
+                this.setState({ajax_fail_error: asynchError});
+                console.log('Error Thrown: ' + asynchError);
+            }
+        }.bind(this),
+      // pass ajax arguments
+        ajaxArguments);
     }
 });
 
