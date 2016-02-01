a4e1acd873bbb21ac47011b5aacbd7a1725dc2c0
jeff levesque <jeff1evesque@yahoo.com>
2016-01-31 23:03:08 -0500

    #2359: ajax_caller.js, adjust docblock references

--- src/js/ajax_caller.js
+++ src/js/ajax_caller.js
@@ -1,9 +1,9 @@
 /**
- * ajax_feature.js: this script utilizes ajax to retrieve data from 'views.py'.
- *                  Specifically, an array of feature (independent variables)
- *                  names, and the generalized count of features that can be
- *                  expected within an observation, is inserted to respective
- *                  DOM elements.
+ * ajax_caller.js: this script utilizes ajax to retrieve data from 'views.py'.
+ *                 Specifically, an array of feature (independent variables)
+ *                 names, and the generalized count of features that can be
+ *                 expected within an observation, is inserted to respective
+ *                 DOM elements.
  *
  * @args, and array of parameters (url, data).
  */
