ac4a555d350d56b94662c73b963c39ffb63df5df
jeff levesque <jeff1evesque@yahoo.com>
2016-02-01 23:03:12 -0500

    #2362: submit.jsx, remove 'onClick' from returned 'input'

--- src/jsx/import/submit.jsx
+++ src/jsx/import/submit.jsx
@@ -14,7 +14,7 @@ var Submit = React.createClass({
      },
   // triggered when 'state properties' change
     render: function(){
-        return(<input type='submit' className='svm-form-submit' onClick={this.formSubmit} />);
+        return(<input type='submit' className='svm-form-submit' />);
     }
 });
 
