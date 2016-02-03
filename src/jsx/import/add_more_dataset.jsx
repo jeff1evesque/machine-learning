91d6849d22eb8f0077cd059c97771c2bca366e71
jeff levesque <jeff1evesque@yahoo.com>
2016-02-02 23:50:23 -0500

    #2357: add_more_dataset.jsx, add skeleton file

---
+++ src/jsx/import/add_more_dataset.jsx
@@ -0,0 +1,16 @@
+/**
+ * add_more_dataset.jsx: append additional input fields, to allow multiple
+ *     dataset uploads.
+ *
+ * @AddMoreDataset, must be capitalized in order for reactjs to render it as a
+ *     component. Otherwise, the variable is rendered as a dom node.
+ *
+ * Note: this script implements jsx (reactjs) syntax.
+ */
+
+var AddMoreDataset = React.createClass({
+
+});
+
+// indicate which class can be exported, and instantiated via 'require'
+export default AddMoreDataset
