14b066774e207d293b9f4ddd88c852e88d47f3a8
jeff levesque <jeff1evesque@yahoo.com>
2016-02-05 23:01:51 -0500

    #2357: data_append.jsx, ensure child components clear

--- src/jsx/import/data_append.jsx
+++ src/jsx/import/data_append.jsx
@@ -43,6 +43,7 @@ var DataAppend = React.createClass({
 
         if (datasetType && datasetType != '--Select--') {
             this.setState({value_dataset_type: event.target.value});
+            this.props.onChange({render_submit: false});
         }
         else {
             this.setState({value_dataset_type: '--Select--'});
