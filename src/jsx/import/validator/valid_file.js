9c7c9cfaef876781844e270daf6d3fbbe6ffbbf7
jeff levesque <jeff1evesque@yahoo.com>
2016-02-12 11:39:45 -0500

    #2352: valid_file.js, add file

---
+++ src/jsx/import/validator/valid_file.js
@@ -0,0 +1,15 @@
+/**
+ * valid_file.js: check if provided argument has proper file extension.
+ */
+function validator(value) {
+  var validExtensions = ['csv', 'xml', 'json'];
+  if ($.inArray(value.split('.').pop().toLowerCase(), validExtensions) >= 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+export function checkValidInt(value) {
+  return validator(value);
+}
