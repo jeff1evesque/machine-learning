851b796b50313bd5a0b6cf3d3e003db0b1ba1b29
jeff levesque <jeff1evesque@yahoo.com>
2016-02-16 22:42:00 -0500

    #2397: valid_string.js, adjust indentation

--- src/jsx/import/validator/valid_string.js
+++ src/jsx/import/validator/valid_string.js
@@ -2,7 +2,7 @@
  * valid_string.js: check if provided argument is string type.
  */
 function validator(value) {
-    return typeof value === 'string';
+  return typeof value === 'string';
 }
 
 export function checkValidInt(value) {
