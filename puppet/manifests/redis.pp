24f7ad26b09b1b50826b6e161547fdb7183f47e4
jeff levesque <jeff1evesque@yahoo.com>
2016-02-25 23:04:40 -0500

    #2349: redis.pp, install redis client, and redis server

---
+++ puppet/manifests/redis.pp
@@ -0,0 +1,31 @@
+## install python, and pip
+class install_pip {
+    include python
+}
+
+## install redis-client
+class install_redis_client {
+    ## set dependency
+    require create_log_directory
+
+    package { 'redis':
+        ensure   => 'installed',
+        provider => 'pip',
+        before   => Package[$packages_general_npm],
+    }
+}
+
+## package: install redis-server
+class install_redis_server {
+    package { 'redis-server':
+        ensure => 'installed',
+    }
+}
+
+## constructor
+class constructor {
+    contain install_pip
+    contain install_redis_client
+    contain install_redis_server
+}
+include constructor
