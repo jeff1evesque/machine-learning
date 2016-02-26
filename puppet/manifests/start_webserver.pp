e86fd07587023e39cf118061fbfa1ee8284192c8
jeff levesque <jeff1evesque@yahoo.com>
2016-02-25 21:46:20 -0500

    #2349: start_webserver.pp, implement class structure

--- puppet/manifests/start_webserver.pp
+++ puppet/manifests/start_webserver.pp
@@ -1,48 +1,68 @@
-include python
-include python::flask
-include python::requests
-
-## define $PATH for all execs, and packages
-Exec {path => ['/usr/bin/']}
-
 ## create log directory
-file {'/vagrant/log/':
-    ensure => 'directory',
-    before => File['server-startup-script'],
+class create_log_directory {
+    file {'/vagrant/log/':
+        ensure => 'directory',
+    }
+}
+
+## install webserver
+class install_server {
+    ## set dependency
+    require create_log_directory
+
+    ## install python, flask, request
+    include python
+    include python::flask
+    include python::requests
 }
 
 ## detect os family: create startup script, start flask server
-case $::osfamily {
-    'redhat': {
-    }
-    'debian': {
+class configure_server {
+    ## set dependency
+    require create_log_directory
+    require install_server
+
+    case $::osfamily {
+        'redhat': {
+        }
+        'debian': {
         ## create startup script (heredoc syntax)
-        file {'server-startup-script':
-            path    => '/etc/init/flask.conf',
-            ensure  => 'present',
-            content => template('/vagrant/puppet/template/webserver.erb'),
-            notify  => Exec['dos2unix-flask'],
-        }
+            file {'server-startup-script':
+                path    => '/etc/init/flask.conf',
+                ensure  => 'present',
+                content => template('/vagrant/puppet/template/webserver.erb'),
+                notify  => Exec['dos2unix-flask'],
+            }
 
-        ## convert clrf (windows to linux) in case host machine is windows.
-        #
-        #  @notify, ensure the webserver service is started. This is similar
-        #      to an exec statement, where the 'refreshonly => true' would be
-        #      implemented on the corresponding listening end point. But, the
-        #      'service' end point does not require the 'refreshonly'
-        #      attribute.
-        exec {'dos2unix-flask':
-            command     => 'dos2unix /etc/init/flask.conf',
-            refreshonly => true,
-            notify      => Service['flask'],
-        }
+            ## convert clrf (windows to linux) in case host machine is windows.
+            #
+            #  @notify, ensure the webserver service is started. This is similar
+            #      to an exec statement, where the 'refreshonly => true' would be
+            #      implemented on the corresponding listening end point. But, the
+            #      'service' end point does not require the 'refreshonly'
+            #      attribute.
+            exec {'dos2unix-flask':
+                command     => 'dos2unix /etc/init/flask.conf',
+                refreshonly => true,
+                notify      => Service['flask'],
+                path        => '/usr/bin/',
+            }
 
-        ## start webserver
-        service {'flask':
-            ensure => 'running',
-            enable => true,
+            ## start webserver
+            service {'flask':
+                ensure => 'running',
+                enable => true,
+            }
         }
-    }
-    default: {
+        default: {
+        }
     }
 }
+
+## constructor
+class constructor {
+    contain create_log_directory
+    contain install_server
+    contain configure_server
+}
+include constructor
