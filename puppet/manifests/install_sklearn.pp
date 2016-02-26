b76f3fe6f96de1dc1ed8cfa70a9b3b41d4cf3bdf
jeff levesque <jeff1evesque@yahoo.com>
2016-02-25 21:57:14 -0500

    #2349: install_sklearn.pp, implement class structures

--- puppet/manifests/install_sklearn.pp
+++ puppet/manifests/install_sklearn.pp
@@ -1,36 +1,64 @@
 ## include puppet modules: this (also) runs 'apt-get update'
 include git
 
-## define $PATH for all execs
-Exec {path => ['/usr/bin/']}
-
 ## create '/vagrant/build/' directory
-file {'/vagrant/build/':
-    ensure => 'directory',
-    before => Vcsrepo['/vagrant/build/scikit-learn'],
+class create_build_directory {
+    file {'/vagrant/build/':
+        ensure => 'directory',
+        before => Vcsrepo['/vagrant/build/scikit-learn'],
+    }
 }
 
 ## download scikit-learn
-vcsrepo {'/vagrant/build/scikit-learn':
-    ensure   => present,
-    provider => git,
-    source   => 'https://github.com/scikit-learn/scikit-learn',
-    revision => '0.16.1',
-    before   => Exec['build-sklearn'],
-    notify   => Exec['build-sklearn'],
+class download_sklearn {
+    ## set dependency
+    require create_build_directory
+
+    vcsrepo {'/vagrant/build/scikit-learn':
+        ensure   => present,
+        provider => git,
+        source   => 'https://github.com/scikit-learn/scikit-learn',
+        revision => '0.16.1',
+        before   => Exec['build-sklearn'],
+        notify   => Exec['build-sklearn'],
+    }
 }
 
 ## build scikit-learn
-exec {'build-sklearn':
-    command     => 'python setup.py build',
-    cwd         => '/vagrant/build/scikit-learn/',
-    notify      => Exec['install-sklearn'],
-    refreshonly => true,
+class build_sklearn {
+    ## set dependency
+    require create_build_directory
+    require download_sklearn
+
+    exec {'build-sklearn':
+        command     => 'python setup.py build',
+        cwd         => '/vagrant/build/scikit-learn/',
+        notify      => Exec['install-sklearn'],
+        refreshonly => true,
+        path        => '/usr/bin',
+    }
 }
 
 ## install scikit-learn
-exec {'install-sklearn':
-    command     => 'python setup.py install',
-    cwd         => '/vagrant/build/scikit-learn/',
-    refreshonly => true,
+class install_sklearn {
+    ## set dependency
+    require create_build_directory
+    require download_sklearn
+    require build_sklearn
+
+    exec {'install-sklearn':
+        command     => 'python setup.py install',
+        cwd         => '/vagrant/build/scikit-learn/',
+        refreshonly => true,
+        path        => '/usr/bin',
+    }
 }
+
+## constructor
+class constructor {
+    contain create_build_directory
+    contain download_sklearn
+    contain build_sklearn
+    contain install_sklearn
+}
+include constructor
