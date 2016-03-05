d4eb1e994b1b4a6c1f9fd985b06698cd76729ba6
jeff levesque <jeff1evesque@yahoo.com>
2016-03-05 09:45:03 -0500

    #2349: install_packages.pp, remove dependency directive

--- puppet/manifests/install_packages.pp
+++ puppet/manifests/install_packages.pp
@@ -14,12 +14,10 @@ case $::osfamily {
   }
 }
 
-$packages_build_dep   = ['matplotlib']
 $packages_general_pip = [
     'jsonschema',
     'xmltodict',
-    'six',
-    'python-matplotlib'
+    'six'
 ]
 $packages_build_size  = size($packages_build_dep) - 1
 
@@ -39,16 +37,6 @@ exec {'enable-multiverse-repository-2':
   command => template('/vagrant/puppet/template/enable_multiverse_2.erb'),
   notify  => Exec["build-package-dependencies-${packages_build_size}"],
   refreshonly => true,
-}
-
-## build package dependencies
-each($packages_build_dep) |$index, $package| {
-  exec {"build-package-dependencies-${index}":
-    command     => "apt-get install build-essential ${package} -y",
-    before      => Package[$packages_general],
-    refreshonly => true,
-    timeout     => 1400,
-  }
 }
 
 ## packages: install general packages (apt, yum)
