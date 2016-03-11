ba2b24b88cda53061b5e65bdbb54a050d1735056
jeff levesque <jeff1evesque@yahoo.com>
2016-03-11 00:28:59 -0500

    #2349: webcompilers.pp, add file

---
+++ puppet/environment/development/modules/package/manifests/webcompilers.pp
@@ -0,0 +1,21 @@
+### Note: the prefix 'package::', corresponds to a puppet convention:
+###
+###       https://github.com/jeff1evesque/machine-learning/issues/2349
+###
+class package:webcompilers {
+    $webcompilers = [
+        'uglify-js',
+        'imagemin',
+        'node-sass',
+        'babel-core',
+        'browserify',
+        'babelify'
+    ]
+
+    ## packages: install general packages (npm)
+    package { $webcompilers:
+        ensure   => 'present',
+        provider => 'npm',
+        require  => Class['nodejs'],
+    }
+}
