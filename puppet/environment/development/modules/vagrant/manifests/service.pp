0c3e0d858319ae87b14089dc161738ce25d96d36
jeff levesque <jeff1evesque@yahoo.com>
2016-03-11 00:24:54 -0500

    #2349: service.pp, add file

---
+++ puppet/environment/development/modules/vagrant/manifests/service.pp
@@ -0,0 +1,25 @@
+### Note: the prefix 'vagrant::', corresponds to a puppet convention:
+###
+###       https://github.com/jeff1evesque/machine-learning/issues/2349
+###
+class vagrant::service {
+    ## create startup script: for 'vagrant-mounted' event
+    file { 'vagrant-startup-script':
+        path    => '/etc/init/workaround-vagrant-bug-6074.conf',
+        ensure  => 'present',
+        content => template('/vagrant/puppet/template/vagrant_mounted.erb'),
+        notify  => Exec['dos2unix-upstart-vagrant'],
+    }
+
+    ## dos2unix upstart: convert clrf (windows to linux) in case host machine
+    #                    is windows.
+    #
+    #  @notify, ensure the webserver service is started. This is similar to an
+    #      exec statement, where the 'refreshonly => true' would be implemented
+    #      on the corresponding listening end point. But, the 'service' end point
+    #      does not require the 'refreshonly' attribute.
+    exec { 'dos2unix-upstart-vagrant':
+        command => 'dos2unix /etc/init/workaround-vagrant-bug-6074.conf',
+        refreshonly => true,
+    }
+}
