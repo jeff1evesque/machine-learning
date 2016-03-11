bd7977ba2f77de6d650b9c1c8af6147d6f6b89b3
jeff levesque <jeff1evesque@yahoo.com>
2016-03-11 00:28:07 -0500

    #2349: redis.pp, add file

---
+++ puppet/environment/development/modules/package/manifests/redis.pp
@@ -0,0 +1,12 @@
+### Note: the prefix 'package::', corresponds to a puppet convention:
+###
+###       https://github.com/jeff1evesque/machine-learning/issues/2349
+###
+class package::redis {
+    include python
+
+    package { 'redis':
+        ensure   => 'installed',
+        provider => 'pip',
+    }
+}
