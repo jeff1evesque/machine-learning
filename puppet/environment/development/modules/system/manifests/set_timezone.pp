044a8c18ea82e7663408e06a5296859b1a46c3c9
jeff levesque <jeff1evesque@yahoo.com>
2016-03-11 00:24:23 -0500

    #2349: set_timezone.pp, add file

---
+++ puppet/environment/development/modules/system/manifests/set_timezone.pp
@@ -0,0 +1,10 @@
+### Note: the prefix 'system::', corresponds to a puppet convention:
+###
+###       https://github.com/jeff1evesque/machine-learning/issues/2349
+###
+class system::set_timezone {
+    class { 'timezone':
+        region   => 'America',
+        locality => 'New_York',
+    }
+}
