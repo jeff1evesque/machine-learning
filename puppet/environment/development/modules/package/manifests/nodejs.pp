3da5f8103e025fd579be3079aee28a3aa782f7de
jeff levesque <jeff1evesque@yahoo.com>
2016-03-12 12:05:24 -0500

    #2349: nodejs.pp, remove file

--- puppet/environment/development/modules/package/manifests/nodejs.pp
+++
@@ -1,9 +0,0 @@
-### Note: the prefix 'package::', corresponds to a puppet convention:
-###
-###       https://github.com/jeff1evesque/machine-learning/issues/2349
-###
-class package::nodejs {
-    class { 'nodejs':
-        repo_url_suffix => '5.x',
-    }
-}
