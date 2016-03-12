13f2647928c3b3a224dd9258989a8fae4d8e3161
jeff levesque <jeff1evesque@yahoo.com>
2016-03-12 01:32:54 -0500

    #2349: vagrant_mounted.pp, include respective modules

--- puppet/environment/development/manifests/vagrant_mounted.pp
+++ puppet/environment/development/manifests/vagrant_mounted.pp
@@ -1,38 +1,8 @@
-## variables
-$mountpoint = '/vagrant/'
+## install dos2unix
+include package::dos2unix
 
-## define $PATH for all execs, and packages
-Exec {path => ['/usr/bin/']}
+## vagrant service: service indicating vagrant mounted
+include vagrant::service
 
-## install compiler dependencies
-package { 'dos2unix':
-    ensure => 'installed',
-    before => File['vagrant-startup-script'],
-}
-
-## create startup script: for 'vagrant-mounted' event
-file { 'vagrant-startup-script':
-    path    => '/etc/init/workaround-vagrant-bug-6074.conf',
-    ensure  => 'present',
-    content => template('/vagrant/puppet/template/vagrant_mounted.erb'),
-    notify  => Exec['dos2unix-upstart-vagrant'],
-}
-
-## dos2unix upstart: convert clrf (windows to linux) in case host machine
-#                    is windows.
-#
-#  @notify, ensure the webserver service is started. This is similar to an
-#      exec statement, where the 'refreshonly => true' would be implemented
-#      on the corresponding listening end point. But, the 'service' end point
-#      does not require the 'refreshonly' attribute.
-exec {'dos2unix-upstart-vagrant':
-    command => 'dos2unix /etc/init/workaround-vagrant-bug-6074.conf',
-    refreshonly => true,
-    notify  => Service['workaround-vagrant-bug-6074'],
-}
-
-## start 'workaround-vagrant-bug-6074' service
-service {'workaround-vagrant-bug-6074':
-    ensure => 'running',
-    enable => true,
-}
+## start service
+include vagrant::start
