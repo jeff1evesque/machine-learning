137bd3a2f4152c6018c33625307d938767b3053f
jeff levesque <jeff1evesque@yahoo.com>
2016-03-12 10:46:47 -0500

    #2349: configure_system.pp, implement 'include'

--- puppet/environment/development/manifests/configure_system.pp
+++ puppet/environment/development/manifests/configure_system.pp
@@ -1,13 +1,2 @@
 ## define system timezone
-class set_timezone {
-    class {'timezone':
-        region   => 'America',
-        locality => 'New_York',
-    }
-}
-
-## constructor
-class constructor {
-    contain set_timezone
-}
-include constructor
+include system::set_timezone
