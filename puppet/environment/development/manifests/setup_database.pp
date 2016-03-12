afe26ba03628f3cc9c9656f581b1ac598d5f4209
jeff levesque <jeff1evesque@yahoo.com>
2016-03-12 11:22:08 -0500

    #23449: setup_database.pp, define docblock

--- puppet/environment/development/manifests/setup_database.pp
+++ puppet/environment/development/manifests/setup_database.pp
@@ -1,3 +1,11 @@
+### setup_database.pp: install client, server, bindings, and initialize
+###                    database with required tables.
+###
+### Note: the prefix 'package::', corresponds to a puppet convention:
+###
+###       https://github.com/jeff1evesque/machine-learning/issues/2349
+###
+
 ## install mariadb
 include database::server
 
