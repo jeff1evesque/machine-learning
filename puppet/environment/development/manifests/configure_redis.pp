915d68b4085a0e7d21ebf159f2dcd121a08c1cf3
jeff levesque <jeff1evesque@yahoo.com>
2016-03-12 10:48:30 -0500

    #2349: configure_redis.pp, implement 'include'

--- puppet/environment/development/manifests/configure_redis.pp
+++ puppet/environment/development/manifests/configure_redis.pp
@@ -1,33 +1,5 @@
 ## install python, and pip
-class install_pip {
-    include python
-}
-
-## install redis-client
-class install_redis_client {
-    ## set dependency
-    require install_pip
-
-    package { 'redis':
-        ensure   => 'installed',
-        provider => 'pip',
-    }
-}
+include redis::client
 
 ## package: install redis-server
-class install_redis_server {
-    # set dependency
-    require install_pip
-
-    package { 'redis-server':
-        ensure => 'installed',
-    }
-}
-
-## constructor
-class constructor {
-    contain install_pip
-    contain install_redis_client
-    contain install_redis_server
-}
-include constructor
+include redis::server
