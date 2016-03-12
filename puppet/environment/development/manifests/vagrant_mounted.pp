434c39da6df9a98c382000ada456ce0cb7f2bcc1
jeff levesque <jeff1evesque@yahoo.com>
2016-03-12 11:22:59 -0500

    #23449: vagrant_mounted.pp, define docblock

--- puppet/environment/development/manifests/vagrant_mounted.pp
+++ puppet/environment/development/manifests/vagrant_mounted.pp
@@ -1,3 +1,12 @@
+### vagrant_mounted.pp: ensure 'vagrant-mounted' event fires, when '/vagrant'
+###                     shared directory, is mounted within the guest virtual
+###                     machine.
+###
+### Note: the prefix 'package::', corresponds to a puppet convention:
+###
+###       https://github.com/jeff1evesque/machine-learning/issues/2349
+###
+
 ## install dos2unix
 include package::dos2unix
 
