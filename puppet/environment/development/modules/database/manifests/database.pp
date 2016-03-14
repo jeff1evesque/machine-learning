d481fd0122197ed4767cb42e54c2982f4ecbb103
jeff levesque <jeff1evesque@yahoo.com>
2016-03-14 11:25:44 -0400

    #2349: database.pp, adjust 'cwd' for database initialization

--- puppet/environment/development/modules/database/manifests/database.pp
+++ puppet/environment/development/modules/database/manifests/database.pp
@@ -3,13 +3,16 @@
 ###       https://github.com/jeff1evesque/machine-learning/issues/2349
 ###
 class database::database {
+    ## variables
+    $environment = 'development'
+
     ## define database tables
     #
     #  @require, syntax involves 'Class Containment'. For more information,
     #      https://puppetlabs.com/blog/class-containment-puppet
     exec {'create-database-tables':
         command => 'python setup_tables.py',
-        cwd     => '/vagrant/puppet/scripts/',
+        cwd     => "/vagrant/puppet/environment/${environment}/scripts/",
         path    => '/usr/bin',
     }
 }
