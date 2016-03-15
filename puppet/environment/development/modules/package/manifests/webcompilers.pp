b7de376b15ec20dca89846ac5f4793007c4b14d0
jeff levesque <jeff1evesque@yahoo.com>
2016-03-14 21:55:20 -0400

    #2349: webcompilers.pp, add missing ':'

--- puppet/environment/development/modules/package/manifests/webcompilers.pp
+++ puppet/environment/development/modules/package/manifests/webcompilers.pp
@@ -2,7 +2,7 @@
 ###
 ###       https://github.com/jeff1evesque/machine-learning/issues/2349
 ###
-class package:webcompilers {
+class package::webcompilers {
     $webcompilers = [
         'uglify-js',
         'imagemin',
