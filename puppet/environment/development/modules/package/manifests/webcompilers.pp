594c507d1d6a29275abcb81ddc019fb0abe6d359
jeff levesque <jeff1evesque@yahoo.com>
2016-03-16 19:22:23 -0400

    #2349: webcompilers.pp, print node, and npm versions

--- puppet/environment/development/modules/package/manifests/webcompilers.pp
+++ puppet/environment/development/modules/package/manifests/webcompilers.pp
@@ -13,6 +13,21 @@ class package::webcompilers {
         'babelify'
     ]
 
+    ## print node / npm version
+    exec { 'echo-general-notice':
+        command => 'echo "installing npm packages with: "',
+        path => '/bin',
+        notify => Exec['echo-versions'],
+        logoutput => true,
+    }
+    exec { 'echo-versions':
+        command => 'npm -v && node -v',
+        path    => '/usr/bin',
+        refreshonly => true,
+        before  => Package[$webcompilers],
+        logoutput => true,
+    }
+
     ## packages: install general packages (npm)
     package { $webcompilers:
         ensure   => 'present',
