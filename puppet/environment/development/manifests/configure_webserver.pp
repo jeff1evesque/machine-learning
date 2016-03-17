8a4feb134d91198a08647e4b3f409d51c739a8d2
jeff levesque <jeff1evesque@yahoo.com>
2016-03-17 15:33:02 -0400

    #2349: configure_webserver.pp, adjust docblock

--- puppet/environment/development/manifests/configure_webserver.pp
+++ puppet/environment/development/manifests/configure_webserver.pp
@@ -1,3 +1,10 @@
+### configure_webserver.pp: implement webserver, with necessary dependencies.
+###
+### Note: the prefix 'package::', corresponds to a puppet convention:
+###
+###       https://github.com/jeff1evesque/machine-learning/issues/2349
+###
+
 ## create log directory
 include system::log_directory
 
