5696011368d313aaf743c93c2dc48997ed3ea7e0
jeff levesque <jeff1evesque@yahoo.com>
2016-03-17 00:24:25 -0400

    #2349: install_sklearn.pp, 'require python'

--- puppet/environment/development/modules/sklearn/manifests/install_sklearn.pp
+++ puppet/environment/development/modules/sklearn/manifests/install_sklearn.pp
@@ -3,6 +3,9 @@
 ###       https://github.com/jeff1evesque/machine-learning/issues/2349
 ###
 class sklearn::install_sklearn {
+    ## set dependency
+    require python
+
     exec {'install-sklearn':
         command => 'python setup.py install',
         cwd     => '/vagrant/build/scikit-learn/',
