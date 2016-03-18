00ad8a7e979a749d1109a498236f9f287099c7ec
jeff levesque <jeff1evesque@yahoo.com>
2016-03-18 19:49:02 -0400

    #2410: service.pp, adjust docblock, add 'notify'

--- puppet/environment/development/modules/vagrant/manifests/service.pp
+++ puppet/environment/development/modules/vagrant/manifests/service.pp
@@ -3,18 +3,23 @@
 ###       https://github.com/jeff1evesque/machine-learning/issues/2349
 ###
 class vagrant::service {
-    ## set dependency
-    require stdlib
-
     ## variables
+    $mountpoint       = '/vagrant/'
     $environment      = 'development'
     $environment_path = "/vagrant/puppet/environment/${environment}"
     $vagrant_service  = template("${environment_path}/template/vagrant_mounted.erb")
 
-    ## dos2unix upstart: convert clrf (windows to linux) in case host machine
-    #                    is windows.
+    ## dos2unix: convert clrf (windows to linux) in case host machine is
+    #            windows.
+    #
+    #  @notify, ensure the webserver service is started. This is similar
+    #      to an exec statement, where the 'refreshonly => true' would be
+    #      implemented on the corresponding listening end point. But, the
+    #      'service' end point does not require the 'refreshonly'
+    #      attribute.
     file { '/etc/init/workaround-vagrant-bug-6074.conf':
         ensure      => file,
         content     => dos2unix($vagrant_service),
+        notify      => Service['workaround-vagrant-bug-6074'],
     }
 }
