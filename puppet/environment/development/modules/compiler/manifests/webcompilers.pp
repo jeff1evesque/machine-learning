ccbeaffe79680fefef5b31942d599ad5d6018253
jeff levesque <jeff1evesque@yahoo.com>
2016-03-19 11:23:11 -0400

    #2419: webcompilers.pp, add 'compiler_path' for template

--- puppet/environment/development/modules/compiler/manifests/webcompilers.pp
+++ puppet/environment/development/modules/compiler/manifests/webcompilers.pp
@@ -4,11 +4,12 @@
 ###
 class compiler::webcompilers {
     ## variables
-    $environment     = 'development'
-    $module          = 'compiler'
-    $environment_dir = "/vagrant/puppet/environment/${environment}"
-    $template_dir    = "${environment_path}/modules/${module}/template"
-    $compiler_path   = "${template_dir}/webcompilers.erb"
+    $environment      = 'development'
+    $module           = 'compiler'
+    $environment_dir  = "/vagrant/puppet/environment/${environment}"
+    $template_dir     = "${environment_path}/modules/${module}/template"
+    $compiler_service = "${template_dir}/webcompilers.erb"
+    $compiler_path    = "${environment_path}/modules/${module}/scripts"
 
     $compilers = [
         'browserify',
