50e23d80dfc81c769b53537286587a887608ff34
jeff levesque <jeff1evesque@yahoo.com>
2016-03-18 23:02:50 -0400

    #2410: webcompilers.pp, reduce docblock, adjust 'content'

--- puppet/environment/development/modules/compiler/manifests/webcompilers.pp
+++ puppet/environment/development/modules/compiler/manifests/webcompilers.pp
@@ -21,29 +21,17 @@ class compiler::webcompilers {
     $compilers.each |String $compiler| {
         ## dos2unix upstart: convert clrf (windows to linux) in case host machine
         #                    is windows.
-        #
-        #  @notify, ensure the webserver service is started. This is similar
-        #      to an exec statement, where the 'refreshonly => true' would be
-        #      implemented on the corresponding listening end point. But, the
-        #      'service' end point does not require the 'refreshonly'
-        #      attribute.
         file { "/etc/init/${compiler}.conf":
             ensure      => file,
             content     => dos2unix(template($compiler_path)),
-            notify      => File[$compiler_bash_path],
+            before      => File[$compiler_bash_path],
         }
 
         ## dos2unix bash: convert clrf (windows to linux) in case host machine is
         #                 windows.
-        #
-        #  @notify, ensure the webserver service is started. This is similar
-        #      to an exec statement, where the 'refreshonly => true' would be
-        #      implemented on the corresponding listening end point. But, the
-        #      'service' end point does not require the 'refreshonly'
-        #      attribute.
         file { $compiler_bash_path:
             ensure      => file,
-            content     => dos2unix($compiler_bash_path/scripts/${compiler}),
+            content     => dos2unix("${compiler_bash_path}/scripts/${compiler}"),
         }
     }
 }
