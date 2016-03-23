f4f8b7dc03c202814f524c05e999b2481225dc7d
jeff levesque <jeff1evesque@yahoo.com>
2016-03-23 07:38:01 -0400

    #2418: redis_client.pp, rename from 'redis.pp'

--- puppet/environment/development/modules/package/manifests/redis_client.pp
+++ puppet/environment/development/modules/package/manifests/redis_client.pp
@@ -2,8 +2,8 @@
 ###
 ###       https://github.com/jeff1evesque/machine-learning/issues/2349
 ###
-class package::redis {
-    include python
+class package::redis_client {
+    require python
 
     package { 'redis':
         ensure   => 'installed',
