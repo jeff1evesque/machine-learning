c2050b2fb1e1be17e94ee79dce2c96b5f6841b0d
jeff levesque <jeff1evesque@yahoo.com>
2016-03-22 23:22:00 -0400

    #2418: service.pp, fix lint errors

--- puppet/environment/development/modules/webserver/manifests/service.pp
+++ puppet/environment/development/modules/webserver/manifests/service.pp
@@ -6,7 +6,7 @@ class webserver::service {
     ## variables
     $environment      = 'development'
     $environment_path = "/vagrant/puppet/environment/${environment}"
-    $flask_service    = template("${environment_path}/template/webserver.erb")
+    $flask_service    = "${environment_path}/template/webserver.erb"
 
     ## include webserver dependencies
     include python
@@ -16,7 +16,7 @@ class webserver::service {
     ## dos2unix: convert clrf (windows to linux) in case host machine is
     #            windows.
     file { '/etc/init/flask.conf':
-        ensure      => file,
-        content     => dos2unix($flask_service),
+        ensure  => file,
+        content => dos2unix(template($flask_service)),
     }
 }
