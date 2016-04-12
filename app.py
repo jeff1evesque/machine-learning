9b53741fadae3d8f6cbf72ce76f0086d55779c69
jeff levesque <jeff1evesque@yahoo.com>
2016-04-12 00:07:11 -0400

    #2441: app.py, attempt to create 'access.log'

--- app.py
+++ app.py
@@ -8,5 +8,20 @@ This file is the acting web server.
 
 '''
 
+import logging
+from logging.handlers import RotatingFileHandler
 from interface import app
-app.run(debug=True, host='0.0.0.0')
+app.run(host='0.0.0.0')
+
+LOG_FILENAME = '/vagrant/log/access.log'
+
+formatter = logging.Formatter(
+    "[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s")
+handler = RotatingFileHandler(LOG_FILENAME, maxBytes=10000000, backupCount=5)
+handler.setLevel(logging.DEBUG)
+handler.setFormatter(formatter)
+app.logger.addHandler(handler)
+
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.DEBUG)
+log.addHandler(handler)
