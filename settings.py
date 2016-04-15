f0e563e1a6b5d6222e2d4d8ff8100bc6f866af29
jeff levesque <jeff1evesque@yahoo.com>
2016-04-14 22:22:17 -0400

    #2461: setttings.py, define root, and log constants

--- settings.py
+++ settings.py
@@ -7,9 +7,15 @@ and to establish required database connection(s).
 
 '''
 
+ROOT = '/vagrant'
 HOST = 'localhost'
 PORT_REDIS = 6379
 
 DB_USERNAME = 'authenticated'
 DB_PASSWORD = 'password'
-DB_LOG_PATH = '/vagrant/log/database.log'
+DB_LOG_PATH = ROOT + '/log/database.log'
+
+ERROR_LOG_PATH   = ROOT + '/log/error.log'
+WARNING_LOG_PATH = ROOT + '/log/warning.log'
+INFO_LOG_PATH    = ROOT + '/log/info.log'
+DEBUG_LOG_PATH   = ROOT + '/log/debug.log'
