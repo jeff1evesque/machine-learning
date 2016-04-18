34a4b8cda832dceae65a5a803dc3745ea44389f5
jeff levesque <jeff1evesque@yahoo.com>
2016-04-17 23:11:55 -0400

    #2461: settings.py, add comments

--- settings.py
+++ settings.py
@@ -7,14 +7,19 @@ and to establish required database connection(s).
 
 '''
 
+## server configuration
 ROOT = '/vagrant'
 HOST = 'localhost'
 PORT_REDIS = 6379
 
+## application database
 DB_USERNAME = 'authenticated'
 DB_PASSWORD = 'password'
 DB_LOG_PATH = ROOT + '/log/database/'
 
+## logging: the possible log levels are constrained by -
+##
+## https://docs.python.org/2/library/logging.html#logging-levels
 LOG_LEVEL        = 'debug'
 ERROR_LOG_PATH   = ROOT + '/log/error'
 WARNING_LOG_PATH = ROOT + '/log/warning'
