7726011ec3a346af19d1dfd06e00359772d5b1e4
jeff levesque <jeff1evesque@yahoo.com>
2016-04-19 15:50:21 -0400

    #2463: validate_dataset.py, implement triple single quote docblock

--- brain/validator/validate_dataset.py
+++ brain/validator/validate_dataset.py
@@ -1,43 +1,43 @@
 #!/usr/bin/python
 
-"""@validate_dataset
+'''@validate_dataset
 
 This script performs validation on the svm dataset(s).
 
-"""
+'''
 
 from jsonschema.validators import Draft4Validator
 from brain.schema.jsonschema_definition import jsonschema_string
 
 
 class Validate_Dataset(object):
-    """Validate_Dataset
+    '''Validate_Dataset
 
     This class provies an interface to validate provided dataset(s) during
     'data_new', and 'data_append' sessions.
 
     Note: this class explicitly inherits the 'new-style' class.
 
-    """
+    '''
 
     def __init__(self, data, svm_session=None):
-        """@__init__
+        '''@__init__
 
         This constructor saves a subset of the passed-in form data.
 
-        """
+        '''
 
         self.data = data
         self.svm_session = svm_session
         self.list_error = []
 
     def validate_label(self):
-        """@validate_label
+        '''@validate_label
 
         This method validates either the dependent variable (observation)
         label(s), or the independent variable (feature) label(s).
 
-        """
+        '''
 
         try:
             Draft4Validator(jsonschema_string()).validate({'value': self.data})
@@ -45,11 +45,11 @@ class Validate_Dataset(object):
             self.list_error.append(str(error))
 
     def validate_value(self):
-        """@validate_value
+        '''@validate_value
 
         This method validates the independent variable (feature) value(s).
 
-        """
+        '''
 
         try:
             float(self.data)
@@ -57,12 +57,12 @@ class Validate_Dataset(object):
             self.list_error.append(str(error))
 
     def get_errors(self):
-        """get_errors
+        '''get_errors
 
         This method gets all current errors. associated with this class
         instance.
 
-        """
+        '''
 
         if len(self.list_error) > 0:
             return self.list_error
