17293daf11e37ded5a8a55317dcc7c7515774f1a
jeff levesque <jeff1evesque@yahoo.com>
2016-05-02 21:31:47 -0400

    #1922: data_append.py, ensure line 80 chars or less

--- brain/session/data_append.py
+++ brain/session/data_append.py
@@ -64,8 +64,9 @@ class Data_Append(Base, Base_Data):
 
         '''
 
+        premodel_settings = self.premodel_data['data']['settings']
         premodel_entity = {
-            'title': self.premodel_data['data']['settings'].get('session_name', None),
+            'title': premodel_settings.get('session_name', None),
             'uid': self.uid,
             'id_entity': session_id,
         }
