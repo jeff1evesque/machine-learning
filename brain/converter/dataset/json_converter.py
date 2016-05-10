98098a2a4eb94cbcb13846d471e2ad78f11a4caf
jeff levesque <jeff1evesque@yahoo.com>
2016-05-10 07:29:06 -0400

    #2540: json_converter.py, implement 'logger'

--- brain/converter/dataset/json_converter.py
+++ brain/converter/dataset/json_converter.py
@@ -30,6 +30,7 @@ def svm_json_converter(raw_data, is_json):
     feature_count = None
     list_dataset = []
     observation_labels = []
+    self.logger = Logger(__name__, 'error', 'error')
 
     if is_json:
         dataset = raw_data
@@ -92,8 +93,10 @@ def svm_json_converter(raw_data, is_json):
         olabel_error = validate_olabel.get_errors()
         flabel_error = validate_flabel.get_errors()
         fvalue_error = validate_fvalue.get_errors()
-        if olabel_error or flabel_error or fvalue_error:
-            print olabel_error, flabel_error, fvalue_error
+        for error in [olabel_error, flabel_error, fvalue_error]:
+            if error:
+                self.logger.log(error)
+        if len(error) > 0:
             return None
 
     # close file
