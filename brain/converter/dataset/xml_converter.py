01af397fd8c971c543a678a4c410a86a336da6bf
jeff levesque <jeff1evesque@yahoo.com>
2016-05-10 07:35:36 -0400

    #2540: xml_converter.py, remove 'self.' prefix for logger

--- brain/converter/dataset/xml_converter.py
+++ brain/converter/dataset/xml_converter.py
@@ -29,7 +29,7 @@ def svm_xml_converter(raw_data):
     feature_count = None
     list_dataset = []
     list_observation_label = []
-    self.logger = Logger(__name__, 'error', 'error')
+    logger = Logger(__name__, 'error', 'error')
 
     # convert xml file to python 'dict'
     dataset = xmltodict.parse(raw_data)
@@ -43,7 +43,7 @@ def svm_xml_converter(raw_data):
 
         list_error = validate.get_errors()
         if list_error:
-            self.logger.log(list_error)
+            logger.log(list_error)
             return None
         else:
             list_observation_label.append(observation_label)
@@ -61,8 +61,8 @@ def svm_xml_converter(raw_data):
             list_error_label = validate.get_errors()
             list_error_value = validate.get_errors()
             if list_error_label or list_error_value:
-                self.logger.log(list_error_label)
-                self.logger.log(list_error_value)
+                logger.log(list_error_label)
+                logger.log(list_error_value)
                 return None
             else:
                 list_dataset.append({
