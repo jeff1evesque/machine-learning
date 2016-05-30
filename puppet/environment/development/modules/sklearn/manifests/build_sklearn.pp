a3fd748d7eeb3e9511ebb88eea82539ffcb2959a
jeff levesque <jeff1evesque@yahoo.com>
2016-05-30 12:39:37 -0400

    #2565: install_sklearn.pp, define 'root_dir'

--- puppet/environment/development/modules/sklearn/manifests/build_sklearn.pp
+++ puppet/environment/development/modules/sklearn/manifests/build_sklearn.pp
@@ -3,9 +3,13 @@
 ###       https://github.com/jeff1evesque/machine-learning/issues/2349
 ###
 class sklearn::build_sklearn {
+    ## local variables
+    $root_dir = '/vagrant'
+
+    ## build sklearn
     exec { 'build-sklearn':
         command => 'python setup.py build',
-        cwd     => '/vagrant/build/scikit-learn/',
+        cwd     => "${root_dir}/build/scikit-learn/",
         path    => '/usr/bin',
     }
 }
