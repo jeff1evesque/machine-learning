d1c33f68b10f4d79d24961fe2696fcacc622e516
jeff levesque <jeff1evesque@yahoo.com>
2016-05-30 12:23:38 -0400

    #2565: server.pp, implement local variables

--- puppet/environment/development/modules/database/manifests/server.pp
+++ puppet/environment/development/modules/database/manifests/server.pp
@@ -3,6 +3,13 @@
 ###       https://github.com/jeff1evesque/machine-learning/issues/2349
 ###
 class database::server {
+    ## local variables
+    $host          = 'localhost'
+    $db            = 'db_machine_learning'
+    $db_username   = 'authenticated'
+    $db_password   = 'password'
+    $root_password = 'password'
+
     ## mysql::server: install, and configure mariadb-server
     #
     #  @password_hash, default password (can be adjusted via cli)
@@ -10,17 +17,17 @@ class database::server {
     #      @max_user_connections, a zero value indicates no limit
     class { '::mysql::server':
         package_name  => 'mariadb-server',
-        root_password => 'password',
+        root_password => $root_password,
         users         => {
-            'authenticated@localhost' => {
+            "${db_username}@${host}" => {
                 ensure                   => 'present',
                 max_connections_per_hour => '0',
                 max_queries_per_hour     => '0',
                 max_updates_per_hour     => '0',
                 max_user_connections     => '0',
-                password_hash            => mysql_password('password'),
+                password_hash            => mysql_password($db_password),
             },
-            'provisioner@localhost'   => {
+            "provisioner@${host}"   => {
                 ensure                   => 'present',
                 max_connections_per_hour => '1',
                 max_queries_per_hour     => '0',
@@ -30,23 +37,23 @@ class database::server {
             },
         },
         grants        => {
-            'authenticated@localhost/db_machine_learning.*' => {
+            "${db_username}@${host}/${db}.*' => {
                 ensure     => 'present',
                 options    => ['GRANT'],
                 privileges => ['INSERT', 'DELETE', 'UPDATE', 'SELECT'],
-                table      => 'db_machine_learning.*',
-                user       => 'authenticated@localhost',
+                table      => "${db}.*",
+                user       => "${db_username}@${host}",
             },
-            'provisioner@localhost/db_machine_learning.*'   => {
+            'provisioner@${host}/${db}.*'   => {
                 ensure     => 'present',
                 options    => ['GRANT'],
                 privileges => ['CREATE'],
-                table      => 'db_machine_learning.*',
-                user       => 'provisioner@localhost',
+                table      => "${db}.*",
+                user       => "provisioner@${host}",
             },
         },
         databases     => {
-            'db_machine_learning' => {
+            $db => {
                 ensure  => 'present',
                 charset => 'utf8',
             },
