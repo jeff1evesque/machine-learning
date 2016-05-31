10d3a519bf8af984e2e541b14fe04fd5f3b39106
jeff levesque <jeff1evesque@yahoo.com>
2016-05-30 21:48:19 -0400

    #2565: setup_tables.py, remove 'try-except' clause

--- puppet/environment/development/modules/database/scripts/setup_tables.py
+++ puppet/environment/development/modules/database/scripts/setup_tables.py
@@ -21,75 +21,70 @@ This file initializes the following database tables within the
 
 import yaml
 import MySQLdb as DB
-from log.logger import Logger
 
 # define configuration
 with open('settings.yml', 'r') as stream:
-    try:
-        # local variables
-        settings = yaml.load(stream)
+    # local variables
+    settings = yaml.load(stream)
 
-        host = settings['general']['host']
-        db = settings['database']['name']
-        provisioner = settings['database']['provisioner']
-        provisioner_password = settings['database']['provisioner_password']
-    except yaml.YAMLError as error:
-        logger = Logger(__name__, 'database', 'database', 'error')
-        logger.log(error)
+    host = settings['general']['host']
+    db = settings['database']['name']
+    provisioner = settings['database']['provisioner']
+    provisioner_password = settings['database']['provisioner_password']
 
-# create connection
-conn = DB.connect(
-    host,
-    provisioner,
-    provisioner_password,
-    db
-)
+    # create connection
+    conn = DB.connect(
+        host,
+        provisioner,
+        provisioner_password,
+        db
+    )
 
-with conn:
-    # create cursor object
-    cur = conn.cursor()
+    with conn:
+        # create cursor object
+        cur = conn.cursor()
 
-    # create 'tbl_feature_count'
-    sql_statement = '''\
-                    CREATE TABLE IF NOT EXISTS tbl_feature_count (
-                        id_size INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                        id_entity INT NOT NULL,
-                        count_features INT NOT NULL
-                    );
-                    '''
-    cur.execute(sql_statement)
+        # create 'tbl_feature_count'
+        sql_statement = '''\
+                        CREATE TABLE IF NOT EXISTS tbl_feature_count (
+                            id_size INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                            id_entity INT NOT NULL,
+                            count_features INT NOT NULL
+                        );
+                        '''
+        cur.execute(sql_statement)
 
-    # create 'tbl_dataset_entity'
-    sql_statement = '''\
-                    CREATE TABLE IF NOT EXISTS tbl_dataset_entity (
-                        id_entity INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                        title VARCHAR (50) NOT NULL,
-                        uid_created INT NOT NULL,
-                        datetime_created DATETIME NOT NULL,
-                        uid_modified INT NULL,
-                        datetime_modified DATETIME NULL
-                    );
-                    '''
-    cur.execute(sql_statement)
+        # create 'tbl_dataset_entity'
+        sql_statement = '''\
+                        CREATE TABLE IF NOT EXISTS tbl_dataset_entity (
+                            id_entity INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                            title VARCHAR (50) NOT NULL,
+                            uid_created INT NOT NULL,
+                            datetime_created DATETIME NOT NULL,
+                            uid_modified INT NULL,
+                            datetime_modified DATETIME NULL
+                        );
+                        '''
+        cur.execute(sql_statement)
 
-    # create 'tbl_observation_label'
-    sql_statement = '''\
-                    CREATE TABLE IF NOT EXISTS tbl_observation_label (
-                        id_label INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                        id_entity INT NOT NULL,
-                        dep_variable_label VARCHAR(75) NOT NULL
-                    );
-                    '''
-    cur.execute(sql_statement)
+        # create 'tbl_observation_label'
+        sql_statement = '''\
+                        CREATE TABLE IF NOT EXISTS tbl_observation_label (
+                            id_label INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                            id_entity INT NOT NULL,
+                            dep_variable_label VARCHAR(75) NOT NULL
+                        );
+                        '''
+        cur.execute(sql_statement)
 
-    # create 'tbl_feature_value'
-    sql_statement = '''\
-                    CREATE TABLE IF NOT EXISTS tbl_feature_value (
-                        id_value INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                        id_entity INT NOT NULL,
-                        dep_variable_label VARCHAR (50) NOT NULL,
-                        indep_variable_label VARCHAR (50) NOT NULL,
-                        indep_variable_value FLOAT NOT NULL
-                    );
-                    '''
-    cur.execute(sql_statement)
+        # create 'tbl_feature_value'
+        sql_statement = '''\
+                        CREATE TABLE IF NOT EXISTS tbl_feature_value (
+                            id_value INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                            id_entity INT NOT NULL,
+                            dep_variable_label VARCHAR (50) NOT NULL,
+                            indep_variable_label VARCHAR (50) NOT NULL,
+                            indep_variable_value FLOAT NOT NULL
+                        );
+                        '''
+        cur.execute(sql_statement)
