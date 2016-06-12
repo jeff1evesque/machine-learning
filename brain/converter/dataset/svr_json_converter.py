02ed5148a1192121e2a3fa31a22d62774ea3c8dc
jeff levesque <jeff1evesque@yahoo.com>
2016-06-12 15:56:40 -0400

    #2586: svr_json_converter.py, add multi-observation case

--- brain/converter/dataset/svr_json_converter.py
+++ brain/converter/dataset/svr_json_converter.py
@@ -47,7 +47,7 @@ def svr_json_converter(raw_data, is_json):
         validate_olabel.validate_label()
 
         # dependent variable with single observation
-        if type(observation['predictors']) == 'dict':
+        if type(observation['predictors']) == dict:
             for feature_label, feature_value in observation['predictors'].items():
                 # validation (part 2)
                 validate_flabel = Validate_Dataset(feature_label)
@@ -65,3 +65,48 @@ def svr_json_converter(raw_data, is_json):
             # generalized feature count in an observation
             if not feature_count:
                 feature_count = len(observation)
+
+        # dependent variable with multiple observations
+        elif type(observation['predictors']) == list:
+            for observation in observation['predictors']:
+                for feature_label, feature_value in observation.items():
+                    # validation (part 2)
+                    validate_flabel = Validate_Dataset(feature_label)
+                    validate_flabel.validate_label()
+                    validate_fvalue = Validate_Dataset(feature_value)
+                    validate_fvalue.validate_value()
+
+                    # restructured data
+                    list_dataset.append({
+                        'dep_variable_label': observation_label,
+                        'indep_variable_label': feature_label,
+                        'indep_variable_value': feature_value
+                    })
+
+                # generalized feature count in an observation
+                if not feature_count:
+                    feature_count = len(observation)
+
+        # list of observation label
+        observation_labels.append(observation_label)
+
+        # check for errors
+        olabel_error = validate_olabel.get_errors()
+        flabel_error = validate_flabel.get_errors()
+        fvalue_error = validate_fvalue.get_errors()
+        for error in [olabel_error, flabel_error, fvalue_error]:
+            if error:
+                logger.log(error)
+        if error and len(error) > 0:
+            return None
+
+    # close file
+    if not is_json:
+        raw_data.close()
+
+    # save observation labels, and return
+    return {
+        'dataset': list_dataset,
+        'observation_labels': observation_labels,
+        'feature_count': feature_count
+    }
