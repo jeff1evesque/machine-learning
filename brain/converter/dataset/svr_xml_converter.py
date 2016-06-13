e64b87a13d9c9f18fd9f8d856b97ae0375b9076f
jeff levesque <jeff1evesque@yahoo.com>
2016-06-12 22:16:43 -0400

    #2586: svr_xml_converter.py, build initial converter logic

--- brain/converter/dataset/svr_xml_converter.py
+++ brain/converter/dataset/svr_xml_converter.py
@@ -33,3 +33,44 @@ def svr_xml_converter(raw_data):
 
     # convert xml file to python 'dict'
     dataset = xmltodict.parse(raw_data)
+
+    # build 'list_dataset'
+    for observation in dataset['dataset']['observation']:
+        for key in observation:
+            if key == 'criterion':
+                list_observation_label.append(observation[key])
+            elif key == 'predictor':
+                for label, predictor in observation['key']:
+                    feature_label = label
+                    feature_value = value
+
+                    validate_label = Validate_Dataset(feature_label)
+                    validate_value = Validate_Dataset(feature_value)
+
+                    validate_label.validate_label()
+                    validate_value.validate_value()
+
+                    list_error_label = validate.get_errors()
+                    list_error_value = validate.get_errors()
+                    if list_error_label or list_error_value:
+                        logger.log(list_error_label)
+                        logger.log(list_error_value)
+                        return None
+                    else:
+                        list_dataset.append({
+                            'dep_variable_label': observation_label,
+                            'indep_variable_label': feature_label,
+                            'indep_variable_value': feature_value
+                        })
+
+        # generalized feature count in an observation
+        if not feature_count:
+            feature_count = len(observation['predictor'])
+
+    # save observation labels, and return
+    raw_data.close()
+    return {
+        'dataset': list_dataset,
+        'observation_labels': list_observation_label,
+        'feature_count': feature_count
+    }
