89d80df20c819a791b9bcd2764a822ade28b3efc
jeff levesque <jeff1evesque@yahoo.com>
2016-06-21 20:28:26 -0400

    #2606: svr_json_converter.py, correctly define 'feature_count'

--- brain/converter/dataset/svr_json_converter.py
+++ brain/converter/dataset/svr_json_converter.py
@@ -62,7 +62,7 @@ def svr_json_converter(raw_data, is_json):
 
                 # generalized feature count in an observation
                 if not feature_count:
-                    feature_count = len(criterion.items())
+                    feature_count = len(predictors)
 
             # criterion with multiple observation
             if type(predictors) == list:
