7a153fb4e43a1271eb40ac1091e7fb2de8ae8995
jeff levesque <jeff1evesque@yahoo.com>
2016-06-27 21:09:43 -0400

    #2153: start.pp, adjust service for cases

--- puppet/environment/development/modules/webserver/manifests/start.pp
+++ puppet/environment/development/modules/webserver/manifests/start.pp
@@ -3,9 +3,21 @@
 ###       https://github.com/jeff1evesque/machine-learning/issues/2349
 ###
 class webserver::start {
-    ## start webserver
-    service { 'flask':
-        ensure => 'running',
-        enable => true,
+    # variables
+    $vagrant_mounted = hiera('general')['vagrant_implement']
+
+    # run flask
+    if $vagrant_mounted {
+        # ensure service starts at boot
+        service { 'flask':
+            ensure => 'running',
+            enable => true,
+        }
+    }
+    else {
+        # run and restart when needed
+        service { 'flask':
+            ensure => 'running',
+        }
     }
 }
