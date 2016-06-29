0fc8505fbf5c6fbe6ebfb401ed48890d7642a019
jeff levesque <jeff1evesque@yahoo.com>
2016-06-29 06:55:19 -0400

    #2153: install_packages.pp, 'require system::webcompiler_directory'

--- puppet/environment/development/manifests/install_packages.pp
+++ puppet/environment/development/manifests/install_packages.pp
@@ -24,6 +24,7 @@ class general_packages {
     ## set dependency
     require apt
     require install_nodejs
+    require system::webcompiler_directory
 
     ## install packages
     contain package::inotify_tools
