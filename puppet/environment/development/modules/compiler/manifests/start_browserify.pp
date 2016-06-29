f933f39d67f62a725dfaa61a7b501924023a83d5
jeff levesque <jeff1evesque@yahoo.com>
2016-06-28 20:31:15 -0400

    #2153: start_browserify.pp, adjust for 'puppet parser validate'

--- puppet/environment/development/modules/compiler/manifests/start_browserify.pp
+++ puppet/environment/development/modules/compiler/manifests/start_browserify.pp
@@ -4,7 +4,8 @@
 ###
 class compiler::start_browserify {
     # variables
-    $vagrant_mounted = hiera('general')['vagrant_implement']
+    $hiera_general   = hiera('general')
+    $vagrant_mounted = $hiera_general['vagrant_implement']
 
     # run sass
     if $vagrant_mounted {
