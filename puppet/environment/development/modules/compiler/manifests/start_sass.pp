7024c4917c1fb1047bb849afbc2f01886b1c33ad
jeff levesque <jeff1evesque@yahoo.com>
2016-07-01 16:23:34 -0400

    #2153: start_sass.pp, implement 'provider => shell'

--- puppet/environment/development/modules/compiler/manifests/start_sass.pp
+++ puppet/environment/development/modules/compiler/manifests/start_sass.pp
@@ -22,7 +22,7 @@ class compiler::start_sass {
         exec { 'sass':
             command  => "./sass ${root_dir}",
             cwd      => "${dev_env_path}/modules/compiler/scripts",
-            path     => '/usr/bin',
+            provider => shell,
         }
     }
 }
