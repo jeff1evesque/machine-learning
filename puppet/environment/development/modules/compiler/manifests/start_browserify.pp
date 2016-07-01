e13c828f0704223207d05c7ba8f7b2dea1418dbe
jeff levesque <jeff1evesque@yahoo.com>
2016-06-30 23:25:49 -0400

    #2153: start_browserify.pp, define 'root_dir', 'dev_env_path'

--- puppet/environment/development/modules/compiler/manifests/start_browserify.pp
+++ puppet/environment/development/modules/compiler/manifests/start_browserify.pp
@@ -5,7 +5,9 @@
 class compiler::start_browserify {
     # variables
     $hiera_general   = hiera('general')
+    $root_dir        = $hiera_general['root']
     $vagrant_mounted = $hiera_general['vagrant_implement']
+    $dev_env_path    = "${root_dir}/puppet/environment/development"
 
     # run sass
     if $vagrant_mounted {
