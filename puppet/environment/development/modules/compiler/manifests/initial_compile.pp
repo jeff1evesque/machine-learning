e44a6bea57a9314de90c606248e1cd34d912cc5f
jeff levesque <jeff1evesque@yahoo.com>
2016-07-01 21:39:45 -0400

    #2153: initial_compile.pp, conditionally compile source

--- puppet/environment/development/modules/compiler/manifests/initial_compile.pp
+++ puppet/environment/development/modules/compiler/manifests/initial_compile.pp
@@ -16,33 +16,36 @@ class compiler::initial_compile {
         'js'
     ]
 
-    $sources.each |String $source| {
-        ## variables
-        $check_files = "if [ \"$(ls -A ${root_dir}/src/${source}/)\" ];"
-        $touch_files = "then touch ${root_dir}/src/${source}/*; fi"
+    if $vagrant_mounted {
+        $sources.each |String $source| {
+            ## variables
+            $check_files = "if [ \"$(ls -A ${root_dir}/src/${source}/)\" ];"
+            $touch_files = "then touch ${root_dir}/src/${source}/*; fi"
 
-        ## touch source: ensure initial build compiles every source file.
-        #
-        #  @touch, changes the modification time to the current system time.
-        #
-        #  Note: the current inotifywait implementation watches close_write,
-        #        move, and create. However, the source files will already exist
-        #        before this 'inotifywait', since the '/vagrant' directory will
-        #        already have been mounted on the initial build.
-        #
-        #  Note: every 'command' implementation checks if directory is
-        #        nonempty, then touch all files in the directory, respectively.
-        exec { "touch-${source}-files":
-            command  => "${check_files} ${touch_files}",
-            provider => shell,
+            ## touch source: ensure initial build compiles every source file.
+            #
+            #  @touch, changes the modification time to the current system time.
+            #
+            #  Note: the current inotifywait implementation watches close_write,
+            #        move, and create. However, the source files will already exist
+            #        before this 'inotifywait', since the '/vagrant' directory will
+            #        already have been mounted on the initial build.
+            #
+            #  Note: every 'command' implementation checks if directory is
+            #        nonempty, then touch all files in the directory, respectively.
+            exec { "touch-${source}-files":
+                command  => "${check_files} ${touch_files}",
+                provider => shell,
+            }
         }
     }
-
-    # manually compile
-    exec { 'rerun-uglifyjs':
-        command  => "./uglifyjs ${root_dir}",
-        cwd      => "${dev_env_path}/modules/compiler/scripts",
-        path     => '/usr/bin',
-        provider => shell,
+    else {
+        # manually compile
+        exec { 'rerun-uglifyjs':
+            command  => "./uglifyjs ${root_dir}",
+            cwd      => "${dev_env_path}/modules/compiler/scripts",
+            path     => '/usr/bin',
+            provider => shell,
+        }
     }
 }
