c9d7cf18ba52affeaff90cfcce4c5e9bb70fdfdc
jeff levesque <jeff1evesque@yahoo.com>
2016-07-07 21:07:40 -0400

    #2632: setup_database.pp, install mariadb client, and bindings

--- puppet/environment/docker/manifests/setup_database.pp
+++ puppet/environment/docker/manifests/setup_database.pp
@@ -8,5 +8,11 @@
 ## install mariadb
 include database::server
 
+## install mariadb client
+include database::client
+
+## install mariad bindings
+include database::bindings
+
 ## create database tables
 include database::database
