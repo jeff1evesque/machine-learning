d914e700dad76b36e0ac5a7296fa01c348f62370
jeff levesque <jeff1evesque@yahoo.com>
2016-07-14 21:48:03 -0400

    #2642: manager.py, adjust docblocks

--- manager.py
+++ manager.py
@@ -1,11 +1,16 @@
 '''@manager
 
-This file implements 'Manager' instance, which allows external scripts to be
-    run within the corresponding flask script.
+This file implements the 'Manager' instance, which allows external scripts to
+    be run within the corresponding flask script.  Specifically, 'pytest' is
+    used to perform corresponding unit testing.
 
 Note: the 'Manager' instance implements the 'Fask-Script':
 
     - https://flask-script.readthedocs.io/en/latest/
+
+Note: the 'pytest' instance can be further reviewed:
+
+    - http://docs.pytest.org/en/latest/usage.html
 
 '''
 
@@ -15,19 +20,26 @@ from flask.ext.script import Manager
 class Manager(object):
     '''@Manager
 
-    This class provides an interface to 'Manager' instance.
-
-    @app, flask app instance, generated from app factory.
-
     Note: this class explicitly inherits the 'new-style' class.
 
     '''
 
     def __init__(app):
+        '''@__init__
+
+        @app, flask app instance, generated from app factory.
+
+        '''
 
         self.manager = Manager(app)
         self.manager.run(pytest())
 
     @manager.command
     def pytest():
+        '''@pytest
+		
+        This method implements pytest within the 'test/' directory.
+
+        '''
+
         pytest.main(['-x', 'test'])
