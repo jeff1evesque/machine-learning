dcc75daf3ecaedb5ec06ad310969568b0e970703
jeff levesque <jeff1evesque@yahoo.com>
2016-07-18 11:44:14 -0400

    #2642: app.py, restore file

--- app.py
+++ app.py
@@ -2,7 +2,6 @@
 
 This file is the acting web server.
 
-@debug, enables debugging, and tracebacks
 @host, tells the OS (guest VM) to accept connections from all public IP
     addresses.
 
@@ -13,20 +12,10 @@ Note: both the handler, and logger has levels. If the level of the logger is
 '''
 
 import sys
-from manager import Manager
 from factory import create_app
 
 # app factory
 app = create_app()
 
 # implement app factory
-if len(sys.argv) >= 2:
-    # unit test
-    if sys.argv[1] == 'test':
-        manager = Manager(app)
-        manager.pytest()
-    # other case
-    else:
-        app.run(host='0.0.0.0')
-else:
-    app.run(host='0.0.0.0')
+app.run(host='0.0.0.0')
