0e7c2819619092459a0fdd7aad4ed02b3e021354
Jeff Levesque <jeff1evesque@yahoo.com>
2016-08-19 07:40:36 -0400

    #2670: app.py, adjust 'pytest.main' implementation

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ from factory import create_app
 # run unit test
 if len(sys.argv) > 1:
     if sys.argv[1] == 'test':
-        pytest.main(['-x', 'test'])
+        pytest.main(['-x', 'test/live_test'])
     elif sys.argv[1] == 'run':
         args = {
             'prefix': 'test',
