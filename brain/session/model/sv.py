64573a714c1fa69936c3d0c9b357c157feed723e
Jeff Levesque <jeff1evesque@yahoo.com>
2016-08-23 23:19:52 -0400

    #2693: sv.py, add mission colon

--- brain/session/model/sv.py
+++ brain/session/model/sv.py
@@ -93,7 +93,7 @@ def sv_model(model, kernel_type, session_id, feature_request, list_error):
             model_prefix = 'svm'
 
         # case 2: create svr model
-        elif model == list_model_type[1]
+        elif model == list_model_type[1]:
             clf = svm.SVR(kernel=kernel_type)
             model_prefix = 'svr'
 
