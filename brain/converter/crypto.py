0654665f9da82becbb188491c2bddac92e99650f
protojas <protojas@gmail.com>
2016-09-02 14:10:56 -0400

    #2700: crypto.py, added documentation

--- brain/converter/crypto.py
+++ brain/converter/crypto.py
@@ -1,13 +1,23 @@
+'''@crypto
+
+This file contains various cryptography wrappers.
+
+
+'''
+
+
 import os
 import hashlib
 import base64
 
 saltlength = 32
 
+# returns a hash and salt from a password p
 def hashpass(p):
     salt = base64.b64encode(os.urandom(saltlength))
     return hashlib.sha512(salt + p).hexdigest()+"$"+salt
 
+# verifies that a password p hashes to a hash h
 def verifypass(p, h):
     h,s = h.split('$')
     return hashlib.sha512(s + p).hexdigest() == h
