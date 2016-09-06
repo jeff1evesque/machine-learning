75424e40036fd433af79990b65bc85ced0082a14
protojas <protojas@gmail.com>
2016-09-06 08:38:57 -0400

    #2700: crypto.py, fixing formatting issues

--- brain/converter/crypto.py
+++ brain/converter/crypto.py
@@ -12,16 +12,18 @@ import base64
 
 saltlength = 32
 
+
 def hashpass(p):
     '''@hashpass
 
     This method returns a hash and salt from a password p
-    
+
     @salt - a random string of saltlength bytes generated to hash the password
-    
+
     '''
     salt = base64.b64encode(os.urandom(saltlength))
     return hashlib.sha512(salt + p).hexdigest()+"$"+salt
+
 
 def verifypass(p, h):
     '''@verifypass
@@ -33,12 +35,5 @@ def verifypass(p, h):
     @s - salt extracted from the hash+salt
 
     '''
-    h,s = h.split('$')
+    h, s = h.split('$')
     return hashlib.sha512(s + p).hexdigest() == h
-
-#hash1 = hashpass('password')
-#hash2 = hashpass('password')
-
-#print 'hashpass returns different values for same password:', hash1 != hash2
-#print 'hashpass verifies same password correctly for both hashes:', verifypass('password', hash1) and verifypass('password', hash2)
-
