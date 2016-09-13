3ad7a75ac5214294539a96281b8436341e12d4a7
Jeff Levesque <jeff1evesque@yahoo.com>
2016-09-12 20:09:09 -0400

    #5: pytest_svm_session.py, 'jsonfile' last concatenation

--- test/live_server/programmatic_interface/pytest_svm_session.py
+++ test/live_server/programmatic_interface/pytest_svm_session.py
@@ -46,8 +46,8 @@ def get_sample_json(jsonfile, model_type):
                 'json',
                 'programmatic_interface',
                 model_type,
-                jsonfile,
                 'file_upload'
+                jsonfile
             ),
             'r'
         ) as json_file:
