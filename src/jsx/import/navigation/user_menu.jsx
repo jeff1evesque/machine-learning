f1fb8464b5ae0b215d4d5a05cdb20a6e5abfaa4a
Jeff Levesque <jeff1evesque@yahoo.com>
2016-09-18 22:03:39 -0400

    #2725: user_menu.jsx, implement '.mn-2'

--- src/jsx/import/navigation/user_menu.jsx
+++ src/jsx/import/navigation/user_menu.jsx
@@ -9,7 +9,7 @@ var UserMenu = React.createClass({
     render: function() {
         return(
             <nav className='main-navigation'>
-                <a href='/login' className='btn'>Sign in</a>
+                <a href='/login' className='btn mn-2'>Sign in</a>
                 <a href='/register' className='btn btn-primary'>Sign up</a>
             </nav>
         );
