7e99dabe62d584089cb5bbc6ea387d4536549b1d
Jeff Levesque <jeff1evesque@yahoo.com>
2016-09-20 19:48:03 -0400

    #2710: content.jsx, define 'render_login', 'render_register'

--- src/jsx/content.jsx
+++ src/jsx/content.jsx
@@ -8,12 +8,32 @@ import SupportVector from './import/content/support_vector.jsx';
 import UserMenu from './import/navigation/user_menu.jsx';
 
 var Content = React.createClass({
+  // initial 'state properties'
+    getInitialState: function() {
+        return {
+            render_login: false,
+            render_register: false,
+        };
+    },
+  // update 'state properties'
+    setClickType: function(event){
+        var clickType = event.target;
+
+        if (clickType && clickType.login) {
+            this.setState({render_register: false});
+            this.setState({render_login: true});
+        }
+        else if (clickType && clickType.register) {
+            this.setState({render_login: false});
+            this.setState({render_register: true});
+        }
+    },
   // display result
     render: function() {
         return(
             <div className='container-inner'>
                 <div className='menu-container'>
-                    <UserMenu />
+                    <UserMenu onChange={this.setClickType} />
                 </div>
                 <div className='main'>
                     <div className='navBar'></div>
