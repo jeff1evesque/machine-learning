a0989d0418dc7971d97ddd2a84086ea4ada3ffa8
Jeff Levesque <jeff1evesque@yahoo.com>
2016-09-20 20:43:29 -0400

    #2710: register.jsx, add initial file

---
+++ src/jsx/import/content/register.jsx
@@ -0,0 +1,42 @@
+/**
+ * register.jsx: registration form.
+ *
+ * @RegisterForm, must be capitalized in order for reactjs to render it as a
+ *     component. Otherwise, the variable is rendered as a dom node.
+ *
+ * Note: this script implements jsx (reactjs) syntax.
+ */
+
+import Spinner from '../general/spinner.jsx';
+
+var RegisterForm = React.createClass({
+  // call back: used to return spinner
+    getSpinner: function() {
+        if (this.state.display_spinner) {
+            return Spinner;
+        }
+        else {
+            return 'span';
+        }
+    },
+  // triggered when 'state properties' change
+    render: function() {
+        var AjaxSpinner = getSpinner();
+
+        return(
+            <form onSubmit={this.handleSubmit} ref='registerForm'>
+                <fieldset className='fieldset-user-info'>
+                    <input type='text' name='user[login]' placeholder='Pick a username'>
+                    <input type='text' name='user[email]' placeholder='Your email address'>
+                    <input type='text' name='user[password]' placeholder='Create a password'>
+                </fieldset>
+
+                <input type="submit">
+                <AjaxSpinner />
+            </form>
+        );
+    }
+});
+
+// indicate which class can be exported, and instantiated via 'require'
+export default RegisterForm
