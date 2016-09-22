ee6f5e75b0864ff622af6e27db979856bce7cd82
Jeff Levesque <jeff1evesque@yahoo.com>
2016-09-21 23:26:11 -0400

    #2710: login.jsx, prefix 'login.getSpinner()'

--- src/jsx/import/content/login.jsx
+++ src/jsx/import/content/login.jsx
@@ -27,7 +27,7 @@ var LoginForm = React.createClass({
     },
   // triggered when 'state properties' change
     render: function() {
-        var AjaxSpinner = getSpinner();
+        var AjaxSpinner = this.getSpinner();
 
         return(
             <div className='main-full-span'>
