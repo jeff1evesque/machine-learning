4d0b6789fce4c393605586ef0595d0f3230850a3
Jeff Levesque <jeff1evesque@yahoo.com>
2016-09-21 20:44:24 -0400

    #2710: login.jsx, add file

---
+++ src/jsx/import/content/login.jsx
@@ -0,0 +1,51 @@
+/**
+ * login.jsx: login form.
+ *
+ * @LoginForm, must be capitalized in order for reactjs to render it as a
+ *     component. Otherwise, the variable is rendered as a dom node.
+ *
+ * Note: this script implements jsx (reactjs) syntax.
+ */
+
+import Spinner from '../general/spinner.jsx';
+
+var LoginForm = React.createClass({
+  // call back: used to return spinner
+    getSpinner: function() {
+        if (this.state.display_spinner) {
+            return Spinner;
+        }
+        else {
+            return 'span';
+        }
+    },
+  // triggered when 'state properties' change
+    render: function() {
+        var AjaxSpinner = getSpinner();
+
+        return(
+            <div className='main-full-span'>
+                <form onSubmit={this.handleSubmit} ref='registerForm'>
+                    <fieldset className='fieldset-login-form'>
+                        <input
+                            type='text'
+                            name='user[login]'
+                            className='input-block'
+                        >
+                        <input
+                            type='text'
+                            name='user[password]'
+                            className='input-block'
+                        >
+                    </fieldset>
+
+                    <input type="submit" className='input-submit'>
+                    <AjaxSpinner />
+                </form>
+            </div>
+        );
+    }
+});
+
+// indicate which class can be exported, and instantiated via 'require'
+export default LoginForm
