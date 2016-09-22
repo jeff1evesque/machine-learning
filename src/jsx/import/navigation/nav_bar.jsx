0defebbadbb99c71840597941be3b0d6e4782285
Jeff Levesque <jeff1evesque@yahoo.com>
2016-09-21 21:17:56 -0400

    #2710: nav_bar.jsx, add skeleton file

---
+++ src/jsx/import/navigation/nav_bar.jsx
@@ -0,0 +1,17 @@
+/**
+ * navbar.jsx: secondary menu..
+ *
+ * Note: this script implements jsx (reactjs) syntax.
+ */
+
+var NavBar = React.createClass({
+  // display result
+    render: function() {
+        return(
+            <div className='main-sidebar'></div>
+        );
+    }
+});
+
+// indicate which class can be exported, and instantiated via 'require'
+export default NavBar
