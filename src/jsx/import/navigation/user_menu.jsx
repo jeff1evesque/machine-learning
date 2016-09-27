47c983501d8d5b0a777a3fc0ae2c7684a6095fab
Jeff Levesque <jeff1evesque@yahoo.com>
2016-09-27 19:47:21 -0400

    #2743: user_menu.jsx, define and implement callbacks

--- src/jsx/import/navigation/user_menu.jsx
+++ src/jsx/import/navigation/user_menu.jsx
@@ -4,7 +4,24 @@
  * Note: this script implements jsx (reactjs) syntax.
  */
 
+import SvgHome from '../general/spinner.jsx';
+
 var UserMenu = React.createClass({
+  // initial 'state properties'
+    getInitialState: function() {
+        return {
+            home_hover: false,
+        };
+    },
+  // call back: return home svg
+    getSvgHome: function() {
+        if (this.state.svg_home) {
+            return SvgHome;
+        }
+        else {
+            return 'span';
+        }
+    },
   // callback for home page
     clickHome: function(event) {
       // prevent page reload
@@ -29,15 +46,32 @@ var UserMenu = React.createClass({
       // return state to parent component
         this.props.onChange({register: true});
     },
+  // callback for mouseOver home svg
+    mouseOverHome: function(event) {
+        this.setState({home_hover: true});
+    },
+  // callback for mouseOut home svg
+    mouseOutHome: function(event) {
+        this.setState({home_hover: false});
+    },
   // display result
     render: function() {
+        var Home = this.getSvgHome();
+
+        {/* return:
+            @homeHover, is accessible within child component as
+                'this.props.homeHover'
+        */}
         return(
             <nav className='main-navigation'>
                 <a href='#'
                    className='icon home'
                    onClick={this.clickHome}
                 >
-                    <img src='static/img/home.svg' alt='Home' />
+                    <Home onMouseOver={this.mouseOverHome}
+                          onMouseOut={this.mouseOutHome}
+                          hover={this.state.home_hover}
+                    />
                 </a>
                 <a href='#'
                    className='btn mn-2'
