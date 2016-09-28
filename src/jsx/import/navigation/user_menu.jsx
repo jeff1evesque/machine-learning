c6f0dfa60f886cc4a5cf53a0abdf265b02e01e92
Jeff Levesque <jeff1evesque@yahoo.com>
2016-09-27 23:12:56 -0400

    #2743: user_menu.jsx, remove 'mouseover' definition and implementation

--- src/jsx/import/navigation/user_menu.jsx
+++ src/jsx/import/navigation/user_menu.jsx
@@ -7,16 +7,6 @@
 import SvgHome from '../svg/svg_home.jsx';
 
 var UserMenu = React.createClass({
-  // initial 'state properties'
-    getInitialState: function() {
-        return {
-            home_hover: false,
-        };
-    },
-  // call back: return home svg
-    getSvgHome: function() {
-        return SvgHome;
-    },
   // callback for home page
     clickHome: function(event) {
       // prevent page reload
@@ -41,32 +31,15 @@ var UserMenu = React.createClass({
       // return state to parent component
         this.props.onChange({register: true});
     },
-  // callback for mouseOver home svg
-    mouseOverHome: function(event) {
-        this.setState({home_hover: true});
-    },
-  // callback for mouseOut home svg
-    mouseOutHome: function(event) {
-        this.setState({home_hover: false});
-    },
   // display result
     render: function() {
-        var Home = this.getSvgHome();
-
-        {/* return:
-            @homeHover, is accessible within child component as
-                'this.props.hover'
-        */}
         return(
             <nav className='main-navigation'>
                 <a href='#'
                    className='icon home'
                    onClick={this.clickHome}
                 >
-                    <Home onMouseOver={this.mouseOverHome}
-                          onMouseOut={this.mouseOutHome}
-                          hover={this.state.home_hover}
-                    />
+                    <SvgHome />
                 </a>
                 <a href='#'
                    className='btn mn-2'
