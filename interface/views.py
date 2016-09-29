f66549c2cd1dc509f91586ff43c2500892b1bef1
Jeff Levesque <jeff1evesque@yahoo.com>
2016-09-29 15:28:54 -0400

    #2750: views.py, define and implement 'error_result'

--- interface/views.py
+++ interface/views.py
@@ -173,26 +173,26 @@ def retrieve_sv_model():
         svr_list = Cache_Model().get_all_titles('svr_model')
         svm_result = []
         svr_result = []
-        result_error = []
+        error_result = []
 
         # get svm model(s)
         if svm_list['result']:
             svm_result = svm_list['result']
         elif svm_list['error']:
-            result_error.extend(svm_list['error'])
+            error_result.extend(svm_list['error'])
 
         # get svr model(s)
         if svr_list['result']:
             svr_result = svr_list['result']
         elif svr_list['error']:
-            result_error.extend(svr_list['error'])
+            error_result.extend(svr_list['error'])
 
         # return combined model(s)
         combined_result = svm_result + svr_result
         if combined_result:
             return json.dumps(combined_result)
-        elif result_error:
-            return json.dumps({'error': error})
+        elif error_result:
+            return json.dumps({'error': error_result})
 
 
 @blueprint.route(
