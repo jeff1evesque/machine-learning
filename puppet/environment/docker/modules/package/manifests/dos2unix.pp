1bbb4f59e743a4262195adf119bfece64d5bd8e0
Jeff Levesque <jeff1evesque@yahoo.com>
2016-09-30 18:13:26 -0400

    #2746: dos2unix.pp, rename from 'tofrodos.pp' for docker

--- puppet/environment/docker/modules/package/manifests/dos2unix.pp
+++ puppet/environment/docker/modules/package/manifests/dos2unix.pp
@@ -2,12 +2,11 @@
 ###
 ###       https://github.com/jeff1evesque/machine-learning/issues/2349
 ###
-### Note: this installs 'dos2unix'
 ###
-class package::tofrodos {
+class package::dos2unix {
     ## local variables
     $hiera_dev = hiera('development')
-    $version   = $hiera_dev['apt']['tofrodos']
+    $version   = $hiera_dev['apt']['dos2unix']
 
     package { 'tofrodos':
         ensure => $version,
