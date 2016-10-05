786c8c94bc81f62b0ada1f0aef1e3a286c1d436d
Jeff Levesque <jeff1evesque@yahoo.com>
2016-10-04 20:37:16 -0400

    #2713: configure_database.py, remove 'argv', add ';' to each statement

--- test/manual/configure_database.py
+++ test/manual/configure_database.py
@@ -5,7 +5,6 @@ This module will truncate database tables.
 '''
 
 import yaml
-from sys import argv
 import MySQLdb as DB
 
 
@@ -35,21 +34,21 @@ def configure_database():
             cur = conn.cursor()
 
             # truncate 'tbl_feature_count'
-            sql_statement = 'TRUNCATE TABLE tbl_feature_count'
+            sql_statement = 'TRUNCATE TABLE tbl_feature_count;'
             cur.execute(sql_statement)
 
             # create 'tbl_dataset_entity'
-            sql_statement = 'TRUNCATE TABLE tbl_dataset_entity'
+            sql_statement = 'TRUNCATE TABLE tbl_dataset_entity;'
             cur.execute(sql_statement)
 
             # create 'tbl_observation_label'
-            sql_statement = 'TRUNCATE TABLE tbl_observation_label'
+            sql_statement = 'TRUNCATE TABLE tbl_observation_label;'
             cur.execute(sql_statement)
 
             # create 'tbl_svm_data'
-            sql_statement = 'TRUNCATE TABLE tbl_svm_data'
+            sql_statement = 'TRUNCATE TABLE tbl_svm_data;'
             cur.execute(sql_statement)
 
             # create 'tbl_svr_data'
-            sql_statement = 'TRUNCATE TABLE tbl_svr_data'
+            sql_statement = 'TRUNCATE TABLE tbl_svr_data;'
             cur.execute(sql_statement)
