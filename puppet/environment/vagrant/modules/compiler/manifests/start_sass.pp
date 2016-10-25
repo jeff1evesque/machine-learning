99e7c86c6c9f429615cc10a552f3f48c49fe7a60
Jeff Levesque <jeff1evesque@yahoo.com>
2016-10-25 16:05:53 -0400

    #2746: start_sass.pp, ensure sass initially run

--- puppet/environment/vagrant/modules/compiler/manifests/start_sass.pp
+++ puppet/environment/vagrant/modules/compiler/manifests/start_sass.pp
@@ -8,6 +8,19 @@ class compiler::start_sass {
     $root_dir        = $hiera_general['root']
     $environment     = $hiera_general['environment']
     $dev_env_path    = "${root_dir}/puppet/environment/${environment}"
+    $src_dir         = "${root_dir}/src/scss"
+    $asset_dir       = "${root_dir}/interface/static/css"
+    $initial_input   = "${src_dir}/style.scss"
+    $initial_output  = "${asset_dir}/style.min.css"
+    $log_file        = "${root_dir}/log/webcompiler/sass.log"
+    $options         = '--output-style compressed'
+
+    # successive compile
+    exec { 'node-sass-watcher':
+        command  => "node-sass -w ${src_dir} -o ${asset_dir}/ ${options} &",
+        path     => '/usr/bin',
+        provider => shell,
+    }
 
     # ensure service starts at boot
     service { 'sass':
