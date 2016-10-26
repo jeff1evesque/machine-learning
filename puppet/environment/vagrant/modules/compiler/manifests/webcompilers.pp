68c335195d10f8c11b5040c6ed315ccfd69968dc
Jeff Levesque <jeff1evesque@yahoo.com>
2016-10-26 09:01:01 -0400

    #2746: webcompilers.pp, ensure creation of 'sass' service

--- puppet/environment/vagrant/modules/compiler/manifests/webcompilers.pp
+++ puppet/environment/vagrant/modules/compiler/manifests/webcompilers.pp
@@ -16,12 +16,10 @@ class compiler::webcompilers {
     $compiler_dir    = "${environment_dir}/modules/${module}/scripts"
     $template_path   = 'compiler/webcompilers.erb'
 
-    ## sass is compiled via 'node-sass':
-    #
-    #  https://github.com/jeff1evesque/machine-learning/issues/2746
     $compilers = [
         'browserify',
         'imagemin',
+        'sass',
         'uglifyjs'
     ]
 
