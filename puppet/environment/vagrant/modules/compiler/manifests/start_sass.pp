b06d7c2c4c9a17c121ae56e29b6ce65488c52b37
Jeff Levesque <jeff1evesque@yahoo.com>
2016-10-26 09:00:31 -0400

    #2746: start_sass, ensure 'sass' service enabled

--- puppet/environment/vagrant/modules/compiler/manifests/start_sass.pp
+++ puppet/environment/vagrant/modules/compiler/manifests/start_sass.pp
@@ -3,22 +3,8 @@
 ###       https://github.com/jeff1evesque/machine-learning/issues/2349
 ###
 class compiler::start_sass {
-    # variables
-    $hiera_general   = hiera('general')
-    $root_dir        = $hiera_general['root']
-    $environment     = $hiera_general['environment']
-    $dev_env_path    = "${root_dir}/puppet/environment/${environment}"
-    $src_dir         = "${root_dir}/src/scss"
-    $asset_dir       = "${root_dir}/interface/static/css"
-    $initial_input   = "${src_dir}/style.scss"
-    $initial_output  = "${asset_dir}/style.min.css"
-    $log_file        = "${root_dir}/log/webcompiler/sass.log"
-    $options         = '--output-style compressed'
-
-    # successive compile
-    exec { 'node-sass-watcher':
-        command  => "node-sass -w ${src_dir} -o ${asset_dir}/ ${options} &",
-        path     => '/usr/bin',
-        provider => shell,
+    service { 'sass':
+        ensure => 'running',
+        enable => true,
     }
 }
