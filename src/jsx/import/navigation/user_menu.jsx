925b6d7d8c1bcd5bad657652fa95d2bba0737995
Jeff Levesque <jeff1evesque@yahoo.com>
2016-10-30 01:19:15 -0400

    #2781: user_menu.jsx, condiitionally return components

--- src/jsx/import/navigation/user_menu.jsx
+++ src/jsx/import/navigation/user_menu.jsx
@@ -9,15 +9,66 @@ import MenuLogin from 'menu-items/menu_login.jsx';
 import MenuRegister from 'menu-items/menu_register.jsx';
 
 var UserMenu = React.createClass({
+  // update 'state properties' from child component
+    displayHome: function(event) {
+        if (event.home) {
+            this.props.onChange({home: event.home});
+        }
+        else {
+            this.props.onChange({render_submit: false});
+        }
+    },
+    displayLogin: function(event) {
+        if (event.login) {
+            this.props.onChange({login: event.login});
+            this.setState({show_login: false});
+            this.setState({show_register: false});
+        }
+        else {
+            this.props.onChange({render_submit: false});
+            this.setState({show_login: true});
+            this.setState({show_register: true});
+        }
+    },
+    displayRegister: function(event) {
+        if (event.register) {
+            this.props.onChange({home: event.register});
+        }
+        else {
+            this.props.onChange({render_submit: false});
+        }
+    },
   // display result
     render: function() {
         var AjaxSpinner = this.getSpinner();
+        if (this.state.show_login) {
+            var Login = <MenuLogin />
+        }
+        else {
+            var Login = 'span'
+        }
+        if (this.state.show_register) {
+            var Register = <MenuRegister />
+        }
+        else {
+            var Register = 'span'
+        }
 
+        {/* return:
+            @classRegister, is accessible within child component as
+                'this.props.show_register'
+            @classLogin, is accessible within child component as
+                'this.props.show_login'
+        */}
         return(
             <nav className='main-navigation {this.state.home_class}'>
-                <MenuHome />
-                <MenuLogin />
-                <MenuRegister />
+                <MenuHome
+                    onChange={this.displayHome}
+                    classRegister={this.state.show_register}
+                    classLogin={this.state.show_login}
+                />
+                <Login onChange={this.displayLogin} />
+                <Register onChange={this.displayRegister} />
             </nav>
         );
     }
