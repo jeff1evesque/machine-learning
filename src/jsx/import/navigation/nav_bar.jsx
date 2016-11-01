e16299b5edee655e621496c0fbae50072fb09c43
Jeff Levesque <jeff1evesque@yahoo.com>
2016-11-01 19:12:10 -0400

    #2727: nav_bar.jsx, add 'Link' implementation

--- src/jsx/import/navigation/nav_bar.jsx
+++ src/jsx/import/navigation/nav_bar.jsx
@@ -4,11 +4,16 @@
  * Note: this script implements jsx (reactjs) syntax.
  */
 
+import { Router, Route, Link, browserHistory } from 'react-router'
+
 var NavBar = React.createClass({
   // display result
     render: function() {
         return(
-            <div className='main-sidebar'></div>
+            <Link to={`/data_new`}><div className='side-menu menu-item'>Add new data</div></Link>
+            <Link to={`/data_append`}><div className='side-menu menu-item'>Append data</div></Link>
+            <Link to={`/model_generate`}><div className='side-menu menu-item'>Generate model</div></Link>
+            <Link to={`/model_predict`}><div className='side-menu menu-item'>Make prediction</div></Link>
         );
     }
 });
