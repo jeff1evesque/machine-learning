0f1859c3440fda51f59cb68b7463ad6ef8463949
Jeff Levesque <jeff1evesque@yahoo.com>
2016-11-01 23:59:34 -0400

    #2727: model_type.jsx, import 'react'

--- src/jsx/import/model/model_type.jsx
+++ src/jsx/import/model/model_type.jsx
@@ -4,6 +4,7 @@
  * Note: this script implements jsx (reactjs) syntax.
  */
 
+import React from 'react';
 import checkValidString from './../validator/valid_string.js';
 
 var ModelType = React.createClass({
