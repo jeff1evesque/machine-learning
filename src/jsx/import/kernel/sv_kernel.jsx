9647c6c7935272e2b217f29481f62344dfef11b9
Jeff Levesque <jeff1evesque@yahoo.com>
2016-11-01 23:59:05 -0400

    #2727: sv_kernel.jsx, import 'react'

--- src/jsx/import/kernel/sv_kernel.jsx
+++ src/jsx/import/kernel/sv_kernel.jsx
@@ -7,6 +7,7 @@
  * Note: this script implements jsx (reactjs) syntax.
  */
 
+import React from 'react';
 import checkValidString from './../validator/valid_string.js';
 
 var SupportVectorKernels = React.createClass({
