16e4c7b63716eb5f0df80754da0368b3f2206871
Jeff Levesque <jeff1evesque@yahoo.com>
2016-11-08 08:21:43 -0500

    #2727: content.jsx, reduce syntax

--- src/jsx/content.jsx
+++ src/jsx/content.jsx
@@ -8,17 +8,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import NavBar from './import/navigation/nav_bar.jsx';
 import UserMenu from './import/navigation/user_menu.jsx';
-import SupportVector from './import/content/support_vector.jsx';
-import LoginForm from './import/content/login.jsx';
-import RegisterForm from './import/content/register.jsx';
 import AppRouter from './router.jsx';
 
 var Page = React.createClass({
   // initial 'state properties'
     getInitialState: function() {
         return {
-            render_login: false,
-            render_registration: false,
             render_subpage: 'SupportVector',
         };
     },
@@ -34,21 +29,6 @@ var Page = React.createClass({
             this.setState({render_subpage: 'RegisterForm'});
         }
     },
-  // call back: generate main content
-    getContent: function() {
-        if (this.state.render_home) {
-            return SupportVector;
-        }
-        else if (this.state.render_login) {
-            return LoginForm;
-        }
-        else if (this.state.render_registration) {
-            return RegisterForm;
-        }
-        else {
-            return SupportVector;
-        }
-    },
   // call back: return side navigation
     getNavBar: function(type) {
         if (this.state.render_login || this.state.render_registration) {
@@ -61,7 +41,6 @@ var Page = React.createClass({
   // display result
     render: function() {
         var SideBar = this.getNavBar();
-
         return(
             <div className='container-inner'>
                 <div className='menu-container'>
@@ -82,6 +61,6 @@ var Page = React.createClass({
 // @indexRoute, is accessible within child component as 'this.props.indexRoute'
 //
 ReactDOM.render(
-    <AppRouter indexRoute={Page} />,
+    <AppRouter indexRoute={Page} subPage={this.state.render_subPage} />,
     document.querySelector('.container')
 );
