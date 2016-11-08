a410cb2c6b6c29e6ff0440908c3ad611c36267a7
Jeff Levesque <jeff1evesque@yahoo.com>
2016-11-08 08:22:19 -0500

    #2727: router.jsx, conditionally render 'Route's

--- src/jsx/router.jsx
+++ src/jsx/router.jsx
@@ -11,11 +11,27 @@ import DataAppend from './import/session-type/data_append.jsx';
 import ModelGenerate from './import/session-type/model_generate.jsx';
 import ModelPredict from './import/session-type/model_predict.jsx';
 import SupportVector from './import/content/support_vector.jsx';
+import LoginForm from './import/content/login.jsx';
+import RegisterForm from './import/content/register.jsx';
 
 // constant: analysis layout
 const AnalysisLayout = (props) => (
     <div className='analysis-container'>
         <SupportVector routerProp={props.children} />
+    </div>
+);
+
+// constant: login layout
+const LoginLayout = (props) => (
+    <div className='main-full-span login-form'>
+        <LoginForm routerProp={props.children} />
+    </div>
+);
+
+// constant: register layout
+const AnalysisLayout = (props) => (
+    <div className='main-full-span login-form'>
+        <RegisterForm routerProp={props.children} />
     </div>
 );
 
@@ -25,7 +41,7 @@ var AppRouter = React.createClass({
     render: function() {
         {/* return:
             @this.props.indexRoute, defined from parent component.
-            @this.props.renderSubpage, defined from parent component.
+            @this.props.subpage, defined from parent component.
             @history, is required per 'react-router's ability to handle url:
 
                 - [GitHub-URL]/issues/2727#issuecomment-258030214
@@ -34,23 +50,27 @@ var AppRouter = React.createClass({
         return(
             <Router history={browserHistory}>
                 <Route component={this.props.indexRoute} >
-                    <Route path='/' component={AnalysisLayout}>
-                        <Route
-                            path='/session/data-new'
-                            component={DataNew}
-                        />
-                        <Route
-                            path='/session/data-append'
-                            component={DataAppend}
-                        />
-                        <Route
-                            path='/session/model-generate'
-                            component={ModelGenerate}
-                        />
-                        <Route
-                            path='/session/model-predict'
-                            component={ModelPredict}
-                        />
+                    <Route path='/'>
+                        <Route path='/session' component={AnalysisLayout}>
+                            <Route
+                                path='/session/data-new'
+                                component={DataNew}
+                            />
+                            <Route
+                                path='/session/data-append'
+                                component={DataAppend}
+                            />
+                            <Route
+                                path='/session/model-generate'
+                                component={ModelGenerate}
+                            />
+                            <Route
+                                path='/session/model-predict'
+                                component={ModelPredict}
+                            />
+                        </Route>
+                        <Route path='/login' component={LoginLayout} />
+                        <Route path='/register' component={RegisterLayout} />
                     </Route>
                 </Route>
             </Router>
