72180f4c7746f3b5291c24101c2bc86b8fab4fcd
Jeff Levesque <jeff1evesque@yahoo.com>
2016-11-09 11:57:15 -0500

    #2727: menu_register.jsx, remove '<a>' wrapper, remove 'preventDefault'

--- src/jsx/import/navigation/menu-items/menu_register.jsx
+++ src/jsx/import/navigation/menu-items/menu_register.jsx
@@ -12,21 +12,13 @@ import React from 'react';
 var MenuRegister = React.createClass({
   // callback for register page
     clickRegister: function(event) {
-      // prevent page reload
-        event.preventDefault();
-
       // return state to parent component
         this.props.onChange({register: true});
     },
   // triggered when 'state properties' change
     render: function(){
         return(
-            <a href='#'
-                className='btn btn-primary'
-                onClick={this.clickRegister}
-            >
-                Sign up
-            </a>
+            <span onClick={this.clickRegister}>Sign up</span>
         )
     }
 });
