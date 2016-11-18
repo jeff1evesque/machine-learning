1e0d02b68f9645429ba77b737989770ba63d59a7
Jeff Levesque <jeff1evesque@yahoo.com>
2016-11-18 13:52:07 -0500

    #2805: views.py, remove trailing slash in URL

--- interface/views.py
+++ interface/views.py
@@ -61,7 +61,7 @@ def method_not_allowed(e):
     return render_template('index.html'), 405
 
 
-@blueprint.route('/load-data/', methods=['POST'], endpoint='load_data')
+@blueprint.route('/load-data', methods=['POST'], endpoint='load_data')
 def load_data():
     '''@load_data
 
@@ -150,7 +150,7 @@ def load_data():
             return json.dumps(response)
 
 
-@blueprint.route('/register/', methods=['POST'])
+@blueprint.route('/register', methods=['POST'])
 def register():
     '''@register
 
@@ -213,7 +213,7 @@ def register():
             })
 
 
-@blueprint.route('/retrieve-session/', methods=['POST'])
+@blueprint.route('/retrieve-session', methods=['POST'])
 def retrieve_session():
     '''@retrieve_session
 
@@ -233,7 +233,7 @@ def retrieve_session():
 
 
 @blueprint.route(
-    '/retrieve-sv-model/',
+    '/retrieve-sv-model',
     methods=['POST'],
     endpoint='retrieve_sv_model'
 )
@@ -273,7 +273,7 @@ def retrieve_sv_model():
 
 
 @blueprint.route(
-    '/retrieve-sv-features/',
+    '/retrieve-sv-features',
     methods=['POST'],
     endpoint='retrieve_sv_features'
 )
