1884d3909b7677ea928ea550e989d41b987deee5
Jeff Levesque <jeff1evesque@yahoo.com>
2016-11-20 15:03:56 -0500

    #2813: factory.py, change 'blueprint' to 'app'

--- factory.py
+++ factory.py
@@ -105,7 +105,7 @@ def create_app(args={'prefix': '', 'settings': ''}):
 
         # instantiate login manager
         login_manager = LoginManager()
-        login_manager.init_app(blueprint)
+        login_manager.init_app(app)
 
         # return
         return app
