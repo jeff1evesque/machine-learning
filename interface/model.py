1c35a38cef4c8abd0c9b797d96eb8e6637df7064
Jeff Levesque <jeff1evesque@yahoo.com>
2016-11-21 12:49:08 -0500

    #2813: model.py, fix indentation

--- interface/model.py
+++ interface/model.py
@@ -8,6 +8,8 @@ This script provides the required interface by 'flask-login':
 
 '''
 
+from flask_login import AnonymousUserMixin
+
 
 class User(object):
     '''
@@ -16,60 +18,63 @@ class User(object):
 
     '''
 
-    def __init__(self, username):
+    def __init__(self, uid):
         '''
 
         This constructor is responsible for defining class variables.
 
         '''
 
-        self.username = username
+        self.id = uid
 
-        @property
-        def is_authenticated(self):
-            '''
+    @property
+    def is_authenticated(self):
+        '''
 
-            This property should return True if the user is authenticated,
-            i.e. they have provided valid credentials. (Only authenticated
-            users will fulfill the criteria of login_required.)
+        This property should return True if the user is authenticated,
+        i.e. they have provided valid credentials. (Only authenticated
+        users will fulfill the criteria of login_required.)
 
-            '''
+        '''
 
-            return self.authenticated
- 
-        @property
-        def is_active(self):
-            '''
+        return self.authenticated
 
-            This property should return True if this is an active user - in
-            addition to being authenticated, they also have activated their
-            account, not been suspended, or any condition your application has
-            for rejecting an account. Inactive accounts may not log in (without
-            being forced of course).
+    @property
+    def is_active(self):
+        '''
 
-            '''
+        This property should return True if this is an active user - in
+        addition to being authenticated, they also have activated their
+        account, not been suspended, or any condition your application has
+        for rejecting an account. Inactive accounts may not log in (without
+        being forced of course).
 
-            return True
- 
-        @property
-        def is_anonymous(self):
-            '''
+        '''
 
-            This property should return True if this is an anonymous user.
-            (Actual users should return False instead.)
+        return True
 
-            '''
+    @property
+    def is_anonymous(self):
+        '''
 
-            return False
+        This property should return True if this is an anonymous user.
+        (Actual users should return False instead.)
 
-        def get_id(self):
-            '''
+        '''
 
-            This method must return a unicode that uniquely identifies this
-            user, and can be used to load the user from the user_loader
-            callback. Note that this must be a unicode - if the ID is natively
-            an int or some other type, you will need to convert it to unicode.
+        return False
 
-            '''
+    def get_id(self):
+        '''
 
-            return str(self.uid)
+        This method must return a unicode that uniquely identifies this
+        user, and can be used to load the user from the user_loader
+        callback. Note that this must be a unicode - if the ID is natively
+        an int or some other type, you will need to convert it to unicode.
+
+        '''
+
+        try:
+            return unicode(self.id)
+        except AttributeError:
+            raise NotImplementedError("No `id` attribute - override get_id")
