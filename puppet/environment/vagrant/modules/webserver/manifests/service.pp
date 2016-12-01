5a7478bc3aeb99cc6175da938df8e51ed1458101
Jeff Levesque <jeff1evesque@yahoo.com>
2016-11-30 19:17:34 -0500

    #2640: service.pp, adjust 'template_path'

--- puppet/environment/vagrant/modules/webserver/manifests/service.pp
+++ puppet/environment/vagrant/modules/webserver/manifests/service.pp
@@ -7,7 +7,7 @@ class webserver::service {
     $hiera_general     = hiera('general')
     $hiera_development = hiera('development')
     $hiera_webserver   = hiera('webserver')
-    $template_path     = 'webserver/webserver.erb'
+    $template_path     = 'webserver/gunicorn.erb'
 
     $vagrant_mounted = $hiera_general['vagrant_implement']
     $root_dir        = $hiera_general['root']
