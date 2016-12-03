f6b7cc5417caec66f99927ecd7e2b4db93fc524d
Jeff Levesque <jeff1evesque@yahoo.com>
2016-12-03 08:51:56 -0500

    #2832: service.pp, rename gunicorn upstart, reorder variables

--- puppet/environment/vagrant/modules/webserver/manifests/service.pp
+++ puppet/environment/vagrant/modules/webserver/manifests/service.pp
@@ -4,18 +4,17 @@
 ###
 class webserver::service {
     ## variables
-    $hiera_general   = hiera('general')
+    $hiera_general     = hiera('general')
+    $hiera_development = hiera('development')
+    $hiera_webserver   = hiera('webserver')
+    $template_path     = 'webserver/gunicorn.erb'
+
     $vagrant_mounted = $hiera_general['vagrant_implement']
     $root_dir        = $hiera_general['root']
     $user            = $hiera_general['user']
     $group           = $hiera_general['group']
 
-    $hiera_development = hiera('development')
-    $hiera_webserver   = hiera('webserver')
-    $nginx_version     = $hiera_development['apt']['nginx']
-    $flask_log_path    = $hiera_webserver['flask_log_path']
-    $log_path          = "${root_dir}${flask_log_path}"
-    $template_path     = 'webserver/webserver.erb'
+    $nginx = $hiera_webserver['nginx']
 
     ## include webserver dependencies
     include python
@@ -29,7 +28,7 @@ class webserver::service {
 
     ## dos2unix: convert clrf (windows to linux) in case host machine is
     #            windows.
-    file { '/etc/init/flask.conf':
+    file { '/etc/init/start_gunicorn.conf':
         ensure  => file,
         content => dos2unix(template($template_path)),
     }
