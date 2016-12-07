eddc7056343c6b2f629835e9cb28ec48ed74f2fe
Jeff Levesque <jeff1evesque@yahoo.com>
2016-12-06 19:04:06 -0500

    #2807: main.jsx, add file

---
+++ src/jsx/import/main.jsx
@@ -0,0 +1,36 @@
+/**
+ * content.jsx: generate main content.
+ *
+ * Note: this script implements jsx (reactjs) syntax.
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import NavBar from './navigation/nav_bar.jsx';
+import UserMenu from './navigation/user_menu.jsx';
+
+var MainContent = React.createClass({
+  // call back: return side navigation
+    isNavBar: function() {
+        if (this.props.renderNavBar) {
+            return NavBar
+        }
+        else {
+            return 'span';
+        }
+    },
+  // display result
+    render: function() {
+        var SideBar = this.isNavBar();
+
+        return(
+            <div className='main'>
+                <SideBar />
+                {this.props.children}
+            </div>
+        );
+    }
+});
+
+// indicate which class can be exported, and instantiated via 'require'
+export default MainContent
