657ec46199b13cda88e7f1ac3de6bf150f319101
Jeff Levesque <jeff1evesque@yahoo.com>
2016-12-29 18:55:55 -0500

    #2818: login.jsx, add 'mapStateToProps', 'MapDispatchToProps'

--- src/jsx/import/content/login.jsx
+++ src/jsx/import/content/login.jsx
@@ -15,6 +15,20 @@ import React from 'react';
 import { connect } from 'react-redux'
 import Spinner from '../general/spinner.jsx';
 
+const mapStateToProps = (state, ownProps) => {
+  return {
+    active: ownProps.filter === state.visibilityFilter
+  }
+}
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    onClick: () => {
+      dispatch(setVisibilityFilter(ownProps.filter))
+    }
+  }
+}
+
 const LoginState = connect(
   mapStateToProps,
   mapDispatchToProps
