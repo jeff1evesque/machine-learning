b0fbbcea37e8d60f4583580554ee75fd0280ceec
Jeff Levesque <jeff1evesque@yahoo.com>
2016-12-29 22:00:11 -0500

    #2818: login.jsx, 'import LoginForm'

--- src/jsx/import/redux/login.jsx
+++ src/jsx/import/redux/login.jsx
@@ -6,6 +6,7 @@
 
 import React from 'react';
 import { connect } from 'react-redux';
+import LoginForm from '../import/content/login.jsx';
 
 const mapStateToProps = (state, ownProps) => {
   return {
