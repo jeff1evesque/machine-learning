0cea06b2ebb63626bbd76ad5ba677d0757b73252
Jeff Levesque <jeff1evesque@yahoo.com>
2016-12-29 20:15:58 -0500

    #2818: login_redux.jsx, add file

---
+++ src/jsx/import/redux/login_redux.jsx
@@ -0,0 +1,30 @@
+/**
+ * login_redux.jsx: redux store for login, and logout processes.
+ *
+ * Note: this script implements jsx (reactjs) syntax.
+ */
+
+import React from 'react';
+import { connect } from 'react-redux';
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    active: ownProps.filter === state.visibilityFilter
+  }
+}
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    onClick: () => {
+      dispatch(setVisibilityFilter(ownProps.filter))
+    }
+  }
+}
+
+const LoginState = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(LoginForm)
+
+// indicate which class can be exported, and instantiated via 'require'
+export default LoginState
