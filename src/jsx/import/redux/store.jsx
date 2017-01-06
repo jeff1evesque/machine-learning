2514040d3ca957b9e8b0c59ebd99cf9e3d46fb20
Jeff Levesque <jeff1evesque@yahoo.com>
2017-01-06 07:36:14 -0500

    #2818: store.jsx, remove 'saveState'

--- src/jsx/import/redux/store.jsx
+++ src/jsx/import/redux/store.jsx
@@ -8,7 +8,6 @@
 
 import { createStore } from 'redux';
 import login from './reducer/login-reducer.jsx';
-import { saveState } from './load-storage.jsx';
 
 // redux store: entire state tree for the application
 const store = createStore(login);
@@ -17,9 +16,6 @@ const store = createStore(login);
 store.subscribe(() => {
   console.log('store changed', store.getState())
 })
-
-// store username into sessionStorage
-saveState('username', store.getState());
 
 // indicate which class can be exported, and instantiated via 'require'
 export default store
