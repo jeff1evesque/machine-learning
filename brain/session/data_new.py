be4a20e640442e23ad94f351e84926cc58a60ded
Jeff Levesque <jeff1evesque@yahoo.com>
2017-01-10 19:06:16 -0500

    #2863: data_new.py, adjust subclassing

--- brain/session/data_new.py
+++ brain/session/data_new.py
@@ -11,19 +11,18 @@ Note: the term 'dataset' used throughout various comments in this file,
 
 '''
 
-from brain.session.base import Base
 from brain.session.base_data import Base_Data
 from brain.database.save_entity import Save_Entity
 
 
-class Data_New(Base, Base_Data):
+class Data_New(Base_Data):
     '''@Data_New
 
     This class provides a generic constructor interface.
 
     Note: this class is invoked within 'load_data.py'
 
-    Note: inherit base methods from superclass 'Base', 'Base_Data
+    Note: inherit base methods from the superclass 'Base_Data
 
     '''
 
