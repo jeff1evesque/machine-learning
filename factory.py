b21ef5a59644d2d4f85df935e0095442bb838d92
Jeff Levesque <jeff1evesque@yahoo.com>
2017-01-13 21:58:07 -0500

    #2863: factory.py, add 'RedisSessionInterface' implementation

--- factory.py
+++ factory.py
@@ -48,6 +48,9 @@ def create_app(args={'prefix': '', 'settings': ''}):
             static_folder='interface/static',
         )
 
+    # replace default cookie session with server-side redis
+    app.session_interface = RedisSessionInterface()
+
     # secret key: used for maintaining flask sessions
     app.secret_key = settings['application']['security_key']
 
