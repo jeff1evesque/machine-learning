63e0aadd25bb0babea8bf38ebbda66db7208094a
Jeff Levesque <jeff1evesque@yahoo.com>
2017-01-15 08:57:59 -0500

    #2697: app.py, attempt to add coveralls argument

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ from factory import create_app
 # run unit test
 if len(sys.argv) > 1:
     if sys.argv[1] == 'test':
-        pytest.main(['test/live_server'])
+        pytest.main(['--cov', 'test/live_server'])
     elif sys.argv[1] == 'run':
         args = {
             'prefix': 'test',
