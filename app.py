2ecae0c3e58a7732365b69dda9f765d7609edfba
Jeff Levesque <jeff1evesque@yahoo.com>
2017-01-15 22:36:07 -0500

    #2697: app.py, change xml file to '.coverage'

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ if len(sys.argv) > 1:
     if sys.argv[1] == 'test':
         pytest.main([
             '--cov', '.',
-            '--cov-report', 'xml:/var/machine-learning/coverage.xml',
+            '--cov-report', '.coverage',
             'test/live_server'
         ])
     elif sys.argv[1] == 'run':
