70733fde2b426a877eca3548850d20801bd9efd7
Jeff Levesque <jeff1evesque@yahoo.com>
2017-01-16 10:46:01 -0500

    #2697: app.py, remove '--config_file'

--- app.py
+++ app.py
@@ -20,7 +20,6 @@ if len(sys.argv) > 1:
     if sys.argv[1] == 'test':
         pytest.main([
             '--cov', '.',
-            '--config_file', '.coveragerc',
             'test/live_server'
         ])
     elif sys.argv[1] == 'run':
