7f91c17a6f491541a29917053f60ab11f792242d
Jeff Levesque <jeff1evesque@yahoo.com>
2017-01-19 16:57:37 -0500

    #2751: pytest_svr_file_upload.py, implement res.json['result']

--- test/live_server/programmatic_interface/file_upload/pytest_svr_file_upload.py
+++ test/live_server/programmatic_interface/file_upload/pytest_svr_file_upload.py
@@ -145,5 +145,9 @@ def test_model_predict(client, live_server):
         data=get_sample_json('svr-model-predict.json', 'svr')
     )
 
-    print json.loads(res.json)['result']
-    assert res.status_code == 200 and res.json['status'] == 0
+    print res.json['result']
+    assert (
+        res.status_code == 200 and
+        res.json['status'] == 0 and
+        res.json['result']
+    )
