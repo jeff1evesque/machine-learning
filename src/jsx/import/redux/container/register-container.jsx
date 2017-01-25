55cccd6a7278195f4dfd06b89b4ceceff330a1f8
Jeff Levesque <jeff1evesque@yahoo.com>
2017-01-25 13:58:41 -0500

    #2864: register-container.jsx, add file

---
+++ src/jsx/import/redux/container/register-container.jsx
@@ -0,0 +1,29 @@
+/**
+ * register-container.jsx: redux store for login, and logout processes.
+ *
+ * Note: this script implements jsx (reactjs) syntax.
+ *
+ * Note: importing 'named export' (multiple export statements in a module),
+ *       requires the object being imported, to be surrounded by { brackets }.
+ *
+ */
+
+import React from 'react';
+import { connect } from 'react-redux';
+import RegisterForm from '../../content/register.jsx';
+
+// transforms redux state tree to react properties
+const mapStateToProps = (state) => {
+    return {
+        username: state
+    }
+}
+
+// pass selected properties from redux state tree to component
+const MenuLoginState = connect(
+    mapStateToProps
+)(RegisterForm)
+
+// indicate which class can be exported, and instantiated via 'require'
+export default RegisterState
+
