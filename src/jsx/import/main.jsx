40728259d4ce9ef05069f8376660cfa8c0e66fd3
Jeff Levesque <jeff1evesque@yahoo.com>
2017-01-28 23:18:56 -0500

    #2864: main.jsx, import from 'register-container.jsx'

--- src/jsx/import/main.jsx
+++ src/jsx/import/main.jsx
@@ -10,7 +10,7 @@ import NavBar from './navigation/nav-bar.jsx';
 import UserMenu from './navigation/user-menu.jsx';
 import SupportVector from './content/support-vector.jsx';
 import LoginState from './redux/container/login-container.jsx';
-import RegisterState from './redux/container/login-container.jsx';
+import RegisterState from './redux/container/register-container.jsx';
 
 var SvContainer = React.createClass({
   // display result
