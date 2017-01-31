2499dab65a586b786514fdc1d878a9487817c2bf
Jeff Levesque <jeff1evesque@yahoo.com>
2017-01-31 16:34:11 -0500

    #2857: menu-register-container.jsx, add file

---
+++ src/jsx/import/redux/container/menu-register-container.jsx
@@ -0,0 +1,29 @@
+/**
+ * menu-register-container.jsx: redux store for login, and logout processes.
+ *
+ * Note: this script implements jsx (reactjs) syntax.
+ *
+ * Note: importing 'named export' (multiple export statements in a module),
+ *       requires the object being imported, to be surrounded by { brackets }.
+ *
+ */
+
+import React from 'react';
+import { connect } from 'react-redux';
+import MenuRegister from '../../navigation/menu-items/menu-register.jsx';
+
+// transforms redux state tree to react properties
+const mapStateToProps = (state) => {
+    return {
+        username: state
+    }
+}
+
+// pass selected properties from redux state tree to component
+const MenuLoginState = connect(
+    mapStateToProps
+)(MenuRegister)
+
+// indicate which class can be exported, and instantiated via 'require'
+export default MenuLoginState
+
