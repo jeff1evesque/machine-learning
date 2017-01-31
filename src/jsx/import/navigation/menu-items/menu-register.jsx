2f8c178b3cda81a1da56fc23fbf6750a4d28624a
Jeff Levesque <jeff1evesque@yahoo.com>
2017-01-31 16:35:18 -0500

    #2857: menu-register.jsx, conditionally render 'Sign up'

--- src/jsx/import/navigation/menu-items/menu-register.jsx
+++ src/jsx/import/navigation/menu-items/menu-register.jsx
@@ -8,24 +8,56 @@
  */
 
 import React from 'react';
-import { Link } from 'react-router'
+import { Link } from 'react-router';
+import { connect } from 'react-redux';
+import { loadState } from '../../redux/load-storage.jsx';
+import setLoginState from '../../redux/action/login-action.jsx';
 
 var MenuRegister = React.createClass({
   // return state to parent component
     menuClicked: function(event) {
         this.props.onChange({menu_clicked: 'register'});
+        this.props.onChange({show_button: true});
+    },
+    componentDidMount: function() {
+        if (
+            loadState('username') &&
+            String(loadState('username')) != 'anonymous'
+        ) {
+          // update component states
+            this.setState({show_button: false});
+
+          // update redux store
+            var action = setLoginState();
+            this.props.dispatch(action);
+        }
+        else {
+          // update component states
+            this.setState({show_button: true});
+        }
+    },
+  // call back: return side navigation
+    renderContent: function() {
+        if (this.props.show_button) {
+            return <Link
+                       to='/register'
+                       activeClassName='active'
+                       className='btn btn-primary'
+                       onClick={this.menuClicked}
+                   >
+                       <span>Sign up</span>
+                   </Link>
+        }
+        else {
+            return 'span';
+        }
     },
   // triggered when 'state properties' change
     render: function(){
+        var selectedContent = this.renderContent();
+
         return(
-            <Link
-                to='/register'
-                activeClassName='active'
-                className='btn btn-primary'
-                onClick={this.menuClicked}
-            >
-                <span>Sign up</span>
-            </Link>
+            {selectedContent}
         )
     }
 });
