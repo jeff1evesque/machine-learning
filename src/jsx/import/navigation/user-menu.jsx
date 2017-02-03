0cf57912acf0d704d3b127158025e8acdf2c9072
jeff levesque <jeff1evesque@yahoo.com>
2017-02-03 15:03:15 -0500

    #2857: user-menu.jsx, reduce and enhance syntax

--- src/jsx/import/navigation/user-menu.jsx
+++ src/jsx/import/navigation/user-menu.jsx
@@ -10,48 +10,8 @@ import MenuLoginState from '../redux/container/menu-login-container.jsx';
 import MenuRegisterState from '../redux/container/menu-register-container.jsx';
 
 var UserMenu = React.createClass({
-  // initial 'state properties'
-    getInitialState: function() {
-        return {
-            page: 'home',
-        };
-    },
-    componentDidUpdate: function() {
-        if (this.props.componentType == 'Home') {
-            this.setState({page: 'home'});
-        }
-        else if (this.props.componentType == 'LoginLayout') {
-            this.setState({page: 'login'});
-        }
-        else if (this.props.componentType == 'RegisterLayout') {
-            this.setState({page: 'register'});
-        }
-    },
-    componentWillMount: function() {
-        if (this.props.componentType == 'Home') {
-            this.setState({page: 'home'});
-        }
-        else if (this.props.componentType == 'LoginLayout') {
-            this.setState({page: 'login'});
-        }
-        else if (this.props.componentType == 'RegisterLayout') {
-            this.setState({page: 'register'});
-        }
-    },
-  // return state to parent, and current component
     renderContent: function() {
-        if (this.state.page == 'home') {
-            return (
-                <nav
-                    className={'main-navigation menu-' + this.state.page}
-                >
-                    <MenuHome />
-                    <MenuLoginState />
-                    <MenuRegisterState />
-                </nav>
-            );
-        }
-        else if (this.state.page == 'login') {
+        if (this.props.componentType == 'LoginLayout') {
             return (
                 <nav
                     className={'main-navigation menu-' + this.state.page}
@@ -60,7 +20,7 @@ var UserMenu = React.createClass({
                 </nav>
             );
         }
-        else if (this.state.page == 'register') {
+        else if (this.props.componentType == 'RegisterLayout') {
             return (
                 <nav
                     className={'main-navigation menu-' + this.state.page}
