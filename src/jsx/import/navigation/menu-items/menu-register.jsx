352fc996a338b872daaff5857f3932a50373be9b
jeff levesque <jeff1evesque@yahoo.com>
2017-02-15 12:34:46 -0500

    #2899: menu-register.jsx, remove 'setPageState' implementation

--- src/jsx/import/navigation/menu-items/menu-register.jsx
+++ src/jsx/import/navigation/menu-items/menu-register.jsx
@@ -10,7 +10,6 @@
 import React from 'react';
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
-import setPageState from '../../redux/action/page-action.jsx';
 
 var MenuRegister = React.createClass({
   // call back: return register button
@@ -25,7 +24,6 @@ var MenuRegister = React.createClass({
                     to='/register'
                     activeClassName='active'
                     className='btn btn-primary'
-                    onClick={this.menuClicked}
                 >
                    <span>Sign up</span>
                 </Link>
@@ -34,11 +32,6 @@ var MenuRegister = React.createClass({
         else {
             return (<span />);
         }
-    },
-    menuClicked: function(event) {
-      // update redux store
-        var action = setPageState({layout: 'register'});
-        this.props.dispatchPage(action);
     },
   // triggered when 'state properties' change
     render: function(){
