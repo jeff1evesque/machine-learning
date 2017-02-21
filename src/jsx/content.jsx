bfa8335cf43f1d60e7de7aab527259a494ae2268
jeff levesque <jeff1evesque@yahoo.com>
2017-02-20 22:18:39 -0500

    #2902: content.jsx, remove 'babel-polyfill'

--- src/jsx/content.jsx
+++ src/jsx/content.jsx
@@ -13,7 +13,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './router.jsx';
 import store from './import/redux/store.jsx';
-import 'babel-polyfill';
 
 // render application
 //
