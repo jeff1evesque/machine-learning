5027a4822a27d2a6bb1f150992962b2a02707bf8
jeff levesque <jeff1evesque@yahoo.com>
2017-02-22 18:37:22 -0500

    #2897: result-action.jsx, add file

---
+++ src/jsx/import/redux/action/result-action.jsx
@@ -0,0 +1,20 @@
+/**
+ * result-action.jsx: send current prediction result to the redux store.
+ *
+ */
+
+function setResult(action) {
+    return {
+        type: 'DISPLAY-RESULT',
+        result: {
+            type: action.analysis.type,
+            data: {
+                keys: action.analysis.data.keys.split(/\s*,\s*/),
+                values: action.analysis.data.values.split(/\s*,\s*/)
+            }
+        }
+    };
+}
+
+// indicate which class can be exported, and instantiated via 'require'
+export default setResult
