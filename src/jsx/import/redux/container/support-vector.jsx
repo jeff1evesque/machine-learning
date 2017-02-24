a885d073408f0172c7530c57457f2b89b307e96d
jeff levesque <jeff1evesque@yahoo.com>
2017-02-24 13:27:52 -0500

    #2897: support-vector.jsx, define 'mapStateToProps'

--- src/jsx/import/redux/container/support-vector.jsx
+++ src/jsx/import/redux/container/support-vector.jsx
@@ -12,17 +12,42 @@ import React from 'react';
 import { connect } from 'react-redux';
 import SupportVector from '../../content/support-vector.jsx';
 import setResult from '../action/results.jsx';
+import { setGotoResultsButton } from '../action/page.jsx';
+
+// transforms redux state tree to react properties
+const mapStateToProps = (state) => {
+  // validate button
+    if (
+        state &&
+        state.page &&
+        state.page.button &&
+        !!state.page.button.goto_results
+    ) {
+        var display = state.page.button.goto_results;
+    }
+    else {
+        var display = false;
+    }
+
+  // return redux to state
+    return {
+        page: {
+            button: {goto_results: display}
+        }
+    }
+}
 
 // wraps each function of the object to be dispatch callable
 const mapDispatchToProps = (dispatch) => {
     return {
-        dispatchResults: dispatch.bind(setResult)
+        dispatchResults: dispatch.bind(setResult),
+        dispatchGotoResultsButton: dispatch.bind(setGotoResultsButton)
     }
 }
 
 // pass selected properties from redux state tree to component
 const SupportVectorState = connect(
-    null,
+    mapStateToProps,
     mapDispatchToProps
 )(SupportVector)
 
