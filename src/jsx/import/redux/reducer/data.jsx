771e9d8298d9f9a9292c34295f959258733162df
jeff levesque <jeff1evesque@yahoo.com>
2017-02-25 12:03:32 -0500

    #2897: data.jsx, add file

---
+++ src/jsx/import/redux/reducer/data.jsx
@@ -0,0 +1,41 @@
+/**
+ * data.jsx: describe data attributes associated with predictions.
+ *
+ * Note: the triple dots is the 'object spread' syntax:
+ *
+ *       http://redux.js.org/docs/recipes/UsingObjectSpreadOperator.html
+ *
+ * Note: this script implements jsx (reactjs) syntax.
+ *
+ */
+
+import 'core-js/modules/es6.object.assign';
+
+const data = (state='default', action) => {
+    var result_keys = false;
+    var result_values = false;
+
+    if (
+        action &&
+        action.results
+    ) {
+        var result_keys = !!action.results.keys ? action.results.keys : null;
+        var result_values = !!action.results.values ? action.results.values : null;
+    }
+
+    switch(action.type) {
+        case 'DISPLAY-SV-RESULTS':
+            return Object.assign({}, state, {
+                status: 'default',
+                results: {
+                    keys: result_keys,
+                    values: result_values
+                }
+            });
+        default:
+            return state;
+    }
+}
+
+// indicate which class can be exported, and instantiated via 'require'
+export default data
