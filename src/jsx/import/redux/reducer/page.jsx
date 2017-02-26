2bbb3d5b5f9115955fc6614d40806ecb2144d6b8
jeff levesque <jeff1evesque@yahoo.com>
2017-02-26 12:05:17 -0500

    #2897: implement 'spread notation'

--- src/jsx/import/redux/reducer/page.jsx
+++ src/jsx/import/redux/reducer/page.jsx
@@ -9,32 +9,43 @@
  *
  */
 
-import 'core-js/modules/es6.object.assign';
-
 const page = (state='default', action) => {
     var submitButtonAnalysis = false;
     var gotoResults = false;
 
+  // assign elements from action
     switch(action.type) {
         case 'SUBMIT-SV-ANALYSIS':
             if (action && action.button && !!action.button.submit_analysis) {
                 var submitButtonAnalysis = action.button.submit_analysis;
+
+                return {
+                   ...state,
+                   {
+                        status: 'default',
+                        button: {
+                            ...submit_analysis: submitButtonAnalysis
+                        }
+                   }
+                }
             }
         case 'GOTO-RESULTS':
             if (action && action.button && !!action.button.goto_results) {
                 var gotoResults = action.button.goto_results;
+
+                return {
+                   ...state,
+                   {
+                        status: 'default',
+                        button: {
+                            ...submit_analysis: submitButtonAnalysis
+                        }
+                   }
+                }
             }
         default:
             return state;
     }
-
-    return assign({}, state, {
-        status: 'default',
-        button: {
-            submit_analysis: submitButtonAnalysis,
-            goto_results: gotoResults
-        }
-    });
 }
 
 // indicate which class can be exported, and instantiated via 'require'
