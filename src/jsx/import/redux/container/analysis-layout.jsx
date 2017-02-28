102c59a21753d1f36a853374857239662682744d
jeff levesque <jeff1evesque@yahoo.com>
2017-02-28 07:02:41 -0500

    #2897: analysis-layout.jsx, remove 'displaySubmitAnalysis'

--- src/jsx/import/redux/container/analysis-layout.jsx
+++ src/jsx/import/redux/container/analysis-layout.jsx
@@ -14,28 +14,21 @@ import AnalysisLayout from '../../layout/analysis.jsx';
 
 // transforms redux state tree to react properties
 const mapStateToProps = (state) => {
-  // local variables
-    var displaySubmitAnalysis = false;
     var displayGotoResults = false;
 
     if (
         state &&
         state.page &&
-        state.page.button
+        state.page.button &&
+        !!state.page.button.goto_results
     ) {
-        if (!!state.page.button.submit_analysis) {
-            var displaySubmitAnalysis = state.page.button.submit_analysis;
-        }
-        if (!!state.page.button.goto_results) {
             var displayGotoResults = state.page.button.goto_results;
-        }
     }
 
   // return redux to state
     return {
         page: {
             button: {
-                submit_analysis: displaySubmitAnalysis,
                 goto_results: displayGotoResults
             }
         }
