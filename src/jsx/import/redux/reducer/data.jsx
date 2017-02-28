bd08072b31b38282c0e6ef988d66f42887aa6c43
jeff levesque <jeff1evesque@yahoo.com>
2017-02-28 18:19:57 -0500

    #2897: data.jsx, reduce syntax

--- src/jsx/import/redux/reducer/data.jsx
+++ src/jsx/import/redux/reducer/data.jsx
@@ -12,22 +12,11 @@
 import 'core-js/modules/es6.object.assign';
 
 const data = (state='default', action) => {
-    var type = 'default';
-    var data = null;
-
-    if (
-        action &&
-        action.results
-    ) {
-        var result_type = !!action.results.type ? action.results.type : 'default';
-        var result_data = !!action.results.data ? action.results.data : null;
-    }
-
     switch(action.type) {
         case 'SET-RESULTS':
             return Object.assign({}, state, {
-                type: result_type,
-                data: result_data
+                type: !!action.results.type ? action.results.type : 'default',
+                data: !!action.results.data ? action.results.data : null
             });
         default:
             return state;
