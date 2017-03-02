2a2e685fcd29d7dc55e38d6ccbab1ea124847e98
jeff levesque <jeff1evesque@yahoo.com>
2017-03-01 20:36:08 -0500

    #2821: generalize and implement 'Submit' button to save

--- src/jsx/import/general/submit-button.jsx
+++ src/jsx/import/general/submit-button.jsx
@@ -1,5 +1,5 @@
 /**
- * submit-analysis.jsx: append 'submit-analysi' button.
+ * submit-button.jsx: append dynamic submit button.
  *
  * @Submit, must be capitalized in order for reactjs to render it as a
  *     component. Otherwise, the variable is rendered as a dom node.
@@ -9,19 +9,21 @@
 
 import React from 'react';
 
-var SubmitAnalysis = React.createClass({
+var Submit = React.createClass({
   // triggered when 'state properties' change
     render: function(){
-        var clickCallback = !!this.props.onClick ? this.props.onClick : null;
+        const buttonValue = !!this.props.btnValue ? this.props.btnValue : null;
+        const clickCallback = !!this.props.onClick ? this.props.onClick : null;
         return(
             <input
                 type='submit'
                 className='form-submit'
                 onClick={clickCallback}
+                value={buttonValue}
             />
         );
     }
 });
 
 // indicate which class can be exported, and instantiated via 'require'
-export default SubmitAnalysis
+export default Submit
