c77963255e3ffc96196f894301dd35d9195f7625
jeff levesque <jeff1evesque@yahoo.com>
2017-03-04 13:33:20 -0500

    #2924: init.pp, remove conditional 'shard2' logic

--- puppet/environment/vagrant/modules/mongodb_cluster/manifests/init.pp
+++ puppet/environment/vagrant/modules/mongodb_cluster/manifests/init.pp
@@ -2,14 +2,8 @@
 ### Configures mongodb cluster
 ###
 
-## local variables: conditionally load hiera
-$hiera_mongodb = lookup('mongodb_cluster')
-$shard2        = $hiera_mongodb['shard']['shard1']['implement']
-
 ## implement mongodb cluster
 contain mongodb_cluster::dependencies
 contain mongodb_cluster::shard1
-if ($shard2) {
-    contain mongodb_cluster::shard2
-}
+contain mongodb_cluster::shard2
 contain mongodb_cluster::databases
