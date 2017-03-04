512b0bf2d8b90e5b3c644b80d9c1aef0dfe87fb1
jeff levesque <jeff1evesque@yahoo.com>
2017-03-04 13:10:01 -0500

    #2924: init.pp, conditionally 'contain' classes

--- puppet/environment/vagrant/modules/mongodb_cluster/manifests/init.pp
+++ puppet/environment/vagrant/modules/mongodb_cluster/manifests/init.pp
@@ -1,9 +1,15 @@
 ###
 ### Configures mongodb cluster
 ###
-class mongodb_cluster {
-    contain mongodb_cluster::dependencies
-    contain mongodb_cluster::shard1
+
+## local variables: conditionally load hiera
+$hiera_mongodb = lookup('mongodb_cluster')
+$shard2        = $hiera_mongodb['shard']['shard1']['implement']
+
+## implement mongodb cluster
+contain mongodb_cluster::dependencies
+contain mongodb_cluster::shard1
+if ($shard2) {
     contain mongodb_cluster::shard2
-    contain mongodb_cluster::databases
 }
+contain mongodb_cluster::databases
