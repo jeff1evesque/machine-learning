7ad4853d91e01c1805091f8ce33dc9b96aa90364
jeff levesque <jeff1evesque@yahoo.com>
2017-03-08 11:19:52 -0500

    #2932: nodejs.pp, ensure nodejs repo '4.x'

--- puppet/environment/docker/modules/package/manifests/nodejs.pp
+++ puppet/environment/docker/modules/package/manifests/nodejs.pp
@@ -6,7 +6,7 @@ class package::nodejs {
     ## install nodejs, with npm
     class { 'nodejs':
         manage_package_repo => true,
-        repo_url_suffix => '5.x',
+        repo_url_suffix => '4.x',
         nodejs_package_ensure => 'latest'
     }
     contain nodejs
