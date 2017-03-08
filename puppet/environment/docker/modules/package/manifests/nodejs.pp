ca291b35870cae9ab8f3e1719866103c68e7c4d1
jeff levesque <jeff1evesque@yahoo.com>
2017-03-08 08:14:07 -0500

    #2932: nodejs.pp, restrict nodejs version

--- puppet/environment/docker/modules/package/manifests/nodejs.pp
+++ puppet/environment/docker/modules/package/manifests/nodejs.pp
@@ -6,8 +6,8 @@ class package::nodejs {
     ## install nodejs, with npm
     class { 'nodejs':
         manage_package_repo => true,
-        repo_url_suffix => '4.x',
-        nodejs_package_ensure => 'latest'
+        repo_url_suffix => '5.x',
+        nodejs_package_ensure => '5.0.0-3nodesource1~trusty1'
     }
     contain nodejs
 }
