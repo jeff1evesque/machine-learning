1e3f2521ad5b5322680c9feea5018759e34e2afb
jeff levesque <jeff1evesque@yahoo.com>
2017-03-09 20:44:33 -0500

    #2924: site.pp, remove nonexistent inclusion

--- puppet/environment/mongodb_cluster/manifests/site.pp
+++ puppet/environment/mongodb_cluster/manifests/site.pp
@@ -2,12 +2,4 @@
 ### site.pp: load all necessary modules.
 ###
 
-contain package
-contain sklearn
-contain vagrant
-contain redis
-contain system
-contain compiler
-contain mariadb
 contain mongodb_cluster
-contain webserver
