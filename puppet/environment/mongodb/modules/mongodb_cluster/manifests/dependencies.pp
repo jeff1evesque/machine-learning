7c175f195cf636d93a369d69f9a6f1e108e69461
jeff levesque <jeff1evesque@yahoo.com>
2017-03-11 00:20:26 -0500

    #2924: dependencies.pp, replace 'host' -> 'fqdn'

--- puppet/environment/mongodb/modules/mongodb_cluster/manifests/dependencies.pp
+++ puppet/environment/mongodb/modules/mongodb_cluster/manifests/dependencies.pp
@@ -5,7 +5,7 @@ class mongodb_cluster::dependencies {
     ## local variables
     $hiera_database = lookup('database')
     $mongodb_node   = $hiera_database['mongodb_cluster']['node']
-    $mongodb_host   = $mongodb_node['host']
+    $mongodb_host   = $mongodb_node['fqdn']
     $mongodb_port   = $mongodb_node['port']
     $mongodb_10gen  = $mongodb_node['manage_package_repo']
 
