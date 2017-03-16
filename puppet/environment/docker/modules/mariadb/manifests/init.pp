aa91ffb2256c5848ff8ee6a94c94d1df15e29b6b
jeff levesque <jeff1evesque@yahoo.com>
2017-03-15 23:33:06 -0400

    #2924: init.pp, reduce syntax for linting

--- puppet/environment/docker/modules/mariadb/manifests/init.pp
+++ puppet/environment/docker/modules/mariadb/manifests/init.pp
@@ -2,17 +2,14 @@
 ### init.pp: install client, and initialize database tables.
 ###
 
-class mariadb {
-    ## install mariadb
-    contain mariadb::server
+## install mariadb
+contain mariadb::server
 
-    ## install mariadb client
-    contain mariadb::client
+## install mariadb client
+contain mariadb::client
 
-    ## install mariad bindings
-    contain mariadb::bindings
+## install mariad bindings
+contain mariadb::bindings
 
-    ## create database tables
-    contain mariadb::database
-}
-contain mariadb
+## create database tables
+contain mariadb::database
