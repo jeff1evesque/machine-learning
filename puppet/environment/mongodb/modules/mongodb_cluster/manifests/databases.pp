2fb587ad73bea99716d23fa6a4e4a45555ba22b9
jeff levesque <jeff1evesque@yahoo.com>
2017-03-24 18:18:31 -0400

    #2943: databases.pp, rename from 'configure_databases.pp'

--- puppet/environment/mongodb/modules/mongodb_cluster/manifests/databases.pp
+++ puppet/environment/mongodb/modules/mongodb_cluster/manifests/databases.pp
@@ -2,13 +2,20 @@
 ### Configures mongodb databases.
 ###
 
-class mongodb_cluster::configure_databases {
+class mongodb_cluster::databases {
     ## local variables
     $hiera_database = lookup('database')
     $hiera_mongodb  = $hiera_database['mongodb_cluster']
     $hiera_user     = $hiera_mongodb['user']
     $admin_user     = $hiera_user['admin']['name']
     $admin_password = $hiera_user['admin']['password']
+
+    file { '/data/db':
+        ensure => 'directory',
+        owner  => root,
+        group  => root,
+        mode   => '0600',
+    }
 
     mongodb::db { 'svm_dataset':
         user          => $admin_user,
