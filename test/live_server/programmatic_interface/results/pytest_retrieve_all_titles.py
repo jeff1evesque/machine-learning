5563cbf0a64663debd26aad61b0c72e529925e1f
jeffrey levesque <jeff1evesque@yahoo.com>
2017-04-23 23:58:49 -0400

    #2922: pytest_retrieve_all_titles.py, add file

---
+++ test/live_server/programmatic_interface/results/pytest_retrieve_all_titles.py
@@ -0,0 +1,73 @@
+'''
+
+This file will test the following cases:
+  - retrieving all prediction titles, with respect to a supplied userid.
+
+Note: the 'pytest' instances can further be reviewed:
+
+    - https://pytest-flask.readthedocs.io/en/latest
+    - http://docs.pytest.org/en/latest/usage.html
+
+'''
+
+import json
+import os.path
+from flask import url_for
+from flask import current_app
+
+
+def get_sample_json(jsonfile, model_type):
+    '''
+
+    Get a sample json dataset.
+
+    '''
+
+    # local variables
+    root = current_app.config.get('ROOT')
+
+    # open file
+    json_dataset = None
+
+    with open(
+        os.path.join(
+            root,
+            'interface',
+            'static',
+            'data',
+            'json',
+            'programmatic_interface',
+            model_type,
+            'results',
+            jsonfile
+        ),
+        'r'
+    ) as json_file:
+        json_dataset = json.load(json_file)
+
+    return json.dumps(json_dataset)
+
+
+def test_retrieve_titles(client, live_server):
+    '''
+
+    This method retrieves all prediction titles, with respect to an
+    identified userid (i.e. uid).
+
+    '''
+
+    @live_server.app.route('/retrieve-prediction-titles')
+    def get_endpoint():
+        return url_for('name.load_data', _external=True)
+
+    live_server.start()
+
+    res = client.post(
+        get_endpoint(),
+        headers={'Content-Type': 'application/json'},
+        data=get_sample_json('retrieve-titles.json', 'combined')
+    )
+
+    # assertion checks
+    assert res.status_code == 200
+    assert res.json['status'] == 0
