004be4277d49d0d0f02fb092e8cc6a3dab41fdc9
jeffrey levesque <jeff1evesque@yahoo.com>
2017-05-07 18:41:57 -0400

    #2837: service.pp, define 'ssl_key' attribute

--- puppet/environment/docker/modules/webserver/manifests/service.pp
+++ puppet/environment/docker/modules/webserver/manifests/service.pp
@@ -12,6 +12,7 @@ class webserver::service {
     $template_path     = 'webserver/gunicorn.erb'
 
     $ssl_cert_path   = '/etc/puppetlabs/puppet/ssl/certs/'
+    $ssl_pkey_path   = '/etc/puppetlabs/puppet/ssl/private_keys/'
     $vagrant_mounted = $hiera_general['vagrant_implement']
     $root_dir        = $hiera_general['root']
     $user            = $hiera_general['user']
@@ -41,7 +42,8 @@ class webserver::service {
     ## nginx: define reverse proxy
     nginx::resource::vhost { $nginx_reverse_proxy['vhost']:
         ssl         => true,
-        ssl_cert    => "${ssl_cert_path}/${nginx_reverse_proxy}['vhost']",
+        ssl_cert    => "${ssl_cert_path}/${nginx_reverse_proxy}['vhost'].pem",
+        ssl_key     => "${ssl_pkey_path}/${nginx_reverse_proxy}['vhost'].pem",
         listen_port => $nginx_reverse_proxy['listen_port'],
         proxy       => $nginx_proxy,
     }
