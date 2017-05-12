b88dc2e92790ee3bcb841acedb63a803fac05b08
jeffrey levesque <jeff1evesque@yahoo.com>
2017-05-12 17:21:57 -0400

    #2844: setup_tables.py, remove svm, and svr tables

--- puppet/environment/vagrant/modules/mariadb/scripts/setup_tables.py
+++ puppet/environment/vagrant/modules/mariadb/scripts/setup_tables.py
@@ -11,16 +11,6 @@ This file initializes the following database tables within the
         @tbl_feature_count, record the number of features expected within an
             observation, with respect to a given 'id_entity'.
 
-        @tbl_svm_data, records a tuple of observation-feature values
-        @tbl_svr_data, records a tuple of criterion-predictor values
-
-            - observation label: synonymous to dependent variable label, and
-                can be 'NULL' if the 'criterion value' is defined
-            - criterion value: can be 'NULL' if the 'observation label' is
-                defined.
-            - feature label / value: synonymous to independent variable label,
-                or predictor label.
-
         @model_type, reference table containing all possible model types.
 
         @tbl_observation_label, record every unique observation label, with
@@ -138,32 +128,6 @@ with conn:
                         id_entity INT NOT NULL,
                         dep_variable_label VARCHAR(75) NOT NULL,
                         INDEX (id_label)
-                    );
-                    '''
-    cur.execute(sql_statement)
-
-    # create 'tbl_svm_data'
-    sql_statement = '''\
-                    CREATE TABLE IF NOT EXISTS tbl_svm_data (
-                        id_value INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                        id_entity INT NOT NULL,
-                        dep_variable_label VARCHAR (50) NOT NULL,
-                        indep_variable_label VARCHAR (50) NOT NULL,
-                        indep_variable_value DECIMAL (65,12) NOT NULL,
-                        INDEX (id_value)
-                    );
-                    '''
-    cur.execute(sql_statement)
-
-    # create 'tbl_svr_data'
-    sql_statement = '''\
-                    CREATE TABLE IF NOT EXISTS tbl_svr_data (
-                        id_value INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                        id_entity INT NOT NULL,
-                        criterion VARCHAR (50) NOT NULL,
-                        indep_variable_label VARCHAR (50) NOT NULL,
-                        indep_variable_value DECIMAL (65,12) NOT NULL,
-                        INDEX (id_value)
                     );
                     '''
     cur.execute(sql_statement)
