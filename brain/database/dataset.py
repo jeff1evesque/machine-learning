5b683987f1c66f0dfcb3dd7350af22187eab59e1
jeffrey levesque <jeff1evesque@yahoo.com>
2017-05-16 20:26:15 -0400

    #2844: dataset.py, generalize with 'query' method

--- brain/database/dataset.py
+++ brain/database/dataset.py
@@ -36,21 +36,21 @@ class Collection(object):
         self.nosql = NoSQL()
         self.db_ml = current_app.config.get('DB_ML')
 
-    def save_collection(self, operation, payload=None):
+    def query(self, operation, payload=None):
         '''
 
-        This method can store, or update an existing SVM dataset stored in
-        corresponding database tables (using EAV data model).
+        This method executes a query, with respect to the desired 'operation'.
 
         @operation, the type of operation:
             - insert_one
             - insert_many
             - update_one
             - update_many
-            - delete_one
-            - delete_many
             - find
             - find_one
+            - map_reduce
+            - delete_one
+            - delete_many
             - drop_collection
 
         '''
@@ -68,64 +68,6 @@ class Collection(object):
             return {'status': False, 'error': response_error}
         else:
             return {'status': True, 'error': None, 'id': response['id']}
-
-    def get_dataset(self, id_entity, model):
-        '''
-
-        This method retrieves a correspondinng dataset, from corresponding
-        'EAV data model' database table(s), using a fixed 'id_entity'.
-
-        @id_entity, this supplied argument corresponds to the 'id_entity'
-            column from the 'tbl_dataset_value' database table.
-
-        @sql_statement, is a sql format string, and not a python string.
-            Therefore, '%s' is used for argument substitution.
-
-        @model, is the model type (i.e. svm, svr)
-
-        '''
-
-        # local variables
-        list_model_type = current_app.config.get('MODEL_TYPE')
-
-        # establish connection
-        self.sql.connect(self.db_ml)
-
-        # case 1: svm data
-        if model == list_model_type[0]:
-            sql_statement = 'SELECT dep_variable_label, '\
-                'indep_variable_label, indep_variable_value '\
-                'FROM tbl_svm_data '\
-                'WHERE id_entity=%s'
-
-        # case 2: svr data
-        elif model == list_model_type[1]:
-            sql_statement = 'SELECT criterion, indep_variable_label, '\
-                'indep_variable_value '\
-                'FROM tbl_svr_data '\
-                'WHERE id_entity=%s'
-
-        # get dataset
-        args = (id_entity)
-        response = self.sql.execute('select', sql_statement, args)
-
-        # retrieve any error(s), disconnect from database
-        response_error = self.sql.get_errors()
-        self.sql.disconnect()
-
-        # return result
-        if response_error:
-            return {
-                'status': False,
-                'error': response_error,
-                'result': None
-            }
-        else:
-            return {
-                'status': True,
-                'error': None,
-                'result': response['result'],
-            }
 
     def get_count(self, id_entity):
         '''
