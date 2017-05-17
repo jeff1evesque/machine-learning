d9bb058c92d73a890897a69b81f48b68afa0e75a
jeffrey levesque <jeff1evesque@yahoo.com>
2017-05-16 20:09:02 -0400

    #2844: dataset.py, rename from 'feature.py'

--- brain/database/dataset.py
+++ brain/database/dataset.py
@@ -2,22 +2,21 @@
 
 '''
 
-This file saves the feature count, as well as the feature instances within
-corresponding database tables.
+This file saves the provided dataset(s) into a series of nosql documents,
+using the mongodb framework.
 
 '''
 
 from flask import current_app
-from brain.database.query import SQL
+from brain.database.query import NoSQL
 
 
-class Feature(object):
+class Collection(object):
     '''
 
-    This class provides an interface to retrieve, and store the expected
-    number of features that can be expected in a given dataset, and each
-    feature instance into corresponding database tables (using EAV data
-    model).
+    This class provides an interface to retrieve, and store the supplied
+    dataset(s), into a mongodb collection. Additional methods, are also
+    provided, to query varying parameters of the specified collection.
 
     Note: this class is invoked within 'base_data.py'
 
@@ -25,7 +24,7 @@ class Feature(object):
 
     '''
 
-    def __init__(self, premodel_data=None):
+    def __init__(self, premodel_data):
         '''
 
         This constructor is responsible for defining class variables.
@@ -34,46 +33,35 @@ class Feature(object):
 
         self.premodel_data = premodel_data
         self.list_error = []
-        self.sql = SQL()
+        self.nosql = NoSQL()
         self.db_ml = current_app.config.get('DB_ML')
-        self.classification = current_app.config.get('MODEL_TYPE')[0]
-        self.regression = current_app.config.get('MODEL_TYPE')[1]
 
-    def save_feature(self, model_type):
+    def save_collection(self, operation, payload=None):
         '''
 
         This method can store, or update an existing SVM dataset stored in
         corresponding database tables (using EAV data model).
 
-        @sql_statement, is a sql format string, and not a python string.
-            Therefore, '%s' is used for argument substitution.
-
-        Note: 'UTC_TIMESTAMP' returns the universal UTC datetime
+        @operation, the type of operation:
+            - insert_one
+            - insert_many
+            - update_one
+            - update_many
+            - delete_one
+            - delete_many
+            - find
+            - find_one
+            - drop_collection
 
         '''
 
         # insert / update dataset value(s)
-        self.sql.connect(self.db_ml)
-        if model_type == self.classification:
-            sql_statement = 'INSERT INTO tbl_svm_data (id_entity, '\
-                'dep_variable_label, indep_variable_label, '\
-                'indep_variable_value) VALUES(%s, %s, %s, %s)'
-        elif model_type == self.regression:
-            sql_statement = 'INSERT INTO tbl_svr_data (id_entity, '\
-                'criterion, indep_variable_label, indep_variable_value) '\
-                'VALUES(%s, %s, %s, %s)'
-        dataset = self.premodel_data['premodel_dataset']
-        args = (
-            self.premodel_data['id_entity'],
-            dataset['dep_variable_label'],
-            dataset['indep_variable_label'],
-            dataset['indep_variable_value'],
-        )
-        response = self.sql.execute('insert', sql_statement, args)
+        self.nosql.connect(self.db_ml)
+        response = self.nosql.execute(operation, payload)
 
         # retrieve any error(s), disconnect from database
-        response_error = self.sql.get_errors()
-        self.sql.disconnect()
+        response_error = self.nosql.get_errors()
+        self.nosql.disconnect()
 
         # return result
         if response_error:
