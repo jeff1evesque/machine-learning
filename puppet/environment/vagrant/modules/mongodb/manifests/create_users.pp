cae4f2afc0616fc6b0f10e7bcb346966c26f7174
jeffrey levesque <jeff1evesque@yahoo.com>
2017-05-25 21:11:36 -0400

    #2844: create_users.pp, define shell 'provider'

--- puppet/environment/vagrant/modules/mongodb/manifests/create_users.pp
+++ puppet/environment/vagrant/modules/mongodb/manifests/create_users.pp
@@ -3,13 +3,18 @@
 ###
 class mongodb::create_users {
     ## local variables
-    $database   = lookup('database')['mongodb']
-    $username   = $database['username']
-    $password   = $database['password']
+    $database    = lookup('database')['mongodb']
+    $username    = $database['username']
+    $password    = $database['password']
 
+    ## create users
+    ##
+    ## @provider, shell allows shebang, and subshells to be executed
+    ##
     exec { 'create-mongodb-users':
-        command => dos2unix(template('mongodb/create-users.erb')),
-        onlyif  => dos2unix(template('mongodb/check-user.erb')),
-        path    => '/usr/bin',
+        command  => dos2unix(template('mongodb/create-users.erb')),
+        onlyif   => dos2unix(template('mongodb/check-user.erb')),
+        path     => '/usr/bin',
+        provider => shell,
     }
 }
