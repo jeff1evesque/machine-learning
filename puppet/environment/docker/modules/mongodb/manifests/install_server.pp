2c43b72043603f9f95fa686f9c6fbe8c85739d63
jeffrey levesque <jeff1evesque@yahoo.com>
2017-06-02 23:00:48 -0400

    #2844: install_serber.pp, define 'require' conditions

--- puppet/environment/docker/modules/mongodb/manifests/install_server.pp
+++ puppet/environment/docker/modules/mongodb/manifests/install_server.pp
@@ -2,7 +2,14 @@
 ### Install mongodb server.
 ###
 class mongodb::install_server {
+    include mongodb::download
+
     package { 'mongodb-org-server':
         ensure  => installed,
+        require => [
+            Exec['apt-key-puppetlabs'],
+            File['mongodb-list-file'],
+            Exec['apt-get-update'],
+        ],
     }
 }
