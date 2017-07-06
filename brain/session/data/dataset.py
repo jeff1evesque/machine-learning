5b6986fd0d148e9b99faf7d644191523a04c16f5
jeffrey levesque <jeff1evesque@yahoo.com>
2017-07-06 14:11:51 -0400

    #2844: reduce syntax, remove 'magic' implementation

--- brain/session/data/dataset.py
+++ brain/session/data/dataset.py
@@ -8,7 +8,6 @@ Note: the term 'dataset' used throughout various comments in this file,
 
 '''
 
-import magic
 from flask import current_app
 from brain.converter.dataset import Dataset
 
@@ -26,70 +25,36 @@ def dataset2dict(id_entity, model_type, upload):
     # local variables
     list_error = []
     converted = []
-    payload = None
     datasets = upload['datasets']
     settings = upload['properties']
-    dataset_type = settings.get('dataset_type', None)
-    list_model_type = current_app.config.get('MODEL_TYPE')
-    f = magic.Magic(mime=True, uncompress=True)
 
     try:
-        if dataset_type == 'file_upload':
+        # programmatic-interface
+        if settings['stream']:
+            converter = Dataset(datasets, model_type)
+            converted.append(converter.json_to_dict())
+
+        # web-interface
+        else:
             for val in datasets:
                 # reset file-pointer
-                val.seek(0)
-
-                # initialize converter
-                converter = Dataset(val, model_type)
+                val['file'].seek(0)
 
                 # convert dataset(s)
-                if f.from_file(val) == 'text/plain':
-                    try:
-                        converted.append(converter.json_to_dict())
-                    except Exception:
-                        pass
+                converter = Dataset(val['file'], model_type)
+                if val['type'] == 'csv':
+                    converted.append(converter.csv_to_dict())
+                elif val['type'] == 'json':
+                    converted = converter.json_to_dict()
+                elif val['type'] == 'xml':
+                    converted = converter.xml_to_dict()
 
-                    try:
-                        converted.append(converter.csv_to_dict())
-                    except Exception:
-                        pass
-
-                elif f.from_file(val) == 'application/xml':
-                    converted.append(converter.xml_to_dict())
-
-            # build new (relevant) dataset
-            payload = {
-                'id_entity': id_entity,
-                'premodel_dataset': converted,
-                'settings': settings
-            }
-
-        elif dataset_type == 'dataset_url':
-            # classification
-            if settings['model_type'] == list_model_type[0]:
-                # conversion
-                converter = Dataset(dataset, model_type)
-                converted.append(converter.json_to_dict())
-
-                # build new (relevant) dataset
-                payload = {
-                    'id_entity': id_entity,
-                    'premodel_dataset': converted,
-                    'settings': settings
-                }
-
-            # regression
-            elif settings['model_type'] == list_model_type[1]:
-                # conversion
-                converter = Dataset(json_upload, model_type)
-                converted.append(converter.json_to_dict())
-
-                # build new (relevant) dataset
-                payload = {
-                    'id_entity': id_entity,
-                    'premodel_dataset': converted,
-                    'settings': settings
-                }
+        # build new (relevant) dataset
+        payload = {
+            'id_entity': id_entity,
+            'premodel_dataset': converted,
+            'settings': settings
+        }
 
     except Exception as error:
         list_error.append(error)
