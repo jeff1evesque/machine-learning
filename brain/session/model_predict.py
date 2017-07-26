6848e3b94d7096b5708760e4323faf877d46a382
jeff levesque <jeff1evesque@yahoo.com>
2017-07-26 15:33:11 -0400

    #2844: model_predict.py, 'model_id' to 'collection'

--- brain/session/model_predict.py
+++ brain/session/model_predict.py
@@ -51,7 +51,7 @@ class ModelPredict(Base):
         super(ModelPredict, self).__init__(prediction_input)
         self.prediction_input = prediction_input
         self.prediction_settings = self.prediction_input['properties']
-        self.model_id = self.prediction_settings['model_id']
+        self.model_id = self.prediction_settings['collection']
         self.predictors = self.prediction_settings['prediction_input[]']
 
     def predict(self):
