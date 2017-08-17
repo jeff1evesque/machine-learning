dc774ac2d8969c28b458d31f95f42c2c1145928d
jeff levesque <jeff1evesque@yahoo.com>
2017-08-16 23:59:47 -0400

    #2878: entity.py, define 'get_collections'

--- brain/database/entity.py
+++ brain/database/entity.py
@@ -118,11 +118,40 @@ class Entity(object):
         else:
             return {'error': None, 'result': response['result']}
 
+    def get_collections(self, uid):
+        '''
+
+        This method is responsible for retrieving all collections, for a
+        specified user, in ascending order, for the 'datetime_created' column.
+
+        @sql_statement, is a sql format string, and not a python string.
+            Therefore, '%s' is used for argument substitution.
+
+        '''
+
+        # select entity
+        self.sql.connect(self.db_ml)
+        sql_statement = 'SELECT collection '\
+            'FROM tbl_dataset_entity '\
+            'WHERE uid_created=%s '\
+            'ORDER BY datetime_created'
+        args = (uid)
+        response = self.sql.execute('select', sql_statement, args)
+
+        # retrieve any error(s)
+        response_error = self.sql.get_errors()
+
+        # return result
+        if response_error:
+            return {'error': response_error, 'result': None}
+        else:
+            return {'error': None, 'result': response['result']}
+
     def get_collection_count(self, uid):
         '''
 
-        This method is responsible for retrieving the total entity count, for
-        specified user.
+        This method is responsible for retrieving the total collection count,
+        for a specified user.
 
         @sql_statement, is a sql format string, and not a python string.
             Therefore, '%s' is used for argument substitution.
