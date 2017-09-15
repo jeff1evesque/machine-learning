5cf0c9272da3333f52c2729371ca034fca983bdc
jeff levesque <jeff1evesque@yahoo.com>
2017-09-15 08:32:34 -0400

    #2923: router.jsx, 'import PageLayoutState'

--- src/jsx/router.jsx
+++ src/jsx/router.jsx
@@ -7,7 +7,7 @@
 import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 import createBrowserHistory from 'history/createBrowserHistory';
-import PageLayout from './import/redux/container/page.jsx';
+import PageLayoutState from './import/redux/container/page.jsx';
 import HomePage from './import/content/home-page.jsx';
 
 const history = createBrowserHistory();
