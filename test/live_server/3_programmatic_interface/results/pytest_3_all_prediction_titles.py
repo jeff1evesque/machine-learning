858638f65da83dec77e8cf60be2989424009e342
jeff levesque <jeff1evesque@yahoo.com>
2017-10-02 22:23:45 -0400

    #3027: fix 'IF' conditional check

--- test/live_server/3_programmatic_interface/results/pytest_3_all_prediction_titles.py
+++ test/live_server/3_programmatic_interface/results/pytest_3_all_prediction_titles.py
@@ -87,8 +87,8 @@ def test_retrieve_titles(client, live_server):
         datetime.datetime.strptime(date_svr, '%Y-%m-%d %H:%M:%S')
 
         if (
-            ['svm-prediction-1', date_svm] == res.json['titles'][0] and
-            ['svr-prediction-1', date_svr] == res.json['titles'][1]
+            [1, 'svm-prediction-1', date_svm] == res.json['titles'][0] and
+            [2, 'svr-prediction-1', date_svr] == res.json['titles'][1]
         ):
             assert True
         else:
