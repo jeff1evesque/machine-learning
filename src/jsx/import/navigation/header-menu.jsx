4c67029188cd7778d5c405d485a50c99e2ea5fa2
jeff levesque <jeff1evesque@yahoo.com>
2017-10-07 09:49:38 -0400

    #2724: implement bootstrap responsive menu with redux

---
+++ src/jsx/import/navigation/header-menu.jsx
@@ -0,0 +1,62 @@
+/**
+ * header-menu.jsx: menu for logged-in, and anonymous users.
+ *
+ * Note: this script implements jsx (reactjs) syntax.
+ */
+
+import React from 'react';
+import HomeLink from './menu-items/home.jsx';
+import LoginLinkState from '../redux/container/login-link.jsx';
+import RegisterLinkState from '../redux/container/register-link.jsx';
+
+var HeaderMenu = React.createClass({
+    renderContent: function() {
+        if (
+            !!this.props &&
+            !!this.props.layout &&
+            !!this.props.layout.type &&
+            this.props.layout.type == 'login'
+        ) {
+            return (
+                <nav
+                    className={'main-navigation menu-login'}
+                >
+                    <HomeLink />
+                </nav>
+            );
+        } else if (
+            !!this.props &&
+            !!this.props.layout &&
+            !!this.props.layout.type &&
+            this.props.layout.type == 'register'
+        ) {
+            return (
+                <nav
+                    className={'main-navigation menu-register'}
+                >
+                    <HomeLink />
+                    <LoginLinkState />
+                    <RegisterLinkState />
+                </nav>
+            );
+        } else {
+            return (
+                <nav
+                    className={'main-navigation menu-home'}
+                >
+                    <HomeLink />
+                    <LoginLinkState />
+                    <RegisterLinkState />
+                </nav>
+            );
+        }
+    },
+  // display result
+    render: function() {
+        var selectedContent = this.renderContent();
+        return(selectedContent);
+    }
+});
+
+// indicate which class can be exported, and instantiated via 'require'
+export default HeaderMenu
