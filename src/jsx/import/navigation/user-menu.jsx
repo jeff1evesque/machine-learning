48b4b86a7f1b53492369dc1efb0e6c87209f9d01
jeff levesque <jeff1evesque@yahoo.com>
2017-10-07 18:53:35 -0400

    #2724: user-menu.jsx, 'import NavDropdown'

--- src/jsx/import/navigation/user-menu.jsx
+++ src/jsx/import/navigation/user-menu.jsx
@@ -5,7 +5,7 @@
  */
 
 import React from 'react';
-import { Navbar, Nav, NavItem, MenuItem } from 'react-bootstrap';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 import LinkContainer from 'react-router-bootstrap';
 
 var UserMenu = React.createClass({
