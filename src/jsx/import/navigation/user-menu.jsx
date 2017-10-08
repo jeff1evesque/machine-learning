14aff8626a3a69ba412e4373ae97e7148719ac25
jeff levesque <jeff1evesque@yahoo.com>
2017-10-07 21:49:08 -0400

    #2724: user-menu.jsx, 'import react-dom'

--- src/jsx/import/navigation/user-menu.jsx
+++ src/jsx/import/navigation/user-menu.jsx
@@ -5,6 +5,7 @@
  */
 
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 import LinkContainer from 'react-router-bootstrap';
 
