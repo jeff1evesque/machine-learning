ba418622338aa3391add8ce4f6bd88e5f888091e
jeff levesque <jeff1evesque@yahoo.com>
2017-10-13 09:00:21 -0400

    #3045: user-menu.jsx, update redux correctly

--- src/jsx/import/navigation/user-menu.jsx
+++ src/jsx/import/navigation/user-menu.jsx
@@ -53,6 +53,10 @@ class UserMenu extends Component {
                     this.setState({ajax_done_error: asynchObject.error});
                 }
                 else if (asynchObject) {
+                  // update redux store
+                    var action = setLogoutState();
+                    this.props.dispatchLogout(action);
+
                     if (
                         this.props &&
                         this.props.location &&
@@ -60,10 +64,6 @@ class UserMenu extends Component {
                         this.props.location.pathname != '/' &&
                         asynchObject.status == '0'
                     ) {
-                      // update redux store
-                        var action = setLogoutState();
-                        this.props.dispatchLogout(action);
-
                       // redirect
                         this.props.history.push('/');
                     }
