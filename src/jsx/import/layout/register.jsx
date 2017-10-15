1d9d4f7670362300a1187327e9e8203bdc78d5c7
Vitao18 <victoronline9@gmail.com>
2017-10-15 03:24:43 -0200

    #3047: layout/register.jsx, stateless functional component

--- src/jsx/import/layout/register.jsx
+++ src/jsx/import/layout/register.jsx
@@ -6,19 +6,16 @@
  */
 
 import React from 'react';
-import ReactDOM from 'react-dom';
 import RegisterState from '../redux/container/register.jsx';
 
-var RegisterLayout = React.createClass({
-    render: function() {
-        return(
-            <div className='main-full-span register-form'>
-                <h1>Create your account</h1>
-                <RegisterState />
-            </div>
-        );
-    }
-});
+const RegisterLayout = () =>
+  (
+    <div className="main-full-span register-form">
+      <h1>Create your account</h1>
+      <RegisterState />
+    </div>
+  );
+
 
 // indicate which class can be exported, and instantiated via 'require'
-export default RegisterLayout
+export default RegisterLayout;
