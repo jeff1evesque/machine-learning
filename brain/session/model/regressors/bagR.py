7f443cb648d97ce907fc7d54bda25c6b6f2ee551
jeff levesque <jeff1evesque@yahoo.com>
2017-10-15 12:40:22 -0400

    #3041: bagR.py, fix linting, add docstring

--- brain/session/model/regressors/bagR.py
+++ brain/session/model/regressors/bagR.py
@@ -1,18 +1,30 @@
-import sys
-sys.path.insert(0, 'brain/session/model')
-from baggen import baggen
+from brain.session.model.baggen import baggen
 
-def bagRgenerate(model,
-                collection,
-                payload,
-                list_error,
-                feat=1.0,
-                samp=1.0):
-    return baggen(model,
-                  True,
-                  collection,
-                  payload,
-                  list_error,
-                  feat,
-                  samp,
-                  None)
+def bagRgenerate(
+    model,
+    collection,
+    payload,
+    list_error,
+    feat=1.0,
+    samp=1.0
+):
+    '''
+
+    Generate an ensemble bagger.
+
+    @model, indicates the type of bagging implementation:
+
+        - bagc, classifier bagger
+        - bagr, regressor bagger
+
+    '''
+
+    return baggen(
+        model,
+        collection,
+        payload,
+        list_error,
+        feat,
+        samp,
+        None
+    )
