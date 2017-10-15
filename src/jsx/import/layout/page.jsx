2355844afba117274b077066a5fdfc55c34d9834
Vitao18 <victoronline9@gmail.com>
2017-10-15 03:04:47 -0200

    #3047: page.jsx, changed to es6 class syntax

--- src/jsx/import/layout/page.jsx
+++ src/jsx/import/layout/page.jsx
@@ -9,8 +9,6 @@
  */
 
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 import { Route } from 'react-router-dom';
 import LoginLayout from './login.jsx';
 import RegisterLayout from './register.jsx';
@@ -20,49 +18,47 @@ import UserMenuState from '../redux/container/user-menu.jsx';
 import HeaderMenuState from '../redux/container/header-menu.jsx';
 import AnalysisLayoutState from '../redux/container/analysis-layout.jsx';
 
-var PageLayout = React.createClass({
+class PageLayout extends React.Component {
   // callback: used to return spinner
-    getSpinner: function() {
-        if (this.props && this.props.effects && this.props.effects.spinner) {
-            return <Spinner />;
-        } else {
-            return null;
-        }
-    },
-    render: function() {
-      // local variables
-        var spinner = this.getSpinner();
+  getSpinner() {
+    if (this.props && this.props.effects && this.props.effects.spinner) {
+      return <Spinner />;
+    }
+    return null;
+  }
+  render() {
+    // local variables
+    const spinner = this.getSpinner();
 
-      // validate username
-        if (
-            this.props &&
+    // validate username
+    if (
+      this.props &&
             this.props.user &&
             !!this.props.user.name &&
-            this.props.user.name != 'anonymous'
-        ) {
-            var mainMenu = <UserMenuState />;
-        }
-        else {
-            var mainMenu = <div className='container'><HeaderMenuState /></div>;
-        }
-
-        return(
-            <div>
-                <div className='menu-container'>
-                    {mainMenu}
-                </div>
-                <div className='main'>
-                    <Route exact path='/login' component={LoginLayout} />
-                    <Route exact path='/logout' component={LoginLayout} />
-                    <Route exact path='/register' component={RegisterLayout} />
-                    <Route path='/session' component={AnalysisLayoutState} />
-                </div>
-                <Route exact path='/' component={HomePageState} />
-                {spinner}
-            </div>
-        );
+            this.props.user.name !== 'anonymous'
+    ) {
+      var mainMenu = <UserMenuState />;
+    } else {
+      var mainMenu = <div className="container"><HeaderMenuState /></div>;
     }
-});
+
+    return (
+      <div>
+        <div className="menu-container">
+          {mainMenu}
+        </div>
+        <div className="main">
+          <Route exact path="/login" component={LoginLayout} />
+          <Route exact path="/logout" component={LoginLayout} />
+          <Route exact path="/register" component={RegisterLayout} />
+          <Route path="/session" component={AnalysisLayoutState} />
+        </div>
+        <Route exact path="/" component={HomePageState} />
+        {spinner}
+      </div>
+    );
+  }
+}
 
 // indicate which class can be exported, and instantiated via 'require'
-export default PageLayout
+export default PageLayout;
