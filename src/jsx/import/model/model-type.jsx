e8a3a54df01d1dbd0bd4e00a0a3071796f0745ed
Vitao18 <victoronline9@gmail.com>
2017-10-15 03:28:07 -0200

    #3047: model-type.jsx, changed to es6 class syntax

--- src/jsx/import/model/model-type.jsx
+++ src/jsx/import/model/model-type.jsx
@@ -7,41 +7,41 @@
 import React from 'react';
 import checkValidString from './../validator/valid-string.js';
 
-var ModelType = React.createClass({
+class ModelType extends React.Component {
   // initial 'state properties'
-    getInitialState: function() {
-        return {
-            value_model_type: '--Select--'
-        };
-    },
+  getInitialState() {
+    return {
+      value_model_type: '--Select--',
+    };
+  }
   // update 'state properties': pass property to parent component
-    changeModelType: function(event){
-        if (checkValidString(event.target.value)) {
-            this.setState({value_model_type: event.target.value});
-            this.props.onChange({value_model_type: event.target.value});
-        } else {
-            this.setState({value_model_type: '--Select--'});
-        }
-    },
-  // triggered when 'state properties' change
-    render: function(){
-      // display result
-        return(
-            <select
-                name='model_type'
-                autoComplete='off'
-                onChange={this.changeModelType}
-                value={this.state.value_model_type}
-            >
-
-                <option value='' defaultValue>--Select--</option>
-                <option value='svm'>Classification</option>
-                <option value='svr'>Regression</option>
-
-            </select>
-        );
+  changeModelType(event) {
+    if (checkValidString(event.target.value)) {
+      this.setState({ value_model_type: event.target.value });
+      this.props.onChange({ value_model_type: event.target.value });
+    } else {
+      this.setState({ value_model_type: '--Select--' });
     }
-});
+  }
+  // triggered when 'state properties' change
+  render() {
+    // display result
+    return (
+      <select
+        name="model_type"
+        autoComplete="off"
+        onChange={this.changeModelType}
+        value={this.state.value_model_type}
+      >
+
+        <option value="" defaultValue>--Select--</option>
+        <option value="svm">Classification</option>
+        <option value="svr">Regression</option>
+
+      </select>
+    );
+  }
+}
 
 // indicate which class can be exported, and instantiated via 'require'
-export default ModelType
+export default ModelType;
