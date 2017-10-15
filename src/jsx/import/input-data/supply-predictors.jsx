1a1e101e99a8c1c0a36dbd09093887e31ba59ac1
Vitao18 <victoronline9@gmail.com>
2017-10-15 01:54:06 -0200

    #3047: supply-predictors.jsx, changed to es6 class syntax

--- src/jsx/import/input-data/supply-predictors.jsx
+++ src/jsx/import/input-data/supply-predictors.jsx
@@ -11,117 +11,116 @@ import React from 'react';
 import checkValidFloat from './../validator/valid-float.js';
 import ajaxCaller from '../general/ajax-caller.js';
 
-var SupplyPredictors = React.createClass({
+class SupplyPredictors extends React.Component {
   // initial 'state properties'
-    getInitialState: function() {
-        return {
-            ajax_done_options: null,
-            ajax_done_error: null,
-            ajax_fail_error: null,
-            ajax_fail_status: null
-        };
-    },
-  // update 'state properties': allow parent component(s) to access properties
-    validIntegerEntered: function(event){
-        {/* get array of input elements, by classname */}
-        var predictors = document.getElementsByClassName('predictionInput');
-
-        {/*
-            Iterate the node list containing the supplied predictors(s). If
-            input value is a valid float, store 'true', within the array.
-        */}
-        var boolArray = Array.prototype.map.call(
-            predictors,
-            function(element) {
-                if (element.value && checkValidFloat(element.value)) {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        );
-
-        {/* check if every element is 'true' */}
-        var datasetFlag = boolArray.every(function(element) {
-            return element == true;
-        });
-
-        if (datasetFlag) {
-            this.props.onChange({submitted_proper_predictor: true});
-        } else {
-            this.props.onChange({submitted_proper_predictor: false});
-        }
-    },
-  // triggered when 'state properties' change
-    render: function(){
-        var options = JSON.parse(this.state.ajax_done_options);
-
-        return(
-            <fieldset className='fieldset-prediction-input'>
-                <legend>Prediction Input</legend>
-
-                {/* array components require unique 'key' value */}
-                {options && options.map(function(value, index){
-                    var suffix = index.toString();
-                    var predictor = this.state['value_predictor_' + suffix];
-
-                    return <input
-                        type='text'
-                        name='prediction_input[]'
-                        className='predictionInput'
-                        placeholder={value}
-                        key={index}
-                        onChange={this.validIntegerEntered}
-                        defaultValue=''
-                    />;
-                }.bind(this))}
-
-            </fieldset>
-        );
-    },
+  getInitialState() {
+    return {
+      ajax_done_options: null,
+      ajax_done_error: null,
+      ajax_fail_error: null,
+      ajax_fail_status: null,
+    };
+  }
   // call back: get session id(s) from server side, and append to form
-    componentDidMount: function () {
-      // variables
-        this.mounted = true;
+  componentDidMount() {
+    // variables
+    this.mounted = true;
 
-      // ajax arguments
-        var ajaxEndpoint = '/retrieve-sv-features';
-        var ajaxData = {'selected-collection': this.props.selectedCollection};
-        var ajaxArguments = {
-            'endpoint': ajaxEndpoint,
-            'data': JSON.stringify(ajaxData),
-            'contentType': 'application/json'
-        };
+    // ajax arguments
+    const ajaxEndpoint = '/retrieve-sv-features';
+    const ajaxData = { 'selected-collection': this.props.selectedCollection };
+    const ajaxArguments = {
+      endpoint: ajaxEndpoint,
+      data: JSON.stringify(ajaxData),
+      contentType: 'application/json',
+    };
 
-      // asynchronous callback: ajax 'done' promise
-        if (this.mounted) {
-            ajaxCaller(function (asynchObject) {
-            // Append to DOM
-                if (asynchObject && asynchObject.error) {
-                    this.setState({ajax_done_error: asynchObject.error});
-                } else if (asynchObject) {
-                    this.setState({ajax_done_options: asynchObject});
-                }
-            }.bind(this),
-            // asynchronous callback: ajax 'fail' promise
-            function (asynchStatus, asynchError) {
-                if (asynchStatus) {
-                    this.setState({ajax_fail_status: asynchStatus});
-                    console.log('Error Status: ' + asynchStatus);
-                }
-                if (asynchError) {
-                    this.setState({ajax_fail_error: asynchError});
-                    console.log('Error Thrown: ' + asynchError);
-                }
-            }.bind(this),
-            // pass ajax arguments
-            ajaxArguments);
-        }
-    },
-    componentWillUnmount() {
-        this.mounted = false;
+    // asynchronous callback: ajax 'done' promise
+    if (this.mounted) {
+      ajaxCaller(
+        (asynchObject) => {
+          // Append to DOM
+          if (asynchObject && asynchObject.error) {
+            this.setState({ ajax_done_error: asynchObject.error });
+          } else if (asynchObject) {
+            this.setState({ ajax_done_options: asynchObject });
+          }
+        },
+        // asynchronous callback: ajax 'fail' promise
+        (asynchStatus, asynchError) => {
+          if (asynchStatus) {
+            this.setState({ ajax_fail_status: asynchStatus });
+            console.log(`Error Status: ${asynchStatus}`);
+          }
+          if (asynchError) {
+            this.setState({ ajax_fail_error: asynchError });
+            console.log(`Error Thrown: ${asynchError}`);
+          }
+        },
+        // pass ajax arguments
+        ajaxArguments,
+      );
     }
-});
+  }
+  componentWillUnmount() {
+    this.mounted = false;
+  }
+  // update 'state properties': allow parent component(s) to access properties
+  validIntegerEntered() {
+    // get array of input elements, by classname
+    const predictors = document.getElementsByClassName('predictionInput');
+
+    /*
+      Iterate the node list containing the supplied predictors(s). If
+      input value is a valid float, store 'true', within the array.
+    */
+    const boolArray = Array.prototype.map.call(
+      predictors,
+      (element) => {
+        if (element.value && checkValidFloat(element.value)) {
+          return true;
+        }
+        return false;
+      },
+    );
+
+    // check if every element is 'true'
+    const datasetFlag = boolArray.every(element => element === true);
+
+    if (datasetFlag) {
+      this.props.onChange({ submitted_proper_predictor: true });
+    } else {
+      this.props.onChange({ submitted_proper_predictor: false });
+    }
+  }
+  // triggered when 'state properties' change
+  render() {
+    const options = JSON.parse(this.state.ajax_done_options);
+
+    return (
+      <fieldset className="fieldset-prediction-input">
+        <legend>Prediction Input</legend>
+
+        {/* array components require unique 'key' value */}
+        {options && options.map((value, index) => {
+                    const suffix = index.toString();
+                    const predictor = this.state[`value_predictor_${suffix}`];
+
+                    return (<input
+                      type="text"
+                      name="prediction_input[]"
+                      className="predictionInput"
+                      placeholder={value}
+                      key={index}
+                      onChange={this.validIntegerEntered}
+                      defaultValue=""
+                    />);
+                })}
+
+      </fieldset>
+    );
+  }
+}
 
 // indicate which class can be exported, and instantiated via 'require'
-export default SupplyPredictors
+export default SupplyPredictors;
