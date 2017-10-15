f506d9c503332a2d7e18b82ae699e85248eaeced
Vitao18 <victoronline9@gmail.com>
2017-10-15 03:30:51 -0200

    #3047: header.jsx, changed to es6 class syntax

--- src/jsx/import/navigation/header-menu.jsx
+++ src/jsx/import/navigation/header-menu.jsx
@@ -9,54 +9,53 @@ import HomeLink from './menu-items/home.jsx';
 import LoginLinkState from '../redux/container/login-link.jsx';
 import RegisterLinkState from '../redux/container/register-link.jsx';
 
-var HeaderMenu = React.createClass({
-    renderContent: function() {
-        if (
-            !!this.props &&
+class HeaderMenu extends React.Component {
+  renderContent() {
+    if (
+      !!this.props &&
             !!this.props.layout &&
             !!this.props.layout.type &&
-            this.props.layout.type == 'login'
-        ) {
-            return (
-                <nav
-                    className={'main-navigation menu-login'}
-                >
-                    <HomeLink />
-                </nav>
-            );
-        } else if (
-            !!this.props &&
+            this.props.layout.type === 'login'
+    ) {
+      return (
+        <nav
+          className="main-navigation menu-login"
+        >
+          <HomeLink />
+        </nav>
+      );
+    } else if (
+      !!this.props &&
             !!this.props.layout &&
             !!this.props.layout.type &&
-            this.props.layout.type == 'register'
-        ) {
-            return (
-                <nav
-                    className={'main-navigation menu-register'}
-                >
-                    <HomeLink />
-                    <LoginLinkState />
-                    <RegisterLinkState />
-                </nav>
-            );
-        } else {
-            return (
-                <nav
-                    className={'main-navigation menu-home'}
-                >
-                    <HomeLink />
-                    <LoginLinkState />
-                    <RegisterLinkState />
-                </nav>
-            );
-        }
-    },
-  // display result
-    render: function() {
-        var selectedContent = this.renderContent();
-        return(selectedContent);
+            this.props.layout.type === 'register'
+    ) {
+      return (
+        <nav
+          className="main-navigation menu-register"
+        >
+          <HomeLink />
+          <LoginLinkState />
+          <RegisterLinkState />
+        </nav>
+      );
     }
-});
+    return (
+      <nav
+        className="main-navigation menu-home"
+      >
+        <HomeLink />
+        <LoginLinkState />
+        <RegisterLinkState />
+      </nav>
+    );
+  }
+  // display result
+  render() {
+    const selectedContent = this.renderContent();
+    return (selectedContent);
+  }
+}
 
 // indicate which class can be exported, and instantiated via 'require'
-export default HeaderMenu
+export default HeaderMenu;
