fbfc7cd440478d4c27400cf00a3d63a953e169ba
Vitao18 <victoronline9@gmail.com>
2017-10-15 01:46:25 -0200

    #3047: supply-dataset-url.jsx, changed to es6 class syntax

--- src/jsx/import/input-data/supply-dataset-url.jsx
+++ src/jsx/import/input-data/supply-dataset-url.jsx
@@ -10,120 +10,114 @@
 import React from 'react';
 import checkValidUrl from './../validator/valid-url.js';
 
-var SupplyDatasetUrl = React.createClass({
+class SupplyDatasetUrl extends React.Component {
   // initial 'state properties'
-    getInitialState: function() {
-        return {
-            additional_elements: []
-        };
-    },
+  getInitialState() {
+    return {
+      additional_elements: [],
+    };
+  }
   // update 'state properties': index for additional input elements
-    handleAddMore: function(event){
-        var elements = this.state.additional_elements;
-        elements.push(true);
-        this.setState({additional_elements: elements});
-        this.props.onChange({submitted_proper_predictor: false});
-    },
-    handleRemove: function(event){
-        var elements = this.state.additional_elements;
-        var datasetBoolean = true;
+  handleAddMore() {
+    const elements = this.state.additional_elements;
+    elements.push(true);
+    this.setState({ additional_elements: elements });
+    this.props.onChange({ submitted_proper_predictor: false });
+  }
+  handleRemove() {
+    const elements = this.state.additional_elements;
+    let datasetBoolean = true;
 
-        if (elements.length > 0) {
-            elements.pop();
-            this.setState({additional_elements: elements});
+    if (elements.length > 0) {
+      elements.pop();
+      this.setState({ additional_elements: elements });
 
-            {/* define boolean to indicate all urls properly defined */}
-            for (index = 0; index < elements.length; index++) {
-
-                const value = this.state['value_dataset_' + index.toString()];
-                if (inputVal === undefined) {
-                    datasetBoolean = false;
-                }
-            }
-
-           {/* allow parent component to know all files properly defined */}
-            if (datasetBoolean) {
-                this.props.onChange({submitted_proper_dataset: true});
-            } else {
-                this.props.onChange({submitted_proper_dataset: false});
-            }
-
-            {/* possibly clear submit button */}
-            this.validUrlEntered();
+      // define boolean to indicate all urls properly defined
+      for (let index = 0; index < elements.length; index++) {
+        const value = this.state[`value_dataset_${index.toString()}`];
+        if (inputVal === undefined) {
+          datasetBoolean = false;
         }
-    },
-  // update 'state properties': allow parent component(s) to access properties
-    validUrlEntered: function(){
-        {/* get array of input elements, by classname */}
-        var dataset = document.getElementsByClassName('dataset-url');
+      }
 
-        {/*
+      // allow parent component to know all files properly defined
+      if (datasetBoolean) {
+        this.props.onChange({ submitted_proper_dataset: true });
+      } else {
+        this.props.onChange({ submitted_proper_dataset: false });
+      }
+
+      // possibly clear submit button
+      this.validUrlEntered();
+    }
+  }
+  // update 'state properties': allow parent component(s) to access properties
+  validUrlEntered() {
+    // get array of input elements, by classname
+    const dataset = document.getElementsByClassName('dataset-url');
+
+    /*
             Iterate the node list containing the supplied dataset(s). If the
             input value is a valid file, store 'true', within the array.
-        */}
-        var boolArray = Array.prototype.map.call(dataset, function(element) {
-            if (element.value && checkValidUrl(element.value)) {
-                return true;
-            } else {
-                return false;
-            }
-        });
+    */
+    const boolArray = Array.prototype.map.call(dataset, (element) => {
+      if (element.value && checkValidUrl(element.value)) {
+        return true;
+      }
+      return false;
+    });
 
-        {/* check if every element is 'true' */}
-        var datasetFlag = boolArray.every(function(element) {
-            return element == true;
-        });
+    // check if every element is 'true'
+    const datasetFlag = boolArray.every(element => element === true);
 
-        if (datasetFlag) {
-            this.props.onChange({submitted_proper_dataset: true});
-        } else {
-            this.props.onChange({submitted_proper_dataset: false});
-        }
-    },
-  // triggered when 'state properties' change
-    render: function(){
-        var inputs = this.state.additional_elements;
-
-        return(
-            <fieldset className='fieldset-supply-dataset'>
-                <legend>Supply Dataset</legend>
-                <input
-                    type='url'
-                    name='dataset[]'
-                    placeholder='Dataset URL'
-                    className='dataset-url'
-                    onChange={this.validUrlEntered}
-                    defaultValue=''
-                />
-
-                <input
-                    type='button'
-                    value='Add more'
-                    onClick={this.handleAddMore}
-                />
-
-                <input
-                    type='button'
-                    value='Remove'
-                    onClick={this.handleRemove}
-                />
-
-                {/* array components require unique 'key' value */}
-                {inputs && inputs.map(function(value, index){
-                    return <input
-                        type='url'
-                        name='dataset[]'
-                        placeholder='Dataset URL'
-                        className='dataset-url'
-                        key={index}
-                        onChange={this.validUrlEntered}
-                        defaultValue=''
-                    />;
-                }.bind(this))}
-            </fieldset>
-        );
+    if (datasetFlag) {
+      this.props.onChange({ submitted_proper_dataset: true });
+    } else {
+      this.props.onChange({ submitted_proper_dataset: false });
     }
-});
+  }
+  // triggered when 'state properties' change
+  render() {
+    const inputs = this.state.additional_elements;
+
+    return (
+      <fieldset className="fieldset-supply-dataset">
+        <legend>Supply Dataset</legend>
+        <input
+          type="url"
+          name="dataset[]"
+          placeholder="Dataset URL"
+          className="dataset-url"
+          onChange={this.validUrlEntered}
+          defaultValue=""
+        />
+
+        <input
+          type="button"
+          value="Add more"
+          onClick={this.handleAddMore}
+        />
+
+        <input
+          type="button"
+          value="Remove"
+          onClick={this.handleRemove}
+        />
+
+        {/* array components require unique 'key' value */}
+        {inputs && inputs.map((value, index) => (<input
+          type="url"
+          name="dataset[]"
+          placeholder="Dataset URL"
+          className="dataset-url"
+          key={index}
+          onChange={this.validUrlEntered}
+          defaultValue=""
+        />))}
+      </fieldset>
+    );
+  }
+}
 
 // indicate which class can be exported, and instantiated via 'require'
-export default SupplyDatasetUrl
+export default SupplyDatasetUrl;
