95812ac0f7f9f336ff0c4466628a8a8a7083334b
Vitao18 <victoronline9@gmail.com>
2017-10-15 01:58:17 -0200

    #3047: sv-kernel.jsx, changed to es6 class syntax

--- src/jsx/import/kernel/sv-kernel.jsx
+++ src/jsx/import/kernel/sv-kernel.jsx
@@ -10,42 +10,42 @@
 import React from 'react';
 import checkValidString from './../validator/valid-string.js';
 
-var SupportVectorKernels = React.createClass({
+class SupportVectorKernels extends React.Component {
   // initial 'state properties'
-    getInitialState: function() {
-        return {
-            value_kernel_type: '--Select--'
-        };
-    },
+  getInitialState() {
+    return {
+      value_kernel_type: '--Select--',
+    };
+  }
   // update 'state properties'
-    changeKernelType: function(event){
-        if (checkValidString(event.target.value)) {
-            this.props.onChange({kernelType: this.state.value_kernel_type});
-        } else {
-            this.props.onChange({kernelType: null});
-        }
-    },
-  // triggered when 'state properties' change
-    render: function(){
-      // display result
-        return(
-            <select
-                name='svm_kernel_type'
-                autoComplete='off'
-                onChange={this.changeKernelType}
-                value={this.state.value_kernel_type}
-            >
-
-                <option value='' defaultValue>--Select--</option>
-                <option value='linear'>Linear</option>
-                <option value='poly'>Polynomial</option>
-                <option value='rbf'>RBF</option>
-                <option value='sigmoid'>Sigmoid</option>
-
-            </select>
-        );
+  changeKernelType(event) {
+    if (checkValidString(event.target.value)) {
+      this.props.onChange({ kernelType: this.state.value_kernel_type });
+    } else {
+      this.props.onChange({ kernelType: null });
     }
-});
+  }
+  // triggered when 'state properties' change
+  render() {
+    // display result
+    return (
+      <select
+        name="svm_kernel_type"
+        autoComplete="off"
+        onChange={this.changeKernelType}
+        value={this.state.value_kernel_type}
+      >
+
+        <option value="" defaultValue>--Select--</option>
+        <option value="linear">Linear</option>
+        <option value="poly">Polynomial</option>
+        <option value="rbf">RBF</option>
+        <option value="sigmoid">Sigmoid</option>
+
+      </select>
+    );
+  }
+}
 
 // indicate which class can be exported, and instantiated via 'require'
-export default SupportVectorKernels
+export default SupportVectorKernels;
