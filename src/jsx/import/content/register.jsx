106d7ca0f43562b42f4bc9ba3cea380300515e9e
jeff levesque <jeff1evesque@yahoo.com>
2017-10-16 17:56:11 -0400

    #3047: register.jsx, fix merge conflict

--- src/jsx/import/content/register.jsx
+++ src/jsx/import/content/register.jsx
@@ -16,80 +16,38 @@ import checkValidString from '../validator/valid-string.js';
 import checkValidEmail from '../validator/valid-email.js';
 import checkValidPassword from '../validator/valid-password.js';
 
-class RegisterForm extends React.Component {
+var RegisterForm = React.createClass({
   // initial 'state properties'
-  getInitialState() {
-    return {
-      ajax_done_result: null,
-      validated_username: true,
-      validated_email: true,
-      validated_password: true,
-      validated_username_server: true,
-      validated_email_server: true,
-      validated_password_server: true,
-      value_username: '',
-      value_email: '',
-      value_password: '',
-    };
-  }
-  componentWillMount() {
-    // update redux store
-    const action = setLayout({ layout: 'register' });
-    this.props.dispatchLayout(action);
-  }
+    getInitialState: function() {
+        return {
+            ajax_done_result: null,
+            validated_username: true,
+            validated_email: true,
+            validated_password: true,
+            validated_username_server: true,
+            validated_email_server: true,
+            validated_password_server: true,
+            value_username: '',
+            value_email: '',
+            value_password: '',
+        };
+    },
   // send form data to serverside on form submission
-  handleSubmit(event) {
-    // prevent page reload
-    event.preventDefault();
+    handleSubmit: function(event) {
+      // prevent page reload
+        event.preventDefault();
 
-    // display spinner
-    const action = setSpinner({ spinner: true });
-    this.props.dispatchSpinner(action);
+      // display spinner
+        const action = setSpinner({'spinner': true})
+        this.props.dispatchSpinner(action);
 
-    // local variables
-    const ajaxEndpoint = '/register';
-    const ajaxArguments = {
-      endpoint: ajaxEndpoint,
-      data: new FormData(this.refs.registerForm),
-    };
+      // local variables
+        var ajaxEndpoint = '/register';
+        var ajaxArguments = {
+            'endpoint': ajaxEndpoint,
+            'data': new FormData(this.refs.registerForm)
+        };
 
-<<<<<<< HEAD
-      // asynchronous callback: ajax 'done' promise
-    ajaxCaller(
-      (asynchObject) => {
-        // Append to DOM
-        if (asynchObject && asynchObject.error) {
-          this.setState({ ajax_done_error: asynchObject.error });
-        } else if (asynchObject) {
-          // local variables
-          const result = asynchObject;
-          const status = (!!result && result.status >= 0) ? result.status : null;
-
-          // backend validation: server handles one error at a time
-          if (!!status || status === 0) {
-            switch (status) {
-              case 0:
-                this.setState({ ajax_done_result: result });
-                break;
-              case 1:
-                this.setState({ validated_password_server: false });
-                break;
-              case 2:
-                this.setState({ validated_username_server: false });
-                break;
-              case 3:
-                this.setState({ validated_email_server: false });
-                break;
-            }
-          }
-        }
-      },
-      // asynchronous callback: ajax 'fail' promise
-      (asynchStatus, asynchError) => {
-        if (asynchStatus) {
-          this.setState({ ajax_fail_status: asynchStatus });
-          console.log(`Error Status: ${asynchStatus}`);
-=======
         if (
             !!this.state.value_username &&
             !!this.state.value_email &&
@@ -108,18 +66,22 @@ class RegisterForm extends React.Component {
 
                   // backend validation: server handles one error at a time
                     if (!!status || status == 0) {
+                        const action = setSpinner({'spinner': false});
                         switch(status) {
                             case 0:
                                 this.setState({ajax_done_result: result});
                                 break;
                             case 1:
                                 this.setState({validated_password_server: false});
+                                this.props.dispatchSpinner(action);
                                 break;
                             case 2:
                                 this.setState({validated_username_server: false});
+                                this.props.dispatchSpinner(action);
                                 break;
                             case 3:
                                 this.setState({validated_email_server: false});
+                                this.props.dispatchSpinner(action);
                                 break;
                         }
                     }
@@ -149,7 +111,7 @@ class RegisterForm extends React.Component {
                 this.setState({validated_password: false});
             }
 
-            const action = setSpinner({'spinner': false})
+            const action = setSpinner({'spinner': false});
             this.props.dispatchSpinner(action);
         }
     },
@@ -162,165 +124,149 @@ class RegisterForm extends React.Component {
         const username = event.target.value;
         const check = checkValidString(username) ? true : false;
         if (!check) {
-            const action = setSpinner({'spinner': false})
+            const action = setSpinner({'spinner': false});
             this.props.dispatchSpinner(action);
->>>>>>> 18f1497... #3052: register.jsx, frontend validate fields
         }
-        if (asynchError) {
-          this.setState({ ajax_fail_error: asynchError });
-          console.log(`Error Thrown: ${asynchError}`);
+
+        this.setState({validated_username_server: true});
+        this.setState({validated_username: check});
+        this.setState({value_username: username});
+    },
+    validateEmail: function(event) {
+        const email = event.target.value;
+        const check = checkValidEmail(email) ? true : false;
+        if (!check) {
+            const action = setSpinner({'spinner': false});
+            this.props.dispatchSpinner(action);
         }
-      },
-      // pass ajax arguments
-      ajaxArguments,
-    );
-  }
-  validateUsername(event) {
-    const username = event.target.value;
-    const check = !!checkValidString(username);
-    if (!check) {
-      const action = setSpinner({ spinner: false });
-      this.props.dispatchSpinner(action);
-    }
 
-    this.setState({ validated_username_server: true });
-    this.setState({ validated_username: check });
-    this.setState({ value_username: username });
-  }
-  validateEmail(event) {
-    const email = event.target.value;
-    const check = !!checkValidEmail(email);
-    if (!check) {
-      const action = setSpinner({ spinner: false });
-      this.props.dispatchSpinner(action);
-    }
+        this.setState({validated_email_server: true});
+        this.setState({validated_email: check});
+        this.setState({value_email: email});
+    },
+    validatePassword: function(event) {
+        const password = event.target.value;
+        const check = checkValidPassword(password) ? true : false;
+        if (!check) {
+            const action = setSpinner({'spinner': false});
+            this.props.dispatchSpinner(action);
+        }
 
-    this.setState({ validated_email_server: true });
-    this.setState({ validated_email: check });
-    this.setState({ value_email: email });
-  }
-  validatePassword(event) {
-    const password = event.target.value;
-    const check = !!checkValidPassword(password);
-    if (!check) {
-      const action = setSpinner({ spinner: false });
-      this.props.dispatchSpinner(action);
-    }
-
-    this.setState({ validated_password_server: true });
-    this.setState({ validated_password: check });
-    this.setState({ value_password: password });
-  }
+        this.setState({validated_password_server: true});
+        this.setState({validated_password: check});
+        this.setState({value_password: password});
+    },
   // triggered when 'state properties' change
-  render() {
-    // frontend validation
-    const usernameClass = this.state.validated_username ? '' : 'invalid';
-    const passwordClass = this.state.validated_password ? '' : 'invalid';
+    render: function() {
+      // frontend validation
+        var usernameClass = this.state.validated_username ?  '' : 'invalid';
+        var passwordClass = this.state.validated_password ? '' : 'invalid';
 
-    if (this.state.validated_email) {
-      var emailClass = '';
-      var emailNote = '';
-    } else {
-      var emailClass = 'invalid';
-      var emailNote = (<span className={emailClass}>
+        if (this.state.validated_email) {
+            var emailClass = '';
+            var emailNote = '';
+        } else {
+            var emailClass = 'invalid';
+            var emailNote = <span className={emailClass}>
                 Please provide a valid email.
-                       </span>);
-    }
+            </span>;
+        }
 
-    // backend validation
-    if (!this.state.validated_password_server) {
-      var passwordNote = (<span className="invalid">
+      // backend validation
+        if (!this.state.validated_password_server) {
+            var passwordNote = <span className='invalid'>
                 (Password requirement not met)
-                          </span>);
-    } else {
-      var passwordNote = null;
-    }
+            </span>;
+        } else {
+            var passwordNote = null;
+        }
 
-    if (!this.state.validated_username_server) {
-      var usernameNote = (<span className="invalid">
+        if (!this.state.validated_username_server) {
+            var usernameNote = <span className='invalid'>
                 (Username is taken)
-                          </span>);
-    } else {
-      var usernameNote = null;
-    }
+            </span>;
+        } else {
+            var usernameNote = null;
+        }
 
-    if (!this.state.validated_email_server) {
-      var emailNote = (<span className="invalid">
+        if (!this.state.validated_email_server) {
+            var emailNote = <span className='invalid'>
                 (Email has already registered)
-                       </span>);
-    } else {
-      var emailNote = null;
-    }
+            </span>;
+        } else {
+            var emailNote = null;
+        }
 
-    if (
-      this.state.ajax_done_result &&
-            this.state.ajax_done_result.status === '0'
-    ) {
-      var redirect = <Redirect to="/login" />;
-    } else {
-      var redirect = null;
-    }
+        if (
+            this.state.ajax_done_result &&
+            this.state.ajax_done_result.status == '0'
+        ) {
+            var redirect = <Redirect to='/login' />;
+        }
+        else {
+            var redirect = null;
+        }
 
-    return (
-      <form onSubmit={this.handleSubmit} ref="registerForm">
-        {redirect}
-        <div className="form-group">
-          <label className={`form-label ${usernameClass}`}>Username</label>
-          <input
-            type="text"
-            name="user[login]"
-            className="input-block"
-            placeholder="Pick a username"
-            onInput={this.validateUsername}
-            value={this.state.value_username}
-          />
-          <p className={`note ${usernameClass}`}>
+        return(
+            <form onSubmit={this.handleSubmit} ref='registerForm'>
+                {redirect}
+                <div className='form-group'>
+                    <label className={'form-label ' + usernameClass}>Username</label>
+                    <input
+                        type='text'
+                        name='user[login]'
+                        className='input-block'
+                        placeholder='Pick a username'
+                        onInput={this.validateUsername}
+                        value={this.state.value_username}
+                    />
+                    <p className={'note ' + usernameClass}>
                         This will be your username. {usernameNote}
-          </p>
-        </div>
+                    </p>
+                </div>
 
-        <div className="form-group">
-          <label className={`form-label ${emailClass}`}>Email Address</label>
-          <input
-            type="text"
-            name="user[email]"
-            className="input-block"
-            placeholder="Your email address"
-            onInput={this.validateEmail}
-            value={this.state.value_email}
-          />
-          <p className="note">
+                <div className='form-group'>
+                    <label className={'form-label ' + emailClass}>Email Address</label>
+                    <input
+                        type='text'
+                        name='user[email]'
+                        className='input-block'
+                        placeholder='Your email address'
+                        onInput={this.validateEmail}
+                        value={this.state.value_email}
+                    />
+                    <p className='note'>
                         You will get updates regarding account changes,
                         or activitites. This email address will not be
                         shared with anyone. {emailNote}
-          </p>
-        </div>
-
-        <div className="form-group">
-          <label className={`form-label ${passwordClass}`}>Password</label>
-          <input
-            type="password"
-            name="user[password]"
-            className="input-block"
-            placeholder="Create a password"
-            onInput={this.validatePassword}
-            value={this.state.value_password}
-          />
-          <p className={`note ${passwordClass}`}>
+                    </p>
+                </div>
+ 
+                <div className='form-group'>
+                    <label className={'form-label ' + passwordClass}>Password</label>
+                    <input
+                        type='password'
+                        name='user[password]'
+                        className='input-block'
+                        placeholder='Create a password'
+                        onInput={this.validatePassword}
+                        value={this.state.value_password}
+                    />
+                    <p className={'note ' + passwordClass}>
                         Use at least one letter, one numeral,
                         and ten characters. {passwordNote}
-          </p>
-        </div>
+                    </p>
+                </div>
 
-        <input
-          type="submit"
-          className="btn btn-primary"
-          value="Create an account"
-        />
-      </form>
-    );
-  }
-}
+                <input
+                    type='submit'
+                    className='btn btn-primary'
+                    value='Create an account'
+                />
+            </form>
+        );
+    }
+});
 
 // indicate which class can be exported, and instantiated via 'require'
-export default RegisterForm;
+export default RegisterForm
