3b62adc1cae0219edebe4d8297c4152c7ad94828
Vitao18 <victoronline9@gmail.com>
2017-10-16 17:02:55 -0200

    #3047, model-generate.jsx, changed to es6 syntax

--- src/jsx/import/session-type/model-generate.jsx
+++ src/jsx/import/session-type/model-generate.jsx
@@ -16,231 +16,231 @@ import Spinner from '../general/spinner.jsx';
 import { setSvButton, setLayout, setContentType } from '../redux/action/page.jsx';
 import ajaxCaller from '../general/ajax-caller.js';
 
-var ModelGenerate = React.createClass({
+const ModelGenerate = React.createClass({
   // initial 'state properties'
-    getInitialState: function() {
-        return {
-            value_collection: '--Select--',
-            value_model_type: '--Select--',
-            ajax_done_options: null,
-            ajax_done_error: null,
-            ajax_fail_error: null,
-            ajax_fail_status: null
-        };
-    },
+  getInitialState() {
+    return {
+      value_collection: '--Select--',
+      value_model_type: '--Select--',
+      ajax_done_options: null,
+      ajax_done_error: null,
+      ajax_fail_error: null,
+      ajax_fail_status: null,
+    };
+  },
   // update 'state properties'
-    changeCollection: function(event){
-        const collection = event.target.value;
-        const modelType  = this.state.value_model_type;
-        const kernelType = this.state.value_kernel_type;
+  changeCollection(event) {
+    const collection = event.target.value;
+    const modelType = this.state.value_model_type;
+    const kernelType = this.state.value_kernel_type;
 
-        if (
-            collection &&
-            collection != '--Select--' &&
+    if (
+      collection &&
+            collection !== '--Select--' &&
             checkValidString(collection)
-        ) {
-            this.setState({value_collection: collection});
+    ) {
+      this.setState({ value_collection: collection });
 
-          // update redux store
-            if (
-                modelType != '--Select--' &&
-                kernelType != '--Select--' &&
+      // update redux store
+      if (
+        modelType !== '--Select--' &&
+                kernelType !== '--Select--' &&
                 checkValidString(modelType) &&
                 checkValidString(kernelType)
-            ) {
-                const action = setSvButton({button: {submit_analysis: true}});
-                this.props.dispatchSvButton(action);
-            } else {
-                const action = setSvButton({button: {submit_analysis: false}});
-                this.props.dispatchSvButton(action);
-            }
-        } else {
-            this.setState({value_collection: '--Select--'});
+      ) {
+        const action = setSvButton({ button: { submit_analysis: true } });
+        this.props.dispatchSvButton(action);
+      } else {
+        const action = setSvButton({ button: { submit_analysis: false } });
+        this.props.dispatchSvButton(action);
+      }
+    } else {
+      this.setState({ value_collection: '--Select--' });
 
-          // update redux store
-            const action = setSvButton({button: {submit_analysis: false}});
-            this.props.dispatchSvButton(action);
-        }
-    },
-    changeModelType: function(event){
-        const collection  = this.state.value_collection;
-        const modelType  = event.target.value;
-        const kernelType = this.state.value_kernel_type;
+      // update redux store
+      const action = setSvButton({ button: { submit_analysis: false } });
+      this.props.dispatchSvButton(action);
+    }
+  },
+  changeModelType(event) {
+    const collection = this.state.value_collection;
+    const modelType = event.target.value;
+    const kernelType = this.state.value_kernel_type;
 
-        if (
-            modelType &&
-            modelType != '--Select--' &&
+    if (
+      modelType &&
+            modelType !== '--Select--' &&
             checkValidString(modelType)
-        ) {
-            this.setState({value_model_type: event.target.value});
+    ) {
+      this.setState({ value_model_type: event.target.value });
 
-          // update redux store
-            if (
-                checkValidString(collection) &&
-                kernelType != '--Select--' &&
+      // update redux store
+      if (
+        checkValidString(collection) &&
+                kernelType !== '--Select--' &&
                 checkValidString(kernelType)
-            ) {
-                const action = setSvButton({button: {submit_analysis: true}});
-                this.props.dispatchSvButton(action);
-            } else {
-                const action = setSvButton({button: {submit_analysis: false}});
-                this.props.dispatchSvButton(action);
-            }
-        } else {
-            this.setState({value_model_type: '--Select--'});
+      ) {
+        const action = setSvButton({ button: { submit_analysis: true } });
+        this.props.dispatchSvButton(action);
+      } else {
+        const action = setSvButton({ button: { submit_analysis: false } });
+        this.props.dispatchSvButton(action);
+      }
+    } else {
+      this.setState({ value_model_type: '--Select--' });
 
-          // update redux store
-            const action = setSvButton({button: {submit_analysis: false}});
-            this.props.dispatchSvButton(action);
-        }
-    },
-    changeKernelType: function(event) {
-        const collection = this.state.value_collection;
-        const modelType  = this.state.value_model_type;
-        const kernelType = event.target.value;
+      // update redux store
+      const action = setSvButton({ button: { submit_analysis: false } });
+      this.props.dispatchSvButton(action);
+    }
+  },
+  changeKernelType(event) {
+    const collection = this.state.value_collection;
+    const modelType = this.state.value_model_type;
+    const kernelType = event.target.value;
 
-        if (
-            kernelType &&
-            kernelType != '--Select--' &&
+    if (
+      kernelType &&
+            kernelType !== '--Select--' &&
             checkValidString(kernelType)
-        ) {
-            this.setState({value_kernel_type: event.target.value});
+    ) {
+      this.setState({ value_kernel_type: event.target.value });
 
-          // update redux store
-            if (
-                checkValidString(collection) &&
-                modelType != '--Select--' &&
+      // update redux store
+      if (
+        checkValidString(collection) &&
+                modelType !== '--Select--' &&
                 checkValidString(modelType)
-            ) {
-                const action = setSvButton({button: {submit_analysis: true}});
-                this.props.dispatchSvButton(action);
-            } else {
-                const action = setSvButton({button: {submit_analysis: false}});
-                this.props.dispatchSvButton(action);
-            }
-        } else {
-            this.setState({value_kernel_type: '--Select--'});
+      ) {
+        const action = setSvButton({ button: { submit_analysis: true } });
+        this.props.dispatchSvButton(action);
+      } else {
+        const action = setSvButton({ button: { submit_analysis: false } });
+        this.props.dispatchSvButton(action);
+      }
+    } else {
+      this.setState({ value_kernel_type: '--Select--' });
 
-          // update redux store
-            const action = setSvButton({button: {submit_analysis: false}});
-            this.props.dispatchSvButton(action);
-        }
-    },
+      // update redux store
+      const action = setSvButton({ button: { submit_analysis: false } });
+      this.props.dispatchSvButton(action);
+    }
+  },
   // triggered when 'state properties' change
-    render: function(){
-        const options = this.state.ajax_done_options;
-        const spinner = !!this.state.display_spinner ? <Spinner /> : null;
+  render() {
+    const options = this.state.ajax_done_options;
+    const spinner = this.state.display_spinner ? <Spinner /> : null;
 
-        return(
-            <fieldset className='fieldset-session-generate'>
-                <legend>Generate Model</legend>
-                <fieldset className='fieldset-select-model'>
-                    <legend>Configurations</legend>
-                    <p>Select past collection, model type, and kernel type</p>
-                    <select
-                        name='collection'
-                        autoComplete='off'
-                        onChange={this.changeCollection}
-                        value={this.state.value_collection}
-                    >
+    return (
+      <fieldset className="fieldset-session-generate">
+        <legend>Generate Model</legend>
+        <fieldset className="fieldset-select-model">
+          <legend>Configurations</legend>
+          <p>Select past collection, model type, and kernel type</p>
+          <select
+            name="collection"
+            autoComplete="off"
+            onChange={this.changeCollection}
+            value={this.state.value_collection}
+          >
 
-                        <option value='' defaultValue>--Select--</option>
+            <option value="" defaultValue>--Select--</option>
 
-                        {/* array components require unique 'key' value */}
-                        {options && options.map(function(value) {
-                            return <option key={value.id} value={value.collection}>
-                                {value.id}: {value.collection}
-                            </option>;
-                        })}
+            {/* array components require unique 'key' value */}
+            {options && options.map(value => (<option key={value.id} value={value.collection}>
+              {value.id}: {value.collection}
+            </option>))}
 
-                    </select>
+          </select>
 
-                    <select
-                        name='model_type'
-                        autoComplete='off'
-                        onChange={this.changeModelType}
-                        value={this.state.value_model_type}
-                    >
+          <select
+            name="model_type"
+            autoComplete="off"
+            onChange={this.changeModelType}
+            value={this.state.value_model_type}
+          >
 
-                        <option value='' defaultValue>--Select--</option>
-                        <option value='svm'>SVM</option>
-                        <option value='svr'>SVR</option>
+            <option value="" defaultValue>--Select--</option>
+            <option value="svm">SVM</option>
+            <option value="svr">SVR</option>
 
-                    </select>
+          </select>
 
-                    <select
-                        name='sv_kernel_type'
-                        autoComplete='off'
-                        onChange={this.changeKernelType}
-                        value={this.state.value_kernel_type}
-                    >
+          <select
+            name="sv_kernel_type"
+            autoComplete="off"
+            onChange={this.changeKernelType}
+            value={this.state.value_kernel_type}
+          >
 
-                        <option value='' defaultValue>--Select--</option>
-                        <option value='linear'>Linear</option>
-                        <option value='poly'>Polynomial</option>
-                        <option value='rbf'>RBF</option>
-                        <option value='sigmoid'>Sigmoid</option>
+            <option value="" defaultValue>--Select--</option>
+            <option value="linear">Linear</option>
+            <option value="poly">Polynomial</option>
+            <option value="rbf">RBF</option>
+            <option value="sigmoid">Sigmoid</option>
 
-                    </select>
-                </fieldset>
+          </select>
+        </fieldset>
 
-                {spinner}
-            </fieldset>
-        );
-    },
+        {spinner}
+      </fieldset>
+    );
+  },
   // call back: get all collections from server side, and append to form
-    componentDidMount: function() {
-      // ajax arguments
-        const ajaxEndpoint = '/retrieve-collections';
-        const ajaxArguments = {
-            'endpoint': ajaxEndpoint,
-            'data': null
-        };
+  componentDidMount() {
+    // ajax arguments
+    const ajaxEndpoint = '/retrieve-collections';
+    const ajaxArguments = {
+      endpoint: ajaxEndpoint,
+      data: null,
+    };
 
       // boolean to show ajax spinner
-        this.setState({display_spinner: true});
+    this.setState({ display_spinner: true });
 
-      // asynchronous callback: ajax 'done' promise
-        ajaxCaller(function (asynchObject) {
+    // asynchronous callback: ajax 'done' promise
+    ajaxCaller(
+      (asynchObject) => {
         // Append to DOM
-            if (asynchObject && asynchObject.error) {
-                this.setState({ajax_done_error: asynchObject.error});
-            } else if (asynchObject) {
-                this.setState({ajax_done_options: asynchObject});
-            }
+        if (asynchObject && asynchObject.error) {
+          this.setState({ ajax_done_error: asynchObject.error });
+        } else if (asynchObject) {
+          this.setState({ ajax_done_options: asynchObject });
+        }
         // boolean to hide ajax spinner
-            this.setState({display_spinner: false});
-        }.bind(this),
+        this.setState({ display_spinner: false });
+      },
       // asynchronous callback: ajax 'fail' promise
-        function (asynchStatus, asynchError) {
-            if (asynchStatus) {
-                this.setState({ajax_fail_status: asynchStatus});
-                console.log('Error Status: ' + asynchStatus);
-            }
-            if (asynchError) {
-                this.setState({ajax_fail_error: asynchError});
-                console.log('Error Thrown: ' + asynchError);
-            }
+      (asynchStatus, asynchError) => {
+        if (asynchStatus) {
+          this.setState({ ajax_fail_status: asynchStatus });
+          console.log(`Error Status: ${asynchStatus}`);
+        }
+        if (asynchError) {
+          this.setState({ ajax_fail_error: asynchError });
+          console.log(`Error Thrown: ${asynchError}`);
+        }
         // boolean to hide ajax spinner
-            this.setState({display_spinner: false});
-        }.bind(this),
+        this.setState({ display_spinner: false });
+      },
       // pass ajax arguments
-        ajaxArguments);
-    },
-    componentWillMount: function() {
-      // update redux store
-        const actionLayout = setLayout({'layout': 'analysis'});
-        this.props.dispatchLayout(actionLayout);
+      ajaxArguments,
+    );
+  },
+  componentWillMount() {
+    // update redux store
+    const actionLayout = setLayout({ layout: 'analysis' });
+    this.props.dispatchLayout(actionLayout);
 
-        const actionContentType = setContentType({'layout': 'model_generate'});
-        this.props.dispatchContentType(actionContentType);
-    },
-    componentWillUnmount: function() {
-      // update redux store
-        const action = setSvButton({button: {submit_analysis: false}});
-        this.props.dispatchSvButton(action);
-    }
+    const actionContentType = setContentType({ layout: 'model_generate' });
+    this.props.dispatchContentType(actionContentType);
+  },
+  componentWillUnmount() {
+    // update redux store
+    const action = setSvButton({ button: { submit_analysis: false } });
+    this.props.dispatchSvButton(action);
+  },
 });
 
 // indicate which class can be exported, and instantiated via 'require'
-export default ModelGenerate
+export default ModelGenerate;
