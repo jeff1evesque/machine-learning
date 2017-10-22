ee18042fd24a0fe50191e6200d4de957e113ce89
jeff levesque <jeff1evesque@yahoo.com>
2017-10-22 18:22:39 -0400

    #3068: bagger.py, fix multiple lines lint error

--- brain/session/model/bagger.py
+++ brain/session/model/bagger.py
@@ -118,11 +118,13 @@ def baggen(
                 if model in regressors:
                     labels.append(float(o['dependent-variable']))
                     sortedf = [float(v) for k, v in sorted(f.items())]
+                    features.append(sortedf)
+
                 elif model in classifiers:
                     labels.append(o['dependent-variable'])
                     sortedf = [v for k, v in sorted(f.items())]
+                    features.append(sortedf)
 
-                if sortedf: features.append(sortedf)
                 if not sorted_labels:
                     sorted_labels = [k for k, v in sorted(f.items())]
 
