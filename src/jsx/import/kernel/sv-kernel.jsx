321f18f8bc572448b591860b14003f711ea06700
Vitao18 <victoronline9@gmail.com>
2017-10-24 01:49:39 -0200

    #3079: sv-kernel.jsx, changed ES6 class syntax

--- src/jsx/import/kernel/sv-kernel.jsx
+++ src/jsx/import/kernel/sv-kernel.jsx
@@ -7,45 +7,45 @@
  * Note: this script implements jsx (reactjs) syntax.
  */
 
-import React from 'react';
+import React, { Component } from 'react';
 import checkValidString from './../validator/valid-string.js';
 
-var SupportVectorKernels = React.createClass({
-  // initial 'state properties'
-    getInitialState: function() {
+class SupportVectorKernels extends Component {
+    // initial 'state properties'
+    getInitialState() {
         return {
-            value_kernel_type: '--Select--'
+            value_kernel_type: '--Select--',
         };
-    },
-  // update 'state properties'
-    changeKernelType: function(event){
+    }
+    // update 'state properties'
+    changeKernelType(event) {
         if (checkValidString(event.target.value)) {
-            this.props.onChange({kernelType: this.state.value_kernel_type});
+            this.props.onChange({ kernelType: this.state.value_kernel_type });
         } else {
-            this.props.onChange({kernelType: null});
+            this.props.onChange({ kernelType: null });
         }
-    },
-  // triggered when 'state properties' change
-    render: function(){
-      // display result
-        return(
+    }
+    // triggered when 'state properties' change
+    render() {
+        // display result
+        return (
             <select
-                name='svm_kernel_type'
-                autoComplete='off'
+                name="svm_kernel_type"
+                autoComplete="off"
                 onChange={this.changeKernelType}
                 value={this.state.value_kernel_type}
             >
 
-                <option value='' defaultValue>--Select--</option>
-                <option value='linear'>Linear</option>
-                <option value='poly'>Polynomial</option>
-                <option value='rbf'>RBF</option>
-                <option value='sigmoid'>Sigmoid</option>
+                <option value="" defaultValue>--Select--</option>
+                <option value="linear">Linear</option>
+                <option value="poly">Polynomial</option>
+                <option value="rbf">RBF</option>
+                <option value="sigmoid">Sigmoid</option>
 
             </select>
         );
     }
-});
+}
 
 // indicate which class can be exported, and instantiated via 'require'
-export default SupportVectorKernels
+export default SupportVectorKernels;
