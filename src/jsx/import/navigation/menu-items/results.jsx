cfc06472d573fdbd429f001c46d602df14c440ac
Vitao18 <victoronline9@gmail.com>
2017-10-24 09:23:28 -0200

    #3079: results.jsx, changed to es6 class

--- src/jsx/import/navigation/menu-items/results.jsx
+++ src/jsx/import/navigation/menu-items/results.jsx
@@ -11,27 +11,27 @@
  *
  */
 
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import ajaxCaller from '../../general/ajax-caller.js';
 
-var ReviewResultsLink = React.createClass({
-  // initial 'state properties'
-    getInitialState: function() {
+class ReviewResultsLink extends Component {
+    // initial 'state properties'
+    getInitialState() {
         return {
             ajax_done_result: null,
             ajax_done_error: null,
             ajax_fail_error: null,
-            ajax_fail_status: null
+            ajax_fail_status: null,
         };
-    },
-  // call back: return login button
-    renderContent: function() {
+    }
+    // call back: return login button
+    renderContent() {
         if (
-                (
-                    this.state.ajax_done_result &&
+            (
+                this.state.ajax_done_result &&
                     this.state.ajax_done_result.titles.length > 0
-                ) ||
+            ) ||
                 (
                     this.props &&
                     this.props.page &&
@@ -48,50 +48,51 @@ var ReviewResultsLink = React.createClass({
                     Review Results
                 </NavLink>
             );
-        } else {
-            return null;
         }
-    },
-    componentWillMount: function() {
-      // local variables
-        var ajaxArguments = {
-            'endpoint': '/retrieve-prediction-titles'
+        return null;
+    }
+    componentWillMount() {
+        // local variables
+        const ajaxArguments = {
+            endpoint: '/retrieve-prediction-titles',
         };
 
-      // asynchronous callback: ajax 'done' promise
-        ajaxCaller(function (asynchObject) {
-          // Append to DOM
-            if (asynchObject && asynchObject.error) {
-                this.setState({ajax_done_error: asynchObject.error});
-            } else if (asynchObject) {
-                this.setState({ajax_done_result: asynchObject});
-            } else {
-                this.setState({ajax_done_result: null});
-            }
-        // boolean to hide ajax spinner
-            this.setState({display_spinner: false});
-        }.bind(this),
-      // asynchronous callback: ajax 'fail' promise
-        function (asynchStatus, asynchError) {
-            if (asynchStatus) {
-                this.setState({ajax_fail_status: asynchStatus});
-                console.log('Error Status: ' + asynchStatus);
-            }
-            if (asynchError) {
-                this.setState({ajax_fail_error: asynchError});
-                console.log('Error Thrown: ' + asynchError);
-            }
-        // boolean to hide ajax spinner
-            this.setState({display_spinner: false});
-        }.bind(this),
-      // pass ajax arguments
-        ajaxArguments);
-    },
-    render: function(){
-        var selectedContent = this.renderContent();
-        return(selectedContent);
+        // asynchronous callback: ajax 'done' promise
+        ajaxCaller(
+            (asynchObject) => {
+                // Append to DOM
+                if (asynchObject && asynchObject.error) {
+                    this.setState({ ajax_done_error: asynchObject.error });
+                } else if (asynchObject) {
+                    this.setState({ ajax_done_result: asynchObject });
+                } else {
+                    this.setState({ ajax_done_result: null });
+                }
+                // boolean to hide ajax spinner
+                this.setState({ display_spinner: false });
+            },
+            // asynchronous callback: ajax 'fail' promise
+            (asynchStatus, asynchError) => {
+                if (asynchStatus) {
+                    this.setState({ ajax_fail_status: asynchStatus });
+                    console.log(`Error Status: ${asynchStatus}`);
+                }
+                if (asynchError) {
+                    this.setState({ ajax_fail_error: asynchError });
+                    console.log(`Error Thrown: ${asynchError}`);
+                }
+                // boolean to hide ajax spinner
+                this.setState({ display_spinner: false });
+            },
+            // pass ajax arguments
+            ajaxArguments,
+        );
     }
-});
+    render() {
+        const selectedContent = this.renderContent();
+        return (selectedContent);
+    }
+}
 
 // indicate which class can be exported, and instantiated via 'require'
-export default ReviewResultsLink
+export default ReviewResultsLink;
