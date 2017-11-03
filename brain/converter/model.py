3a826ddf53d5b855f482fdf8fb79a670df66501e
protojas <protojas@gmail.com>
2017-11-02 23:47:13 -0400

    #3068: fixing Model.py

--- brain/converter/model.py
+++ brain/converter/model.py
@@ -7,7 +7,7 @@ This file serializes, and deserializes an SVM object
 '''
 
 from six.moves import cPickle as pickle
-from sklearn import svm, preprocessing
+from sklearn import svm, preprocessing, ensemble
 
 
 class Model(object):
@@ -32,6 +32,8 @@ class Model(object):
         self.acceptable = [
             svm.classes.SVC,
             svm.classes.SVR,
+            ensemble.classes.BaggingClassifier,
+            ensemble.classes.BaggingRegressor,
             preprocessing.label.LabelEncoder
         ]
 
