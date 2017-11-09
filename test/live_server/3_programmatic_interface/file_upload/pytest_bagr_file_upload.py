77d792e229cac8b18ba185eb6ed907e68ab54a42
protojas <protojas@gmail.com>
2017-11-08 20:47:05 -0500

    #3067: unit tests need to be updated

---
+++ test/live_server/3_programmatic_interface/file_upload/pytest_bagr_file_upload.py
@@ -0,0 +1,169 @@
+'''
+
+This file will test the following svm sessions:
+  - data_new: stores supplied dataset into a SQL database.
+  - data_append: appends supplied dataset to an already stored dataset in an
+                 SQL database.
+  - model_generate: generate an model by selecting a particular range of
+                    dataset (session), and store it into a NoSQL cache.
+  - model_predict: generate a prediction by selecting a particular cached
+                   model from the NoSQL cache.
+
+Note: the 'pytest' instances can further be reviewed:
+
+    - https://pytest-flask.readthedocs.io/en/latest
+    - http://docs.pytest.org/en/latest/usage.html
+
+'''
+
+import json
+import os.path
+from flask import current_app, url_for
+
+
+def get_sample_json(jsonfile, model_type):
+    '''
+
+    Get a sample json dataset.
+
+    '''
+
+    # local variables
+    root = current_app.config.get('ROOT')
+
+    # open file
+    json_dataset = None
+
+    with open(
+        os.path.join(
+            root,
+            'interface',
+            'static',
+            'data',
+            'json',
+            'programmatic_interface',
+            model_type,
+            'file_upload',
+            jsonfile
+        ),
+        'r'
+    ) as json_file:
+        json_dataset = json.load(json_file)
+
+    return json.dumps(json_dataset)
+
+
+def test_data_new(client, live_server):
+    '''
+
+    This method tests the 'data_new' session.
+
+    '''
+
+    @live_server.app.route('/load-data')
+    def get_endpoint():
+        return url_for('name.load_data', _external=True)
+
+    live_server.start()
+
+    res = client.post(
+        get_endpoint(),
+        headers={'Content-Type': 'application/json'},
+        data=get_sample_json('-data-new.json', 'bagr')
+    )
+
+    # assertion checks
+    assert res.status_code == 200
+    assert res.json['status'] == 0
+
+
+def test_data_append(client, live_server):
+    '''
+
+    This method tests the 'data_new' session.
+
+    '''
+
+    @live_server.app.route('/load-data')
+    def get_endpoint():
+        return url_for('name.load_data', _external=True)
+
+    live_server.start()
+
+    res = client.post(
+        get_endpoint(),
+        headers={'Content-Type': 'application/json'},
+        data=get_sample_json('bagr-data-append.json', 'bagr')
+    )
+
+    # assertion checks
+    assert res.status_code == 200
+    assert res.json['status'] == 0
+
+
+def test_model_generate(client, live_server):
+    '''
+
+    This method tests the 'model_generate' session.
+
+    '''
+
+    @live_server.app.route('/load-data')
+    def get_endpoint():
+        return url_for('name.load_data', _external=True)
+
+    live_server.start()
+
+    res = client.post(
+        get_endpoint(),
+        headers={'Content-Type': 'application/json'},
+        data=get_sample_json('bagr-model-generate.json', 'bagr')
+    )
+
+    # assertion checks
+    assert res.status_code == 200
+    assert res.json['status'] == 0
+
+
+def test_model_predict(client, live_server):
+    '''
+
+    This method tests the 'model_predict' session.
+
+    Note: for debugging, the following syntax will output the corresponding
+          json values, nested within 'json.loads()', to the travis ci:
+
+          raise ValueError(res.json['result']['key1'])
+
+    '''
+
+    @live_server.app.route('/load-data')
+    def get_endpoint():
+        return url_for('name.load_data', _external=True)
+
+    live_server.start()
+
+    res = client.post(
+        get_endpoint(),
+        headers={'Content-Type': 'application/json'},
+        data=get_sample_json('bagr-model-predict.json', 'bagr')
+    )
+
+    # assertion checks
+    assert not res.json['result']['confidence']['decision_function']
+    assert res.json['result']['confidence']['classes'] == [
+        'dep-variable-1',
+        'dep-variable-2',
+        'dep-variable-3',
+        'dep-variable-4',
+        'dep-variable-5'
+    ]
+    assert res.json['result']['confidence']['probability'] == [
+        0.2,
+        0.0,
+        0.1,
+        0.1,
+        0.6
+    ]
+    assert res.json['result']['model'] == 'bagr'
+    assert res.json['result']['result'] == 'dep-variable-5'
