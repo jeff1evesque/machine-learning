4a49d57c27d7ab2be905db1407f07e7dab160e1b
Jeffrey M. Levesque <jeff1evesque@yahoo.com>
2017-12-08 19:05:28 -0500

    #3132: define 'RangeSlider' via 'react-rangeslider'

---
+++ src/jsx/import/general/rangeSlider.jsx
@@ -0,0 +1,51 @@
+/**
+ * rangeSlider.jsx: create responsive range slider.
+ *
+ * @RangeSlider, must be capitalized in order for reactjs to render it as a
+ *     component. Otherwise, the variable is rendered as a dom node.
+ *
+ * Note: this script implements jsx (reactjs) syntax.
+ */
+
+import React, { Component } from 'react'
+import Slider from 'react-rangeslider'
+
+class RangeSlider extends Component {
+    constructor (props, context) {
+        super(props, context)
+        this.state = {
+            min: -10,
+            max: 10,
+            step: 0.5,
+            value: 0,
+        }
+    }
+
+    handleChange = (value) => {
+        this.setState({
+            value: value
+        })
+    }
+
+    render() {
+        const min = this.props.min ? this.props.min : this.state.min;
+        const max = this.props.max ? this.props.max : this.state.max;
+        const step = this.props.step ? this.props.step : this.state.step;
+        const value = this.props.value ? this.props.value : this.state.value;
+
+        return (
+            <div className='slider'>
+                <Slider
+                    min={min}
+                    max={max}
+                    step={step}
+                    value={value}
+                    onChange={this.handleChange}
+                />
+                <div className='value'>{value}</div>
+            </div>
+        )
+    }
+}
+
+export default RangeSlider
