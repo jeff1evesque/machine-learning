3e959e9fa8cf641da3ff60dd2ec6288dd031aec6
Jeffrey M. Levesque <jeff1evesque@yahoo.com>
2017-12-11 22:17:07 -0500

    #3146: breakpoints.js, create file

---
+++ src/jsx/import/general/breakpoints.js
@@ -0,0 +1,11 @@
+/**
+ * breakpoints.jsx: define consistent custom breakpoints.
+ */
+
+const breakpoints = [
+  { name: 'small', maxWidth: 600 },
+  { name: 'medium', minWidth: 601, maxWidth: 1199 },
+  { name: 'large', minWidth: 1200 },
+];
+
+export default breakpoints
