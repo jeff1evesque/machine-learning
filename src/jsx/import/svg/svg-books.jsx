7412fac99ddeed6c13ea5062b8c3b17eef39f29e
Jeffrey M. Levesque <jeff1evesque@yahoo.com>
2017-12-17 19:51:25 -0500

    #3150: svg-books.jsx, add leaning book

--- src/jsx/import/svg/svg-books.jsx
+++ src/jsx/import/svg/svg-books.jsx
@@ -23,26 +23,26 @@ class SvgBooksIcon extends Component {
     }
     // callback for mouseOver svg
     mouseOverHome(event) {
-        this.setState({ inner_color: colors['green-3'] });
+        this.setState({ outer_color: colors['green-3'] });
     }
     // callback for mouseOut svg
     mouseOutHome(event) {
-        this.setState({ inner_color: colors['gray-6'] });
+        this.setState({ outer_color: colors['gray-6'] });
     }
     // triggered when 'state properties' change
     render() {
         return(
             <svg
                 version='1.1'
-                width='50px'
-                height='50px'
+                width='45px'
+                height='45px'
                 viewBox='0 0 225 225'
                 preserveAspectRatio='xMidYMid meet'
                 onMouseOver={this.mouseOverHome}
                 onMouseOut={this.mouseOutHome}
             >
                 <g
-                    transform='translate(20, 265) scale(0.1, -0.1)'
+                    transform='translate(20, 280) scale(0.1, -0.1)'
                     stroke='none'
                 >
                     <path
@@ -60,10 +60,27 @@ class SvgBooksIcon extends Component {
                         `}
                     />
                     <path
+                        fill={this.state.outer_color}
+                        d={`
+                            M1420 1715 c-30 -7 -90 -21 -134 -30 l-78 -17 6 -41 c4
+                            -23 9 -43 11 -45 3 -4 146 26 236 49 l37 9 -10 42 c-5
+                            23 -10 43 -11 44 -1 1 -27 -4 -57 -11z
+                        `}
+                    />
+                    <path
                         fill={this.state.inner_color}
                         d={`
                             M280 1455 l0 -325 190 0 190 0 0 325 0 325 -190 0
                             -190 0 0 -325z
+                        `}
+                    />
+                    <path
+                        fill={this.state.inner_color}
+                        d={`
+                            M1370 1512 c-63 -14 -117 -27 -119 -28 -3 -3 89 -444
+                            94 -452 2 -2 225 44 257 54 14 4 15 10 3 62 -7 31 -30
+                            132 -51 225 -29 132 -40 167 -53 166 -9 0 -68 -12 -131
+                            -27z
                         `}
                     />
                     <path
@@ -87,6 +104,13 @@ class SvgBooksIcon extends Component {
                             0 0 -90z
                         `}
                     />
+                    <path
+                        fill={this.state.outer_color}
+                        d={`
+                            M1480 961 l-113 -26 7 -40 c3 -22 8 -42 10 -44 5 -5
+                            257 49 266 58 4 4 3 23 -3 44 -12 44 -11 44 -167 8z
+                        `}
+                        />
                 </g>
             </svg>
         );
