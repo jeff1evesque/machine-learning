a287ff2ccbcf6eb79f7d01c64fee81efa04c1d8c
Jeffrey M. Levesque <jeff1evesque@yahoo.com>
2017-12-22 20:27:02 -0500

    #3172: page.jsx, replace 'class' with 'className'

--- src/jsx/import/layout/page.jsx
+++ src/jsx/import/layout/page.jsx
@@ -54,7 +54,7 @@ class PageLayout extends Component {
                     <div className='menu-container'>
                         {mainMenu}
                     </div>
-                    <div class='content'>
+                    <div className='content'>
                         <Route exact path='/' component={HomePageState} />
                         <Route exact path='/login' component={LoginLayout} />
                         <Route exact path='/logout' component={LoginLayout} />
