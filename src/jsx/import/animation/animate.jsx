6bb6430b2344a0a7f59b38c96cbb14293d25c9da
Jeffrey M. Levesque <jeff1evesque@yahoo.com>
2017-12-23 14:49:51 -0500

    #2893: animate.jsx, adjust docstring

--- src/jsx/import/animation/animate.jsx
+++ src/jsx/import/animation/animate.jsx
@@ -1,12 +1,12 @@
 /**
- * animate.jsx: d3js animation of ball collisions.
+ * animate.jsx: animates provided 'node' from parent:
+ *
+ *   - https://github.com/react-d3-library/react-d3-library
  *
  * @Animate, must be capitalized in order for reactjs to render it as a
  *     component. Otherwise, the variable is rendered as a dom node.
  *
- * Note: this script implements jsx (reactjs) syntax, and requires the parent
- *       calling this component, to define the 'node' property, which will
- *       represent the d3js object.
+ * Note: this script implements jsx (reactjs) syntax.
  *
  * Note: importing 'named export' (multiple export statements in a module),
  *       requires the object being imported, to be surrounded by { brackets }.
