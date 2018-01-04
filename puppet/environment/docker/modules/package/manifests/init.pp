0bfefd1dff6a5c5ab323f8eadf42a97f16fa1de5
Jeffrey M. Levesque <jeff1evesque@yahoo.com>
2018-01-03 23:46:09 -0500

    #3174: init.pp, remove dupe code, check key in hash

--- puppet/environment/docker/modules/package/manifests/init.pp
+++ puppet/environment/docker/modules/package/manifests/init.pp
@@ -11,43 +11,25 @@ class package {
     ## local variables
     $packages      = lookup('development')
     $hiera_general = lookup('general')
-    $node_packages = lookup('dependencies')
     $root_dir      = $hiera_general['root']
 
     ## iterate 'packages' hash
     $packages.each |String $provider, $providers| {
         if ($provider in ['apt', 'npm', 'pip']) {
-            $providers['general'].each|String $package, String $version| {
-                package { $package:
-                    ensure   => $version,
-                    provider => $provider,
-                    require  => [
-                        Class['apt'],
-                        Class['python'],
-                        Class['package::nodejs'],
-                        Class['package::python_dev']
-                    ],
+            if has_key($providers, 'general') {
+                $providers['general'].each|String $package, String $version| {
+                    package { $package:
+                        ensure   => $version,
+                        provider => $provider,
+                        require  => [
+                            Class['apt'],
+                            Class['python'],
+                            Class['package::nodejs'],
+                            Class['package::python_dev']
+                        ],
+                    }
                 }
             }
-        }
-    }
-
-    ## create node directory
-    file { "${root_dir}/src/node_modules":
-        ensure => 'directory',
-        owner  => root,
-        group  => root,
-        mode   => '0755',
-    }
-
-    ## iterate 'package.json'
-    $node_packages.each|String $package, String $version| {
-        nodejs::npm { "install-${package}":
-            ensure          => $version,
-            package         => $package,
-            install_options => '--no-bin-links',
-            target          => "${root_dir}/src/node_modules",
-            require         => File["${root_dir}/src/node_modules"],
         }
     }
 }
