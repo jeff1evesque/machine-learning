d4fb1b164fd4a423ffdfc843d748a59601d6f04b
Jeffrey M. Levesque <jeff1evesque@yahoo.com>
2018-01-06 09:31:52 -0500

    #2426: nav-bar.jsx, fix lint

--- src/jsx/import/navigation/nav-bar.jsx
+++ src/jsx/import/navigation/nav-bar.jsx
@@ -8,54 +8,53 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import ReviewResultsLinkState from '../redux/container/review-results-link.jsx';
 
-const NavBar = () =>
-    // display result
-    (
-        <div className='lcolumn'>
-            <ul className='side-menu'>
-                <li>
-                    <NavLink
-                        to='/session/data-new'
-                        activeClassName='active'
-                        className='menu-item'
-                    >
-                            Add new data
-                    </NavLink>
-                </li>
+const NavBar = () => (
+    <div className='lcolumn'>
+        <ul className='side-menu'>
+            <li>
+                <NavLink
+                    activeClassName='active'
+                    className='menu-item'
+                    to='/session/data-new'
+                >
+                    {'Add new data'}
+                </NavLink>
+            </li>
 
-                <li>
-                    <NavLink
-                        to='/session/data-append'
-                        activeClassName='active'
-                        className='menu-item'
-                    >
-                            Append data
-                    </NavLink>
-                </li>
+            <li>
+                <NavLink
+                    activeClassName='active'
+                    className='menu-item'
+                    to='/session/data-append'
+                >
+                    {'Append data'}
+                </NavLink>
+            </li>
 
-                <li>
-                    <NavLink
-                        to='/session/model-generate'
-                        activeClassName='active'
-                        className='menu-item'
-                    >
-                            Generate model
-                    </NavLink>
-                </li>
+            <li>
+                <NavLink
+                    activeClassName='active'
+                    className='menu-item'
+                    to='/session/model-generate'
+                >
+                    {'Generate model'}
+                </NavLink>
+            </li>
 
-                <li>
-                    <NavLink
-                        to='/session/model-predict'
-                        activeClassName='active'
-                        className='menu-item'
-                    >
-                            Make prediction
-                    </NavLink>
-                </li>
+            <li>
+                <NavLink
+                    activeClassName='active'
+                    className='menu-item'
+                    to='/session/model-predict'
+                >
+                    {'Make prediction'}
+                </NavLink>
+            </li>
 
-                <li><ReviewResultsLinkState /></li>
-            </ul>
-        </div>
-    );
+            <li><ReviewResultsLinkState /></li>
+        </ul>
+    </div>
+);
+
 // indicate which class can be exported, and instantiated via 'require'
 export default NavBar;
