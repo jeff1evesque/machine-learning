0593678148e7edc47fb678305b972ee53e9558a9
Jeffrey M. Levesque <jeff1evesque@yahoo.com>
2018-01-06 17:18:59 -0500

    #2426: svg-pencil-note.jsx, fix lint

--- src/jsx/import/svg/svg-pencil-note.jsx
+++ src/jsx/import/svg/svg-pencil-note.jsx
@@ -11,7 +11,6 @@ import colors from '../general/colors.js';
 import React, { Component } from 'react';
 
 class SvgPencilNote extends Component {
-    // initial 'state properties'
     constructor() {
         super();
         this.state = {
@@ -19,67 +18,67 @@ class SvgPencilNote extends Component {
             inner_color: colors['gray-5'],
             outer_color: colors['gray-6'],
         }
-        this.mouseOverImage = this.mouseOverImage.bind(this);
-        this.mouseOutImage = this.mouseOutImage.bind(this);
+        this.handleMouseOver = this.handleMouseOver.bind(this);
+        this.handleMouseOut = this.handleMouseOut.bind(this);
     }
-    // callback for mouseOver svg
-    mouseOverImage(event) {
+
+    handleMouseOver(event) {
         this.setState({ inner_color: colors['green-3'] });
     }
-    // callback for mouseOut svg
-    mouseOutImage(event) {
+
+    handleMouseOut(event) {
         this.setState({ inner_color: colors['gray-5'] });
     }
-    // triggered when 'state properties' change
+
     render() {
         return(
             <svg
-                version='1.1'
-                xmlns='http://www.w3.org/2000/svg'
-                width='45px'
-                height='28px'
-                x='0px'
-                y='0px'
-                viewBox='0 0 512 512'
                 enableBackground='new 0 0 45 45'
+                height='28px'
+                onMouseOver={this.handleMouseOver}
+                onMouseOut={this.handleMouseOut}
                 preserveAspectRatio='xMidYMid meet'
-                onMouseOver={this.mouseOverImage}
-                onMouseOut={this.mouseOutImage}
+                version='1.1'
+                viewBox='0 0 512 512'
+                width='45px'
+                x='0px'
+                xmlns='http://www.w3.org/2000/svg'
+                y='0px'
             >
                 <g>
                     <path
-                        fill={this.state.outer_color}
                         d={`
                             M448, 177.14V 448c 0, 35.344 -28.656, 64 -64, 64H 64c
                             -35.344, 0 -64 -28.656 -64 -64V 128c 0 -35.344, 28.656
                             -64, 64 -64h 270.844l -63.969, 64H 64v 320h 320V
                             241.156L 448, 177.14z
                         `}
+                        fill={this.state.outer_color}
                     />
                     <path
-                        fill={this.state.pencil_color}
                         d={`
                             M444.125, 0 L421.5, 22.625 l67.875, 67.891L 512, 67.875L
                             444.125, 0z
                         `}
+                        fill={this.state.pencil_color}
                     />
                     <path
-                        fill={this.state.pencil_color}
                         d={`
                             M398.875, 45.25L 376.25, 67.875 l67.875, 67.891 l22.625
                             -22.625L 398.875, 45.25z
                         `}
+                        fill={this.state.pencil_color}
                     />
                     <path
-                        fill={this.state.inner_color}
                         d={`
                             M150, 294.188 l67.875, 67.875L 421.5, 158.406l -67.875
                             -67.891L 150, 294.188z
                         `}
+                        fill={this.state.inner_color}
                     />
                     <path
-                        fill={this.state.pencil_color}
                         d='M128, 384h 64l -64 -64V 384z'
+                        fill={this.state.pencil_color}
                     />
                 </g>
             </svg>
