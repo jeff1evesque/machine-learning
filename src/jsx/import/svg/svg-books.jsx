97534d25156160685b9ac024cb060c96da50f890
Jeffrey M. Levesque <jeff1evesque@yahoo.com>
2018-01-06 17:04:02 -0500

    #2426: svg-books.svg, fix lint

--- src/jsx/import/svg/svg-books.jsx
+++ src/jsx/import/svg/svg-books.jsx
@@ -11,106 +11,105 @@ import colors from '../general/colors.js';
 import React, { Component } from 'react';
 
 class SvgBooks extends Component {
-    // initial 'state properties'
     constructor() {
         super();
         this.state = {
             inner_color: colors['gray-5'],
             outer_color: colors['gray-6'],
         }
-        this.mouseOverImage = this.mouseOverImage.bind(this);
-        this.mouseOutImage = this.mouseOutImage.bind(this);
+        this.handleMouseOver = this.handleMouseOver.bind(this);
+        this.handleMouseOut = this.handleMouseOut.bind(this);
     }
-    // callback for mouseOver svg
-    mouseOverImage(event) {
+
+    handleMouseOver(event) {
         this.setState({ outer_color: colors['green-3'] });
     }
-    // callback for mouseOut svg
-    mouseOutImage(event) {
+
+    handleMouseOut(event) {
         this.setState({ outer_color: colors['gray-6'] });
     }
-    // triggered when 'state properties' change
+
     render() {
         return(
             <svg
-                version='1.1'
-                width='45px'
                 height='45px'
-                viewBox='0 0 225 225'
+                onMouseOver={this.handleMouseOver}
+                onMouseOut={this.handleMouseOut}
                 preserveAspectRatio='xMidYMid meet'
-                onMouseOver={this.mouseOverImage}
-                onMouseOut={this.mouseOutImage}
+                version='1.1'
+                viewBox='0 0 225 225'
+                width='45px'
             >
                 <g
-                    transform='translate(20, 280) scale(0.1, -0.1)'
                     stroke='none'
+                    transform='translate(20, 280) scale(0.1, -0.1)'
                 >
                     <path
-                        fill={this.state.outer_color}
                         d={`
                             M280 1970 l0 -90 190 0 190 0 0 90 0 90 -190 0 -190
                             0 0 -90z
                         `}
+                        fill={this.state.outer_color}
                     />
                     <path
-                        fill={this.state.outer_color}
                         d={`
                             M750 1970 l0 -90 185 0 185 0 0 90 0 90 -185 0 -185
                             0 0 -90z
                         `}
+                        fill={this.state.outer_color}
                     />
                     <path
-                        fill={this.state.outer_color}
                         d={`
                             M1420 1715 c-30 -7 -90 -21 -134 -30 l-78 -17 6 -41 c4
                             -23 9 -43 11 -45 3 -4 146 26 236 49 l37 9 -10 42 c-5
                             23 -10 43 -11 44 -1 1 -27 -4 -57 -11z
                         `}
+                        fill={this.state.outer_color}
                     />
                     <path
-                        fill={this.state.inner_color}
                         d={`
                             M280 1455 l0 -325 190 0 190 0 0 325 0 325 -190 0
                             -190 0 0 -325z
                         `}
+                        fill={this.state.inner_color}
                     />
                     <path
-                        fill={this.state.inner_color}
                         d={`
                             M1370 1512 c-63 -14 -117 -27 -119 -28 -3 -3 89 -444
                             94 -452 2 -2 225 44 257 54 14 4 15 10 3 62 -7 31 -30
                             132 -51 225 -29 132 -40 167 -53 166 -9 0 -68 -12 -131
                             -27z
                         `}
+                        fill={this.state.inner_color}
                     />
                     <path
-                        fill={this.state.inner_color}
                         d={`
                             M750 1455 l0 -325 185 0 185 0 0 325 0 325 -185 0
                             -185 0 0 -325z
                         `}
+                        fill={this.state.inner_color}
                     />
                     <path
-                        fill={this.state.outer_color}
                         d={`
                             M280 940 l0 -90 190 0 190 0 0 90 0 90 -190 0 -190
                             0 0 -90z
                         `}
+                        fill={this.state.outer_color}
                     />
                     <path
-                        fill={this.state.outer_color}
                         d={`
                             M750 940 l0 -90 185 0 185 0 0 90 0 90 -185 0 -185
                             0 0 -90z
                         `}
+                        fill={this.state.outer_color}
                     />
                     <path
-                        fill={this.state.outer_color}
                         d={`
                             M1480 961 l-113 -26 7 -40 c3 -22 8 -42 10 -44 5 -5
                             257 49 266 58 4 4 3 23 -3 44 -12 44 -11 44 -167 8z
                         `}
-                        />
+                        fill={this.state.outer_color}
+                    />
                 </g>
             </svg>
         );
