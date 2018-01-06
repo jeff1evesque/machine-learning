340b4d539f2405ff3ae940e0cdc82df82bc4fc0e
Jeffrey M. Levesque <jeff1evesque@yahoo.com>
2018-01-05 23:24:44 -0500

    #2425: model-type.jsx, fix lint errors

--- src/jsx/import/model/model-type.jsx
+++ src/jsx/import/model/model-type.jsx
@@ -8,16 +8,23 @@ import React, { Component } from 'react';
 import checkValidString from './../validator/valid-string.js';
 
 class ModelType extends Component {
-    // initial 'state properties'
+    // prob validation: static method, similar to class A {}; A.b = {};
+    static propTypes = {
+        onChange: PropTypes.shape({
+            value_model_type: PropTypes.string.isRequired,
+        }),
+    }
+
     constructor() {
         super();
         this.state = {
             value_model_type: '--Select--',
         };
-        this.changeModelType = this.changeModelType.bind(this);
+        this.handleModelType = this.handleModelType.bind(this);
     }
+
     // update 'state properties': pass property to parent component
-    changeModelType(event) {
+    handleModelType(event) {
         if (checkValidString(event.target.value)) {
             this.setState({ value_model_type: event.target.value });
             this.props.onChange({ value_model_type: event.target.value });
@@ -25,21 +32,26 @@ class ModelType extends Component {
             this.setState({ value_model_type: '--Select--' });
         }
     }
-    // triggered when 'state properties' change
+
     render() {
         // display result
         return (
             <select
+                autoComplete='off'
                 className='form-control fullspan'
                 name='model_type'
-                autoComplete='off'
-                onChange={this.changeModelType}
+                onChange={this.handleModelType}
                 value={this.state.value_model_type}
             >
 
-                <option value='' defaultValue>--Select--</option>
-                <option value='svm'>Classification</option>
-                <option value='svr'>Regression</option>
+                <option
+                    defaultValue
+                    value=''
+                >
+                    {'--Select--'}
+                </option>
+                <option value='svm'>{'Classification'}</option>
+                <option value='svr'>{'Regression'}</option>
 
             </select>
         );
