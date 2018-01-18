1aaa700c2bea04e91c49bdd2f42a88d5615a9f41
Jeffrey M. Levesque <jeff1evesque@yahoo.com>
2018-01-18 15:03:17 -0500

    #1919: session.py, implement 'In'

--- brain/schema/session.py
+++ brain/schema/session.py
@@ -7,7 +7,7 @@ This file validates each session properties.
 '''
 
 from flask import current_app
-from voluptuous import Schema, Required, Optional, All, Any, Length
+from voluptuous import Schema, Required, Optional, All, Any, In, Length
 
 
 def validate_data_new(data):
@@ -24,12 +24,12 @@ def validate_data_new(data):
 
     '''
 
-    model_type = ','.join(current_app.config.get('MODEL_TYPE'))
-    dataset_type = ','.join(current_app.config.get('DATASET_TYPE'))
+    model_type = current_app.config.get('MODEL_TYPE')
+    dataset_type = current_app.config.get('DATASET_TYPE')
     schema = Schema({
         Required('collection'): All(unicode, Length(min=1)),
-        Required('dataset_type'): Any(dataset_type),
-        Required('model_type'): Any(model_type),
+        Required('dataset_type'): In(dataset_type),
+        Required('model_type'): In(model_type),
         Required('session_type'): 'data_new',
         Required('session_name'): All(unicode, Length(min=1)),
         Optional('stream'): Any('True', 'False'),
@@ -51,12 +51,12 @@ def validate_data_append(data):
 
     '''
 
-    model_type = ','.join(current_app.config.get('MODEL_TYPE'))
-    dataset_type = ','.join(current_app.config.get('DATASET_TYPE'))
+    model_type = current_app.config.get('MODEL_TYPE')
+    dataset_type = current_app.config.get('DATASET_TYPE')
     schema = Schema({
         Required('collection'): All(unicode, Length(min=1)),
-        Required('dataset_type'): Any(dataset_type),
-        Required('model_type'): Any(model_type),
+        Required('dataset_type'): In(dataset_type),
+        Required('model_type'): In(model_type),
         Required('session_type'): 'data_append',
         Optional('stream'): Any('True', 'False'),
     })
@@ -77,14 +77,14 @@ def validate_model_generate(data):
 
     '''
 
-    model_type = ','.join(current_app.config.get('MODEL_TYPE'))
-    sv_kernel_type = ','.join(current_app.config.get('SV_KERNEL_TYPE'))
+    model_type = current_app.config.get('MODEL_TYPE')
+    sv_kernel_type = current_app.config.get('SV_KERNEL_TYPE')
     schema = Schema({
         Required('collection'): All(unicode, Length(min=1)),
-        Required('model_type'): Any(model_type),
+        Required('model_type'): In(model_type),
         Required('session_type'): 'model_generate',
         Optional('stream'): Any('True', 'False'),
-        Required('sv_kernel_type'): Any(sv_kernel_type),
+        Required('sv_kernel_type'): In(sv_kernel_type),
     })
     schema(data)
 
