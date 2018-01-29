363963800da2e9a814982582b664b38e2d0dbe32
Jeffrey M. Levesque <jeff1evesque@yahoo.com>
2018-01-29 17:15:18 -0500

    #2600: settings.py, check 'data_append' web-interface

--- brain/validator/settings.py
+++ brain/validator/settings.py
@@ -47,14 +47,24 @@ class Validator(object):
 
         # validation on 'data_new', 'data_append' session
         if session_type in ['data_new', 'data_append']:
-            schema = Schema({
-                Required('collection'): All(unicode, Length(min=1)),
-                Required('dataset_type'): In(dataset_type),
-                Required('model_type'): In(model_type),
-                Required('session_type'): Any('data_new', 'data_append'),
-                Optional('session_name'): All(unicode, Length(min=1)),
-                Optional('stream'): Any('True', 'False'),
-            })
+            if premodel_settings['stream']  == 'True' or session_type == 'data_new':
+                schema = Schema({
+                    Required('collection'): All(unicode, Length(min=1)),
+                    Required('dataset_type'): In(dataset_type),
+                    Required('model_type'): In(model_type),
+                    Required('session_type'): Any('data_new', 'data_append'),
+                    Required('session_name'): All(unicode, Length(min=1)),
+                    Optional('stream'): Any('True', 'False'),
+                })
+
+            elif session_type == 'data_append':
+                schema = Schema({
+                    Required('collection'): All(unicode, Length(min=1)),
+                    Required('dataset_type'): In(dataset_type),
+                    Required('model_type'): In(model_type),
+                    Required('session_type'): 'data_append',
+                    Optional('stream'): Any('True', 'False'),
+                })
 
         # validation on 'model_generate' session
         if session_type == 'model_generate':
