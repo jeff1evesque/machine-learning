b422019ea4ee5316eaca252a7c6b02177b81ace3
Jeffrey M. Levesque <jeff1evesque@yahoo.com>
2018-01-28 21:37:07 -0500

    #2600: settings.py, change 'Require' to 'Optional'

--- brain/validator/settings.py
+++ brain/validator/settings.py
@@ -52,7 +52,7 @@ class Validator(object):
                 Required('dataset_type'): In(dataset_type),
                 Required('model_type'): In(model_type),
                 Required('session_type'): Any('data_new', 'data_append'),
-                Required('session_name'): All(unicode, Length(min=1)),
+                Optional('session_name'): All(unicode, Length(min=1)),
                 Optional('stream'): Any('True', 'False'),
             })
 
