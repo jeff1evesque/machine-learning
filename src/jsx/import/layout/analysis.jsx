b64856bc6dd73485fcf62059bd882142aeac1275
Jeffrey M. Levesque <jeff1evesque@yahoo.com>
2018-02-04 11:17:51 -0500

    #3191: analysis.jsx, remove 'this.state.ajax_done'

--- src/jsx/import/layout/analysis.jsx
+++ src/jsx/import/layout/analysis.jsx
@@ -309,7 +309,7 @@ class AnalysisLayout extends Component {
     }
 
     showErrors() {
-        if (this.state.ajax_done && this.state.ajax_done_error) {
+        if (this.state.ajax_done_error) {
             return (
                 <ReactJson
                     onAdd='false'
