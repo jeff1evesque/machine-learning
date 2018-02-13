f8bf5af57c47faf2d0946cae47cef9303db8c88a
Jeffrey M. Levesque <jeff1evesque@yahoo.com>
2018-02-12 23:07:37 -0500

    #3087: login.test.jsx, add trivial 'input' existence

--- test/jest/__tests__/login.test.jsx
+++ test/jest/__tests__/login.test.jsx
@@ -4,11 +4,23 @@
  */
 
 import React from 'react';
-import { shallow, mount, render } from 'enzyme';
+import { shallow } from 'enzyme';
 import LoginForm from '../../../src/jsx/import/content/login.jsx'
 
 describe('Login Component', () => {
-   it('should render without throwing an error', () => {
-       expect(shallow(<LoginForm />).exists(<form ref='loginForm'></form>)).toBe(true)
-   });
+    it('should render without throwing an error', () => {
+        expect(shallow(<LoginForm />).exists(<form ref='loginForm'></form>)).toBe(true)
+    });
+
+    it('renders email input', () => {
+        expect(shallow(<LoginForm />).find('[name="user[login]"]').length).toEqual(1)
+    });
+
+    it('renders password input', () => {
+        expect(shallow(<LoginForm />).find('[name="user[password]"]').length).toEqual(1)
+    });
+
+    it('renders submit input', () => {
+        expect(shallow(<LoginForm />).find('[type="submit"]').length).toEqual(1)
+    });
 });
