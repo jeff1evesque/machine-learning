48a2fc91705ee72421f415b159461bdc26612e8a
Jeffrey M. Levesque <jeff1evesque@yahoo.com>
2018-03-22 23:35:02 -0400

    #3208: setup_tables.py, fix indentation

--- puppet/environment/docker/modules/mariadb/scripts/setup_tables.py
+++ puppet/environment/docker/modules/mariadb/scripts/setup_tables.py
@@ -208,12 +208,12 @@ with conn:
                         RETURNS BINARY(36)
                         LANGUAGE SQL  DETERMINISTIC  CONTAINS SQL  SQL SECURITY INVOKER
                     RETURN
-                    LCASE(CONCAT_WS('-',
-                        HEX(SUBSTR(_bin,  5, 4)),
-                        HEX(SUBSTR(_bin,  3, 2)),
-                        HEX(SUBSTR(_bin,  1, 2)),
-                        HEX(SUBSTR(_bin,  9, 2)),
-                        HEX(SUBSTR(_bin, 11))
+                        LCASE(CONCAT_WS('-',
+                            HEX(SUBSTR(_bin,  5, 4)),
+                            HEX(SUBSTR(_bin,  3, 2)),
+                            HEX(SUBSTR(_bin,  1, 2)),
+                            HEX(SUBSTR(_bin,  9, 2)),
+                            HEX(SUBSTR(_bin, 11))
                     ));
                     '''
     cur.execute(sql_statement)
