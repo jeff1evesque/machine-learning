742578a4af6574abfd730a0e47686c9c79d09a61
Jeffrey M. Levesque <jeff1evesque@yahoo.com>
2018-03-22 23:33:50 -0400

    #3208: create stored functions for time based uuid

--- puppet/environment/docker/modules/mariadb/scripts/setup_tables.py
+++ puppet/environment/docker/modules/mariadb/scripts/setup_tables.py
@@ -185,6 +185,39 @@ with conn:
     #                below tables will completely define our sql implementation.        #
     # ################################################################################# #
 
+    # stored functions: time based uuid
+    #
+    # Note: https://mariadb.com/kb/en/library/guiduuid-performance/#code-to-do-it
+    #
+    sql_statement = '''\
+                    CREATE FUNCTION UuidToBin(_uuid BINARY(36))
+                        RETURNS BINARY(16)
+                        LANGUAGE SQL  DETERMINISTIC  CONTAINS SQL  SQL SECURITY INVOKER
+                    RETURN
+                        UNHEX(CONCAT(
+                            SUBSTR(_uuid, 15, 4),
+                            SUBSTR(_uuid, 10, 4),
+                            SUBSTR(_uuid,  1, 8),
+                            SUBSTR(_uuid, 20, 4),
+                            SUBSTR(_uuid, 25) ));
+                    '''
+    cur.execute(sql_statement)
+
+    sql_statement = '''\
+                    CREATE FUNCTION UuidFromBin(_bin BINARY(16))
+                        RETURNS BINARY(36)
+                        LANGUAGE SQL  DETERMINISTIC  CONTAINS SQL  SQL SECURITY INVOKER
+                    RETURN
+                    LCASE(CONCAT_WS('-',
+                        HEX(SUBSTR(_bin,  5, 4)),
+                        HEX(SUBSTR(_bin,  3, 2)),
+                        HEX(SUBSTR(_bin,  1, 2)),
+                        HEX(SUBSTR(_bin,  9, 2)),
+                        HEX(SUBSTR(_bin, 11))
+                    ));
+                    '''
+    cur.execute(sql_statement)
+
     # general permission
     sql_statement = '''\
                     CREATE TABLE IF NOT EXISTS Permission (
