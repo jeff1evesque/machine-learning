1f3052e06ad5bc686125a43cc3280a843cd8684c
Jeffrey M. Levesque <jeff1evesque@yahoo.com>
2018-03-26 18:25:21 -0400

    #3208: setup_tables.py, fix 'UNIQUE' typo

--- puppet/environment/docker/modules/mariadb/scripts/setup_tables.py
+++ puppet/environment/docker/modules/mariadb/scripts/setup_tables.py
@@ -199,7 +199,7 @@ with conn:
                 Username VARCHAR (50) NOT NULL,
                 Password VARCHAR (1069) NOT NULL,
                 Joined DATETIME NOT NULL,
-                UNIQUE (UserName)
+                UNIQUE (Username)
             );
             '''
     cur.execute(query)
