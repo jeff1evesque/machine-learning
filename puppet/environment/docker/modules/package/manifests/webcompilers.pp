ce83d6a779f302308e645e961d8629dd2a9a3c96
Jeffrey M. Levesque <jeff1evesque@yahoo.com>
2018-04-01 22:56:03 -0400

    #2935: webcompilers.pp, fix interpolation

--- puppet/environment/docker/modules/package/manifests/webcompilers.pp
+++ puppet/environment/docker/modules/package/manifests/webcompilers.pp
@@ -5,20 +5,20 @@ class package::webcompilers {
     include package::nodejs
 
     ## hiera attributes
-    $hiera_dev          = lookup('development')
-    $version_uglify_js  = $hiera_dev['npm']['custom']['uglify-js']
-    $version_node_sass  = $hiera_dev['npm']['custom']['node-sass']
-    $version_babel_core = $hiera_dev['npm']['custom']['babel-core']
-    $version_browserify = $hiera_dev['npm']['custom']['browserify']
-    $version_babelify   = $hiera_dev['npm']['custom']['babelify']
+    $hiera              = lookup('development')
+    $version_uglify_js  = $hiera['npm']['custom']['uglify-js']
+    $version_node_sass  = $hiera['npm']['custom']['node-sass']
+    $version_babel_core = $hiera['npm']['custom']['babel-core']
+    $version_browserify = $hiera['npm']['custom']['browserify']
+    $version_babelify   = $hiera['npm']['custom']['babelify']
 
     ## variables
     $webcompilers = [
         "uglify-js@${version_uglify_js}",
-        "node-sass@${$version_node_sass}",
-        "babel-core@${$version_babel_core}",
-        "browserify@${$version_browserify}",
-        "babelify@${version_babelify}"
+        "node-sass@${version_node_sass}",
+        "babel-core@${version_babel_core}",
+        "browserify@${version_browserify}",
+        "babelify@${version_babelify}",
     ]
 
     ## packages: install general packages (npm)
