e96723a39cb5a01d1de95c1eef09ed64f17d73ac
Jeffrey M. Levesque <jeff1evesque@yahoo.com>
2018-04-03 18:41:53 -0400

    #2935: init.pp, provide additional parameters

--- puppet/environment/docker/modules/nginx/manifests/init.pp
+++ puppet/environment/docker/modules/nginx/manifests/init.pp
@@ -2,11 +2,22 @@
 ### init.pp: configure, and start nginx.
 ###
 class nginx (
-    $type        = $::nginx::params::type,
-    $vhost       = $::nginx::params::vhost,
-    $host_port   = $::nginx::params::host_port,
-    $listen_port = $::nginx::params::listen_port,
-    $proxy       = $::nginx::params::proxy,
+    $type           = $::nginx::params::type,
+    $vhost          = $::nginx::params::vhost,
+    $host_port      = $::nginx::params::host_port,
+    $listen_port    = $::nginx::params::listen_port,
+    $webserver_port = $::nginx::params::webserver_port,
+    $proxy          = $::nginx::params::proxy,
+    $self_signed    = $::nginx::params::self_signed,
+    $cert_path      = $::nginx::params::cert_path,
+    $pkey_path      = $::nginx::params::pkey_path,
+    $cert_country   = $::nginx::params::country,
+    $cert_org       = $::nginx::params::org,
+    $cert_state     = $::nginx::params::state,
+    $cert_locality  = $::nginx::params::locality,
+    $cert_unit      = $::nginx::params::unit,
+    $cert_bit       = $::nginx::params::bit,
+    $cert_days      = $::nginx::params::days,
 ) inherits ::nginx::params {
     ## ensure log directory
     require system::log_directory
