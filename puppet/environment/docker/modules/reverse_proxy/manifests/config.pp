4f35b5304c04f0db01e299d21f120a7cece0f452
Jeffrey M. Levesque <jeff1evesque@yahoo.com>
2018-04-04 15:25:08 -0400

    #2935: config.pp, standardize parameterized values

--- puppet/environment/docker/modules/reverse_proxy/manifests/config.pp
+++ puppet/environment/docker/modules/reverse_proxy/manifests/config.pp
@@ -3,17 +3,21 @@
 ###
 class reverse_proxy::config {
     ## local variables
-    $self_signed       = $reverse_proxy::self_signed
-    $vhost             = $reverse_proxy::vhost
-    $cert_path         = $reverse_proxy::cert_path
-    $pkey_path         = $reverse_proxy::pkey_path
-    $cert_country      = $reverse_proxy::cert_country
-    $cert_org          = $reverse_proxy::cert_org
-    $cert_state        = $reverse_proxy::cert_state
-    $cert_locality     = $reverse_proxy::cert_locality
-    $cert_unit         = $reverse_proxy::cert_unit
-    $cert_bit          = $reverse_proxy::cert_bit
-    $cert_days         = $reverse_proxy::cert_days
+    $type              = $::reverse_proxy::type
+    $self_signed       = $::reverse_proxy::self_signed
+    $vhost             = $::reverse_proxy::vhost
+    $host_port         = $::reverse_proxy::host_port
+    $webserver_port    = $::reverse_proxy::webserver_port
+    $proxy             = $::reverse_proxy::proxy
+    $cert_path         = $::reverse_proxy::cert_path
+    $pkey_path         = $::reverse_proxy::pkey_path
+    $cert_country      = $::reverse_proxy::cert_country
+    $cert_org          = $::reverse_proxy::cert_org
+    $cert_state        = $::reverse_proxy::cert_state
+    $cert_locality     = $::reverse_proxy::cert_locality
+    $cert_unit         = $::reverse_proxy::cert_unit
+    $cert_bit          = $::reverse_proxy::cert_bit
+    $cert_days         = $::reverse_proxy::cert_days
 
     ## nginx reverse proxy
     ##
@@ -21,30 +25,30 @@ class reverse_proxy::config {
     ##
     reverse_proxy::resource::vhost { $reverse_proxy::vhost:
         ssl             => true,
-        ssl_cert        => "${::reverse_proxy::cert_path}/${::reverse_proxy::vhost}_${::reverse_proxy::type}.crt",
-        ssl_key         => "${::reverse_proxy::pkey_path}/${::reverse_proxy::vhost}_${::reverse_proxy::type}.key",
+        ssl_cert        => "${cert_path}/${vhost}_${type}.crt",
+        ssl_key         => "${pkey_path}/${vhost}_${type}.key",
         listen_port     => $reverse_proxy::listen_port,
         ssl_port        => $reverse_proxy::listen_port,
         error_pages     => {
-            '497' => "https://\$host:${::reverse_proxy::host_port}\$request_uri",
+            '497' => "https://\$host:${host_port}\$request_uri",
         },
-        proxy           => "${::ngnix::proxy}:${webserver_port}",
+        proxy           => "${proxy}:${webserver_port}",
     }
 
     ## create ssl certificate
     if $self_signed {
-        file { "/root/build/ssl-nginx-${::reverse_proxy::type}":
+        file { "/root/build/ssl-nginx-${type}":
             ensure         => present,
-            content        => dos2unix(template("webserver/ssl-${::reverse_proxy::type}.erb")),
+            content        => dos2unix(template("webserver/ssl-${type}.erb")),
             owner          => 'root',
             group          => 'root',
             mode           => '0700',
             require        => File['/root/build'],
-            notify         => Exec["create-certificate-${::reverse_proxy::type}"],
+            notify         => Exec["create-certificate-${type}"],
         }
 
-        exec { "create-certificate-${::reverse_proxy::type}":
-            command        => "./ssl-nginx-${::reverse_proxy::type}",
+        exec { "create-certificate-${type}":
+            command        => "./ssl-nginx-${type}",
             cwd            => '/root/build',
             path           => '/usr/bin',
             provider       => shell,
