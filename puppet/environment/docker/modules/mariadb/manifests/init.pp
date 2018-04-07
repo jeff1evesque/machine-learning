decd71b876fbdcc96e1314bdc48fb0307c3ff7e4
Jeffrey M. Levesque <jeff1evesque@yahoo.com>
2018-04-07 11:11:13 -0400

    #2935: init.pp, provide class variables

--- puppet/environment/docker/modules/mariadb/manifests/init.pp
+++ puppet/environment/docker/modules/mariadb/manifests/init.pp
@@ -2,7 +2,18 @@
 ### init.pp: install client, and initialize database tables.
 ###
 class mariadb (
-
+    $root_puppet      = $::mariadb::params::root_puppet,
+    $pyyaml_version   = $::mariadb::params::pyyaml_version,
+    $db_host          = $::mariadb::params::db_host,
+    $db               = $::mariadb::params::db,
+    $db_user          = $::mariadb::params::db_user,
+    $db_pass          = $::mariadb::params::db_pass,
+    $provisioner      = $::mariadb::params::provisioner,
+    $provisioner_pass = $::mariadb::params::provisioner_password
+    $tester           = $::mariadb::params::tester
+    $tester_pass      = $::mariadb::params::tester_password
+    $root_pass        = $::mariadb::params::root_password
+    $bind_address     = $::mariadb::params::bind_address
 ) inherits ::mariadb::params {
     class { 'mariadb::server' } ->
     class { 'mariadb::client' } ->
