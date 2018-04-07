0bd7b1e1d7f1072ec5ac6e457b93f6a1911639bb
Jeffrey M. Levesque <jeff1evesque@yahoo.com>
2018-04-07 13:09:22 -0400

    #2935: init.pp, remove unnecessary 'contain'

--- puppet/environment/docker/modules/redis/manifests/init.pp
+++ puppet/environment/docker/modules/redis/manifests/init.pp
@@ -7,8 +7,6 @@ class redis (
 ) inherits ::redis::params {
     class { 'redis::install' } ->
     class { 'redis::config' } ~>
-    class { 'redis::run' }
+    class { 'redis::run' } ->
     Class['redis']
-    contain package::redis_server
-    contain redis::configuration
 }
