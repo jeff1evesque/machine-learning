d876fd5ffa0e2c5f0544a72c6ef527ece499d6ef
Jeffrey M. Levesque <jeff1evesque@yahoo.com>
2018-04-08 15:49:14 -0400

    #2935: init.pp, define redis class variables

--- puppet/environment/docker/modules/redis/manifests/init.pp
+++ puppet/environment/docker/modules/redis/manifests/init.pp
@@ -3,7 +3,10 @@
 ###
 
 class redis (
-
+    $run          = $::redis::params::run,
+    $version      = $::redis::params::version,
+    $bind_address = $::redis::params::bind_address,
+    $config_file  = $::redis::params::config_file,
 ) inherits ::redis::params {
     class { 'redis::install' } ->
     class { 'redis::config' } ~>
