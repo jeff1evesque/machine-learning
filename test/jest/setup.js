b98763c5f10c0872fe97d056ff4c37ac0d2edeab
Jeffrey M. Levesque <jeff1evesque@yahoo.com>
2018-05-14 00:41:25 -0400

    #3087: setup.js, adjust docstring

--- test/jest/setup.js
+++ test/jest/setup.js
@@ -1,4 +1,10 @@
-// https://stackoverflow.com/a/47926947
+//
+// allow the use of 'sessionStorage' within jest + enzyme:
+//
+//     https://stackoverflow.com/a/47926947
+//     https://github.com/jeff1evesque/machine-learning/
+//         issues/3087#issuecomment-388694964
+//
 mock-local-storage
 
 // fail tests for console warnings + errors
