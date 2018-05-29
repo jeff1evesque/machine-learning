311e31c3dd17689210e31ae8fbe5b90da8b9974e
jeffrey levesque <jeff1evesque@yahoo.com>
2018-05-29 00:05:25 -0400

    #3087: setup.js, adjust 'mock-local-storage' reference

--- test/jest/setup.js
+++ test/jest/setup.js
@@ -2,28 +2,95 @@
 // allow the use of 'sessionStorage' within jest + enzyme:
 //
 //     https://stackoverflow.com/a/47926947
-//     https://stackoverflow.com/a/32911774
 //     https://github.com/jeff1evesque/machine-learning/
 //         issues/3087#issuecomment-388694964
 //
-var localStorageMock = (function() {
-  var store = {};
-  return {
-    getItem: function(key) {
-      return store[key];
-    },
-    setItem: function(key, value) {
-      store[key] = value.toString();
-    },
-    clear: function() {
-      store = {};
-    },
-    removeItem: function(key) {
-      delete store[key];
+// Mock localStorage
+(function () {
+
+    function createStorage() {
+        let s = {},
+            noopCallback = () => {},
+            _itemInsertionCallback = noopCallback;
+
+        Object.defineProperty(s, 'setItem', {
+            get: () => {
+                return (k, v) => {
+                    k = k + '';
+                    if (!s.hasOwnProperty(k)) {
+                        _itemInsertionCallback(s.length);
+                    }
+                    s[k] = v + '';
+                };
+            }
+        });
+        Object.defineProperty(s, 'getItem', {
+            get: () => {
+                return k => {
+                    k = k + '';
+                    if (s.hasOwnProperty(k)) {
+                        return s[k];
+                    } else {
+                        return null;
+                    }
+                };
+            }
+        });
+        Object.defineProperty(s, 'removeItem', {
+            get: () => {
+                return k => {
+                    k = k + '';
+                    if (s.hasOwnProperty(k)) {
+                        delete s[k];
+                    }
+                };
+            }
+        });
+        Object.defineProperty(s, 'clear', {
+            get: () => {
+                return () => {
+                    for (let k in s) {
+                        if (s.hasOwnProperty(k)) {
+                            delete s[k];
+                        }
+                    }
+                };
+            }
+        });
+        Object.defineProperty(s, 'length', {
+            get: () => {
+                return Object.keys(s).length;
+            }
+        });
+        Object.defineProperty(s, "key", {
+            value: k => {
+                let key = Object.keys(s)[k];
+                return (!key) ? null : key;
+            },
+        });
+        Object.defineProperty(s, 'itemInsertionCallback', {
+            get: () => {
+                return _itemInsertionCallback;
+            },
+            set: v => {
+                if (!v || typeof v != 'function') {
+                    v = noopCallback;
+                }
+                _itemInsertionCallback = v;
+            }
+        });
+        return s;
     }
-  };
-})();
-Object.defineProperty(window, 'localStorage', { value: localStorageMock });
+
+    const global = require("global")
+    const window = require("global/window")
+
+    global.localStorage = createStorage();
+    global.sessionStorage = createStorage();
+
+    window.localStorage = global.localStorage;
+    window.sessionStorage = global.sessionStorage;
+}());
 
 // fail tests for console warnings + errors
 console.error = x => { throw x; };
