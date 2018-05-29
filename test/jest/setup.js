314e9cdadc9df462f8e90b3660a223424b85e86a
jeffrey levesque <jeff1evesque@yahoo.com>
2018-05-28 23:57:42 -0400

    #3087: setup.js, adjust 'mock-local-storage' reference

--- test/jest/setup.js
+++ test/jest/setup.js
@@ -2,12 +2,28 @@
 // allow the use of 'sessionStorage' within jest + enzyme:
 //
 //     https://stackoverflow.com/a/47926947
+//     https://stackoverflow.com/a/32911774
 //     https://github.com/jeff1evesque/machine-learning/
 //         issues/3087#issuecomment-388694964
 //
-global.window = {}
-import localStorage from 'mock-local-storage'
-window.localStorage = global.localStorage
+var localStorageMock = (function() {
+  var store = {};
+  return {
+    getItem: function(key) {
+      return store[key];
+    },
+    setItem: function(key, value) {
+      store[key] = value.toString();
+    },
+    clear: function() {
+      store = {};
+    },
+    removeItem: function(key) {
+      delete store[key];
+    }
+  };
+})();
+Object.defineProperty(window, 'localStorage', { value: localStorageMock });
 
 // fail tests for console warnings + errors
 console.error = x => { throw x; };
