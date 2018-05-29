f150062b193c11e7b096b7c6a5fda38f062f518a
jeffrey levesque <jeff1evesque@yahoo.com>
2018-05-29 00:43:28 -0400

    #3087: setup.js, revert 'moduleDirectories'

--- test/jest/jest.config.js
+++ test/jest/jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
     'collectCoverage': true,
     'coverageDirectory': '.',
     'moduleDirectories': [
-        '../../src/jsx/node_modules'
+        'node_modules'
     ],
     'setupTestFrameworkScriptFile': '../../test/jest/setup.js',
 }
