310e88b6a4addecb807b5c63f740ee162f571f2d
jeffrey levesque <jeff1evesque@yahoo.com>
2018-05-28 23:43:55 -0400

    #3087: jest.config.js, reduce duplicate definitions

--- test/jest/jest.config.js
+++ test/jest/jest.config.js
@@ -1,10 +1,9 @@
 module.exports = {
     'verbose': true,
     'collectCoverage': true,
-    'coverageDirectory': '/var/machine-learning',
+    'coverageDirectory': '.',
     'moduleDirectories': [
-        '/var/machine-learning/src/jsx/node_modules'
+        'node_modules'
     ],
-    'setupTestFrameworkScriptFile': '/var/machine-learning/src/jsx/setup.js',
-    'setupTestFrameworkScriptFile': 'mock-local-storage',
+    'setupTestFrameworkScriptFile': '../../test/jest/setup.js',
 }
