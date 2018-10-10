fad91d64d635b4903c263eee695a3268871c76db
jeffrey levesque <jeff1evesque@yahoo.com>
2018-10-10 19:21:50 -0400

    #3287: base_data.py, remove 'unorderable types' error

--- brain/session/base_data.py
+++ brain/session/base_data.py
@@ -114,6 +114,7 @@ class BaseData(Base):
         if (
             collection_adjusted and
             collection_count and
+            collection_count['result'] and
             collection_count['result'] < self.max_collection and
             document_count and
             document_count['result'] < self.max_document
